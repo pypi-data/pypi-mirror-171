5a1b720b61ffd67661b4ea1bd220b6f0ea5a84f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package proflow/0.1.4/proflow-0.1.4-py3-none-any.whl

---
+++ packages/proflow/proflow-0.1.4-py3-none-any.whl/proflow/tests/mocks.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass
+class Mock_Nested_State:
+    na: int = 7
+    nab: int = 7
+
+
+@dataclass
+class Mock_Temporal_State:
+    hr: int = 0
+    dd: int = 0
+    row_index: int = 0
+
+
+@dataclass
+class Mock_Model_State_Shape:
+    a: float
+    b: float
+    temporal: Mock_Temporal_State = field(default_factory=lambda: Mock_Temporal_State())
+    c: float = 0
+    d: float = 0
+    ind: int = 0
+    target: str = "a"
+    lst: list = None
+    nested: Mock_Nested_State = Mock_Nested_State()
+    matrix: List[List] = field(default_factory=lambda: [[1, 2, 3], [4, 5, 6]])
+    nested_lst_obj: List[Mock_Nested_State] = field(
+        default_factory=lambda: [Mock_Nested_State(1, 2), Mock_Nested_State(3, 4)])
+
+
+@dataclass
+class Mock_Config_Shape:
+    foo: int = 1
+    bar: int = 3
+    roo: dict = field(default_factory=lambda: {
+        'abc': 5
+    })
+    arr: list = field(default_factory=lambda: [1, 2, 3])
+
+
+@dataclass
+class Mock_Parameters_Shape:
+    foo: int = 1
+    bar: int = 3
+    roo: dict = field(default_factory=lambda: {
+        'abc': 5
+    })
+    arr: list = field(default_factory=lambda: [1, 2, 3])
+
+
+@dataclass
+class Mock_External_State_Shape:
+    data_a: List[int] = field(default_factory=lambda: [1, 1, 2, 3])
+    data_b: List[int] = field(default_factory=lambda: [5, 1, 2, 3])

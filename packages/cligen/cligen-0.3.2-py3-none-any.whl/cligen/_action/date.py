5ca39ba89052a41c2fe7ea46694b5b4684e70e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:35 +0000

    Add package cligen/0.3.2/cligen-0.3.2-py3-none-any.whl

---
+++ packages/cligen/cligen-0.3.2-py3-none-any.whl/cligen/_action/date.py
@@ -0,0 +1,61 @@
+import argparse
+import datetime
+import typing
+
+_parameters = """\
+default: today
+"""
+
+
+class DateAction(argparse.Action):
+    # argparse action for parsing dates with or without dashes
+    #
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--verbose', '-v', action=DateAction)
+
+    def __init__(
+        self,
+        option_strings: typing.Any,
+        dest: typing.Any,
+        nargs: typing.Optional[typing.Any] = None,
+        **kwargs: typing.Any
+    ):
+        if nargs != 1 and nargs not in [None, '?', '*']:
+            raise ValueError('DateAction can only have one argument')
+        default = kwargs.get('default')
+        if isinstance(default, str):
+            kwargs['default'] = self.special(default)
+        super(DateAction, self).__init__(option_strings, dest, nargs=nargs, **kwargs)
+
+    def __call__(
+        self,
+        parser: typing.Any,
+        namespace: argparse.Namespace,
+        values: typing.Union[str, typing.Sequence[str], None],
+        option_string: typing.Optional[str] = None,
+    ) -> None:
+        # this is only called if the option is specified
+        if values is None:
+            return None
+        assert isinstance(values, str)
+        s = values
+        for c in './-_':
+            s = s.replace(c, '')
+        try:
+            val = datetime.datetime.strptime(s, '%Y%m%d').date()
+        except ValueError:
+            val = self.special(s)
+        #    val = self.const
+        setattr(namespace, self.dest, val)
+
+    def special(self, date_s: str) -> DefaultVal:
+        if isinstance(date_s, str):
+            today = datetime.date.today()
+            one_day = datetime.timedelta(days=1)
+            if date_s == 'today':
+                return DefaultVal(today)
+            if date_s == 'yesterday':
+                return DefaultVal(today - one_day)
+            if date_s == 'tomorrow':
+                return DefaultVal(today + one_day)
+            raise ValueError

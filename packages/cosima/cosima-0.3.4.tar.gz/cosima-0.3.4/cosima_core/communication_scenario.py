fc4b154e6a6f9de99574b747324577f0c00f51ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:21 +0000

    Add package cosima/0.3.4/cosima-0.3.4.tar.gz

---
+++ packages/cosima/cosima-0.3.4.tar.gz/cosima-0.3.4/cosima_core/communication_scenario.py
@@ -0,0 +1,123 @@
+from time import sleep
+
+from mosaik import scenario
+
+from cosima_core.config import AGENTS_WITH_PV_PLANT
+import cosima_core.config as cfg
+from cosima_core.util.util_functions import start_omnet, \
+    check_omnet_connection, stop_omnet, \
+    get_host_names, log
+
+
+def main(num_agents=None, omnet_network=None, parallel=None, offset=0, sim_end=None,
+         infrastructure_changes=None):
+    if num_agents:
+        cfg.NUMBER_OF_AGENTS = num_agents
+    if infrastructure_changes:
+        cfg.INFRASTRUCTURE_CHANGES = infrastructure_changes
+    if omnet_network:
+        cfg.START_MODE = "cmd"
+        cfg.NETWORK = omnet_network
+    if parallel is None:
+        parallel = cfg.PARALLEL
+        offset = cfg.OFFSET
+    if sim_end:
+        cfg.SIMULATION_END = sim_end
+
+    sim_config = {
+        'Collector': {
+            'python': 'cosima_core.simulators.collector:Collector'},
+        'CommunicationSimulator': {'python': 'cosima_core.simulators.communication_simulator:CommunicationSimulator'},
+        'Agent': {
+            'python': 'cosima_core.simulators.tic_toc_example.agent_simulator:Agent'},
+        'ICTController': {
+            'python': 'cosima_core.simulators.ict_controller_simulator:ICTController'},
+        'CSV': {'python': 'cosima_core.simulators.tic_toc_example.mosaik_csv:CSV'}
+
+    }
+    omnet_process = start_omnet(cfg.START_MODE, cfg.NETWORK)
+    check_omnet_connection(cfg.PORT)
+    if cfg.TRACK_PERFORMANCE:
+        from cosima_core.util.track_performance import PerformanceTracker
+        tracker = PerformanceTracker()
+
+    world = scenario.World(sim_config, debug=True, time_resolution=0.001,
+                           cache=False)
+
+    agent_names = get_host_names(num_hosts=cfg.NUMBER_OF_AGENTS)
+    client_attribute_mapping = {}
+    # for each client in OMNeT++, save the connect-attribute for mosaik
+    for agent_name in agent_names:
+        client_attribute_mapping[agent_name] = cfg.CONNECT_ATTR + agent_name
+
+    agents = {}
+    collector = world.start('Collector').Monitor()
+    agent_prefix = 'client'
+    communication_simulator = world.start('CommunicationSimulator',
+                                          step_size=cfg.USED_STEP_SIZE,
+                                          port=cfg.PORT,
+                                          client_attribute_mapping=client_attribute_mapping).CommunicationModel()
+
+    if len(cfg.INFRASTRUCTURE_CHANGES) > 0:
+        ict_controller = world.start('ICTController',
+                                     infrastructure_changes=cfg.
+                                     INFRASTRUCTURE_CHANGES).ICT()
+
+    for idx in range(cfg.NUMBER_OF_AGENTS):
+        current_agent_name = agent_prefix + str(idx)
+        agents[current_agent_name] = world.start('Agent',
+                                                 agent_name=current_agent_name,
+                                                 content_path=cfg.CONTENT_PATH,
+                                                 step_size=cfg.USED_STEP_SIZE,
+                                                 agent_names=
+                                                 get_host_names(
+                                                     num_hosts=cfg.NUMBER_OF_AGENTS),
+                                                 parallel=parallel,
+                                                 offset=offset).A()
+    for agent in agents.values():
+        if agent.eid in AGENTS_WITH_PV_PLANT:
+            pv_sim = world.start('CSV', sim_start=cfg.START, datafile=cfg.PV_DATA,
+                                 delimiter=',')
+            pv = pv_sim.PV.create(1)[0]
+            world.connect(pv, agents[agent.eid], 'P', weak=True)
+            world.connect(agents[agent.eid], pv, 'ACK')
+
+            world.set_initial_event(pv.sid)
+        else:
+            world.set_initial_event(agent.sid, time=0)
+
+    if len(cfg.INFRASTRUCTURE_CHANGES) > 0:
+        world.set_initial_event(ict_controller.sid)
+
+    if parallel:
+        world.set_initial_event(agents['client1'].sid, time=offset)
+
+    for agent_name, agent in agents.items():
+        # connect agents with communication_models
+        world.connect(agent, communication_simulator, f'message', weak=True)
+
+    for client_name, attr in client_attribute_mapping.items():
+        # connect communication_models with agents
+        world.connect(communication_simulator, agents[client_name],
+                      attr)
+        world.connect(communication_simulator, collector,
+                      attr)
+    if len(cfg.INFRASTRUCTURE_CHANGES) > 0:
+        # connect ict controller to collector
+        world.connect(ict_controller, collector, f'ict_message')
+        # connect ict controller with communication_simulator
+        world.connect(ict_controller, communication_simulator, f'ict_message')
+        world.connect(communication_simulator, ict_controller, f'ctrl_message', weak=True)
+
+    until = cfg.SIMULATION_END * cfg.USED_STEP_SIZE
+    log(f'run until {until}')
+    world.run(until=until)
+    log("end of process")
+    sleep(5)
+    stop_omnet(omnet_process)
+    if cfg.TRACK_PERFORMANCE:
+        tracker.save_results()
+
+
+if __name__ == '__main__':
+    main()

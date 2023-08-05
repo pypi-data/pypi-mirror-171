9fb1f469f650e604e783bf33387c2645a0fa4bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:02 +0000

    Add package networklab/1.4.0.dev1/networklab-1.4.0.dev1.tar.gz

---
+++ packages/networklab/networklab-1.4.0.dev1.tar.gz/networklab-1.4.0.dev1/setup.py
@@ -0,0 +1,38 @@
+"""setup.py file."""
+from setuptools import setup, find_packages
+import netsim
+import sys
+
+with open("requirements.txt", "r") as fs:
+  reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
+
+if sys.version_info < (3, 7):
+  raise RuntimeError("This package requires Python 3.7+")
+
+setup(
+  name="networklab",
+  version=netsim.__version__,
+  packages=find_packages(),
+  author="Ivan Pepelnjak",
+  author_email="ip@ipspace.net",
+  description="CLI-based Virtual Networking Lab Abstraction Layer",
+  classifiers=[
+    "Topic :: Utilities",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: MacOS",
+  ],
+  url="https://github.com/ipspace/netlab",
+  include_package_data=True,
+  setup_requires=["wheel"],
+  python_requires='>=3.7',  # Due to e.g. 'capture_output' in subprocess.run
+  install_requires=reqs,
+  entry_points={
+    "console_scripts": ["netlab=netsim.cli:lab_commands"]
+  },
+  package_data = {
+    "netsim": ["templates/*", "ansible/*", "extra/*"]
+  }
+)

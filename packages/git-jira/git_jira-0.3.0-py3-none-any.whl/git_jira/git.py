b44a58190018c5c76812b87d78c4296222f37ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:58 +0000

    Add package git-jira/0.3.0/git_jira-0.3.0-py3-none-any.whl

---
+++ packages/git-jira/git_jira-0.3.0-py3-none-any.whl/git_jira/git.py
@@ -0,0 +1,23 @@
+from sh import git
+import re
+
+class GitBranch:
+    def __init__(self, name=None):
+        if name:
+            self.name = name
+        else:
+            self.name = str(git.branch("--show-current"))[:-1] #Find a better way
+            try:
+                self.issue_key = re.match('^[A-Z]{2}-[0-9]*', self.name)[0]
+            except:
+                self.issue_key = None
+
+    def create(self):
+        git.checkout("-b", self.name)
+
+class GitRepo:
+    def __init__(self):
+        pass
+
+    def get_branches(self):
+        return [b for b in git("--no-pager", "branch", "--all", "--format='%(refname:short)'").stdout.decode().replace("'", "").split('\n') if len(b)>0]

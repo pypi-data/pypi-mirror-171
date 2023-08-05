c43ac1a06bba827d8f68ce77daca35ae931421d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:44 +0000

    Add package mirutil/9.3/mirutil-9.3.tar.gz

---
+++ packages/mirutil/mirutil-9.3.tar.gz/mirutil-9.3/src/mirutil/codal.py
@@ -0,0 +1,22 @@
+"""
+
+    """
+
+import re
+
+
+def find_fn_and_suf_fr_codal_get_resp(r) :
+    hdr = r.headers
+    cd = hdr["Content-Disposition"]
+
+    pat = 'filename=(.+)\.(\w+)'
+    rf = re.findall(pat , cd)
+    if len(rf) == 0 :
+        return
+
+    g0 = rf[0]
+    fn , suf = g0
+    return {
+            "fn"  : fn ,
+            "suf" : suf
+            }

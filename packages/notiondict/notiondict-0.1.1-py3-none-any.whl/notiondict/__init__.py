7793b7731ab40cfbccee06dd79afd5644d805f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:36 +0000

    Add package notiondict/0.1.1/notiondict-0.1.1-py3-none-any.whl

---
+++ packages/notiondict/notiondict-0.1.1-py3-none-any.whl/notiondict/__init__.py
@@ -0,0 +1,13 @@
+'''
+Author: pixelhegel pixelhegel@gmail.com
+Date: 2022-10-08 10:39:55
+LastEditors: pixelhegel pixelhegel@gmail.com
+LastEditTime: 2022-10-10 21:36:38
+FilePath: /NotionDict/notiondict/__init__.py
+Description: 
+
+Copyright (c) 2022 by pixelhegel pixelhegel@gmail.com, All Rights Reserved. 
+'''
+__author__ = 'Pixelhegel'
+__email__ = 'pixelhegel@gmail.com'
+__version__ = '0.1.1'

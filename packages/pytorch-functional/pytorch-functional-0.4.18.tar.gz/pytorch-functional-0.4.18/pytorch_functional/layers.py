969582e9db8ee8a84de6c6725b0cef89a8990e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package pytorch-functional/0.4.18/pytorch-functional-0.4.18.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.4.18.tar.gz/pytorch-functional-0.4.18/pytorch_functional/layers.py
@@ -0,0 +1,85 @@
+#  Copyright (c) 2022 Szymon Mikler
+
+import torch
+from torch import nn
+
+
+class AddOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a + b
+
+
+class SubOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a - b
+
+
+class MulOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a * b
+
+
+class ModOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return torch.remainder(a, b)
+
+
+class MatmulOpLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(a, b):
+        return a @ b
+
+
+class AnyOpLayer(nn.Module):
+    def __init__(self, op):
+        super().__init__()
+        self.forward = op
+
+
+class ConcatLayer(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, *tensors):
+        return torch.cat(tensors=tensors, dim=self.dim)
+
+
+class StackLayer(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, *tensors):
+        return torch.stack(tensors=tensors, dim=self.dim)
+
+
+class ReshapeLayer(nn.Module):
+    def __init__(self, shape, batch_size_included=False):
+        super().__init__()
+        if not batch_size_included:
+            self.shape = (-1, *shape)
+        else:
+            self.shape = shape
+
+    def forward(self, inputs):
+        return torch.reshape(input=inputs, shape=self.shape)

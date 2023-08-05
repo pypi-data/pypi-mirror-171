8ff082bf84125d79063270073c48d29d26dfb1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:14 +0000

    Add package meishiki/0.0.1/meishiki-0.0.1-py3-none-any.whl

---
+++ packages/meishiki/meishiki-0.0.1-py3-none-any.whl/meishiki/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from meishiki.meishiki import build_meishiki
+from meishiki.unsei import build_unsei
+from meishiki.output import output_html, output_stdio
+
+__all__ = ["build_meishiki", "build_unsei", "output_html", "output_stdio"]

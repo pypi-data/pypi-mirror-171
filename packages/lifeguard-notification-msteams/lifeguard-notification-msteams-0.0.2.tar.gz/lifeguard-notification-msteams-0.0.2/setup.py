16170ff0320625d1e127a4227b86e4fc47c8f903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:00 +0000

    Add package lifeguard-notification-msteams/0.0.2/lifeguard-notification-msteams-0.0.2.tar.gz

---
+++ packages/lifeguard-notification-msteams/lifeguard-notification-msteams-0.0.2.tar.gz/lifeguard-notification-msteams-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lifeguard-notification-msteams",
+    version="0.0.2",
+    url="https://github.com/LifeguardSystem/lifeguard-notification-msteams",
+    author="Diego Rubin",
+    author_email="contact@diegorubin.dev",
+    license="GPL2",
+    scripts=[],
+    include_package_data=True,
+    description="Lifeguard integration with MS Teams",
+    install_requires=["lifeguard", "pymsteams"],
+    classifiers=["Development Status :: 3 - Alpha"],
+    packages=find_packages(),
+)

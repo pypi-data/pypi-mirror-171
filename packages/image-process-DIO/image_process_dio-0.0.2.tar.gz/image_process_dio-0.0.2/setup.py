f81ce4ccee879bac626e49215a4cf8cd673dc420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:46 +0000

    Add package image-process-DIO/0.0.2/image_process_dio-0.0.2.tar.gz

---
+++ packages/image-process-DIO/image_process_dio-0.0.2.tar.gz/image_process_dio-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_process_dio",
+    version="0.0.2",
+    author="glauciodrumond",
+    author_email="glauciotad@gmail.com",
+    description="Test package for image processing",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/glauciodrumond/unimed-datascience/IMAGE_PROCESS_PKG",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.7',
+)

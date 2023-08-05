e9536540818a327972a3cf639d36c0f8ec5392f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:13 +0000

    Add package SplatStats/0.5.0/SplatStats-0.5.0.tar.gz

---
+++ packages/SplatStats/SplatStats-0.5.0.tar.gz/SplatStats-0.5.0/SplatStats/__init__.py
@@ -0,0 +1,15 @@
+
+# Constants
+from .constants import *
+from .colors import *
+# Functions
+from .auxiliary import *
+from .parsers import *
+from .stats import *
+from .files import *
+from .plots import *
+from .plotsAux import *
+# Classes
+# from .History import *
+from .Battle import *
+from .Player import *

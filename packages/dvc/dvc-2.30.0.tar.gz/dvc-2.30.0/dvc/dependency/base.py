fcf7f502b1d5675385fdc08c267936962c5ed755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:42 +0000

    Add package dvc/2.30.0/dvc-2.30.0.tar.gz

---
+++ packages/dvc/dvc-2.30.0.tar.gz/dvc-2.30.0/dvc/dependency/base.py
@@ -0,0 +1,38 @@
+from typing import Type
+
+from dvc.exceptions import DvcException
+from dvc.fs import download as fs_download
+from dvc.output import Output
+
+
+class DependencyDoesNotExistError(DvcException):
+    def __init__(self, path):
+        msg = f"dependency '{path}' does not exist"
+        super().__init__(msg)
+
+
+class DependencyIsNotFileOrDirError(DvcException):
+    def __init__(self, path):
+        msg = f"dependency '{path}' is not a file or directory"
+        super().__init__(msg)
+
+
+class DependencyIsStageFileError(DvcException):
+    def __init__(self, path):
+        super().__init__(f"DVC file '{path}' cannot be a dependency.")
+
+
+class Dependency(Output):
+    IS_DEPENDENCY = True
+
+    DoesNotExistError = DependencyDoesNotExistError  # type: Type[DvcException]
+    IsNotFileOrDirError = (
+        DependencyIsNotFileOrDirError
+    )  # type: Type[DvcException]
+    IsStageFileError = DependencyIsStageFileError  # type: Type[DvcException]
+
+    def update(self, rev=None):
+        pass
+
+    def download(self, to, jobs=None):
+        fs_download(self.fs, self.fs_path, to, jobs=jobs)

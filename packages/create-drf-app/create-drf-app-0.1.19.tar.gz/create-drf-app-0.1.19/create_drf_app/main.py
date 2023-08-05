6f6afb3d13af703dad791d041e95ccbec9041f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:13 +0000

    Add package create-drf-app/0.1.19/create-drf-app-0.1.19.tar.gz

---
+++ packages/create-drf-app/create-drf-app-0.1.19.tar.gz/create-drf-app-0.1.19/create_drf_app/main.py
@@ -0,0 +1,52 @@
+import os
+import platform
+import pathlib
+
+import typer
+from cookiecutter.main import cookiecutter
+
+OS = platform.system()
+
+app = typer.Typer()
+
+@app.callback()
+def callback():
+    """
+    DRF scaffold
+    """
+
+def get_template_path(template_name):
+    script_path = os.path.abspath(__file__)
+    current_dir_path = os.path.dirname(script_path)
+    templates_path = os.path.join(current_dir_path, "templates")
+    return os.path.join(templates_path, template_name)
+    
+
+def activate_venv(path):
+
+    # set current directory
+    os.chdir(path)
+    print("🔨 setting up your virutal environment")
+    os.system("python -m venv venv")
+
+    env_activate_script = ""
+    if OS == "Windows":
+        env_activate_script = "Scripts"
+
+    activate_file = os.path.join(path, "env", env_activate_script, "activate")
+    print("✏️ downloading packages")
+    os.system(f"{activate_file} && pip install -r requirements.txt")
+
+
+@app.command("")
+def main(template_name):
+    print("🔨 setting up your project")
+    template_path = get_template_path(template_name)
+    path = cookiecutter(template_path)
+    activate_venv(path)
+    print("💥 your DRF project is ready!")
+    folder_name = pathlib.PurePath(path)
+    if OS == "Windows":
+        activate_file = os.path.join(path, "env", "Scripts", "activate")
+        print(f"cd {folder_name.name} && {activate_file}")
+

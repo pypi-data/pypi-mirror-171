9236252e52167d5dbe3141e4a715abcee7b199c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:33 +0000

    Add package jptest2/2.0.9/jptest2-2.0.9.tar.gz

---
+++ packages/jptest2/jptest2-2.0.9.tar.gz/jptest2-2.0.9/src/jptest2/__init__.py
@@ -0,0 +1,5 @@
+from .JPFixture import JPPreRun, JPPostRun
+from .JPTest import JPTest
+from .JPTestComparison import JPTestComparison
+from .JPTestGet import JPTestGet
+from .notebook import Notebook

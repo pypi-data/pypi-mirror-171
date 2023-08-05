a51f35b3cf6b2f0228f13ab0bb5744235d316f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package calc-joao-lcareis/0.0.1/calc_joao_lcareis-0.0.1-py3-none-any.whl

---
+++ packages/calc-joao-lcareis/calc_joao_lcareis-0.0.1-py3-none-any.whl/calc-joao-lcareis/file1_name.py
@@ -0,0 +1,12 @@
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+def subtracao(numero1, numero2):
+    return numero1 - numero2
+
+
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+print(soma(num1, num2))
+print(subtracao(num1, num2))

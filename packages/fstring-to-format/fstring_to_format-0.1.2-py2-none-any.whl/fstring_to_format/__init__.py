04857a111c9177977fb78ebac429d55a9e4a2bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:33 +0000

    Add package fstring-to-format/0.1.2/fstring_to_format-0.1.2-py2-none-any.whl

---
+++ packages/fstring-to-format/fstring_to_format-0.1.2-py2-none-any.whl/fstring_to_format/__init__.py
@@ -0,0 +1,12 @@
+"""
+fstring_to_format converts Python f-string expressions to .format() for backwards compatibility
+"""
+
+from __future__ import absolute_import
+
+# meta
+__title__ = "fstring_to_format"
+__author__ = "Eftal Gezer"
+__license__ = "GNU GPL v3"
+__copyright__ = "Copyright 2022, Eftal Gezer"
+__version__ = "0.1.2"

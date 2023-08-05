9c8aa7572bd8307321ab4b2d207bee1de3d00d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package temgymbasic/0.5.2/temgymbasic-0.5.2.tar.gz

---
+++ packages/temgymbasic/temgymbasic-0.5.2.tar.gz/temgymbasic-0.5.2/pyqtgraph_examples/beam_tilt_shift_basic_example_pyqt.py
@@ -0,0 +1,22 @@
+from temgymbasic import components as comp
+from temgymbasic.model import Model
+from temgymbasic.run import run_pyqt
+from pyqt5 import QApplication
+import sys 
+
+def main():
+    #Create List of Components
+    components = [comp.DoubleDeflector(name = 'Double Deflector', z_up = 0.4, z_low = 0.3),
+                  comp.Sample(name = 'Sample', z = 0.2),
+                  comp.Lens(name = 'Objective Lens', z = 0.1, f = -0.1)]
+    
+    #Generate Model
+    model_ = Model(components, beam_z = 0.6, beam_type = 'paralell', num_rays = 128)
+    
+    
+    run_pyqt(model_)
+        
+if __name__ == '__main__':
+    AppWindow = QApplication(sys.argv)
+    main()
+    sys.exit(AppWindow.exec_())
---
+++ packages/temgymbasic/temgymbasic-0.5.2.tar.gz/temgymbasic-0.5.2/pyqtgraph_examples/lens_example_pyqt.py
@@ -0,0 +1,16 @@
+from temgymbasic import components as comp
+from temgymbasic.model import Model
+from temgymbasic.run import run_pyqt
+from pyqt5 import QApplication
+import sys 
+
+def main():
+    components = [comp.Lens(name = 'Lens', z = 0.5, f = -0.5)]
+    
+    model_ = Model(components, beam_z = 1.0, beam_type = 'point', num_rays = 32, beam_semi_angle = 0.15)
+    run_pyqt(model_)    
+
+if __name__ == '__main__':
+    AppWindow = QApplication(sys.argv)
+    main()
+    sys.exit(AppWindow.exec_())

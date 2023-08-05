354d2408c894ec4f2d6c1aaa8902c01e08f160a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:08 +0000

    Add package zdiab-tools/0.2/zdiab_tools-0.2.tar.gz

---
+++ packages/zdiab-tools/zdiab_tools-0.2.tar.gz/zdiab_tools-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zdiab_tools',
+      version='0.2',
+      description='Rasa preprocessing package',
+      packages=['zdiab_tools'],
+      author = 'zohair diab',
+      author_email = 'zohairdiab07@gmail.com',
+      zip_safe=False)

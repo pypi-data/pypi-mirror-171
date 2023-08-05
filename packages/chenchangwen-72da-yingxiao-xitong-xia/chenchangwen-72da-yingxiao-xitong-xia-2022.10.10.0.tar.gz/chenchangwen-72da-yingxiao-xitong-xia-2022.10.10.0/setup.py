2fa563e90d0fff66aed3d839a164f51c4f276f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:04 +0000

    Add package chenchangwen-72da-yingxiao-xitong-xia/2022.10.10.0/chenchangwen-72da-yingxiao-xitong-xia-2022.10.10.0.tar.gz

---
+++ packages/chenchangwen-72da-yingxiao-xitong-xia/chenchangwen-72da-yingxiao-xitong-xia-2022.10.10.0.tar.gz/chenchangwen-72da-yingxiao-xitong-xia-2022.10.10.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import Chenchangwen72daYingxiaoXitongXia
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('Chenchangwen72daYingxiaoXitongXia'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="chenchangwen-72da-yingxiao-xitong-xia",
+    version=Chenchangwen72daYingxiaoXitongXia.__version__,
+    url="https://github.com/apachecn/chenchangwen-72da-yingxiao-xitong-xia",
+    author=Chenchangwen72daYingxiaoXitongXia.__author__,
+    author_email=Chenchangwen72daYingxiaoXitongXia.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="陈昌文：72大营销系统（下）",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "chenchangwen-72da-yingxiao-xitong-xia=Chenchangwen72daYingxiaoXitongXia.__main__:main",
+            "Chenchangwen72daYingxiaoXitongXia=Chenchangwen72daYingxiaoXitongXia.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

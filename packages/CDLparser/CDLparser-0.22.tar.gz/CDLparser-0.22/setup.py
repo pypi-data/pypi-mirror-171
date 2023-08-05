5ed81e55b4ad303e3cf2960534e809dec347cce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:13 +0000

    Add package CDLparser/0.22/CDLparser-0.22.tar.gz

---
+++ packages/CDLparser/CDLparser-0.22.tar.gz/CDLparser-0.22/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="CDLparser",
+    version="0.22",
+    author="Ben Sanderson",
+    description="A python netcdf CDF parser",
+    packages=["cdlparser"]
+    )

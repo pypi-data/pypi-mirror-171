f2899964f44307e8a6b0f32cc36ac3a67aabbe4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:45 +0000

    Add package bhargabchipkg/0.0.2/bhargabchipkg-0.0.2.tar.gz

---
+++ packages/bhargabchipkg/bhargabchipkg-0.0.2.tar.gz/bhargabchipkg-0.0.2/README.md
@@ -0,0 +1,3 @@
+# bhargabchipkg
+This is a helpful open source package available in your python to get chi square result
+
---
+++ packages/bhargabchipkg/bhargabchipkg-0.0.2.tar.gz/bhargabchipkg-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(name='bhargabchipkg',
+      packages = ['bhargabchipkg'],
+      version='0.0.2',
+      description='helpful chi-square package',
+      url='https://github.com/bhargabganguli/bhargabchipkg',
+      download_url = 'https://github.com/bhargabganguli/bhargabchipkg/archive/0.1.tar.gz', #FILL IN LATER
+      author='Bhargab',
+      author_email='bhargab.ganguli@gmail.com',
+      keywords = ['chi_test',],
+      license='MIT', #YOUR LICENSE HERE!
+
+      install_requires=['pandas','numpy','scipy',],  #YOUR DEPENDENCIES HERE
+  
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', # Your License Here  
+        'Programming Language :: Python :: 3',    # List Python versions that you support Here  
+        'Programming Language :: Python :: 3.4',
+        ],
+)

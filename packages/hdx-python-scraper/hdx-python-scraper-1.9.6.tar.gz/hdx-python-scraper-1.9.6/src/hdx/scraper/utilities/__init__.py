f60ae72f6fbde880c47119a4267b14802e6b0d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package hdx-python-scraper/1.9.6/hdx-python-scraper-1.9.6.tar.gz

---
+++ packages/hdx-python-scraper/hdx-python-scraper-1.9.6.tar.gz/hdx-python-scraper-1.9.6/src/hdx/scraper/utilities/__init__.py
@@ -0,0 +1,94 @@
+import re
+from datetime import datetime
+from typing import Any, Dict, Optional, Tuple
+
+from hdx.data.dataset import Dataset
+
+template = re.compile("{{.*?}}")
+
+
+def string_params_to_dict(string: str) -> Dict[str, str]:
+    params = dict()
+    if not string:
+        return params
+    for name_par in string.split(","):
+        name, par = name_par.strip().split(":")
+        params[name] = par.strip()
+    return params
+
+
+def match_template(input: str) -> Tuple[Optional[str], Optional[str]]:
+    """Try to match {{XXX}} in input string
+
+    Args:
+        input (str): String in which to look for template
+
+    Returns:
+        Tuple[Optional[str], Optional[str]]: (Matched string with brackets, matched string without brackets)
+    """
+    match = template.search(input)
+    if match:
+        template_string = match.group()
+        return template_string, template_string[2:-2]
+    return None, None
+
+
+def get_rowval(row: Dict, valcol: str) -> Any:
+    """Get the value of a particular column in a row expanding any template it contains
+
+    Args:
+        row (Dict): Dictionary
+        valcol (str): Column which may be a string in which to look for template
+
+    Returns:
+        Any: Value of column or template
+    """
+    if "{{" in valcol:
+        repvalcol = valcol
+        for match in template.finditer(valcol):
+            template_string = match.group()
+            replace_string = f'row["{template_string[2:-2]}"]'
+            repvalcol = repvalcol.replace(template_string, replace_string)
+        return eval(repvalcol)
+    else:
+        result = row[valcol]
+        if isinstance(result, str):
+            return result.strip()
+        return result
+
+
+def get_date_from_dataset_date(
+    dataset: Dataset, today: Optional[datetime] = None
+) -> Optional[datetime]:
+    """Return the date or end date of a dataset
+
+    Args:
+        dataset (Dataset): Dataset object
+        today (Optional[datetime]): Date to use for today. Defaults to None (datetime.utcnow)
+
+    Returns:
+        Optional[datetime]: Date or end date of a dataset
+    """
+    if today is None:
+        date_info = dataset.get_date_of_dataset()
+    else:
+        date_info = dataset.get_date_of_dataset(today=today)
+    return date_info.get("enddate")
+
+
+def get_isodate_from_dataset_date(
+    dataset: Dataset, today: Optional[datetime] = None
+) -> Optional[str]:
+    """Return the date or end date of a dataset as an iso formatted date
+
+    Args:
+        dataset (Dataset): Dataset object
+        today (Optional[datetime]): Date to use for today. Defaults to None (datetime.utcnow)
+
+    Returns:
+        Optional[str]: Date or end date of a dataset
+    """
+    date = get_date_from_dataset_date(dataset, today)
+    if date:
+        date = date.strftime("%Y-%m-%d")
+    return date
---
+++ packages/hdx-python-scraper/hdx-python-scraper-1.9.6.tar.gz/hdx-python-scraper-1.9.6/tests/hdx/scraper/test_scrapers_subnational.py
@@ -0,0 +1,408 @@
+from hdx.location.adminlevel import AdminLevel
+from hdx.utilities.dateparse import parse_date
+
+from hdx.scraper.base_scraper import BaseScraper
+from hdx.scraper.outputs.json import JsonFile
+from hdx.scraper.outputs.update_tabs import update_subnational
+from hdx.scraper.runner import Runner
+
+from .conftest import run_check_scraper
+
+
+class TestScrapersSubnational:
+    def test_get_subnational(self, configuration):
+        BaseScraper.population_lookup = dict()
+        today = parse_date("2020-10-01")
+        adminlevel = AdminLevel(configuration)
+        level = "subnational"
+        scraper_configuration = configuration[f"scraper_{level}"]
+        runner = Runner(("AFG",), today)
+        keys = runner.add_configurables(
+            scraper_configuration, level, adminlevel=adminlevel
+        )
+        assert keys == ["gam", "ipc_somalia"]
+
+        name = "gam"
+        headers = (
+            ["Malnutrition Estimate"],
+            ["#severity+malnutrition+num+subnational"],
+        )
+        values = [
+            {
+                "AF17": 3.371688,
+                "AF31": 3.519166,
+                "AF09": 1.524646,
+                "AF21": 1.319626,
+                "AF10": 1.40426,
+                "AF24": 1.043487,
+                "AF33": 2.745447,
+                "AF29": 2.478977,
+                "AF11": 1.022871,
+                "AF23": 1.340286,
+                "AF30": 1.677612,
+                "AF32": 1.687488,
+                "AF28": 0.6210205,
+                "AF01": 1.282291,
+                "AF27": 1.378641,
+                "AF02": 3.552082,
+                "AF14": 0.7653555,
+                "AF15": 0.953823,
+                "AF19": 1.684882,
+                "AF07": 2.090165,
+                "AF05": 0.9474334,
+                "AF06": 2.162038,
+                "AF34": 1.6455,
+                "AF16": 1.927783,
+                "AF12": 4.028857,
+                "AF13": 9.150105,
+                "AF08": 1.64338,
+                "AF03": 2.742952,
+                "AF20": 1.382376,
+                "AF22": 1.523334,
+                "AF18": 0.9578965,
+                "AF25": 0.580423,
+                "AF04": 0.501081,
+                "AF26": 4.572629,
+            }
+        ]
+        sources = [
+            (
+                "#severity+malnutrition+num+subnational",
+                "2020-10-01",
+                "UNICEF",
+                "https://data.humdata.org/dataset/87b86e7d-e9b2-4922-a48e-1f10afd528e6/resource/eabba7e7-16d0-436c-a62c-df6edd03be7c/download/unicef_who_wb_global_expanded_databases_severe_wasting.xlsx",
+            )
+        ]
+        run_check_scraper(
+            name,
+            runner,
+            level,
+            headers,
+            values,
+            sources,
+            set_not_run=False,
+        )
+
+        jsonout = JsonFile(configuration["json"], [level])
+        outputs = {"json": jsonout}
+        update_subnational(
+            runner,
+            adminlevel,
+            outputs,
+            names=(name,),
+        )
+        assert jsonout.json[f"{level}_data"] == [
+            {
+                "#adm1+code": "AF01",
+                "#adm1+name": "Kabul",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.282291",
+            },
+            {
+                "#adm1+code": "AF02",
+                "#adm1+name": "Kapisa",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "3.552082",
+            },
+            {
+                "#adm1+code": "AF03",
+                "#adm1+name": "Parwan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "2.742952",
+            },
+            {
+                "#adm1+code": "AF04",
+                "#adm1+name": "Maidan Wardak",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.501081",
+            },
+            {
+                "#adm1+code": "AF05",
+                "#adm1+name": "Logar",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.9474334",
+            },
+            {
+                "#adm1+code": "AF06",
+                "#adm1+name": "Nangarhar",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "2.162038",
+            },
+            {
+                "#adm1+code": "AF07",
+                "#adm1+name": "Laghman",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "2.090165",
+            },
+            {
+                "#adm1+code": "AF08",
+                "#adm1+name": "Panjsher",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.64338",
+            },
+            {
+                "#adm1+code": "AF09",
+                "#adm1+name": "Baghlan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.524646",
+            },
+            {
+                "#adm1+code": "AF10",
+                "#adm1+name": "Bamyan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.40426",
+            },
+            {
+                "#adm1+code": "AF11",
+                "#adm1+name": "Ghazni",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.022871",
+            },
+            {
+                "#adm1+code": "AF12",
+                "#adm1+name": "Paktika",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "4.028857",
+            },
+            {
+                "#adm1+code": "AF13",
+                "#adm1+name": "Paktya",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "9.150105",
+            },
+            {
+                "#adm1+code": "AF14",
+                "#adm1+name": "Khost",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.7653555",
+            },
+            {
+                "#adm1+code": "AF15",
+                "#adm1+name": "Kunar",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.953823",
+            },
+            {
+                "#adm1+code": "AF16",
+                "#adm1+name": "Nuristan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.927783",
+            },
+            {
+                "#adm1+code": "AF17",
+                "#adm1+name": "Badakhshan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "3.371688",
+            },
+            {
+                "#adm1+code": "AF18",
+                "#adm1+name": "Takhar",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.9578965",
+            },
+            {
+                "#adm1+code": "AF19",
+                "#adm1+name": "Kunduz",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.684882",
+            },
+            {
+                "#adm1+code": "AF20",
+                "#adm1+name": "Samangan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.382376",
+            },
+            {
+                "#adm1+code": "AF21",
+                "#adm1+name": "Balkh",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.319626",
+            },
+            {
+                "#adm1+code": "AF22",
+                "#adm1+name": "Sar E Pul",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.523334",
+            },
+            {
+                "#adm1+code": "AF23",
+                "#adm1+name": "Ghor",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.340286",
+            },
+            {
+                "#adm1+code": "AF24",
+                "#adm1+name": "Daykundi",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.043487",
+            },
+            {
+                "#adm1+code": "AF25",
+                "#adm1+name": "Uruzgan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.580423",
+            },
+            {
+                "#adm1+code": "AF26",
+                "#adm1+name": "Zabul",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "4.572629",
+            },
+            {
+                "#adm1+code": "AF27",
+                "#adm1+name": "Kandahar",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.378641",
+            },
+            {
+                "#adm1+code": "AF28",
+                "#adm1+name": "Jawzjan",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "0.6210205",
+            },
+            {
+                "#adm1+code": "AF29",
+                "#adm1+name": "Faryab",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "2.478977",
+            },
+            {
+                "#adm1+code": "AF30",
+                "#adm1+name": "Hilmand",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.677612",
+            },
+            {
+                "#adm1+code": "AF31",
+                "#adm1+name": "Badghis",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "3.519166",
+            },
+            {
+                "#adm1+code": "AF32",
+                "#adm1+name": "Hirat",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.687488",
+            },
+            {
+                "#adm1+code": "AF33",
+                "#adm1+name": "Farah",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "2.745447",
+            },
+            {
+                "#adm1+code": "AF34",
+                "#adm1+name": "Nimroz",
+                "#country+code": "AFG",
+                "#country+name": "Afghanistan",
+                "#severity+malnutrition+num+subnational": "1.6455",
+            },
+        ]
+
+        scraper_configuration = configuration["other"]
+        runner.add_configurables(
+            scraper_configuration, level, adminlevel=adminlevel
+        )
+        name = "gam_other"
+        headers = (
+            ["Malnutrition Estimate"],
+            ["#severity+malnutrition+num+subnational"],
+        )
+        values = [{"AF09": 1.524646, "AF24": 1.043487}]
+        sources = [
+            (
+                "#severity+malnutrition+num+subnational",
+                "2020-10-01",
+                "UNICEF",
+                "https://data.humdata.org/dataset/87b86e7d-e9b2-4922-a48e-1f10afd528e6/resource/eabba7e7-16d0-436c-a62c-df6edd03be7c/download/unicef_who_wb_global_expanded_databases_severe_wasting.xlsx",
+            )
+        ]
+        run_check_scraper(name, runner, level, headers, values, sources)
+
+    def test_fixed_country(self, configuration):
+        BaseScraper.population_lookup = dict()
+        today = parse_date("2020-10-01")
+        adminlevel = AdminLevel(configuration["admin1"])
+        level = "subnational"
+        scraper_configuration = configuration[f"scraper_{level}"]
+        runner = Runner(("SOM",), today)
+        runner.add_configurables(
+            scraper_configuration, level, adminlevel=adminlevel
+        )
+        name = "ipc_somalia"
+        headers = (
+            ["FoodInsecurityIPCP3+"],
+            ["#affected+food+ipc+p3plus+num"],
+        )
+        values = [
+            {
+                "SO11": "141240",
+                "SO12": "304640",
+                "SO13": "266310",
+                "SO14": "190270",
+                "SO15": "206550",
+                "SO16": "377780",
+                "SO17": "328470",
+                "SO18": "640920",
+                "SO19": "380590",
+                "SO20": "192450",
+                "SO21": "378550",
+                "SO22": "774030",
+                "SO23": "412570",
+                "SO24": "924800",
+                "SO25": "276220",
+                "SO26": "356170",
+                "SO27": "145310",
+                "SO28": "383900",
+            }
+        ]
+        sources = [
+            (
+                "#affected+food+ipc+p3plus+num",
+                "2022-12-31",
+                "National IPC Technical Working Group",
+                "https://data.humdata.org/dataset/somalia-acute-food-insecurity-country-data",
+            )
+        ]
+        run_check_scraper(
+            name,
+            runner,
+            level,
+            headers,
+            values,
+            sources,
+            set_not_run=False,
+        )

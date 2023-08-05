a667f386f5045746208ecca6b5a925cf3d358b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:14 +0000

    Add package lamindb/0.8.0/lamindb-0.8.0-py2.py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.8.0-py2.py3-none-any.whl/lamindb/dev/object/_core.py
@@ -0,0 +1,24 @@
+from anndata import AnnData
+from pandas import DataFrame
+
+
+def infer_suffix(dmem):
+    """Infer LaminDB storage file suffix from a data object."""
+    if isinstance(dmem, AnnData):
+        return ".h5ad"
+    elif isinstance(dmem, DataFrame):
+        return ".feather"
+    else:
+        raise NotImplementedError
+
+
+def write_to_file(dmem, filepath: str):
+    if isinstance(dmem, AnnData):
+        dmem.write(filepath)
+    elif isinstance(dmem, DataFrame):
+        try:
+            dmem.to_feather(filepath)
+        except ValueError:
+            dmem.reset_index().to_feather(filepath)
+    else:
+        raise NotImplementedError

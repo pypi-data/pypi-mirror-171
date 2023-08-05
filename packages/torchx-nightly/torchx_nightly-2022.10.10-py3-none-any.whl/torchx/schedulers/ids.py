d38856876b05a070ad406fa83757d7b7e2b99b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:34 +0000

    Add package torchx-nightly/2022.10.10/torchx_nightly-2022.10.10-py3-none-any.whl

---
+++ packages/torchx-nightly/torchx_nightly-2022.10.10-py3-none-any.whl/torchx/schedulers/ids.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import struct
+
+
+def make_unique(name: str) -> str:
+    """
+    Appends a unique 64-bit string to the input argument.
+
+    Returns:
+        string in format $name-$unique_suffix
+    """
+    rand_suffix = random_id()
+    return f"{name}-{rand_suffix}"
+
+
+def random_uint64() -> int:
+    """
+    random_uint64 returns an random unsigned 64 bit int.
+    """
+    return struct.unpack("!Q", os.urandom(8))[0]
+
+
+def random_id() -> str:
+    """
+    Generates an alphanumeric string ID that matches the requirements from
+    https://kubernetes.io/docs/concepts/overview/working-with-objects/names/
+    """
+    START_CANDIDATES = "bcdfghjklmnpqrstvwxz"
+    END_CANDIDATES = START_CANDIDATES + "012345679"
+
+    out = ""
+    v = random_uint64()
+    while v > 0:
+        if out == "":
+            candidates = START_CANDIDATES
+        else:
+            candidates = END_CANDIDATES
+
+        char = v % len(candidates)
+        v = v // len(candidates)
+        out += candidates[char]
+    return out

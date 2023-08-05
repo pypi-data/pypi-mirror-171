00c0d866cf5bb007f0bfe45f6c80a77fe57c4eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:43 +0000

    Add package jieyabolahan-yingxiao-neimu-biji/2022.10.10.0/jieyabolahan_yingxiao_neimu_biji-2022.10.10.0-py3-none-any.whl

---
+++ packages/jieyabolahan-yingxiao-neimu-biji/jieyabolahan_yingxiao_neimu_biji-2022.10.10.0-py3-none-any.whl/JieyabolahanYingxiaoNeimuBiji/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jieyabolahan-yingxiao-neimu-biji
+https://github.com/apachecn/jieyabolahan-yingxiao-neimu-biji"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.10.0"

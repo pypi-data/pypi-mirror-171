3a7afaf1b75de2ea3aedf9d656050c17dc1a7d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package Splittic/1.0/Splittic-1.0.tar.gz

---
+++ packages/Splittic/Splittic-1.0.tar.gz/Splittic-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Splittic',
+    url='https://github.com/Dart2004/Splittic-AI',
+    author='Daniel Klimmer',
+    author_email='danielklimmer2000@protonmail.com',
+    packages=['splitticai'],
+    install_requires=['requests', 'bs4'],
+    version='1.0',
+    license='MIT',
+    description='The Most Advanced AI Ever',
+    long_description=open('README.rst','r').read()
+)

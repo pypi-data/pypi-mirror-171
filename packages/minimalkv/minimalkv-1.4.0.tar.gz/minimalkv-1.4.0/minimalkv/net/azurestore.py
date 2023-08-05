1bbe167726951a0b8607c4198b22820b9133b04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:35 +0000

    Add package minimalkv/1.4.0/minimalkv-1.4.0.tar.gz

---
+++ packages/minimalkv/minimalkv-1.4.0.tar.gz/minimalkv-1.4.0/minimalkv/net/azurestore.py
@@ -0,0 +1,10 @@
+# coding=utf8
+
+try:
+    from azure.storage.blob import BlockBlobService  # noqa: F401
+
+    from ._azurestore_old import AzureBlockBlobStore
+except ImportError:
+    from ._azurestore_new import AzureBlockBlobStore  # type: ignore
+
+__all__ = ["AzureBlockBlobStore"]

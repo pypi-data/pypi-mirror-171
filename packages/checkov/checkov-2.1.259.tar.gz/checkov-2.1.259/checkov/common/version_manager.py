8b033a2d8a5dbfddf86e8981981d4ed27d04d9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:37 +0000

    Add package checkov/2.1.259/checkov-2.1.259.tar.gz

---
+++ packages/checkov/checkov-2.1.259.tar.gz/checkov-2.1.259/checkov/common/version_manager.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing import cast
+
+from update_checker import UpdateChecker  # type:ignore[import]
+
+
+def check_for_update(package: str, version: str) -> str | None:
+    try:
+        checker = UpdateChecker()
+        result = checker.check(package, version)
+        return cast(str, result.available_version)
+    except Exception:  # nosec
+        return None

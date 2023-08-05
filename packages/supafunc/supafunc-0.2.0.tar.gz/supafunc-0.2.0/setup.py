69a0736e2cd034d1d1f825bd5493548bd6522ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:19 +0000

    Add package supafunc/0.2.0/supafunc-0.2.0.tar.gz

---
+++ packages/supafunc/supafunc-0.2.0.tar.gz/supafunc-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['supafunc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.21.3,<0.22.0']
+
+setup_kwargs = {
+    'name': 'supafunc',
+    'version': '0.2.0',
+    'description': 'Library for Supabase Functions',
+    'long_description': 'None',
+    'author': 'Joel Lee',
+    'author_email': 'joel@joellee.org',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

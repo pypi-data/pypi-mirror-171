bb6418202838cecd6b17279c44ae67f43f513044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:06 +0000

    Add package statute-utils/0.0.5/statute-utils-0.0.5.tar.gz

---
+++ packages/statute-utils/statute-utils-0.0.5.tar.gz/statute-utils-0.0.5/statute_utils/__init__.py
@@ -0,0 +1,11 @@
+from .formula import (
+    ProvisionLabel,
+    ProvisionPattern,
+    ProvisionSubject,
+    match_provision,
+    match_provisions,
+)
+from .spanner import get_statutory_provision_spans
+from .statute_formula import StatuteID
+from .statute_matcher import StatuteDesignation, StatuteMatcher
+from .statute_objs import StatuteLabel

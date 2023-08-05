d87aa8784c0a3d07a51ebde1d46518aa7f2a1177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:26 +0000

    Add package bdnd-probability/0.1/bdnd_probability-0.1.tar.gz

---
+++ packages/bdnd-probability/bdnd_probability-0.1.tar.gz/bdnd_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bdnd_probability',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['bdnd_probability'],
+      author='Adeniran Adedolapo',
+      author_email='adedolpson@gmail.com',
+      zip_safe=False)
+

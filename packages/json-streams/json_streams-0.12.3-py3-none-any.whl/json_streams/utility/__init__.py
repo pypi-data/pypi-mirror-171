2557ace6cc00e78b793d40c4fbb0c925e11a4519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:25 +0000

    Add package json-streams/0.12.3/json_streams-0.12.3-py3-none-any.whl

---
+++ packages/json-streams/json_streams-0.12.3-py3-none-any.whl/json_streams/utility/__init__.py
@@ -0,0 +1,69 @@
+"""Util functions.
+"""
+from enum import Enum
+import os
+from typing import Any, Generator, IO, Union
+
+from json_streams import types
+
+# pylint: disable=unsubscriptable-object
+
+
+def get_name_of_file(fp: types.File) -> str:
+    return fp.name if hasattr(fp, "name") else ""
+
+
+def is_jsonl(name: types.Pathlike) -> bool:
+    """Test if a filename is json lines.
+    Also returns True for '<stdin>' and '<stdout>'.
+
+    Arguments:
+        name {str} -- name to test
+
+    Returns:
+        {bool} -- True if this name is jsonl.
+    """
+    if name in ["<stdin>", "<stdout>"]:
+        return True
+    base, suffix = os.path.splitext(name)
+    if suffix == ".gz":
+        _, suffix = os.path.splitext(base)
+    # print('suffix = {suffix}'.format(suffix=suffix))
+    return suffix in [".jsonl", ".jl"]
+
+
+def is_gzip(name: types.Pathlike) -> bool:
+    """Test if a filename is gzip."""
+    _, suffix = os.path.splitext(name)
+    return suffix == ".gz"
+
+
+def to_bytes(s: Union[str, bytes, bytearray]) -> Union[bytes, bytearray]:
+    """Convert str to bytes, otherwise pass through s
+
+    Args:
+        s (Union[str, bytes, bytearray]): the argument to assure bytes
+
+    Returns:
+        Union[bytes, bytearray]: the possible converted argument
+    """
+    if isinstance(s, (bytes, bytearray)):
+        return s
+    return s.encode("utf-8")
+
+
+class Sink:
+    def __init__(self, sink: Generator[None, Any, None]):
+        self.sink = sink
+
+    def __enter__(self):
+        next(self.sink)
+        return self.sink
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.sink.close()
+
+
+class JsonFormat(str, Enum):
+    JSON = "json"
+    JSON_LINES = "jsonl"

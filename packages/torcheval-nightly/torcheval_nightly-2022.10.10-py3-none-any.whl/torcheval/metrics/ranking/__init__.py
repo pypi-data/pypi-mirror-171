cfbf3375da36d18fa168c63aa7b8826124dc7ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:31 +0000

    Add package torcheval-nightly/2022.10.10/torcheval_nightly-2022.10.10-py3-none-any.whl

---
+++ packages/torcheval-nightly/torcheval_nightly-2022.10.10-py3-none-any.whl/torcheval/metrics/ranking/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from torcheval.metrics.ranking.hit_rate import HitRate
+from torcheval.metrics.ranking.reciprocal_rank import ReciprocalRank
+from torcheval.metrics.ranking.weighted_calibration import WeightedCalibration
+
+__all__ = [
+    "HitRate",
+    "ReciprocalRank",
+    "WeightedCalibration",
+]

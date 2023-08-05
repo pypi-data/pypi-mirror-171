55b35345c9e3f6c1f70b1d5a0150f2b0d16cabe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:31 +0000

    Add package lnschema_core/0.11.0/lnschema_core-0.11.0.tar.gz

---
+++ packages/lnschema_core/lnschema_core-0.11.0.tar.gz/lnschema_core-0.11.0/lnschema_core/__init__.py
@@ -0,0 +1,70 @@
+"""Data provenance & flow (`yvzi`).
+
+Import the package::
+
+   import lnschema_core
+
+Data objects & transformations:
+
+.. autosummary::
+   :toctree: .
+
+   dobject
+   dtransform
+   dtransform_in
+
+Users, storage locations, and usage statistics:
+
+.. autosummary::
+   :toctree: .
+
+   user
+   storage
+   usage
+
+Data transformations:
+
+.. autosummary::
+   :toctree: .
+
+   jupynb
+   pipeline
+   pipeline_run
+
+Tracking versions & migrations:
+
+.. autosummary::
+   :toctree: .
+
+   version_yvzi
+   migration_yvzi
+
+Auxiliary modules:
+
+.. autosummary::
+   :toctree: .
+
+   type
+   id
+
+"""
+# This is lnschema-module yvzi.
+_schema_id = "yvzi"
+_migration = "3d244a8d3148"
+__version__ = "0.11.0"  # denote a pre-release for 0.1.0 with 0.1a1
+
+from . import id, type  # noqa
+from ._core import (  # noqa
+    dobject,
+    dtransform,
+    dtransform_in,
+    jupynb,
+    migration_yvzi,
+    pipeline,
+    pipeline_run,
+    storage,
+    usage,
+    usage_type,
+    user,
+    version_yvzi,
+)

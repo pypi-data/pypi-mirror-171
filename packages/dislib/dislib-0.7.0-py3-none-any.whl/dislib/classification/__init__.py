5054137af06c44a8317af4dd22017debae00b51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:41 +0000

    Add package dislib/0.7.0/dislib-0.7.0-py3-none-any.whl

---
+++ packages/dislib/dislib-0.7.0-py3-none-any.whl/dislib/classification/__init__.py
@@ -0,0 +1,4 @@
+from dislib.classification.csvm.base import CascadeSVM
+from dislib.trees.forest import RandomForestClassifier
+
+__all__ = ["CascadeSVM", "RandomForestClassifier"]
---
+++ packages/dislib/dislib-0.7.0-py3-none-any.whl/dislib/decomposition/__init__.py
@@ -0,0 +1,4 @@
+from dislib.decomposition.pca.base import PCA
+from dislib.decomposition.qr.base import qr
+
+__all__ = ['PCA', 'qr']

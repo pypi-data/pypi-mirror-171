e8bc3debd5b1bb31c48f12bcc3ee585bd22b4afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:46 +0000

    Add package temgymbasic/0.5.3/temgymbasic-0.5.3.tar.gz

---
+++ packages/temgymbasic/temgymbasic-0.5.3.tar.gz/temgymbasic-0.5.3/pyqtgraph_examples/biprism_example_pyqt.py
@@ -0,0 +1,21 @@
+from temgymbasic import components as comp
+from temgymbasic.model import Model
+from temgymbasic.run import run_pyqt
+from pyqt5 import QApplication
+import sys 
+
+def main():
+    components = [comp.Biprism(name = 'Condenser Biprism', z = 0.7),
+                  comp.Sample(name = 'Sample', z = 0.5, width = 0.2),
+                  comp.Biprism(name = 'Biprism 1', z = 0.4),
+                  comp.Biprism(name = 'Biprism 2', z = 0.2)]
+
+    model_ = Model(components, beam_z = 1.0, beam_type = 'point', num_rays = 4096, beam_semi_angle = 0.1)
+    run_pyqt(model_)
+
+if __name__ == '__main__':
+    AppWindow = QApplication(sys.argv)
+    main()
+    sys.exit(AppWindow.exec_())
+
+

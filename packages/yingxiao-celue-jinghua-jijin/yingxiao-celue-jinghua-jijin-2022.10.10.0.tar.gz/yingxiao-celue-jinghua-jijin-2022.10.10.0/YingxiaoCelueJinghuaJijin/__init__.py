b8831ef7e9ee7b7b4d6bb844bf6b56cc47310c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package yingxiao-celue-jinghua-jijin/2022.10.10.0/yingxiao-celue-jinghua-jijin-2022.10.10.0.tar.gz

---
+++ packages/yingxiao-celue-jinghua-jijin/yingxiao-celue-jinghua-jijin-2022.10.10.0.tar.gz/yingxiao-celue-jinghua-jijin-2022.10.10.0/YingxiaoCelueJinghuaJijin/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yingxiao-celue-jinghua-jijin
+https://github.com/apachecn/yingxiao-celue-jinghua-jijin"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.10.0"

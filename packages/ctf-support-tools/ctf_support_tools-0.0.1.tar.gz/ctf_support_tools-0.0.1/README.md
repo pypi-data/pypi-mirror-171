21ecbebca2f5efea1a10854b6444e66827fa4868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package ctf-support-tools/0.0.1/ctf_support_tools-0.0.1.tar.gz

---
+++ packages/ctf-support-tools/ctf_support_tools-0.0.1.tar.gz/ctf_support_tools-0.0.1/README.md
@@ -0,0 +1,5 @@
+# CTF tools package
+
+## Purpose
+
+This is package is to be used for educational purposes only and shall not be used in production
---
+++ packages/ctf-support-tools/ctf_support_tools-0.0.1.tar.gz/ctf_support_tools-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:	
+    requirements = fh.read()
+
+setuptools.setup(
+	name="ctf_support_tools",
+	version="0.0.1",
+	author="Sergey Sobolev",
+	author_email="sergey.sobolev@outlook.com",
+	description="CTF statistics collection package",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/sergey-sobolev/ctf-tools",
+	packages=setuptools.find_packages(),
+	install_requires=requirements,
+	classifiers=[
+		"Programming Language :: Python :: 3.8",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)

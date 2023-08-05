1486b3e4c3eb1d04decce5896ab19dcec1cf79f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:05 +0000

    Add package tkitAutoRewriter/0.0.0.716653891/tkitAutoRewriter-0.0.0.716653891.tar.gz

---
+++ packages/tkitAutoRewriter/tkitAutoRewriter-0.0.0.716653891.tar.gz/tkitAutoRewriter-0.0.0.716653891/tkitAutoRewriter/__init__.py
@@ -0,0 +1,4 @@
+from .api import auto_request,SentenceClassification,AutoReWrite,dff_text,get_markdown_images,get_markdown_images_format
+from .diff import dff_text
+# from rewriter import *
+from .cut import auto_cut_markdown

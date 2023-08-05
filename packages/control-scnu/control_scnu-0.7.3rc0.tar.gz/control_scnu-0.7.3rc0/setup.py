fab2b5d839f8eeb5366c1764e9c40b12b381aec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:06 +0000

    Add package control-scnu/0.7.3rc0/control_scnu-0.7.3rc0.tar.gz

---
+++ packages/control-scnu/control_scnu-0.7.3rc0.tar.gz/control_scnu-0.7.3rc0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('requirements.txt', encoding="utf-8") as f:
+    requirements = f.readlines()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("description.txt", "r", encoding="utf-8") as fd:
+    description = fd.read()
+
+
+setuptools.setup(
+    name="control_scnu",
+    # Replace with your own username
+    version="0.7.3rc",
+    author="HuaGuangEdu",
+    author_email="huaguangcase@163.com",
+    description=description,
+    install_requires=requirements,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7, <3.9',
+    include_package_data=True,
+)

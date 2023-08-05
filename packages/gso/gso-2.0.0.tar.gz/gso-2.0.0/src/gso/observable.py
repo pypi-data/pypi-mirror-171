3c743d41a8d6fb454d1bf475e30f2ad67cfa70fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:37 +0000

    Add package gso/2.0.0/gso-2.0.0.tar.gz

---
+++ packages/gso/gso-2.0.0.tar.gz/gso-2.0.0/src/gso/observable.py
@@ -0,0 +1,23 @@
+import abc
+
+
+class Observable(metaclass=abc.ABCMeta):
+    def __init__(self, initial_value):
+        self.__value = initial_value
+        self.__observers = []
+
+    @property
+    def value(self):
+        return self.__value
+
+    def apply(self, action):
+        self.__value = action(self.__value)
+        self.__notify(action)
+
+    def attach_observer(self, observer):
+        self.__observers.append(observer)
+
+    def __notify(self, *actions):
+        for observer in self.__observers:
+            for action in actions:
+                observer.notify_state_changed(action)

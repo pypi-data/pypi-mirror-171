6d95f3661eae819d614128e565a5bfd310218f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:07 +0000

    Add package x539-dangan-vol4/2022.10.10.0/x539-dangan-vol4-2022.10.10.0.tar.gz

---
+++ packages/x539-dangan-vol4/x539-dangan-vol4-2022.10.10.0.tar.gz/x539-dangan-vol4-2022.10.10.0/X539DanganVol4/README.md
@@ -0,0 +1,27 @@
+# 539档案第四期
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/x539-dangan-vol4
+docker run -tid -p <port>:80 apachecn0/x539-dangan-vol4
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install x539-dangan-vol4
+x539-dangan-vol4 <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g xwusanjiu-dangan-vol4
+xwusanjiu-dangan-vol4 <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/x539-dangan-vol4/x539-dangan-vol4-2022.10.10.0.tar.gz/x539-dangan-vol4-2022.10.10.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import X539DanganVol4
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('X539DanganVol4'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="x539-dangan-vol4",
+    version=X539DanganVol4.__version__,
+    url="https://github.com/apachecn/x539-dangan-vol4",
+    author=X539DanganVol4.__author__,
+    author_email=X539DanganVol4.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="539档案第四期",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "x539-dangan-vol4=X539DanganVol4.__main__:main",
+            "X539DanganVol4=X539DanganVol4.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

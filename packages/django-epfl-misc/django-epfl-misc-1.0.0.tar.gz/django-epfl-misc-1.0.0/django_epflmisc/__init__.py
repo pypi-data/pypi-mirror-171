6504be2fdce11758e80dec77917d201be37a5e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:43 +0000

    Add package django-epfl-misc/1.0.0/django-epfl-misc-1.0.0.tar.gz

---
+++ packages/django-epfl-misc/django-epfl-misc-1.0.0.tar.gz/django-epfl-misc-1.0.0/django_epflmisc/__init__.py
@@ -0,0 +1,4 @@
+# (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, 2021-2022.
+# See the LICENSE file for more details.
+
+__version__ = "1.0.0"
---
+++ packages/django-epfl-misc/django-epfl-misc-1.0.0.tar.gz/django-epfl-misc-1.0.0/tests/__init__.py
@@ -0,0 +1,2 @@
+# (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, 2021-2022.
+# See the LICENSE file for more details.

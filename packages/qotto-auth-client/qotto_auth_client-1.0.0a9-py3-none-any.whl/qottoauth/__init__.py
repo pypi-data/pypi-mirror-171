813847a427ab2e614619cc460413e33508db1eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:50 +0000

    Add package qotto-auth-client/1.0.0a9/qotto_auth_client-1.0.0a9-py3-none-any.whl

---
+++ packages/qotto-auth-client/qotto_auth_client-1.0.0a9-py3-none-any.whl/qottoauth/__init__.py
@@ -0,0 +1,33 @@
+from qottoauth.api import (
+    QottoAuthApi,
+    QottoAuthApiError,
+)
+from qottoauth.service import (
+    QottoAuthService,
+)
+from qottoauth.models import (
+    Application,
+    Permission,
+    Namespace,
+    Matching,
+    Organization,
+    Authorization,
+    User,
+    Member,
+    Account,
+)
+
+__all__ = [
+    'QottoAuthApi',
+    'QottoAuthApiError',
+    'QottoAuthService',
+    'Application',
+    'Permission',
+    'Namespace',
+    'Matching',
+    'Organization',
+    'Authorization',
+    'User',
+    'Member',
+    'Account',
+]

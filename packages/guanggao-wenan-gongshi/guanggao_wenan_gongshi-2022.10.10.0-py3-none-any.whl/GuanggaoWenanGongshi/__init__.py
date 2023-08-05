21842727e33dedeabff9ba01086205cf7774ccc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:39 +0000

    Add package guanggao-wenan-gongshi/2022.10.10.0/guanggao_wenan_gongshi-2022.10.10.0-py3-none-any.whl

---
+++ packages/guanggao-wenan-gongshi/guanggao_wenan_gongshi-2022.10.10.0-py3-none-any.whl/GuanggaoWenanGongshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""guanggao-wenan-gongshi
+https://github.com/apachecn/guanggao-wenan-gongshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.10.0"

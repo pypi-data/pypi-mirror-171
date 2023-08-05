ebad6a0ca8eb0817fc5b2d6bf8e904b1886afa35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:14 +0000

    Add package WhamBamPy/0.0.6/WhamBamPy-0.0.6.tar.gz

---
+++ packages/WhamBamPy/WhamBamPy-0.0.6.tar.gz/WhamBamPy-0.0.6/whambampy/defaults.py
@@ -0,0 +1,10 @@
+"""
+Vars here can be passed to __init__ method in lowercase version
+to override the default, i.e.: whm(domain, username, api_token, whm_port=2086) would override the port
+"""
+
+API_VERSION = 1
+WHM_PORT = 2087
+WHM_PROTOCOL = 'https://'
+API_TYPE = 'json-api'
+VERIFY_SSL = False
---
+++ packages/WhamBamPy/WhamBamPy-0.0.6.tar.gz/WhamBamPy-0.0.6/whambampy/whm.py
@@ -0,0 +1,66 @@
+import logging
+import requests
+import urllib3
+
+from typing import Any
+
+from whambampy.api_commands import API_COMMANDS
+from whambampy.defaults import *
+
+from urllib3.exceptions import InsecureRequestWarning
+
+
+urllib3.disable_warnings(InsecureRequestWarning)
+logger = logging.getLogger(__name__)
+
+
+class WHM:
+    def __init__(self, host, username, api_token, *args, **kwargs):
+        self.host = host
+        self.username = username,
+        self.api_token = api_token
+
+        self._api_version = kwargs.get('api_version', API_VERSION)
+        self._whm_port = kwargs.get('whm_port', WHM_PORT)
+        self._whm_protocol = kwargs.get('whm_protocol', WHM_PROTOCOL)
+        self._api_type = kwargs.get('api_type', API_TYPE)
+        self._verify_ssl = kwargs.get('verify_ssl', VERIFY_SSL)
+        self._headers = self._get_auth_header()
+        self._sec_token = self._get_sec_token()
+
+    def _get_auth_header(self) -> dict:
+        return {
+            "Authorization": f"whm {self.username}:{self.api_token}",
+        }
+
+    def _get_sec_token(self) -> str:
+        auth_url = f"{self._whm_protocol}{self.host}:{self._whm_port}/{self._api_type}/create_user_session?" \
+            f"api.version={self._api_version}&user={self.username}&service=whostmgrd"
+        auth_resp = requests.get(
+            url=auth_url,
+            headers=self._headers,
+            verify=self._verify_ssl,
+        )
+        if auth_resp.status_code != 200:
+            raise PermissionError('Unable to login with the provided credentials')
+        return auth_resp.json()['data'].get('cp_security_token')
+
+    def _validate_function(self, function: str) -> bool:
+        return function in API_COMMANDS
+
+    def call(self, function: str, **params) -> Any:
+        if self._validate_function(function):
+            params['api.version'] = self._api_version
+            url = f"{self._whm_protocol}{self.host}:{self._whm_port}{self._sec_token}/{self._api_type}/{function}"
+            return requests.get(
+                url=url,
+                headers=self._headers,
+                params=params,
+                verify=self._verify_ssl,
+            ).json()
+        else:
+            raise TypeError('Called function is not the list of available functions')
+
+
+def whm(host, username, api_token, *args, **kwargs):
+    return WHM(host, username, api_token, *args, **kwargs)

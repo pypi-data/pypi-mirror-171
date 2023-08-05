1702013c484a446c53e43df7e88c48cedefc5a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:31 +0000

    Add package flask-sso-saml/0.2.0/flask-sso-saml-0.2.0.tar.gz

---
+++ packages/flask-sso-saml/flask-sso-saml-0.2.0.tar.gz/flask-sso-saml-0.2.0/run-tests.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019, 2022 Esteban J. G. Gabancho.
+#
+# Flask-SSO-SAML is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+pydocstyle flask_sso_saml tests docs && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test

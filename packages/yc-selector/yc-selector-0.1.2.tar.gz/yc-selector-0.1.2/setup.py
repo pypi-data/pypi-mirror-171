c0891b6228d9279b4c372167d404bcde6926336f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:05 +0000

    Add package yc-selector/0.1.2/yc-selector-0.1.2.tar.gz

---
+++ packages/yc-selector/yc-selector-0.1.2.tar.gz/yc-selector-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+   name='yc-selector',
+   version='0.1.2',
+   author='Dima Frolenko',
+   author_email='orangefrol@gmail.com',
+   packages=find_packages(),
+#    entry_points = {
+#         'console_scripts': ['nf=nf_lite.command_line:main'],
+#    },
+   license='LICENSE.txt',
+   description='Yandex Cloud module for routing cloud functions',
+   long_description=open('README.txt').read(),
+   install_requires=open('requirements.txt').readlines()
+)

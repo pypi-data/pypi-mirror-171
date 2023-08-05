1bda6e723036ae12b6dbd50bc17abd99e41bf52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:50 +0000

    Add package etna/1.13.0/etna-1.13.0.tar.gz

---
+++ packages/etna/etna-1.13.0.tar.gz/etna-1.13.0/etna/models/__init__.py
@@ -0,0 +1,29 @@
+from etna import SETTINGS
+from etna.models.autoarima import AutoARIMAModel
+from etna.models.base import BaseAdapter
+from etna.models.base import BaseModel
+from etna.models.base import Model
+from etna.models.base import PerSegmentModel
+from etna.models.catboost import CatBoostModelMultiSegment
+from etna.models.catboost import CatBoostModelPerSegment
+from etna.models.catboost import CatBoostMultiSegmentModel
+from etna.models.catboost import CatBoostPerSegmentModel
+from etna.models.deadline_ma import DeadlineMovingAverageModel
+from etna.models.holt_winters import HoltModel
+from etna.models.holt_winters import HoltWintersModel
+from etna.models.holt_winters import SimpleExpSmoothingModel
+from etna.models.linear import ElasticMultiSegmentModel
+from etna.models.linear import ElasticPerSegmentModel
+from etna.models.linear import LinearMultiSegmentModel
+from etna.models.linear import LinearPerSegmentModel
+from etna.models.moving_average import MovingAverageModel
+from etna.models.naive import NaiveModel
+from etna.models.sarimax import SARIMAXModel
+from etna.models.seasonal_ma import SeasonalMovingAverageModel
+from etna.models.sklearn import SklearnMultiSegmentModel
+from etna.models.sklearn import SklearnPerSegmentModel
+from etna.models.tbats import BATSModel
+from etna.models.tbats import TBATSModel
+
+if SETTINGS.prophet_required:
+    from etna.models.prophet import ProphetModel
---
+++ packages/etna/etna-1.13.0.tar.gz/etna-1.13.0/etna/pipeline/pipeline.py
@@ -0,0 +1,99 @@
+from typing import Sequence
+
+from etna.datasets import TSDataset
+from etna.models.base import BaseModel
+from etna.models.base import DeepBaseModel
+from etna.models.base import PredictIntervalAbstractModel
+from etna.pipeline.base import BasePipeline
+from etna.transforms.base import Transform
+
+
+class Pipeline(BasePipeline):
+    """Pipeline of transforms with a final estimator."""
+
+    def __init__(self, model: BaseModel, transforms: Sequence[Transform] = (), horizon: int = 1):
+        """
+        Create instance of Pipeline with given parameters.
+
+        Parameters
+        ----------
+        model:
+            Instance of the etna Model
+        transforms:
+            Sequence of the transforms
+        horizon:
+            Number of timestamps in the future for forecasting
+        """
+        self.model = model
+        self.transforms = transforms
+        super().__init__(horizon=horizon)
+
+    def fit(self, ts: TSDataset) -> "Pipeline":
+        """Fit the Pipeline.
+
+        Fit and apply given transforms to the data, then fit the model on the transformed data.
+
+        Parameters
+        ----------
+        ts:
+            Dataset with timeseries data
+
+        Returns
+        -------
+        :
+            Fitted Pipeline instance
+        """
+        self.ts = ts
+        self.ts.fit_transform(self.transforms)
+        self.model.fit(self.ts)
+        self.ts.inverse_transform()
+        return self
+
+    def _forecast(self) -> TSDataset:
+        """Make predictions."""
+        if self.ts is None:
+            raise ValueError("Something went wrong, ts is None!")
+
+        if isinstance(self.model, DeepBaseModel):
+            future = self.ts.make_future(future_steps=self.horizon, tail_steps=self.model.encoder_length)
+            predictions = self.model.forecast(ts=future, horizon=self.horizon)
+        else:
+            future = self.ts.make_future(self.horizon)
+            predictions = self.model.forecast(ts=future)
+        return predictions
+
+    def forecast(
+        self, prediction_interval: bool = False, quantiles: Sequence[float] = (0.025, 0.975), n_folds: int = 3
+    ) -> TSDataset:
+        """Make predictions.
+
+        Parameters
+        ----------
+        prediction_interval:
+            If True returns prediction interval for forecast
+        quantiles:
+            Levels of prediction distribution. By default 2.5% and 97.5% taken to form a 95% prediction interval
+        n_folds:
+            Number of folds to use in the backtest for prediction interval estimation
+
+        Returns
+        -------
+        :
+            Dataset with predictions
+        """
+        if self.ts is None:
+            raise ValueError(
+                f"{self.__class__.__name__} is not fitted! Fit the {self.__class__.__name__} "
+                f"before calling forecast method."
+            )
+        self._validate_quantiles(quantiles=quantiles)
+        self._validate_backtest_n_folds(n_folds=n_folds)
+
+        if prediction_interval and isinstance(self.model, PredictIntervalAbstractModel):
+            future = self.ts.make_future(self.horizon)
+            predictions = self.model.forecast(ts=future, prediction_interval=prediction_interval, quantiles=quantiles)
+        else:
+            predictions = super().forecast(
+                prediction_interval=prediction_interval, quantiles=quantiles, n_folds=n_folds
+            )
+        return predictions

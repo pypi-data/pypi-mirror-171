e1616e4931488ddc682b84d89cbf1ff4a52146af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:26 +0000

    Add package testing-providers/0.1.1a0/testing_providers-0.1.1a0.tar.gz

---
+++ packages/testing-providers/testing_providers-0.1.1a0.tar.gz/testing_providers-0.1.1a0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['testing_providers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['anyio>=3.6.1,<4.0.0',
+ 'distmqtt>=0.35.2,<0.36.0',
+ 'trio-websocket>=0.9.2,<0.10.0']
+
+setup_kwargs = {
+    'name': 'testing-providers',
+    'version': '0.1.1a0',
+    'description': 'Testing utilities for networking apps.',
+    'long_description': '# Testing providers\n\nTo wrap an async function in an async provider.\nVery useful to test network interfaces or to test applications mocking outer services.\n\n## Supported providers\n\n- [x] MQTT broker (anyio)\n- [x] TCP server (anyio)\n- [x] WebSocket server (trio)\n- [ ] WebSocket server (asyncio)\n',
+    'author': 'didimelli',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

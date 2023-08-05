cfb4c7ffa62b775d3b4af8fe29f4f772e8406204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package projectkiwi/0.4.10/projectkiwi-0.4.10.tar.gz

---
+++ packages/projectkiwi/projectkiwi-0.4.10.tar.gz/projectkiwi-0.4.10/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+VERSION = '0.4.10'
+
+setup(
+  name = 'projectkiwi',
+  packages = ['projectkiwi'],
+  version = VERSION,
+  license='MIT',
+  description = 'Python tools for project-kiwi.org',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Michael Thoreau',
+  author_email = 'michael@project-kiwi.org',
+  url = 'https://github.com/michaelthoreau/projectkiwi',
+  keywords = ['GIS', 'ML', 'OTHERBUZZWORDS'],
+  python_requires='>=3.3',
+  install_requires=[
+    'numpy',
+    'pillow',
+    'pydantic',
+    'requests',
+  ],
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

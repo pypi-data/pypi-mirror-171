a3f6c733e64026783991f127556d0872a8c27f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package DataNormalizer/0.0.2.dev0/DataNormalizer-0.0.2.dev0-py3-none-any.whl

---
+++ packages/DataNormalizer/DataNormalizer-0.0.2.dev0-py3-none-any.whl/DataNormalizer/__init__.py
@@ -0,0 +1,11 @@
+from .validate import Validate
+from .diagnose import Diagnose
+from .normalise import Normalise
+
+# Metadata
+__doc__ = "Package to help with normalizing data needed for the platform!"
+# Github CI/CD greps for the version number.
+__version__ = "0.0.2-dev0"
+__author__ = "Realstats"
+__email__ = "hello@realstats.com"
+__license__ = "MIT"

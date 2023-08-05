2daf82da030332da76cbd1ba0f6971c15e58301a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package cosmology/2022.10.9/cosmology-2022.10.9-py3-none-any.whl

---
+++ packages/cosmology/cosmology-2022.10.9-py3-none-any.whl/cosmology/__init__.py
@@ -0,0 +1,11 @@
+# author: Nicolas Tessore <n.tessore@ucl.ac.uk>
+# license: MIT
+'''main module of the cosmology package'''
+
+__version__ = '2022.10.9'
+
+__all__ = [
+    'Cosmology',
+]
+
+from .background import Cosmology

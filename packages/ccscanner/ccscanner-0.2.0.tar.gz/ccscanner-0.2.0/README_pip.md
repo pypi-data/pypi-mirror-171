f0d3db2f945f5d4b36eafbd7e106079173cfa220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:03 +0000

    Add package ccscanner/0.2.0/ccscanner-0.2.0.tar.gz

---
+++ packages/ccscanner/ccscanner-0.2.0.tar.gz/ccscanner-0.2.0/README_pip.md
@@ -0,0 +1,16 @@
+# ccscanner
+## Description
+CCScanner 0.1.9 is released.
+
+Centris is available at: https://github.com/WOOSEUNGHOON/Centris-public.
+
+## Install
+```
+pip install ccscanner
+```
+
+## Usage
+run command:
+```
+$ ccscanner_print -d ${directory to scan} -t ${file to save}
+```
---
+++ packages/ccscanner/ccscanner-0.2.0.tar.gz/ccscanner-0.2.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README_pip.md", "r", encoding="utf-8") as read_f:
+    long_description = read_f.read()
+    
+with open('ccscanner/requirements.txt', 'r') as read_f:
+    requires_list = read_f.readlines()
+requires_list = [i.strip() for i in requires_list]
+
+setuptools.setup(
+    name="ccscanner",
+    version="0.2.0",
+    author="anonymous repo",
+    author_email="",
+    description="A SBOM scanner for C/C++",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages = setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points = {
+        'console_scripts': [
+            'ccscanner_print = ccscanner.scanner:main',
+        ]
+    },
+    install_requires=requires_list
+)

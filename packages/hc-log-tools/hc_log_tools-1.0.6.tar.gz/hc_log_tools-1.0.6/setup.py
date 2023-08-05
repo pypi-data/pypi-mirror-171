95f14e5e3cd1ceca513ad2e6aaae4eeb462116d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:50 +0000

    Add package hc-log-tools/1.0.6/hc_log_tools-1.0.6.tar.gz

---
+++ packages/hc-log-tools/hc_log_tools-1.0.6.tar.gz/hc_log_tools-1.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+# get requirements from requirements.txt
+install_reqs = parse_requirements("requirements.txt", session=False)
+requirements = list(install_reqs)
+
+try:
+    requirements = [str(ir.req) for ir in install_reqs]
+except:
+    requirements = [str(ir.requirement) for ir in install_reqs]
+
+setup(
+    packages=find_packages(),
+    name="hc_log_tools",
+    version="v1.0.6",
+    author="xukai.shi",
+    author_email="shixukai@163.com",
+    install_requires = requirements,
+    entry_points={
+        'console_scripts': [
+            'hc_log_tools = src.entry:entry',
+        ]
+    },
+)

1ec31920b23bb2d912b8dbcb5d3f941ce1061e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:22 +0000

    Add package arka/0.0.1/arka-0.0.1.tar.gz

---
+++ packages/arka/arka-0.0.1.tar.gz/arka-0.0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="arka",
+    version="0.0.1",
+    description="short description",
+    long_description="long description",
+    packages=find_packages(where="."),
+    python_requires=">=3.10, <4",
+)

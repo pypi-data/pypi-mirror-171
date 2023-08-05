dde3fc6392606dff0fd0d242c1ec0f53d1fde218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:02 +0000

    Add package xperimental-data-conv/1.0.5/xperimental-data-conv-1.0.5.tar.gz

---
+++ packages/xperimental-data-conv/xperimental-data-conv-1.0.5.tar.gz/xperimental-data-conv-1.0.5/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(name='xperimental-data-conv',
+      version='1.0.5',
+      url='https://github.com/SynBioDex/Experimental-Data-Convertor',
+      license='BSD 3-clause',
+      maintainer='Jet Mante',
+      maintainer_email='jet.mante@colorado.edu',
+      include_package_data=True,
+      description='convert excel resources into sbol and flapjack and upload them',
+      packages=find_packages(include=['xperimental_data_conv']),
+      long_description=open('README.md').read(),
+      install_requires=['excel2flapjack~=1.0.2',
+                        'excel2sbol~=1.0.24'],
+      zip_safe=False)

6d9bc3a927b9dca81e058219d0396ab13ea25516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:10 +0000

    Add package mofa-jiazhi-chuangzaofa/2022.10.10.0/mofa_jiazhi_chuangzaofa-2022.10.10.0-py3-none-any.whl

---
+++ packages/mofa-jiazhi-chuangzaofa/mofa_jiazhi_chuangzaofa-2022.10.10.0-py3-none-any.whl/MofaJiazhiChuangzaofa/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mofa-jiazhi-chuangzaofa
+https://github.com/apachecn/mofa-jiazhi-chuangzaofa"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.10.0"

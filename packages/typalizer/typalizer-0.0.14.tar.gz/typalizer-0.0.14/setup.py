f09314c077b4171973a5c764b1fc7d8af8c4a7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:05 +0000

    Add package typalizer/0.0.14/typalizer-0.0.14.tar.gz

---
+++ packages/typalizer/typalizer-0.0.14.tar.gz/typalizer-0.0.14/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+VERSION = '0.0.14'
+DESCRIPTION = 'Type Analysis tool for Pandas DataFrames'
+LONG_DESCRIPTION = 'A package that helps data analysts, engineers, and scientists to efficiently identify the data type distribution in a pandas dataframe'
+
+# Setting up
+setup(
+    name="typalizer",
+    version=VERSION,
+    author="Mursil Khan",
+    author_email="<mursilkhan30@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=['typalizer'],
+    install_requires=['pandas'],
+    keywords=['python', 'data', 'analysis', 'type analysis', 'pandas', 'dataframes'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

f5952dd69bce3e4268dff45a2764a02e2f9fad50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package pupil-labs-realtime-api/1.2.0a3/pupil_labs_realtime_api-1.2.0a3.tar.gz

---
+++ packages/pupil-labs-realtime-api/pupil_labs_realtime_api-1.2.0a3.tar.gz/pupil_labs_realtime_api-1.2.0a3/docs/conf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+try:
+    from importlib.metadata import version as import_version
+except ImportError:
+    from importlib_metadata import version as import_version
+
+extensions = [
+    'jaraco.packaging.sphinx',
+    'rst.linker',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+]
+
+master_doc = "index"
+
+link_files = {
+    '../CHANGES.rst': dict(
+        using=dict(GH='https://github.com'),
+        replace=[
+            dict(
+                pattern=r'(Issue #|\B#)(?P<issue>\d+)',
+                url='{package_url}/issues/{issue}',
+            ),
+            dict(
+                pattern=r'(?m:^((?P<scm_version>v?\d+(\.\d+){1,2}))\n[-=]+\n)',
+                with_scm='{text}\n{rev[timestamp]:%d %b %Y}\n',
+            ),
+            dict(
+                pattern=r'PEP[- ](?P<pep_number>\d+)',
+                url='https://www.python.org/dev/peps/pep-{pep_number:0>4}/',
+            ),
+        ],
+    )
+}
+
+# Be strict about any broken references:
+nitpicky = True
+nitpick_ignore = [
+    # FIXME: Add link to https://github.com/ramonhagenaars/nptyping as soon as
+    # they have public sphinx documentation
+    ("py:class", "nptyping.types._ndarray.NDArray"),
+    ("py:class", "nptyping.types._number.UInt"),
+    ("py:class", "aiortsp.rtsp.reader.RTSPReader"),
+    ("py:class", "asyncio.streams.StreamReader"),
+    ("py:class", "asyncio.streams.StreamWriter"),
+]
+
+
+# Include Python intersphinx mapping to prevent failures
+# jaraco/skeleton#51
+extensions += ['sphinx.ext.intersphinx']
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'av': ('https://pyav.org/docs/stable', None),
+    'zeroconf': ('https://python-zeroconf.readthedocs.io/en/stable/', None),
+}
+
+html_theme = 'furo'
+autosummary_generate = True
+
+project = "Pupil Labs' Realtime Python API"
+release = import_version('pupil-labs-realtime-api')
+# for example take major/minor
+version = '.'.join(release.split('.')[:2])
+html_title = f"{project} {release}"
+
+todo_include_todos = True
+todo_emit_warnings = True
---
+++ packages/pupil-labs-realtime-api/pupil_labs_realtime_api-1.2.0a3.tar.gz/pupil_labs_realtime_api-1.2.0a3/docs/guides/index.rst
@@ -0,0 +1,11 @@
+Guides
+======
+
+An introduction to the Pupil Labs Realtime Python API can be found in the `official
+Pupil Labs documentation <https://docs.pupil-labs.com/invisible/how-tos/integrate-with-the-real-time-api/introduction/>`_.
+For more advanced guides, see below.
+
+.. toctree::
+   :maxdepth: 5
+
+   under-the-hood

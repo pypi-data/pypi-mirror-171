7a249f7e4b1121307e41c6429b2fe9ff21aff263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package pywikibot/7.7.1/pywikibot-7.7.1.tar.gz

---
+++ packages/pywikibot/pywikibot-7.7.1.tar.gz/pywikibot-7.7.1/pywikibot/scripts/i18n/__init__.py
@@ -0,0 +1,11 @@
+"""
+This is a fake __init__.py to help pkgutil find the i18n data. There
+is no actual python code in this package.
+"""
+#
+# (C) Pywikibot team, 2011-2022
+#
+# Distributed under the terms of the MIT license.
+#
+# Translations by translators are licensed CC BY 3.0
+#

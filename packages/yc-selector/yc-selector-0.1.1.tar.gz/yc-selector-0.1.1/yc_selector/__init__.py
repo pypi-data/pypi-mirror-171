61c470eb58a2d6952ced60158569e1ddd1cd4035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:04 +0000

    Add package yc-selector/0.1.1/yc-selector-0.1.1.tar.gz

---
+++ packages/yc-selector/yc-selector-0.1.1.tar.gz/yc-selector-0.1.1/yc_selector/__init__.py
@@ -0,0 +1,48 @@
+import json
+class Query:
+    def __init__(self,event,context):
+        self.query=event['queryStringParameters']
+        self.body=event['body']
+        self.json=json.loads(self.body)
+        self.original={
+            'event':event,
+            'context':context
+        }
+
+
+
+routes={}
+
+def route(path:str):
+    print(f'decorating with {path}')
+    def retry(func):
+        routes[path]=func
+        return func
+    return retry
+
+
+def call_from_routes(route:str,query):
+    if route in routes:
+        return {
+        'statusCode': 200,
+        'body': routes[route](query),
+        'headers':{
+            'Content-Type': 'application/json'
+        }
+    }
+    else:
+        print(f"ERROR:Cant find route {route}")
+        return {
+        'statusCode': 404,
+        'body': f'Route not {route} not found',
+    }
+
+def accept_request(event,context,routevar="Action"):
+    action=event['params'][routevar]
+    query=Query(event,context)
+    return call_from_routes(action,query=query)
+    #print(locals())
+
+def init(event, context):
+    accept_request(event,context)
+

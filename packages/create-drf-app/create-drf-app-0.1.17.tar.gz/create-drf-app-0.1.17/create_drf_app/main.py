d30621f4a1ce7413b7d359312528701356afea5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:05 +0000

    Add package create-drf-app/0.1.17/create-drf-app-0.1.17.tar.gz

---
+++ packages/create-drf-app/create-drf-app-0.1.17.tar.gz/create-drf-app-0.1.17/create_drf_app/main.py
@@ -0,0 +1,45 @@
+import os
+import platform
+from re import L
+
+import typer
+from cookiecutter.main import cookiecutter
+
+OS = platform.system()
+
+app = typer.Typer()
+
+@app.callback()
+def callback():
+    """
+    DRF scaffold
+    """
+
+def get_template_path(template_name):
+    script_path = os.path.abspath(__file__)
+    module_path = os.path.dirname(script_path)
+    templates_path = os.path.join(module_path, "templates")
+    return os.path.join(templates_path, template_name)
+    
+
+def activate_venv(path):
+
+    os.chdir(path)
+    env = os.system("python -m venv env")
+    parent = os.getcwd()
+    project_path = path
+
+    env_activate_script = ""
+    if OS == "Windows":
+        env_activate_script = "Scripts"
+
+    activate_file = os.path.join(path, "env", env_activate_script, "activate")
+    os.system(f"{activate_file} && pip install -r requirements.txt")
+
+
+@app.command("")
+def main(template_name):
+    template_path = get_template_path(template_name)
+    path = cookiecutter(template_path)
+    activate_venv(path)
+

94c1d1a8a8e72f16d3ce54e181b90a996d914166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package CFEDemands/0.5.0.dev0/CFEDemands-0.5.0.dev0-py2.py3-none-any.whl

---
+++ packages/CFEDemands/CFEDemands-0.5.0.dev0-py2.py3-none-any.whl/cfe/__init__.py
@@ -0,0 +1,9 @@
+import demands
+from . import estimation
+from . import df_utils
+from . import dgp
+from . import input_files
+from .result import Result, from_dataset
+#from demands import engel_curves
+from .regression import Regression, load
+__version__ = '0.5.0dev'

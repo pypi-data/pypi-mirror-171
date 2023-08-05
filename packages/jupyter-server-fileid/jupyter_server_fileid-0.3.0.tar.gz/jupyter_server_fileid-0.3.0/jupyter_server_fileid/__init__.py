5cd9ea8345b7045d8d251383499dadd41f469a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:24 +0000

    Add package jupyter-server-fileid/0.3.0/jupyter_server_fileid-0.3.0.tar.gz

---
+++ packages/jupyter-server-fileid/jupyter_server_fileid-0.3.0.tar.gz/jupyter_server_fileid-0.3.0/jupyter_server_fileid/__init__.py
@@ -0,0 +1,9 @@
+"""A Jupyter Server extension providing an implementation of the File ID service."""
+from .extension import FileIdExtension
+
+version_info = (0, 3, 0, "", "")
+__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
+
+
+def _jupyter_server_extension_points():
+    return [{"module": "jupyter_server_fileid", "app": FileIdExtension}]

1d1c4bfea560c0b4e147173b243c4b760750f890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:35 +0000

    Add package qub-amphibian-report-generator/0.0.32/qub-amphibian-report-generator-0.0.32.tar.gz

---
+++ packages/qub-amphibian-report-generator/qub-amphibian-report-generator-0.0.32.tar.gz/qub-amphibian-report-generator-0.0.32/report_generator/report_generator_gui/__init__.py
@@ -0,0 +1,6 @@
+""" # Report generator GUI.
+
+This package consists of two modules and a subpackage:
+- UI package: This contains UI windows utilised in the gui. This includes the setup gui and the main window gui.
+- app.py: This module is one entry point for starting the GUI
+- main.py: This module is one entry point for starting the GUI"""

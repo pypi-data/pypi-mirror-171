970fef6218d4043ae4f30a0937edc82389918895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:58 +0000

    Add package moto/4.0.8.dev3/moto-4.0.8.dev3.tar.gz

---
+++ packages/moto/moto-4.0.8.dev3.tar.gz/moto-4.0.8.dev3/moto/apigateway/utils.py
@@ -0,0 +1,26 @@
+import string
+import json
+import yaml
+from moto.moto_api._internal import mock_random as random
+
+
+def create_id():
+    size = 10
+    chars = list(range(10)) + list(string.ascii_lowercase)
+    return "".join(str(random.choice(chars)) for x in range(size))
+
+
+def deserialize_body(body):
+    try:
+        api_doc = json.loads(body)
+    except json.JSONDecodeError:
+        api_doc = yaml.safe_load(body)
+
+    if "openapi" in api_doc or "swagger" in api_doc:
+        return api_doc
+
+    return None
+
+
+def to_path(prop):
+    return "/" + prop

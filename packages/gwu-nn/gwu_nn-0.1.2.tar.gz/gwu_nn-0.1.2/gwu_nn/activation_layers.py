50dd2426d6e3253c4e2fd1db693b3d8154ad4038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:22 +0000

    Add package gwu-nn/0.1.2/gwu_nn-0.1.2.tar.gz

---
+++ packages/gwu-nn/gwu_nn-0.1.2.tar.gz/gwu_nn-0.1.2/gwu_nn/activation_layers.py
@@ -0,0 +1,60 @@
+from gwu_nn.activation_functions import SigmoidActivation, RELUActivation, SoftmaxActivation
+
+
+class ActivationLayer:
+    """The ActivationLayer class acts as a connector between a layer and an
+    activation function. It ensures that the activation function is used 
+    correctly during forward and backward propogation."""
+    def __init__(self, activation):
+        self.type = "Activation"
+        layer_activation = activation
+        self.activation = layer_activation.activation
+        self.activation_prime = layer_activation.activation_partial_derivative
+
+    def init_weights(self, input_size):
+        pass
+
+    def forward_propagation(self, input):
+        """Applies the classes activation function to the provided input
+
+        Args:
+            input (np.array): output calculated forward up to this layer
+
+        Returns:
+            np.array(float): forward pass (output) up to this layer
+        """
+        self.input = input
+        return self.activation(input)
+
+    def backward_propagation(self, output_error, learning_rate):
+        """Applies the classes activation function to the provided input
+
+        Args:
+            output_error (np.array): output_error calculated backwards to this layer
+
+        Returns:
+            np.array(float): backwards pass (output_error) up to this layer
+        """
+        return output_error * self.activation_prime(self.input)
+
+
+class Sigmoid(ActivationLayer):
+    """Layer that applies the Sigmoid activation function. Inheirits
+    forward_propagation and backward_prop from ActivationLayer"""
+    def __init__(self):
+        super().__init__(SigmoidActivation)
+        self.name = "Sigmoid"
+
+class RELU(ActivationLayer):
+    """Layer that applies the ReLU activation function. Inheirits
+    forward_propagation and backward_prop from ActivationLayer"""
+    def __init__(self):
+        super().__init__(RELUActivation)
+        self.name = "RELU"
+
+class Softmax(ActivationLayer):
+    """Layer that applies the Softmax activation function. Inheirits
+    forward_propagation and backward_prop from ActivationLayer"""
+    def __init__(self):
+        super().__init__(SoftmaxActivation)
+        self.name = "Softmax"
---
+++ packages/gwu-nn/gwu_nn-0.1.2.tar.gz/gwu_nn-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='gwu_nn',
+      version='0.1.2',
+      description='Neural Network library for the George Washington University',
+      url='https://gwu-nn.readthedocs.io/en/latest/index.html',
+      author='Joel Klein',
+      author_email='jdk51405@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      license='MIT',
+      classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      packages=['gwu_nn'],
+      install_requires=[
+        'numpy >= 1.20',
+      ],
+      zip_safe=False,
+      python_requires='>=3.8'
+)

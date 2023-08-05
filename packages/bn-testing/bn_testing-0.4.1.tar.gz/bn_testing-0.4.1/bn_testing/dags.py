69df15977b0ceeb0a7e1ed184eb9450ac5a2ba21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package bn-testing/0.4.1/bn_testing-0.4.1.tar.gz

---
+++ packages/bn-testing/bn_testing-0.4.1.tar.gz/bn_testing-0.4.1/bn_testing/dags.py
@@ -0,0 +1,93 @@
+import networkx as nx
+import numpy as np
+
+from abc import ABCMeta
+from itertools import combinations
+
+from bn_testing.helpers import _generate_int_suffixes
+
+
+class DAG(metaclass=ABCMeta):
+
+    def generate(self, n_nodes, random=None):
+        raise NotImplementedError()
+
+    def init(self, random):
+        self.random = random
+
+    def make_node_names(self, n_nodes):
+        return _generate_int_suffixes(
+            prefix='f',
+            n=n_nodes)
+
+    def show(self, dag):
+        pos = nx.spring_layout(dag, seed=self.random)
+        nx.draw_networkx_nodes(dag, pos=pos, node_size=100)
+        nx.draw_networkx_edges(dag, pos=pos)
+
+
+class ScaleFree(DAG):
+
+    def __init__(self, alpha=0.4, beta=0.5, gamma=0.1):
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+    def generate(self, n_nodes):
+
+        dag = nx.scale_free_graph(
+            n=n_nodes,
+            seed=self.random,
+            alpha=self.alpha,
+            beta=self.beta,
+            gamma=self.gamma,
+            create_using=nx.DiGraph,
+        )
+
+        dag = nx.relabel_nodes(
+            dag,
+            mapping=dict(zip(dag.nodes(), self.make_node_names(n_nodes)))
+        )
+        return dag
+
+
+class ErdosReny(DAG):
+    """
+
+    Args:
+        p (float): Erdös-Renyi probability
+    """
+
+    def __init__(self, p=0.1):
+        self.p = p
+
+    def _select_edges(self, edges_iter, p):
+        """
+        TODO
+        """
+
+        # TODO Use np.fromiter
+        edges = np.array([a for a in edges_iter])
+
+        selection = self.random.choice(
+            [False, True],
+            p=[1-self.p, self.p],
+            size=edges.shape[0]
+        )
+        return edges[selection]
+
+    def generate(self, n_nodes):
+
+        nodes = self.make_node_names(n_nodes)
+
+        dag = nx.DiGraph()
+        dag.add_nodes_from(nodes)
+
+        # Shuffle nodes inplace
+        self.random.shuffle(nodes)
+
+        all_forward_edges = combinations(nodes, 2)
+        edges_selected = self._select_edges(all_forward_edges, self.p)
+
+        dag.add_edges_from(edges_selected)
+        return dag
---
+++ packages/bn-testing/bn_testing-0.4.1.tar.gz/bn_testing-0.4.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from pathlib import Path
+
+
+with open('VERSION') as f:
+    version = f.read().strip()
+
+setup(
+    name="bn_testing",
+    version=version,
+    packages=["bn_testing"],
+    python_requires='>=3.8.0',
+    python_version='3.10',
+    # dependencies
+    install_requires=[
+        'numpy>=1.20.0',
+        'pandas>=1.3.0',
+        'networkx>=2.5',
+        'pymc>4.1.2',
+    ],
+    tests_require=[
+        "pytest",
+        "sphinx",
+        "sphinx_rtd_theme",
+    ],
+
+    # metadata for upload to PyPI
+    author="Tobias Windisch",
+    author_email="tobias.windisch@posteo.de",
+    description="A test bench to benchmark learn algorithms for graphical models",
+    license="GNU GPL3",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+    keywords="graphical models",
+    url="https://github.com/windisch/bn_testing",
+)

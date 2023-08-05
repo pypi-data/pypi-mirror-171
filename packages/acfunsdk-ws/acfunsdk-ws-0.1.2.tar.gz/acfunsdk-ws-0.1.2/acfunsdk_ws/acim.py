beff554b19f993feceb7fe20123c5cd834e1d28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:00 +0000

    Add package acfunsdk-ws/0.1.2/acfunsdk-ws-0.1.2.tar.gz

---
+++ packages/acfunsdk-ws/acfunsdk-ws-0.1.2.tar.gz/acfunsdk-ws-0.1.2/acfunsdk_ws/acim.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+from .acws import AcWebSocket
+from acfunsdk import Acer
+from acfunsdk.source import AcSource
+
+__author__ = 'dolacmeo'
+
+
+class AcIM(AcWebSocket):
+
+    def __init__(self, acer: Acer, ws_links: [list, None] = None):
+        assert acer.is_logined is True
+        ws_links = AcSource.websocket_links if ws_links is None else ws_links
+        super().__init__(acer, ws_links)
+
+    def reader(self, seq_id: int, command, result):
+        pass
+
+    def im_get_sessions(self):
+        message = self.protos.MessageSession_Request()
+        return self.task(*message)
+
+    def im_session_start(self, uid: int):
+        message = self.protos.SessionCreate_Request(uid)
+        return self.task(*message)
+
+    def im_session_close(self, uid: int):
+        message = self.protos.SessionRemove_Request(uid)
+        return self.task(*message)
+
+    def im_pull_message(self, uid: int, minSeq: int, maxSeq: int, count: int = 10):
+        message = self.protos.MessagePullOld_Request(uid, minSeq, maxSeq, count)
+        return self.task(*message)
+
+    def im_send(self, uid: int, content: str):
+        message = self.protos.MessageContent_Request(uid, content)
+        return self.task(*message)
+
+    def im_send_image(self, uid: int, image_data: bytes):
+        message = self.protos.MessageImage_Request(uid, image_data)
+        return self.task(*message)

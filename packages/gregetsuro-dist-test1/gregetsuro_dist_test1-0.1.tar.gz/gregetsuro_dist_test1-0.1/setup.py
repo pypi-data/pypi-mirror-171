8d06dbc468f62474fa9073b4b8475d61c096e3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package gregetsuro-dist-test1/0.1/gregetsuro_dist_test1-0.1.tar.gz

---
+++ packages/gregetsuro-dist-test1/gregetsuro_dist_test1-0.1.tar.gz/gregetsuro_dist_test1-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gregetsuro_dist_test1',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Greg Montana',
+      author_email = 'gregorymontana@gmail.com',
+      zip_safe=False)

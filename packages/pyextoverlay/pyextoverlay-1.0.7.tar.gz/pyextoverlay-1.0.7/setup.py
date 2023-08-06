597575019bc74e0cdfe402068d9fdb4d89b1ad19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:18 +0000

    Add package pyextoverlay/1.0.7/pyextoverlay-1.0.7.tar.gz

---
+++ packages/pyextoverlay/pyextoverlay-1.0.7.tar.gz/pyextoverlay-1.0.7/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pyextoverlay']
+setup_kwargs = {
+    'name': 'pyextoverlay',
+    'version': '1.0.7',
+    'description': 'Qt overlay library',
+    'long_description': None,
+    'author': 'Xenely14',
+    'author_email': 'a.maryatkin14@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

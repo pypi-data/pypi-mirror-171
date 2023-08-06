1443048690c278045694d57556fc614d2473fcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:48 +0000

    Add package path4gmns/0.8.7/path4gmns-0.8.7-py3-none-any.whl

---
+++ packages/path4gmns/path4gmns-0.8.7-py3-none-any.whl/path4gmns/__init__.py
@@ -0,0 +1,12 @@
+from .accessibility import *
+from .colgen import *
+from .dtaapi import *
+from .utils import *
+from .zonesyn import *
+
+
+__version__ = '0.8.7'
+
+
+# print out the current version
+print(f'path4gmns, version {__version__}')

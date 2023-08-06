2a6629417681a21a5a8be456d54ab80c685d18f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:07 +0000

    Add package platts-sdk/0.3.7/platts_sdk-0.3.7-py3-none-any.whl

---
+++ packages/platts-sdk/platts_sdk-0.3.7-py3-none-any.whl/platts_sdk/tok.py
@@ -0,0 +1,48 @@
+from functools import lru_cache
+import requests
+import logging
+
+
+class TokenClient:
+    def __init__(self, username: str, password: str, apikey: str):
+        self.token_endpoint = "https://api.platts.com/auth/api"
+        self.endpoint = "https://api.platts.com"
+        self.un = username
+        self.pw = password
+        self.apikey = apikey
+        return
+
+    def get(self, path, params):
+        headers = {
+            "appkey": self.apikey,
+            "Authorization": f"Bearer {self.token}",
+            "User-Agent": "platts-py-sdk",
+        }
+
+        r = requests.get(f"{self.endpoint}{path}", headers=headers, params=params)
+        r.raise_for_status()
+        try:
+            return r.json()
+        except Exception as err:
+            if r.status_code >= 500:
+                logging.error(err)
+            else:
+                logging.error(r.status_code, r.json())
+            raise
+
+    @property
+    @lru_cache()
+    def token(self):
+        body = {"username": self.un, "password": self.pw}
+        headers = {"appkey": self.apikey}
+
+        r = requests.post(self.token_endpoint, data=body, headers=headers)
+        r.raise_for_status()
+        try:
+            return r.json()["access_token"]
+        except Exception as err:
+            if r.status_code >= 500:
+                logging.error(f"[{r.status_code}] - {err}")
+            else:
+                logging.error(f"[{r.status_code}] -  {r.json()}")
+            raise

e50a9cfeae502929c6bf2953a64c40cde9757917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:57 +0000

    Add package jupyter-scatter/0.7.3/jupyter_scatter-0.7.3-py3-none-any.whl

---
+++ packages/jupyter-scatter/jupyter_scatter-0.7.3-py3-none-any.whl/jscatter/utils.py
@@ -0,0 +1,60 @@
+from matplotlib.colors import LogNorm, PowerNorm, Normalize
+import ipywidgets as widgets
+from urllib.parse import urlparse
+
+def to_uint8(x):
+  return int(max(0, min(x * 255, 255)))
+
+
+def to_hex(color):
+    if isinstance(color, list):
+        rgb = [to_uint8(c) for c in color]
+        return f'#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}'
+    else:
+        return color
+
+
+def with_left_label(label_text, widget, label_width: int = 128):
+    container = widgets.HBox()
+    label_layout = widgets.Layout(width=f'{label_width}px')
+    label = widgets.HTML(label_text, layout=label_layout)
+    container.children = (label, widget)
+
+    return container
+
+def any_not(l, value = None):
+    return any([x is not value for x in l])
+
+def tolist(l):
+    try:
+        return l.tolist()
+    except:
+        return l
+
+def uri_validator(x):
+    try:
+        result = urlparse(x)
+        return all([result.scheme, result.netloc, result.path])
+    except:
+        return False
+
+def sorting_to_dict(sorting):
+    out = dict()
+    for order_idx, original_idx in enumerate(sorting):
+        out[original_idx] = order_idx
+    return out
+
+def create_default_norm():
+    return Normalize(clip=True)
+
+def to_ndc(X, norm):
+    return (norm(X).data * 2) - 1
+
+def to_scale_type(norm):
+    if (isinstance(norm, LogNorm)):
+        return 'log_10'
+
+    if (isinstance(norm, PowerNorm)):
+        return f'pow_{norm.gamma}'
+
+    return 'linear'

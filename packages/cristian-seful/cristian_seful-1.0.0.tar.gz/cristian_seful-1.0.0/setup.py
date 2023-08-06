d9ef67a8d0f1ffc904b2d9047fd6372f0da0549a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:27 +0000

    Add package cristian-seful/1.0.0/cristian_seful-1.0.0.tar.gz

---
+++ packages/cristian-seful/cristian_seful-1.0.0.tar.gz/cristian_seful-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='cristian_seful',
+    version='1.0.0',
+    author='Cristian Iordache',
+    author_email='iordachecristian000@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A super nice library'
+)
+

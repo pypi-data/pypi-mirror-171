20c42134166624316d706362eb38765841815c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:31 +0000

    Add package egen111222-fast-doc-example-app/0.1/egen111222-fast-doc-example-app-0.1.tar.gz

---
+++ packages/egen111222-fast-doc-example-app/egen111222-fast-doc-example-app-0.1.tar.gz/egen111222-fast-doc-example-app-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='egen111222-fast-doc-example-app',
+      version='0.1',
+      description='Python Distribution Utilities',
+      author='Kirill',
+      author_email='egen13@ukr.net',
+      packages=['project'],
+     )

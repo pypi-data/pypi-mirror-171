3247ebe24ad3ce1690a63f797be56b2d9ed80000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:43 +0000

    Add package OrgAlkCalc/0.1.8/OrgAlkCalc-0.1.8.tar.gz

---
+++ packages/OrgAlkCalc/OrgAlkCalc-0.1.8.tar.gz/OrgAlkCalc-0.1.8/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+with open("README.md","r") as fh:
+  long_description = fh.read()
+
+setup(
+  url = "https://github.com/charles-turner-1/OrgAlkCalc",
+  author="Charles Turner",
+  author_email='charlesturner0987@gmail.com',
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+  install_requires=["numpy","pandas","scipy","lmfit" ,"matplotlib" ,"openpyxl"],
+  extras_require = {"dev": ["pytest>=3.7",],},
+  description="This is the cleaned up organic-alkalinity-sausage-machine, now known more sensibly as OrgAlkCalc",
+  py_modules=["OrgAlkCalc"],
+  package_dir={'': 'src'},
+  license="MIT license",
+  include_package_data=True,
+  name='OrgAlkCalc',
+  version='0.1.8',
+  zip_safe=False,
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+)

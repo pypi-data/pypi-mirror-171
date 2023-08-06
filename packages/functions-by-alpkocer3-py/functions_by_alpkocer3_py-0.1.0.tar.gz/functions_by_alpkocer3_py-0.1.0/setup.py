c7e1c9550da2a2680dda5e19c0214b20d512f5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:16 +0000

    Add package functions-by-alpkocer3-py/0.1.0/functions_by_alpkocer3_py-0.1.0.tar.gz

---
+++ packages/functions-by-alpkocer3-py/functions_by_alpkocer3_py-0.1.0.tar.gz/functions_by_alpkocer3_py-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_alpkocer3']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-alpkocer3-py',
+    'version': '0.1.0',
+    'description': 'Test Project',
+    'long_description': '',
+    'author': 'Alp Kocer',
+    'author_email': 'gokalpkocer@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

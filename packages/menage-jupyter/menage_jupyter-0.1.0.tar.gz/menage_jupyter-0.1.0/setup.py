2aa48bf44fb9f6828167801fb1905d7241a86fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:40 +0000

    Add package menage-jupyter/0.1.0/menage_jupyter-0.1.0.tar.gz

---
+++ packages/menage-jupyter/menage_jupyter-0.1.0.tar.gz/menage_jupyter-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['menage_jupyter']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'menage-jupyter',
+    'version': '0.1.0',
+    'description': 'Simple package to menage jupyter files',
+    'long_description': None,
+    'author': 'Bartłomiej Chwiłkowski',
+    'author_email': 'bartekchwilkowski@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

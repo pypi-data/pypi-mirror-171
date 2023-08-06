682d657683871258f1d6dbd2d5dc093440c9dd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:25 +0000

    Add package LazarusLoot/0.1.dev0/LazarusLoot-0.1.dev0.tar.gz

---
+++ packages/LazarusLoot/LazarusLoot-0.1.dev0.tar.gz/LazarusLoot-0.1.dev0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='LazarusLoot',
+    version='0.1dev',
+    packages=['src',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('Readme.md').read(),
+)

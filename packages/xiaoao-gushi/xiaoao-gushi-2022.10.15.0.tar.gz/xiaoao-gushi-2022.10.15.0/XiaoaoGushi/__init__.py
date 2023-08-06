4b0cf4c771ec9e30f9ceceae59b390d964455f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:52 +0000

    Add package xiaoao-gushi/2022.10.15.0/xiaoao-gushi-2022.10.15.0.tar.gz

---
+++ packages/xiaoao-gushi/xiaoao-gushi-2022.10.15.0.tar.gz/xiaoao-gushi-2022.10.15.0/XiaoaoGushi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""xiaoao-gushi
+https://github.com/apachecn/xiaoao-gushi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

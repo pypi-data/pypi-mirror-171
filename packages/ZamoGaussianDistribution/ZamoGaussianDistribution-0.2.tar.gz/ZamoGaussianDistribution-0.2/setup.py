f8623b144dabdd4da6a61e6d5316197e241985fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:28 +0000

    Add package ZamoGaussianDistribution/0.2/ZamoGaussianDistribution-0.2.tar.gz

---
+++ packages/ZamoGaussianDistribution/ZamoGaussianDistribution-0.2.tar.gz/ZamoGaussianDistribution-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ZamoGaussianDistribution',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['ZamoGaussianDistribution'],
+      author='Zamokwakhe Mthethwa',
+      author_email='cihle.zm.01@gmail.com',
+      zip_safe=False)

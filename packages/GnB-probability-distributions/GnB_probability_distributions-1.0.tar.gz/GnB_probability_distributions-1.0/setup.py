119ecb12214894a35f6493ac1f31c6c735d45486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:35 +0000

    Add package GnB-probability-distributions/1.0/GnB_probability_distributions-1.0.tar.gz

---
+++ packages/GnB-probability-distributions/GnB_probability_distributions-1.0.tar.gz/GnB_probability_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GnB_probability_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['GnB_probability_distributions'],
+      author= 'Giovanni Cortes Morales',
+      author_email= 'giocortes19@gmail.com',
+      zip_safe=False)

686bb1e1a74816d5cb91ce290803163118da8ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:37 +0000

    Add package glist/0.1.dev1/glist-0.1.dev1.tar.gz

---
+++ packages/glist/glist-0.1.dev1.tar.gz/glist-0.1.dev1/glist/__meta__.py
@@ -0,0 +1,12 @@
+# `name` is the name of the package as used for `pip install package`
+name = "glist"
+# `path` is the name of the package for `import package`
+path = name.lower().replace("-", "_").replace(" ", "_")
+# Your version number should follow https://python.org/dev/peps/pep-0440 and
+# https://semver.org
+version = "0.1.dev1"
+author = "Joshua Mathias"
+author_email = "joshuaamathias@gmail.com"
+description = "Read and write from Google Sheets as if it were a csv."  # One-liner
+url = "https://github.com/JoshuaMathias/glist"  # your project homepage
+license = "CCO-1.0"  # See https://choosealicense.com

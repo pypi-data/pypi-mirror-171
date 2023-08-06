44932a62c741d02272e9981a53a81d7fbbb6bb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:36 +0000

    Add package fast-tmp/1.0.5/fast_tmp-1.0.5-py3-none-any.whl

---
+++ packages/fast-tmp/fast_tmp-1.0.5-py3-none-any.whl/fast_tmp/amis/view/divider.py
@@ -0,0 +1,6 @@
+# 一条横线
+from pydantic import BaseModel
+
+
+class Divider(BaseModel):
+    type: str = "divider"
---
+++ packages/fast-tmp/fast_tmp-1.0.5-py3-none-any.whl/fast_tmp/responses.py
@@ -0,0 +1,14 @@
+from typing import Any, List
+
+from pydantic import BaseModel
+
+
+class ListDataWithPage(BaseModel):  # 带分页的数据
+    items: List[dict]
+    total: int = 0
+
+
+class BaseRes(BaseModel):
+    status: int = 0
+    msg: str = ""
+    data: Any = {}

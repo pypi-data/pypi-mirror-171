5d202b0595211ba8416b191aee20bad75520359b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:47 +0000

    Add package TestMath-rjkhan/0.0.5/TestMath_rjkhan-0.0.5.tar.gz

---
+++ packages/TestMath-rjkhan/TestMath_rjkhan-0.0.5.tar.gz/TestMath_rjkhan-0.0.5/TestMath_rjkhan/example.py
@@ -0,0 +1,61 @@
+def add(number1, number2):
+    """
+    return sum of integer
+
+    :param number1: should be integer number
+    :param number2: should be integer number
+    :type number1: Integer
+    :type number2: Integer
+    :return: addition of integer
+    :rtype: int
+
+    """
+    return number1 + number2
+
+
+def sub(number1, number2):
+    """
+       return sum of integer
+
+       :param number1: should be integer number
+       :param number2: should be integer number
+       :type number1: Integer
+       :type number2: Integer
+       :return: subtraction of integer
+       :rtype: int
+
+       """
+    return number1 - number2
+
+
+def mul(number1, number2):
+    """
+       return sum of integer
+
+       :param number1: should be integer number
+       :param number2: should be integer number
+       :type number1: Integer
+       :type number2: Integer
+       :return: multiply of integer
+       :rtype: int
+
+       """
+    return number1 * number2
+
+
+
+def div(number1, number2):
+    """
+    return sum of integer
+
+    :param number1: should be integer number
+    :param number2: should be integer number
+    :type number1: Integer
+    :type number2: Integer
+    :return: divide of integer
+    :rtype: int
+    """
+    return number1 / number2
+
+
+

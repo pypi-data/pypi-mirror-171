753bb745a3a1e451da971b447f9a5df9849eef38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:42 +0000

    Add package Jiangziyi/0.0.3/Jiangziyi-0.0.3-py2.py3-none-any.whl

---
+++ packages/Jiangziyi/Jiangziyi-0.0.3-py2.py3-none-any.whl/Jiangziyi/Jiangziyi.py
@@ -0,0 +1,15 @@
+'''
+Author: Vincent Young
+Date: 2022-09-14 05:16:47
+LastEditors: Vincent Young
+LastEditTime: 2022-10-13 22:08:30
+FilePath: /Jiangziyi/Jiangziyi/Jiangziyi.py
+Telegram: https://t.me/missuo
+
+Copyright © 2022 by Vincent, All Rights Reserved. 
+'''
+
+
+def main():
+    text = "蒋紫怡是卡尔顿CS女神！\n\nWeChat: babyii1218"
+    print(text)

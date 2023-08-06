76c8b9962a8c4e73231b9bf6a1644a3331700927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:35 +0000

    Add package weather-utsav/1.0.0/weather_utsav-1.0.0.tar.gz

---
+++ packages/weather-utsav/weather_utsav-1.0.0.tar.gz/weather_utsav-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="weather_utsav",
+    version="1.0.0",
+    description="This is a Weather Provider.",
+    author="Utsav",
+    packages=['weather_utsav'],
+    install_requires=['requests']
+)

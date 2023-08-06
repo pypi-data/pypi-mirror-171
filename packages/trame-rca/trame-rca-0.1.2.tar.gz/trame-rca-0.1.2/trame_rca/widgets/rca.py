951341ba691c8a18db1ce1b9025eb76a7237a798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:42 +0000

    Add package trame-rca/0.1.2/trame-rca-0.1.2.tar.gz

---
+++ packages/trame-rca/trame-rca-0.1.2.tar.gz/trame-rca-0.1.2/trame_rca/widgets/rca.py
@@ -0,0 +1,67 @@
+from trame_client.widgets.core import AbstractElement
+from .. import module
+
+
+class HtmlElement(AbstractElement):
+    def __init__(self, _elem_name, children=None, **kwargs):
+        super().__init__(_elem_name, children, **kwargs)
+        if self.server:
+            self.server.enable_module(module)
+
+
+# Expose your vue component(s)
+class RemoteControlledArea(HtmlElement):
+    def __init__(self, **kwargs):
+        super().__init__(
+            "remote-controlled-area",
+            **kwargs,
+        )
+        self._attr_names += [
+            "name",
+            "origin",
+        ]
+
+
+class DisplayArea(HtmlElement):
+    def __init__(self, **kwargs):
+        super().__init__(
+            "display-area",
+            **kwargs,
+        )
+        self._attr_names += [
+            "name",
+            "origin",
+        ]
+
+
+class FpsDisplay(HtmlElement):
+    def __init__(self, **kwargs):
+        super().__init__(
+            "fps-display",
+            **kwargs,
+        )
+        self._attr_names += [
+            "name",
+            ("fps_delta", "fpsDelta"),
+        ]
+
+
+class ImageDisplayArea(HtmlElement):
+    def __init__(self, **kwargs):
+        super().__init__(
+            "image-display-area",
+            **kwargs,
+        )
+        self._attr_names += ["name", "origin", ("pool_size", "poolSize")]
+
+
+class VideoDisplayArea(HtmlElement):
+    def __init__(self, **kwargs):
+        super().__init__(
+            "video-display-area",
+            **kwargs,
+        )
+        self._attr_names += [
+            "name",
+            "origin",
+        ]

244ae2093d5407d0097050645ebea7253095c714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package mpl-draggable-line/0.4.1/mpl_draggable_line-0.4.1.tar.gz

---
+++ packages/mpl-draggable-line/mpl_draggable_line-0.4.1.tar.gz/mpl_draggable_line-0.4.1/mpl_draggable_line/__init__.py
@@ -0,0 +1,18 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+from ._line import DraggableHLine, DraggableLine, DraggableVLine
+
+__author__ = "Ian Hunt-Isaak"
+__email__ = "ianhuntisaak@gmail.com"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__email__",
+    "DraggableLine",
+    "DraggableVLine",
+    "DraggableHLine",
+]

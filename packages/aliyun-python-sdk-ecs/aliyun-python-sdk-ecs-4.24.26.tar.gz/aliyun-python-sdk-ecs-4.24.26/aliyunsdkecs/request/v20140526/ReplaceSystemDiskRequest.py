e4677f48d2ee8451ec94e3826d883ccb93e519e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:25 +0000

    Add package aliyun-python-sdk-ecs/4.24.26/aliyun-python-sdk-ecs-4.24.26.tar.gz

---
+++ packages/aliyun-python-sdk-ecs/aliyun-python-sdk-ecs-4.24.26.tar.gz/aliyun-python-sdk-ecs-4.24.26/aliyunsdkecs/request/v20140526/ReplaceSystemDiskRequest.py
@@ -0,0 +1,113 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkecs.endpoint import endpoint_data
+
+class ReplaceSystemDiskRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ecs', '2014-05-26', 'ReplaceSystemDisk','ecs')
+		self.set_method('POST')
+
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+	def get_ResourceOwnerId(self): # Long
+		return self.get_query_params().get('ResourceOwnerId')
+
+	def set_ResourceOwnerId(self, ResourceOwnerId):  # Long
+		self.add_query_param('ResourceOwnerId', ResourceOwnerId)
+	def get_ImageId(self): # String
+		return self.get_query_params().get('ImageId')
+
+	def set_ImageId(self, ImageId):  # String
+		self.add_query_param('ImageId', ImageId)
+	def get_ClientToken(self): # String
+		return self.get_query_params().get('ClientToken')
+
+	def set_ClientToken(self, ClientToken):  # String
+		self.add_query_param('ClientToken', ClientToken)
+	def get_SecurityEnhancementStrategy(self): # String
+		return self.get_query_params().get('SecurityEnhancementStrategy')
+
+	def set_SecurityEnhancementStrategy(self, SecurityEnhancementStrategy):  # String
+		self.add_query_param('SecurityEnhancementStrategy', SecurityEnhancementStrategy)
+	def get_KeyPairName(self): # String
+		return self.get_query_params().get('KeyPairName')
+
+	def set_KeyPairName(self, KeyPairName):  # String
+		self.add_query_param('KeyPairName', KeyPairName)
+	def get_Platform(self): # String
+		return self.get_query_params().get('Platform')
+
+	def set_Platform(self, Platform):  # String
+		self.add_query_param('Platform', Platform)
+	def get_Password(self): # String
+		return self.get_query_params().get('Password')
+
+	def set_Password(self, Password):  # String
+		self.add_query_param('Password', Password)
+	def get_PasswordInherit(self): # Boolean
+		return self.get_query_params().get('PasswordInherit')
+
+	def set_PasswordInherit(self, PasswordInherit):  # Boolean
+		self.add_query_param('PasswordInherit', PasswordInherit)
+	def get_DiskId(self): # String
+		return self.get_query_params().get('DiskId')
+
+	def set_DiskId(self, DiskId):  # String
+		self.add_query_param('DiskId', DiskId)
+	def get_Architecture(self): # String
+		return self.get_query_params().get('Architecture')
+
+	def set_Architecture(self, Architecture):  # String
+		self.add_query_param('Architecture', Architecture)
+	def get_ResourceOwnerAccount(self): # String
+		return self.get_query_params().get('ResourceOwnerAccount')
+
+	def set_ResourceOwnerAccount(self, ResourceOwnerAccount):  # String
+		self.add_query_param('ResourceOwnerAccount', ResourceOwnerAccount)
+	def get_OwnerAccount(self): # String
+		return self.get_query_params().get('OwnerAccount')
+
+	def set_OwnerAccount(self, OwnerAccount):  # String
+		self.add_query_param('OwnerAccount', OwnerAccount)
+	def get_OwnerId(self): # Long
+		return self.get_query_params().get('OwnerId')
+
+	def set_OwnerId(self, OwnerId):  # Long
+		self.add_query_param('OwnerId', OwnerId)
+	def get_InstanceId(self): # String
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self, InstanceId):  # String
+		self.add_query_param('InstanceId', InstanceId)
+	def get_SystemDiskSize(self): # Integer
+		return self.get_query_params().get('SystemDisk.Size')
+
+	def set_SystemDiskSize(self, SystemDiskSize):  # Integer
+		self.add_query_param('SystemDisk.Size', SystemDiskSize)
+	def get_UseAdditionalService(self): # Boolean
+		return self.get_query_params().get('UseAdditionalService')
+
+	def set_UseAdditionalService(self, UseAdditionalService):  # Boolean
+		self.add_query_param('UseAdditionalService', UseAdditionalService)

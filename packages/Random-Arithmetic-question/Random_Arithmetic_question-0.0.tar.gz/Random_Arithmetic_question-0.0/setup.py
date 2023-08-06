f2589a331a8b1d945a66bdc86a5bd2d6531639b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:45 +0000

    Add package Random-Arithmetic-question/0.0/Random_Arithmetic_question-0.0.tar.gz

---
+++ packages/Random-Arithmetic-question/Random_Arithmetic_question-0.0.tar.gz/Random_Arithmetic_question-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Random_Arithmetic_question',
+      version='0.0',
+      description='Random Arithmetic Question',
+      packages=['Random_Arithmetic_question'],
+      author_email='dharmisha2207@gmail.com',
+      zip_safe=False)

c663639b04f3b9ffc9f5a50e815693f5e711b182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:13 +0000

    Add package clease/1.0.3/clease-1.0.3.tar.gz

---
+++ packages/clease/clease-1.0.3.tar.gz/clease-1.0.3/cxx/include/basis_function.hpp
@@ -0,0 +1,42 @@
+#ifndef BASIS_FUNCTION_H
+#define BASIS_FUNCTION_H
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "symbols_with_numbers.hpp"
+
+typedef std::map<std::string, double> dict_dbl_t;
+typedef std::vector<dict_dbl_t> bf_raw_t;
+
+class BasisFunction {
+   public:
+    BasisFunction();
+    BasisFunction(const bf_raw_t &raw_bfs, const Symbols &symb_with_num);
+    BasisFunction(const BasisFunction &other);
+    BasisFunction &operator=(const BasisFunction &other);
+    ~BasisFunction();
+
+    /** Return the basis function value for a given decoration number and symbol ID */
+    double get(unsigned int dec_num, unsigned int symb_id) const;
+
+    /** Return the size (number of basis functions) */
+    unsigned int size() const {
+        return num_bfs;
+    };
+
+    /** Stream operator */
+    friend std::ostream &operator<<(std::ostream &out, const BasisFunction &bf);
+
+   private:
+    const Symbols *symb_ptr{nullptr};
+    std::vector<double> bfs;
+    unsigned int num_bfs{0};
+    unsigned int num_bf_values{0};
+
+    /** Return the corresponding index into the flattened array */
+    unsigned int get_index(unsigned int dec_num, unsigned int symb_id) const;
+    void swap(const BasisFunction &other);
+};
+
+#endif

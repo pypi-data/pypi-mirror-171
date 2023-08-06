5d9d8c1f79f76c9c78bd37f39fc820ef32f9cc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:24 +0000

    Add package aioprometheus-api-client/0.0.2/aioprometheus_api_client-0.0.2-py3-none-any.whl

---
+++ packages/aioprometheus-api-client/aioprometheus_api_client-0.0.2-py3-none-any.whl/aioprometheus_api_client/__init__.py
@@ -0,0 +1,10 @@
+"""A collection of tools to collect and manipulate prometheus metrics."""
+
+__title__ = "prometheus-connect"
+__version__ = "0.0.2"
+
+from .prometheus_connect import PrometheusConnect  # noqa F403
+from .metric import Metric  # noqa F401
+from .metrics_list import MetricsList  # noqa F401
+from .metric_snapshot_df import MetricSnapshotDataFrame  # noqa F401
+from .metric_range_df import MetricRangeDataFrame  # noqa F401

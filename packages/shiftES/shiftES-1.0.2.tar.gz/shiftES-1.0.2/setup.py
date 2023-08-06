d64180edf1ca708a4b6f650993bb57e43a549dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:50 +0000

    Add package shiftES/1.0.2/shiftES-1.0.2.tar.gz

---
+++ packages/shiftES/shiftES-1.0.2.tar.gz/shiftES-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'shiftES',
+    author = 'John C. Thomas',
+    description='Implimentation of "A Robust Nonparametric Measure of Effect Size Based on an Analog of Cohens d...", R. Wilcox (2018). https://dx.doi.org/10.22237/jmasm/1551905677',
+    author_email = 'jcthomas000@gmail.com',
+    version='1.0.2',
+    install_requires = ['numpy', 'pandas', 'openpyx'],
+    readme = "README.md",
+    scripts = ['shiftES/calculate_effectsize.py'],
+    python_requires = '>=3.6',
+    include_package_data=True, #uses MANIFEST.in
+)

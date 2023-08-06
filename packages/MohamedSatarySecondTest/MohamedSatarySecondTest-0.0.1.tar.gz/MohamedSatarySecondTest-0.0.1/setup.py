4fda9f183b9fecf0e05067904c47d51dc20705b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:00 +0000

    Add package MohamedSatarySecondTest/0.0.1/MohamedSatarySecondTest-0.0.1.tar.gz

---
+++ packages/MohamedSatarySecondTest/MohamedSatarySecondTest-0.0.1.tar.gz/MohamedSatarySecondTest-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+      name='MohamedSatarySecondTest',
+      version='0.0.1',
+      description='''Testing out uploading a library''',
+      py_modules=['helloWorld'],
+      package_dir={'': 'src'},
+      )

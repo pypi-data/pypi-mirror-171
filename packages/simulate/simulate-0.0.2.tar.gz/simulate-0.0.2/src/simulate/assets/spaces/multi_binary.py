67b1ceb3554c03893221b18fe2ff041e2118fbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:20 +0000

    Add package simulate/0.0.2/simulate-0.0.2.tar.gz

---
+++ packages/simulate/simulate-0.0.2.tar.gz/simulate-0.0.2/src/simulate/assets/spaces/multi_binary.py
@@ -0,0 +1,68 @@
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from typing import Any, List, Optional, Union
+
+import numpy as np
+
+from .space import Space
+
+
+class MultiBinary(Space):
+    """
+    An n-shape binary space.
+
+    The argument to MultiBinary defines n, which could be a number or a `list` of numbers.
+
+    Example Usage:
+
+    >> self.observation_space = spaces.MultiBinary(5)
+
+    >> self.observation_space.sample()
+
+        array([0,1,0,1,0], dtype=int8)
+
+    >> self.observation_space = spaces.MultiBinary([3,2])
+
+    >> self.observation_space.sample()
+
+        array([[0, 0],
+               [0, 1],
+               [1, 1]], dtype=int8)
+
+    """
+
+    def __init__(self, n: int, seed: Optional[int] = None):
+        self.n = n
+        if type(n) in [tuple, list, np.ndarray]:
+            input_n = n
+        else:
+            input_n = (n,)
+        super(MultiBinary, self).__init__(input_n, np.int8, seed)
+
+    def sample(self) -> Union[np.ndarray, List[np.ndarray]]:
+        return self.np_random.randint(low=0, high=2, size=self.n, dtype=self.dtype)
+
+    def contains(self, x: Any) -> bool:
+        if isinstance(x, list) or isinstance(x, tuple):
+            x = np.array(x)  # Promote list to array for contains check
+        if self.shape != x.shape:
+            return False
+        return ((x == 0) | (x == 1)).all()
+
+    def to_jsonable(self, sample_n: List[Any]):
+        return np.array(sample_n).tolist()
+
+    def from_jsonable(self, sample_n: List[Any]) -> List[Any]:
+        return [np.asarray(sample) for sample in sample_n]
+
+    def __repr__(self) -> str:
+        return "MultiBinary({})".format(self.n)
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, MultiBinary) and self.n == other.n

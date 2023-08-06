b9a76bf23e04912dbbfd8eb55b299ec3ef619194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:11 +0000

    Add package cleantest/0.1.1/cleantest-0.1.1.tar.gz

---
+++ packages/cleantest/cleantest-0.1.1.tar.gz/cleantest-0.1.1/src/cleantest/provider/data/env_data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Environment variable management for test providers."""
+
+import os
+from typing import Any, Dict
+
+
+class EnvDataStore:
+    def __init__(self) -> None:
+        self.__env = {}
+
+    def append(self, env_var: str, value: str) -> None:
+        if env_var not in self.__env.keys():
+            self.__env.update({env_var: []})
+
+        self.__env[env_var].append(value)
+
+    def get(self, env_var: str) -> Any | None:
+        """Retrieve environment variable from store.
+
+        Args:
+            env_var (str): Environment variable to retrieve.
+
+        Returns:
+            Any | None: Environment variable value. Returns None if variable is not in store.
+        """
+        try:
+            if type(self.__env[env_var]) == list:
+                return os.pathsep.join(self.__env[env_var])
+        except KeyError:
+            return None
+
+    @property
+    def _raw_env(self) -> Dict[str, Any]:
+        return self.__env

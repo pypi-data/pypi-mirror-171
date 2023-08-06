a1d3dd7109942752aa2efe10e7270ffd826d7544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:13 +0000

    Add package balcony/0.0.12/balcony-0.0.12.tar.gz

---
+++ packages/balcony/balcony-0.0.12.tar.gz/balcony-0.0.12/balcony/__init__.py
@@ -0,0 +1,13 @@
+"""
+Any resource that app system is going to use must be exported here.
+Otherwise different import statements would create new global static objects
+for AppRegistry etc.
+"""
+try:
+    from .registries import app_registry, AppRegistry
+    from .app import BaseBalconyApp
+    from .factories import BalconyAWS
+except ImportError:
+    from registries import app_registry, AppRegistry
+    from app import BaseBalconyApp
+    from factories import BalconyAWS

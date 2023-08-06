b6b8350c392957a86f2ed5fbba68b228c807931a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:52 +0000

    Add package smpl/0.0.147/smpl-0.0.147-py3-none-any.whl

---
+++ packages/smpl/smpl-0.0.147-py3-none-any.whl/smpl/stat/__init__.py
@@ -0,0 +1,13 @@
+"""Simplified statistics."""
+
+from .stat import unv, usd, unv_lambda, poisson_dist, normalize, novar_mean, mean, noisy, no_dist,R2,Chi2,fft, get_interesting_domain
+
+__all__ = ['unv',
+           'usd',
+           'unv_lambda', 'poisson_dist',
+           'no_dist',
+           'normalize', 'novar_mean',
+           'mean', 'noisy',
+           'R2','Chi2','fft',
+           'get_interesting_domain',
+           ]

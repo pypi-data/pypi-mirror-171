395532c3ef46d72f399974c818c8e8a03996d210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:59 +0000

    Add package faketory/0.1.0/faketory-0.1.0-py3-none-any.whl

---
+++ packages/faketory/faketory-0.1.0-py3-none-any.whl/faketory/fake.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2022 warevil <jg@warevil.dev>
+
+import operator
+
+from faker import Faker
+
+_fake = Faker()
+
+
+class Fake:
+    def __init__(self, __method_name, **fields):
+        self.method = operator.attrgetter(__method_name)(_fake)
+        self.fields = fields
+
+    def generate(self):
+        return self.method(**self.fields)

05e62be509b201abaffce5b17cd86f0c1b833345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:00 +0000

    Add package strong-augment/0.0.2/strong_augment-0.0.2.tar.gz

---
+++ packages/strong-augment/strong_augment-0.0.2.tar.gz/strong_augment-0.0.2/strong_augment/__init__.py
@@ -0,0 +1,3 @@
+from ._augment import *
+from ._operations import *
+from ._shift import *
---
+++ packages/strong-augment/strong_augment-0.0.2.tar.gz/strong_augment-0.0.2/test/test_augmentation.py
@@ -0,0 +1,17 @@
+import numpy
+from strong_augment import StrongAugment
+
+from utils import EXAMPLE_IMAGE
+
+
+def test_repeat_agumentation():
+    trnsf = StrongAugment()
+    for __ in range(128):
+        trnsf(EXAMPLE_IMAGE)
+    augmented = numpy.array(trnsf(EXAMPLE_IMAGE))
+    repeated = numpy.array(trnsf.repeat(EXAMPLE_IMAGE))
+    assert numpy.equal(augmented, repeated).all()
+    trnsf = StrongAugment(max_affine=0)
+    augmented = numpy.array(trnsf(EXAMPLE_IMAGE))
+    repeated = numpy.array(trnsf.repeat(EXAMPLE_IMAGE))
+    assert numpy.equal(augmented, repeated).all()

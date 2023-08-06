bb41854414430b07cd9d505e3dd53f603cf989e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:16 +0000

    Add package forta-agent/0.1.5/forta_agent-0.1.5.tar.gz

---
+++ packages/forta-agent/forta_agent-0.1.5.tar.gz/forta_agent-0.1.5/src/forta_agent/__init__.py
@@ -0,0 +1,13 @@
+from .finding import Finding, FindingSeverity, FindingType
+from .block_event import BlockEvent
+from .transaction_event import TransactionEvent, TxEventBlock
+from .block import Block
+from .transaction import Transaction
+from .receipt import Receipt, Log
+from .trace import Trace, TraceAction, TraceResult
+from .event_type import EventType
+from .network import Network
+from .utils import get_json_rpc_url, create_block_event, create_transaction_event, get_web3_provider, keccak256, get_transaction_receipt, get_alerts, fetch_jwt, decode_jwt, verify_jwt
+from web3 import Web3
+
+web3Provider = Web3(Web3.HTTPProvider(get_json_rpc_url()))

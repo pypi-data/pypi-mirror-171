dbf4ba69020f6c07b9c8677c0393d76424fd2e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:46 +0000

    Add package ranglaogong-bianyouqiande-64ge-xiguan/2022.10.11.0/ranglaogong-bianyouqiande-64ge-xiguan-2022.10.11.0.tar.gz

---
+++ packages/ranglaogong-bianyouqiande-64ge-xiguan/ranglaogong-bianyouqiande-64ge-xiguan-2022.10.11.0.tar.gz/ranglaogong-bianyouqiande-64ge-xiguan-2022.10.11.0/RanglaogongBianyouqiande64geXiguan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""ranglaogong-bianyouqiande-64ge-xiguan
+https://github.com/apachecn/ranglaogong-bianyouqiande-64ge-xiguan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

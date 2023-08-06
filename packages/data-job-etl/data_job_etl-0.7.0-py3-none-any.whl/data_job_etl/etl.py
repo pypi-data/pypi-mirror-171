e15e5776bffb6f7a5758e53704211a42fda01ad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:17 +0000

    Add package data-job-etl/0.7.0/data_job_etl-0.7.0-py3-none-any.whl

---
+++ packages/data-job-etl/data_job_etl-0.7.0-py3-none-any.whl/data_job_etl/etl.py
@@ -0,0 +1,11 @@
+from data_job_etl.transform.transform import transform
+from data_job_etl.load.load import Loader
+
+
+def transform_and_load():
+    pivotted_jobs = transform()
+
+    loader = Loader(pivotted_jobs)
+    loader.load()
+
+transform_and_load()

0df555989e988c015c1f977849cbc229bb576556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:14 +0000

    Add package testPypier/1.2/testPypier-1.2.tar.gz

---
+++ packages/testPypier/testPypier-1.2.tar.gz/testPypier-1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(
+    name="testPypier",
+    version="1.2",
+    author="testPypier",
+    author_email="testPypier@gmail.com",
+    description='''
+testPypier
+    ''',
+    long_description_content_type="text/markdown",
+    long_description='''
+testPypier
+    ''',
+    packages=find_packages(),
+    keywords=["testPypier",],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    
+)
+        

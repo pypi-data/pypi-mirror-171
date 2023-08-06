bd6a2797d5c69b42b59fc30ee97453d09404ce27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:08 +0000

    Add package hsv2munsell/0.0.2/hsv2munsell-0.0.2.tar.gz

---
+++ packages/hsv2munsell/hsv2munsell-0.0.2.tar.gz/hsv2munsell-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'Utility to convert HSV to Munsell scale'
+LONG_DESCRIPTION = 'Utility to convert HSV to Munsell scale'
+
+# Setting up
+setup(
+        name="hsv2munsell", 
+        version=VERSION,
+        author="Sumit Badal",
+        author_email="<sbadal01@yahoo.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[],
+        
+        keywords=['python', 'munsell'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ],
+        include_package_data=True,
+        package_data={'': ['calib/*.png']},
+)

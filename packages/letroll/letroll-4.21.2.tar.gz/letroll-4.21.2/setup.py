c891ae3b961598fdd1fdea6ecd2a12b2ffff25ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:29 +0000

    Add package letroll/4.21.2/letroll-4.21.2.tar.gz

---
+++ packages/letroll/letroll-4.21.2.tar.gz/letroll-4.21.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['letroll']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'letroll',
+    'version': '4.21.2',
+    'description': '',
+    'long_description': '',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>3.0',
+}
+
+
+setup(**setup_kwargs)

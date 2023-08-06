a781cda3452fcb6b4ce2754b6c0776e08bbcb4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:51 +0000

    Add package ansys-dpf-gate/0.2.3/ansys_dpf_gate-0.2.3-py3-none-any.whl

---
+++ packages/ansys-dpf-gate/ansys_dpf_gate-0.2.3-py3-none-any.whl/ansys/dpf/gate/_version.py
@@ -0,0 +1,7 @@
+"""Version for ansys-dpf-gate"""
+# major, minor, patch
+version_info = 0, 2, 3
+__ansys_version__ = "231"
+
+# Nice string for the version
+__version__ = ".".join(map(str, version_info))

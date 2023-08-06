94f81528e8a748870aa41f698a44ac3ddc603451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:08 +0000

    Add package flox/0.6.0/flox-0.6.0.tar.gz

---
+++ packages/flox/flox-0.6.0.tar.gz/flox-0.6.0/flox/cache.py
@@ -0,0 +1,11 @@
+from functools import partial
+
+try:
+    import cachey
+    import dask
+
+    # 1MB cache
+    cache = cachey.Cache(1e6)
+    memoize = partial(cache.memoize, key=dask.base.tokenize)
+except ImportError:
+    memoize = lambda x: x  # type: ignore

011688231c47418753454072853ed99dc8750ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:09 +0000

    Add package minimalkv/1.4.1/minimalkv-1.4.1.tar.gz

---
+++ packages/minimalkv/minimalkv-1.4.1.tar.gz/minimalkv-1.4.1/tests/test_readonly_decorator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf8
+
+import pytest
+
+from minimalkv.decorator import ReadOnlyDecorator
+from minimalkv.memory import DictStore
+
+
+class TestReadOnlyDecorator(object):
+    def test_readonly(self):
+        store0 = DictStore()
+        store0.put("file1", b"content")
+
+        store = ReadOnlyDecorator(store0)
+        with pytest.raises(AttributeError):
+            store.put("file1", b"content2")
+        with pytest.raises(AttributeError):
+            store.delete("file1")
+        assert store.get("file1") == b"content"
+        assert "file1" in store
+        assert set(store.keys()) == {"file1"}

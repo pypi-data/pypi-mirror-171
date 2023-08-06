d93c71c3df902a7d6f8ebf3f1555f96f62c77dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:15 +0000

    Add package mandarina/0.1.1/mandarina-0.1.1-py3-none-any.whl

---
+++ packages/mandarina/mandarina-0.1.1-py3-none-any.whl/mandarina/math.py
@@ -0,0 +1,10 @@
+"""
+This module provides various mathematical functions.
+"""
+
+def is_in_closed_interval(n, left, right):
+    """
+    Check if a number is within the closed interval
+    between left and right.
+    """
+    return left <= n <= right

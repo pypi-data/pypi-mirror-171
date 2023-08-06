fd2f7b7d29b35dbb9f7788ec1f0e4708f21687c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:27 +0000

    Add package ogo-utils/0.1.20/ogo-utils-0.1.20.tar.gz

---
+++ packages/ogo-utils/ogo-utils-0.1.20.tar.gz/ogo-utils-0.1.20/README.md
@@ -0,0 +1,14 @@
+# OGO Clients Utils | Python == 3.9.10
+
+> This project provides utilities for my daily use.
+
+## Installation
+
+```shell
+python3 -m pip install ogo-utils
+```
+
+## Set config
+```shell
+poetry config pypi-token.pypi pypi-token
+```
---
+++ packages/ogo-utils/ogo-utils-0.1.20.tar.gz/ogo-utils-0.1.20/ogoutils/diary.py
@@ -0,0 +1,51 @@
+import os
+from datetime import datetime
+from pathlib import Path
+
+from ogoutils.encryption import encryption
+from ogoutils.decryption import decryption
+
+pwd = Path.cwd()
+
+file_path = str(pwd / 'diary')
+crp_file_path = str(pwd / 'diary.crp')
+
+
+def read_diary():
+    password = input('Введите пароль: ')
+    res = decryption(file=crp_file_path, password=password)
+
+    if not res:
+        return read_diary()
+
+    with open(file_path, 'r') as f:
+        print(f.read())
+
+    encryption(file=file_path, password=password)
+
+
+def write_diary():
+    """ Write your thoughts into the diary file """
+    password = input('Введите пароль: ')
+
+    is_diary_exists = os.path.isfile(crp_file_path)
+
+    if is_diary_exists:
+        res = decryption(file=crp_file_path, password=password)
+        if not res:
+            return write_diary()
+
+    with open(file_path, 'a+') as f:
+        note = input('\nOGO-DIARY: Привет! Напиши о чем думаешь:\n>>> ')
+        f.write(f'{datetime.now().isoformat()}\n{note}\n\n')
+
+    with open(file_path, 'r') as f:
+        print('\nПоследние записи:')
+        print('\n', f.read())
+
+    encryption(file=file_path, password=password)
+
+
+if __name__ == "__main__":
+    write_diary()
+    # read_diary()

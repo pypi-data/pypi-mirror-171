6c2f67916450af24530f072860761e4e9141efcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:01 +0000

    Add package mgrc/1.0.3/mgrc-1.0.3.tar.gz

---
+++ packages/mgrc/mgrc-1.0.3.tar.gz/mgrc-1.0.3/src/mgrc/help.py
@@ -0,0 +1,22 @@
+#help.pypi
+#mgrc module name, help.py
+#mgrc includes this module, help.py
+#pypi package mgrc, version 1.0.3
+#mgrc 1.0.3
+#mgrc==1.0.3
+#mgrc 1.0.3, help.py
+#from mgrc import help
+t = '''#example
+#import time; run userFunction per duration (e.g., per second; per minute; per hour), module - t.py
+
+from mgrc import t
+
+#ds: duration seconds; function, def ds(sec):
+
+from t import ds
+
+#user assigns the value seconds to the function; user indicates qty of seconds duration, to run userFunction per sec for user-assigned duration, as:
+
+sec = "4"
+ds(sec)'''
+print(t)

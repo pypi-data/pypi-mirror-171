e3a90026fea5e2e7a240be1c176a39a0147d4c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:26 +0000

    Add package openmetadata-ingestion/0.12.1.3/openmetadata_ingestion-0.12.1.3-py3.9.egg

---
+++ packages/openmetadata-ingestion/openmetadata_ingestion-0.12.1.3-py3.9.egg/metadata/antlr/split_listener.py
@@ -0,0 +1,51 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""
+Helper class to handle FQN splitting logic
+"""
+from metadata.generated.antlr.EntityLinkListener import EntityLinkListener
+from metadata.generated.antlr.EntityLinkParser import EntityLinkParser
+from metadata.generated.antlr.FqnListener import FqnListener
+from metadata.generated.antlr.FqnParser import FqnParser
+
+
+class FqnSplitListener(FqnListener):
+    def __init__(self):
+        self.xs = []
+
+    def enterQuotedName(self, ctx: FqnParser.QuotedNameContext):
+        self.xs.append(ctx.getText())
+
+    def enterUnquotedName(self, ctx: FqnParser.UnquotedNameContext):
+        self.xs.append(ctx.getText())
+
+    def split(self):
+        return self.xs
+
+
+class EntityLinkSplitListener(EntityLinkListener):
+    def __init__(self):
+        self.xs = []
+
+    def enterEntityAttribute(self, ctx: EntityLinkParser.EntityAttributeContext):
+        self.xs.append(ctx.getText())
+
+    def enterEntityType(self, ctx: EntityLinkParser.EntityTypeContext):
+        self.xs.append(ctx.getText())
+
+    def enterEntityField(self, ctx: EntityLinkParser.EntityFieldContext):
+        self.xs.append(ctx.getText())
+
+    def enterEntityFqn(self, ctx: EntityLinkParser.EntityFqnContext):
+        self.xs.append(ctx.getText())
+
+    def split(self):
+        return self.xs
---
+++ packages/openmetadata-ingestion/openmetadata_ingestion-0.12.1.3-py3.9.egg/metadata/ingestion/sink/elasticsearch_mapping/glossary_term_search_index_mapping.py
@@ -0,0 +1,241 @@
+import textwrap
+
+GLOSSARY_TERM_ELASTICSEARCH_INDEX_MAPPING = textwrap.dedent(
+    """
+     {
+    "mappings":{
+          "properties": {
+            "id": {
+              "type": "text"
+            },
+            "name": {
+              "type":"text",
+              "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256
+                }
+              }
+            },
+            "fullyQualifiedName": {
+              "type":"text"
+            },
+            "displayName": {
+              "type": "text",
+              "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256
+                }
+              }
+            },
+            "description": {
+              "type": "text"
+            },
+            "version": {
+              "type": "float"
+            },
+            "updatedAt": {
+               "type": "date",
+              "format": "epoch_second"
+            },
+            "updatedBy": {
+               "type": "text"
+            },
+            "href": {
+              "type": "text"
+            },
+            "synonyms": {
+              "type": "text"
+            },
+             "glossary": {
+               "properties": {
+                "id": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 36
+                    }
+                  }
+                },
+                "type": {
+                  "type": "keyword"
+                },
+                "name": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                        "ignore_above": 256
+                    }
+                 }
+               },
+              "fullyQualifiedName": {
+                "type": "text"
+              },
+              "description": {
+                "type": "text"
+              },
+              "deleted": {
+               "type": "text"
+              },
+              "href": {
+               "type": "text"
+              }
+             }
+             },
+            "children": {
+              "properties": {
+                "id": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 36
+                    }
+                  }
+                },
+                "type": {
+                  "type": "keyword"
+                },
+                "name": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                        "ignore_above": 256
+                    }
+                 }
+               },
+               "fullyQualifiedName": {
+                "type": "text"
+              },
+              "description": {
+                "type": "text"
+              },
+              "deleted": {
+               "type": "text"
+              },
+              "href": {
+               "type": "text"
+              }
+             }
+            },
+            "relatedTerms": {
+              "properties": {
+                "id": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 36
+                    }
+                  }
+                },
+                "type": {
+                  "type": "keyword"
+                },
+                "name": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                        "ignore_above": 256
+                    }
+                 }
+               },
+               "fullyQualifiedName": {
+                "type": "text"
+              },
+              "description": {
+                "type": "text"
+              },
+              "deleted": {
+               "type": "text"
+              },
+              "href": {
+               "type": "text"
+              }
+             }
+            },
+             "reviewers": {
+              "properties": {
+                "id": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                        "type": "keyword",
+                        "ignore_above": 36
+                    }
+                  }
+                },
+                "type": {
+                  "type": "keyword"
+                },
+                "name": {
+                  "type": "keyword",
+                  "fields": {
+                    "keyword": {
+                      "type": "keyword",
+                        "ignore_above": 256
+                    }
+                 }
+               },
+               "fullyQualifiedName": {
+                "type": "text"
+              },
+              "description": {
+                "type": "text"
+              },
+              "deleted": {
+               "type": "text"
+              },
+              "href": {
+               "type": "text"
+              }
+             }
+            },
+            "usageCount": {
+               "type": "integer"
+             },
+             "tags": {
+              "properties": {
+                        "tagFQN": {
+                          "type": "keyword"
+                        },
+                        "labelType": {
+                          "type": "keyword"
+                        },
+                        "description": {
+                          "type": "text"
+                        },
+                        "source": {
+                          "type": "keyword"
+                        },
+                        "state": {
+                          "type": "keyword"
+                        }
+                      }
+            },
+            "deleted": {
+              "type": "text"
+            },
+            "status": {
+              "type": "text"
+            },
+            "suggest": {
+              "type": "completion",
+              "contexts": [
+                {
+                    "name": "deleted",
+                    "type": "category",
+                    "path": "deleted"
+                }
+             ]
+            }
+         }
+      }
+   }
+    """
+)

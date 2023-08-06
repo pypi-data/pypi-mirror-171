9148e52bb20814425df5f9b2918b2cda1ceb5528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:26 +0000

    Add package jurende-gongju/2022.10.15.0/jurende_gongju-2022.10.15.0-py3-none-any.whl

---
+++ packages/jurende-gongju/jurende_gongju-2022.10.15.0-py3-none-any.whl/JurendeGongju/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""jurende-gongju
+https://github.com/apachecn/jurende-gongju"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

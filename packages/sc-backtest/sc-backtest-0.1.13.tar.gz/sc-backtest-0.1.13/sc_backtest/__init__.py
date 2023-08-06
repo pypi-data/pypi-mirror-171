0f24732af5049b2f712803bdff062e03b16a059a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:20 +0000

    Add package sc-backtest/0.1.13/sc-backtest-0.1.13.tar.gz

---
+++ packages/sc-backtest/sc-backtest-0.1.13.tar.gz/sc-backtest-0.1.13/sc_backtest/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 13:22:34 2021
+
+@author: chang.sun
+"""
+
+import sc_backtest.ta_personal as ta2
+import sc_backtest.ta_personal3 as ta3
+import sc_backtest.technical_analysis as ta
+from .simpletest import simpletest
+import sc_backtest.backtest as bt
+import sc_backtest.dataset as dataset

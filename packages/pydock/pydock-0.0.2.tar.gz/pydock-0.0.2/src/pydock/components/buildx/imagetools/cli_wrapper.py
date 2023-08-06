7d48079518e0c70c0667b6742a979919eca4ea1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:57 +0000

    Add package pydock/0.0.2/pydock-0.0.2.tar.gz

---
+++ packages/pydock/pydock-0.0.2.tar.gz/pydock-0.0.2/src/pydock/components/buildx/imagetools/cli_wrapper.py
@@ -0,0 +1,11 @@
+from ....client_config import DockerCLICaller
+from ....utils import run
+from .models import Manifest
+
+
+class ImagetoolsCLI(DockerCLICaller):
+    def inspect(self, name: str) -> Manifest:
+        """Returns the manifest of a Docker image in a registry without pulling it"""
+        full_cmd = self.docker_cmd + ["buildx", "imagetools", "inspect", "--raw", name]
+        result = run(full_cmd)
+        return Manifest.parse_raw(result)

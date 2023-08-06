997607a58d42b8e121ffda93aadf4ba3a9fba634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:37 +0000

    Add package xf-auth/0.7.0/xf_auth-0.7.0.tar.gz

---
+++ packages/xf-auth/xf_auth-0.7.0.tar.gz/xf_auth-0.7.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+	name='xf_auth',
+	version='0.7.0',
+	description='A package to handle auth on flask. Made by Xatal',
+	url='https://github.com/Masakuata/xatal_flask_auth',
+	author='Edson Manuel Carballo Vera',
+	author_email='edsonmanuelcarballovera@gmail.com',
+	license='MIT License',
+	packages=['xf_auth'],
+	install_requires=[
+		'cryptography==3.1.1',
+		"Flask~=2.2.2",
+		"Werkzeug~=2.2.2",
+		"requests~=2.28.1",
+		"setuptools~=60.2.0",
+	],
+
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Framework :: Flask',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
+		'Programming Language :: Python :: 3.10',
+		'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+		'Topic :: Internet :: WWW/HTTP :: Session'
+	],
+)

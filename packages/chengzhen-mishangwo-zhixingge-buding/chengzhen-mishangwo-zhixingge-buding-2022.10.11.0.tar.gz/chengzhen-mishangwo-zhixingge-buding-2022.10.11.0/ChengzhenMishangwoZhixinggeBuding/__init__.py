a9504129afdc7fbea09724087351183e3a6068ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:40 +0000

    Add package chengzhen-mishangwo-zhixingge-buding/2022.10.11.0/chengzhen-mishangwo-zhixingge-buding-2022.10.11.0.tar.gz

---
+++ packages/chengzhen-mishangwo-zhixingge-buding/chengzhen-mishangwo-zhixingge-buding-2022.10.11.0.tar.gz/chengzhen-mishangwo-zhixingge-buding-2022.10.11.0/ChengzhenMishangwoZhixinggeBuding/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chengzhen-mishangwo-zhixingge-buding
+https://github.com/apachecn/chengzhen-mishangwo-zhixingge-buding"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

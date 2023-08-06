7ca477cb8b868488822f443b5838898f1eb62f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:45 +0000

    Add package solara/0.12.1/solara-0.12.1-py2.py3-none-any.whl

---
+++ packages/solara/solara-0.12.1-py2.py3-none-any.whl/solara/components/__init__.py
@@ -0,0 +1,21 @@
+# isort: skip_file
+from .misc import *  # noqa: #F401 F403
+from .datatable import DataTable, DataFrame  # noqa: #F401 F403
+from .details import Details  # noqa: #F401 F403
+from .file_browser import FileBrowser  # noqa: #F401 F403
+from .image import Image  # noqa: #F401 F403
+from .markdown import Markdown, MarkdownIt  # noqa: #F401 F403
+from .slider import DateSlider, FloatSlider, IntSlider, ValueSlider  # noqa: #F401 F403
+from .sql_code import SqlCode  # noqa: #F401 F403
+from .togglebuttons import (  # noqa: #F401 F403
+    ToggleButtonsMultiple,
+    ToggleButtonsSingle,
+)
+from .pivot_table import PivotTableView, PivotTable, PivotTableCard  # noqa: #F401 F403
+from .title import Title  # noqa: #F401 F403
+from .link import Link  # noqa: #F401 F403
+from .applayout import AppLayout  # noqa: #F401 F403
+from .tab_navigation import TabNavigation  # noqa: #F401 F403
+from .markdown_editor import MarkdownEditor  # noqa: #F401 F403
+from .select import Select, SelectMultiple  # noqa: #F401 F403
+from .matplotlib import FigureMatplotlib  # noqa: #F401 F403

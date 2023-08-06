cca3bf1a21b52fd0d9949b4faca4d4846a6cba8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:44 +0000

    Add package or-gausbindistributions/0.1/or_gausbindistributions-0.1.tar.gz

---
+++ packages/or-gausbindistributions/or_gausbindistributions-0.1.tar.gz/or_gausbindistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='or_gausbindistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['or_gausbindistributions'],
+      zip_safe=False)

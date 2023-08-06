3231f02558f34298bbee7b6e35d394517659529e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:08 +0000

    Add package durin/0.0.58/durin-0.0.58.tar.gz

---
+++ packages/durin/durin-0.0.58.tar.gz/durin-0.0.58/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("requirements.txt", "r") as fp:
+    requirements = fp.read().split("\n")
+
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name="durin",
+    version="0.0.58",
+    install_requires=requirements,
+    packages=["durin"],
+    license="LGPLv3",
+    maintainer="Jens E. Pedersen",
+    maintainer_email="jeped@kth.se",
+    extras_require={"aestream": ["aestream"]},
+    scripts=["bin/durin"],
+    description="Python control interface for the Durin robot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)

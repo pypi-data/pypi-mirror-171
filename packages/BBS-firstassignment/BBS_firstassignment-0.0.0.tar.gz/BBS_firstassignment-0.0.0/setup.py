f43e1f80efda5326c81e66dbaccccf5ecf65fa11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:05 +0000

    Add package BBS-firstassignment/0.0.0/BBS_firstassignment-0.0.0.tar.gz

---
+++ packages/BBS-firstassignment/BBS_firstassignment-0.0.0.tar.gz/BBS_firstassignment-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='BBS_firstassignment',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)

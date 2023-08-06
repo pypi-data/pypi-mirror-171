c3debb6e0f415b453e6887ad63778065bea6c954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:17 +0000

    Add package cbra/0.162.0/cbra-0.162.0.tar.gz

---
+++ packages/cbra/cbra-0.162.0.tar.gz/cbra-0.162.0/cbra/ext/oauth2/tests/authorization.py
@@ -0,0 +1,7 @@
+"""Declares :class:`AuthorizationEndpointTest`."""
+from .base import BaseOpenAuthorizationTest
+
+
+class AuthorizationEndpointTest(BaseOpenAuthorizationTest):
+    __module__: str = 'cbra.ext.oauth2.tests'
+    pass

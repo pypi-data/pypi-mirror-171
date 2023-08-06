6d65a234b7c65962e1f2bbdb1d262559a52e205e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:16 +0000

    Add package SigProfilerAssignment/0.0.14/SigProfilerAssignment-0.0.14-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.14-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.14'
+version = '0.0.14'
+Update = '1. Add support for multi-sample segmentation files. 2. Move decomposition plots to SigProfilerAssignment.'
+    
+    

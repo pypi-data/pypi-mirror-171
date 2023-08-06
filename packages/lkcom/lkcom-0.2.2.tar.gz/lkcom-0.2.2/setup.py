6dc63322b3f11ce9249d813df20c5483b7a5f4d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:05 +0000

    Add package lkcom/0.2.2/lkcom-0.2.2.tar.gz

---
+++ packages/lkcom/lkcom-0.2.2.tar.gz/lkcom-0.2.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lkcom",
+    version="0.2.2",
+    author="Lukas Kontenis",
+    author_email="dse.ssd@gmail.com",
+    description="A Python library of useful routines.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/lukaskontenis/lkcom/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'numpy', 'matplotlib>=2.1.0', 'Pillow>=8.0.1', 'imageio>=2.9.0'
+    ],
+    python_requires='>=3.6'
+)

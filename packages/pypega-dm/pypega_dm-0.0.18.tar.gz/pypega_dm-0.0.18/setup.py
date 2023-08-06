4c3e2af35a76d5b51cabe3745ddbfdb20009d732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:50 +0000

    Add package pypega-dm/0.0.18/pypega_dm-0.0.18.tar.gz

---
+++ packages/pypega-dm/pypega_dm-0.0.18.tar.gz/pypega_dm-0.0.18/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypega_dm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pypega>=0.0.47']
+
+setup_kwargs = {
+    'name': 'pypega-dm',
+    'version': '0.0.18',
+    'description': 'Python package for orchestrating Pega environments',
+    'long_description': '# Pega Deployment Manager (Python)\n\nThis is a simple example package. You can use\n[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)\nto write your content.',
+    'author': 'Rob Smart',
+    'author_email': 'rob.smart@pega.com',
+    'maintainer': 'Rob Smart',
+    'maintainer_email': 'rob.smart@pega.com',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)

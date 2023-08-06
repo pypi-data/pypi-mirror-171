5d0dffd1b7ebbf684ac7087ece9cee4830b5bb79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:44 +0000

    Add package congming-nanren-yaodu-nvren-xinlixue/2022.10.11.0/congming_nanren_yaodu_nvren_xinlixue-2022.10.11.0-py3-none-any.whl

---
+++ packages/congming-nanren-yaodu-nvren-xinlixue/congming_nanren_yaodu_nvren_xinlixue-2022.10.11.0-py3-none-any.whl/CongmingNanrenYaoduNvrenXinlixue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""congming-nanren-yaodu-nvren-xinlixue
+https://github.com/apachecn/congming-nanren-yaodu-nvren-xinlixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

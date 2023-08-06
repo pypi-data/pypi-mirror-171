f013812fb63407f4d187fd8292865b190b8e41c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package MysciPackTest/1.0.0/MysciPackTest-1.0.0.tar.gz

---
+++ packages/MysciPackTest/MysciPackTest-1.0.0.tar.gz/MysciPackTest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="MysciPackTest",
+    version="1.0.0",
+    description="A sample package description",
+    author="Mhammed Jeddou",
+    author_email="jeddou.ehtp@gmail.com",
+    packages=["mysci_01"],
+    install_requires=[])

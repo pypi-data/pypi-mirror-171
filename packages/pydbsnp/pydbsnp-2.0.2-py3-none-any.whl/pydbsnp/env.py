c934321d84dc44b5f0c1732d6ece3ca61437799a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:00 +0000

    Add package pydbsnp/2.0.2/pydbsnp-2.0.2-py3-none-any.whl

---
+++ packages/pydbsnp/pydbsnp-2.0.2-py3-none-any.whl/pydbsnp/env.py
@@ -0,0 +1,45 @@
+#===============================================================================
+# env.py
+#===============================================================================
+
+"""Handling environment variables for pydbsnp"""
+
+
+
+
+# Imports ======================================================================
+
+import os
+import os.path
+
+
+
+
+# Constants ====================================================================
+
+FTP_BASENAME_GRCH37 = 'GCF_000001405.25.gz'
+FTP_BASENAME_GRCH38 = 'GCF_000001405.39.gz'
+VCF_GRCH37 = os.environ.get(
+    'PYDBSNP_VCF_GRCH37', 
+    os.path.join(os.path.dirname(__file__), FTP_BASENAME_GRCH37)
+)
+VCF_GRCH38 = os.environ.get(
+    'PYDBSNP_VCF_GRCH38', 
+    os.path.join(os.path.dirname(__file__), FTP_BASENAME_GRCH38)
+)
+BUILD_TO_VCF = {
+    'hg19': VCF_GRCH37, 'GRCh37': VCF_GRCH37,
+    'hg38': VCF_GRCH38, 'GRCh38': VCF_GRCH38
+}
+RSID_GRCH37 = os.environ.get(
+    'PYDBSNP_RSID_GRCH37', 
+    os.path.join(os.path.dirname(__file__), 'GCF_000001405.25.rsid.gz')
+)
+RSID_GRCH38 = os.environ.get(
+    'PYDBSNP_RSID_GRCH38', 
+    os.path.join(os.path.dirname(__file__), 'GCF_000001405.39.rsid.gz')
+)
+BUILD_TO_RSID = {
+    'hg19': RSID_GRCH37, 'GRCh37': RSID_GRCH37,
+    'hg38': RSID_GRCH38, 'GRCh38': RSID_GRCH38
+}

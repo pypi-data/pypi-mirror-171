505f368967eb154f5a2f52799b57d3212ed7f322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:39 +0000

    Add package aiqingguangpu-liaotian-saocaozuo/2022.10.11.0/aiqingguangpu-liaotian-saocaozuo-2022.10.11.0.tar.gz

---
+++ packages/aiqingguangpu-liaotian-saocaozuo/aiqingguangpu-liaotian-saocaozuo-2022.10.11.0.tar.gz/aiqingguangpu-liaotian-saocaozuo-2022.10.11.0/AiqingguangpuLiaotianSaocaozuo/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""aiqingguangpu-liaotian-saocaozuo
+https://github.com/apachecn/aiqingguangpu-liaotian-saocaozuo"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

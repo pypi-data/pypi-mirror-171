362f8dcf1bc05dfd74362145bf94faa30a6f1e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:01 +0000

    Add package openbabel-wheel/3.1.1.10/openbabel-wheel-3.1.1.10.tar.gz

---
+++ packages/openbabel-wheel/openbabel-wheel-3.1.1.10.tar.gz/openbabel-wheel-3.1.1.10/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+from pathlib import Path
+
+from cmake_build_extension import BuildExtension, CMakeExtension
+from setuptools import setup
+from distutils import sysconfig
+
+setup(
+    ext_modules=[
+        CMakeExtension(name="OpenBabel",
+                       install_prefix="openbabel",
+                       source_dir=str(Path(__file__).parent.absolute() / "source"),
+                       cmake_configure_options=[
+                            "-DPYTHON_EXECUTABLE=" + sys.executable,
+                            "-DCMAKE_BUILD_TYPE=Release",
+                            "-DWITH_INCHI=ON",
+                            "-DPYTHON_BINDINGS=ON",
+                            "-DRUN_SWIG=ON",
+                            "-DPYTHON_INCLUDE_PATH="+sysconfig.get_config_var('INCLUDEPY'),
+                            "-DPYTHON_INCLUDE_DIR="+sysconfig.get_config_var('INCLUDEPY'),
+                            "-DBUILD_BY_PIP=ON",
+                       ]),
+    ],
+    cmdclass=dict(build_ext=BuildExtension),
+)

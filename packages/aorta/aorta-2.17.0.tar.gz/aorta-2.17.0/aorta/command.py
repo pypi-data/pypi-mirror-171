9183a5540c223a741ad1ea491b8f77b61d7fd607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:30 +0000

    Add package aorta/2.17.0/aorta-2.17.0.tar.gz

---
+++ packages/aorta/aorta-2.17.0.tar.gz/aorta-2.17.0/aorta/command.py
@@ -0,0 +1,13 @@
+"""Declares :class:`Command`."""
+from .basemessage import BaseMessage
+from .messagemetaclass import MessageMetaclass
+
+
+class CommandMetaclass(MessageMetaclass):
+    envelope_field: str = 'spec'
+    message_type: str = 'unimatrixone.io/command'
+
+
+class Command(BaseMessage, metaclass=CommandMetaclass):
+    """The parameters for a :term:`Command`."""
+    __abstract__: bool = True

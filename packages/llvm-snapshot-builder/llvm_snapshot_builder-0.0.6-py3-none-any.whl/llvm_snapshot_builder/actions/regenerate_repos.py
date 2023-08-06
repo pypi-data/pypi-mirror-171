d1d71becbc53c0a6d29466742b55cc3a675ac38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:50 +0000

    Add package llvm-snapshot-builder/0.0.6/llvm_snapshot_builder-0.0.6-py3-none-any.whl

---
+++ packages/llvm-snapshot-builder/llvm_snapshot_builder-0.0.6-py3-none-any.whl/llvm_snapshot_builder/actions/regenerate_repos.py
@@ -0,0 +1,26 @@
+"""
+CoprActionRegenerateRepos
+"""
+
+from ..mixins.build_walker_mixin import CoprBuildWalkerMixin
+from ..mixins.client_mixin import CoprClientMixin
+from ..copr_project_ref import CoprProjectRef
+from .action import CoprAction
+
+
+class CoprActionRegenerateRepos(CoprClientMixin, CoprAction):
+    """
+    Regenerates the repositories for the given project.
+    NOTE: The regeneration of repository data is not finished when this function returns.
+    """
+
+    def __init__(self, proj: CoprProjectRef, ** kwargs):
+        """ Initializes the action. """
+        self.__proj = proj
+        super().__init__(**kwargs)
+
+    def run(self) -> bool:
+        """ Runs the action. """
+        self.client.project_proxy.regenerate_repos(
+            ownername=self.__proj.owner, projectname=self.__proj.name)
+        return True

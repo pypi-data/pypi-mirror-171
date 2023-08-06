d144a091b0e200dcc28c2047928c96e4034047e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:49 +0000

    Add package hmile/0.2.1/hmile-0.2.1.tar.gz

---
+++ packages/hmile/hmile-0.2.1.tar.gz/hmile-0.2.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='hmile',
+    version='0.2.1',
+    author='theophanedroid',
+    description='Python 3.x module to render financial results in tensorboard ',
+    packages=['Hmile',],
+    license='MIT license',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "mplfinance",
+        "ta",
+        "pandas",
+        "elasticsearch",
+        "torch",
+        "numpy",
+        "tensorflow",
+        "tensorboard",
+        "yfinance",
+    ]
+)

93abe304acc9d11c9b455568f5b4d76a1bd93758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:03 +0000

    Add package mingwangxing-linghunde-yanhua-zhilv/2022.10.14.0/mingwangxing_linghunde_yanhua_zhilv-2022.10.14.0-py3-none-any.whl

---
+++ packages/mingwangxing-linghunde-yanhua-zhilv/mingwangxing_linghunde_yanhua_zhilv-2022.10.14.0-py3-none-any.whl/MingwangxingLinghundeYanhuaZhilv/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mingwangxing-linghunde-yanhua-zhilv
+https://github.com/apachecn/mingwangxing-linghunde-yanhua-zhilv"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.14.0"

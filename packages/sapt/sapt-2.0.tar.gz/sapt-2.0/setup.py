6b12890ff92f24ed8bfe7835baa6cb595fe82252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:20 +0000

    Add package sapt/2.0/sapt-2.0.tar.gz

---
+++ packages/sapt/sapt-2.0.tar.gz/sapt-2.0/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from codecs import open
+from os import path
+import re
+
+package_name = "sapt"
+
+setup(
+    name=package_name,
+
+    version="2.0",
+
+    license="MIT License",
+
+    entry_points={
+        "console_scripts": [
+            "sapt = appUtil:sapt"
+        ]
+    }
+)

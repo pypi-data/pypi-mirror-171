7a03e4486d040941e5df5f252bba5fbcd2c1289a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:44 +0000

    Add package dialogbot/0.1.0/dialogbot-0.1.0.tar.gz

---
+++ packages/dialogbot/dialogbot-0.1.0.tar.gz/dialogbot-0.1.0/dialogbot/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+
+from .bot import Bot
+from .kg.bot import KGBot
+from .search.searchbot import SearchBot
+from .gpt.gptbot import GPTBot
+
+
+__version__ = "0.1.0"

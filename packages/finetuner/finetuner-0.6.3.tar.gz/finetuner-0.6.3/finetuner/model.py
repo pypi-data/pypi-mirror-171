2bcebb4a7cfec1c858e932f8a269579fb796d47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:41 +0000

    Add package finetuner/0.6.3/finetuner-0.6.3.tar.gz

---
+++ packages/finetuner/finetuner-0.6.3.tar.gz/finetuner-0.6.3/finetuner/model.py
@@ -0,0 +1,29 @@
+import inspect
+
+from stubs import model
+from stubs.model import *  # noqa F401
+
+
+def get_header() -> Tuple[str, ...]:
+    """Get table header."""
+    return 'name', 'task', 'output_dim', 'architecture', 'description'
+
+
+def get_row(model_stub) -> Tuple[str, ...]:
+    """Get table row."""
+    return (
+        model_stub.descriptor,
+        model_stub.task,
+        str(model_stub.output_shape[1]),
+        model_stub.architecture,
+        model_stub.description,
+    )
+
+
+def list_model_classes() -> Dict[str, ModelStubType]:
+    rv = {}
+    members = inspect.getmembers(model, inspect.isclass)
+    for name, stub in members:
+        if name != 'MLPStub' and not name.startswith('_') and type(stub) != type:
+            rv[name] = stub
+    return rv

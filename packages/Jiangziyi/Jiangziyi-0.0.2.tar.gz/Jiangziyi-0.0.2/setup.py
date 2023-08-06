01d84b4d2688800dba0e99db4494ffa15ca7e209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:33 +0000

    Add package Jiangziyi/0.0.2/Jiangziyi-0.0.2.tar.gz

---
+++ packages/Jiangziyi/Jiangziyi-0.0.2.tar.gz/Jiangziyi-0.0.2/setup.py
@@ -0,0 +1,51 @@
+'''
+Author: Vincent Young
+Date: 2022-07-24 04:27:43
+LastEditors: Vincent Young
+LastEditTime: 2022-10-13 22:05:02
+FilePath: /Jiangziyi/setup.py
+Telegram: https://t.me/missuo
+
+Copyright © 2022 by Vincent, All Rights Reserved. 
+'''
+from setuptools import setup, find_packages
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Jiangziyi",
+    author="missuo",
+    version="0.0.2",
+    license='MIT',
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    author_email="i@yyt.moe",
+    description="For Angela Jiang",
+    url='https://github.com/missuo/Jiangziyi',
+    packages=find_packages(),
+    include_package_data=False,
+    platforms='any',
+    zip_safe=False,
+
+    install_requires=[
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+
+)

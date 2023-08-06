a965b78bba46fe0c18006c8afa0fc06949f6bb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:27 +0000

    Add package nianlide-mimi-vol3/2022.10.14.0/nianlide-mimi-vol3-2022.10.14.0.tar.gz

---
+++ packages/nianlide-mimi-vol3/nianlide-mimi-vol3-2022.10.14.0.tar.gz/nianlide-mimi-vol3-2022.10.14.0/NianlideMimiVol3/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nianlide-mimi-vol3
+https://github.com/apachecn/nianlide-mimi-vol3"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.14.0"

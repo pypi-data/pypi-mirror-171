aaeefbd36f4a46a3c41e523faa37c62e66a84290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:58 +0000

    Add package marvel-case-aaw/0.1.0/marvel_case_aaw-0.1.0.tar.gz

---
+++ packages/marvel-case-aaw/marvel_case_aaw-0.1.0.tar.gz/marvel_case_aaw-0.1.0/README.md
@@ -0,0 +1,1 @@
+# Marvel_case_study
---
+++ packages/marvel-case-aaw/marvel_case_aaw-0.1.0.tar.gz/marvel_case_aaw-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='marvel_case_aaw',
+    version='0.1.0',    
+    description='Marvel case study',
+    url='',
+    author='Aaradhya Wadhwa',
+    author_email='aaradhya7101@gmail.com',
+    license='BSD 2-clause',
+    packages=['mypckg'],
+    install_requires=[
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.9',
+    ],
+)

75d6fb68cefd35107403c2803fced4605a0c59f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:47 +0000

    Add package FiNoodle/0.1.5/FiNoodle-0.1.5.tar.gz

---
+++ packages/FiNoodle/FiNoodle-0.1.5.tar.gz/FiNoodle-0.1.5/src/FiNoodle/main.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from moodle_compile.setup import importQuestions
+from datetime import datetime
+
+def main(root = Path("questions"), globPattern = {
+        "exportGlob": "",
+        "blackListGlob": "None",
+        "IterateChildren": False
+    }):
+    print("============")
+    print("Finale 0.1.5")
+    print("============")
+    print()
+
+    print("[{}] Starting Finale".format(datetime.now().strftime("%H:%M:%S")))
+
+    #targeted export by glob & exclusion by glob
+    #export Children decides whether or not you want to export 
+    #children of glob pattern category
+    #make sure blackListGlob isn't accidentally blacklisting everything
+    #use Null to match with nothing and "" to match with everything
+    xml = importQuestions(root, globPattern)
+    with open("out.xml", "w") as f:
+        f.write(xml)
+
+if __name__ == '__main__':
+    main()

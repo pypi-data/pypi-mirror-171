f5a774817fefbd76a2b80c24ed8255fd7d9be1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:09 +0000

    Add package windmill-api/1.38.3/windmill_api-1.38.3-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.38.3-py3-none-any.whl/windmill_api/models/completed_job_flow_status_failure_module.py
@@ -0,0 +1,111 @@
+from typing import Any, Dict, List, Type, TypeVar, Union, cast
+
+import attr
+
+from ..models.completed_job_flow_status_failure_module_branch_chosen import (
+    CompletedJobFlowStatusFailureModuleBranchChosen,
+)
+from ..models.completed_job_flow_status_failure_module_iterator import CompletedJobFlowStatusFailureModuleIterator
+from ..models.completed_job_flow_status_failure_module_type import CompletedJobFlowStatusFailureModuleType
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="CompletedJobFlowStatusFailureModule")
+
+
+@attr.s(auto_attribs=True)
+class CompletedJobFlowStatusFailureModule:
+    """ """
+
+    type: CompletedJobFlowStatusFailureModuleType
+    job: Union[Unset, str] = UNSET
+    count: Union[Unset, int] = UNSET
+    iterator: Union[Unset, CompletedJobFlowStatusFailureModuleIterator] = UNSET
+    forloop_jobs: Union[Unset, List[str]] = UNSET
+    branch_chosen: Union[Unset, CompletedJobFlowStatusFailureModuleBranchChosen] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        type = self.type.value
+
+        job = self.job
+        count = self.count
+        iterator: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.iterator, Unset):
+            iterator = self.iterator.to_dict()
+
+        forloop_jobs: Union[Unset, List[str]] = UNSET
+        if not isinstance(self.forloop_jobs, Unset):
+            forloop_jobs = self.forloop_jobs
+
+        branch_chosen: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.branch_chosen, Unset):
+            branch_chosen = self.branch_chosen.to_dict()
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "type": type,
+            }
+        )
+        if job is not UNSET:
+            field_dict["job"] = job
+        if count is not UNSET:
+            field_dict["count"] = count
+        if iterator is not UNSET:
+            field_dict["iterator"] = iterator
+        if forloop_jobs is not UNSET:
+            field_dict["forloop_jobs"] = forloop_jobs
+        if branch_chosen is not UNSET:
+            field_dict["branch_chosen"] = branch_chosen
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        type = CompletedJobFlowStatusFailureModuleType(d.pop("type"))
+
+        job = d.pop("job", UNSET)
+
+        count = d.pop("count", UNSET)
+
+        iterator: Union[Unset, CompletedJobFlowStatusFailureModuleIterator] = UNSET
+        _iterator = d.pop("iterator", UNSET)
+        if not isinstance(_iterator, Unset):
+            iterator = CompletedJobFlowStatusFailureModuleIterator.from_dict(_iterator)
+
+        forloop_jobs = cast(List[str], d.pop("forloop_jobs", UNSET))
+
+        branch_chosen: Union[Unset, CompletedJobFlowStatusFailureModuleBranchChosen] = UNSET
+        _branch_chosen = d.pop("branch_chosen", UNSET)
+        if not isinstance(_branch_chosen, Unset):
+            branch_chosen = CompletedJobFlowStatusFailureModuleBranchChosen.from_dict(_branch_chosen)
+
+        completed_job_flow_status_failure_module = cls(
+            type=type,
+            job=job,
+            count=count,
+            iterator=iterator,
+            forloop_jobs=forloop_jobs,
+            branch_chosen=branch_chosen,
+        )
+
+        completed_job_flow_status_failure_module.additional_properties = d
+        return completed_job_flow_status_failure_module
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties

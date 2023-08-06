d6d3807b04b0a823ed5ca7a02a927ef03bcd2553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:41 +0000

    Add package gtja-vintex-qyt/0.0.4/gtja_vintex_qyt-0.0.4-py3-none-any.whl

---
+++ packages/gtja-vintex-qyt/gtja_vintex_qyt-0.0.4-py3-none-any.whl/gtja_vintex_qyt/main.py
@@ -0,0 +1,6 @@
+from gtja_vintex_qyt import GTJAVintexQyt
+
+if __name__ == '__main__':
+    vintex_qyt_client = GTJAVintexQyt("vintex_username", "vintex_password")
+    vintex_qyt_client.qc0020()
+    vintex_qyt_client.qc0021()

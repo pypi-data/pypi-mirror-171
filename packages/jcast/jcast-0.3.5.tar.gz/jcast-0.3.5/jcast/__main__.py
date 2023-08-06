6c3088d905592679968c528c84c4c13a7e9ad4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:04 +0000

    Add package jcast/0.3.5/jcast-0.3.5.tar.gz

---
+++ packages/jcast/jcast-0.3.5.tar.gz/jcast-0.3.5/jcast/__main__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""jcast.__main__: executed when jcast directory is called as script."""
+
+from jcast.main import main
+
+main()
---
+++ packages/jcast/jcast-0.3.5.tar.gz/jcast-0.3.5/tests/test_translate.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+""" Tests for sequence translation related functionalities """
+
+
+import unittest
+import jcast.helpers as h
+
+class GenomeTest(unittest.TestCase):
+    """
+    Test cases involving reading genome sequence and references
+    """
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_translate_cdna(self):
+        """ test for the make_pep function """
+
+        # Insulin 4 transcript protein coding region and protein sequences downloaded
+        # http://uswest.ensembl.org/Homo_sapiens/Transcript/Sequence_Protein?db=core;g=ENSG00000120211;r=9:5231419-5235304;t=ENST00000239316
+        ins4_cdna = ('ATGGCCAGCCTGTTCCGGTCCTATCTGCCAGCAATCTGGCTGCTGCTGAGCCAACTCCTT'
+                     'AGAGAAAGCCTAGCAGCAGAGCTGAGGGGATGTGGTCCCCGATTTGGAAAACACTTGCTG'
+                     'TCATATTGCCCCATGCCTGAGAAGACATTCACCACCACCCCAGGAGGGTGGCTGCTGGAA'
+                     'TCTGGACGTCCCAAAGAAATGGTGTCAACCTCCAACAACAAAGATGGACAAGCCTTAGGT'
+                     'ACGACATCAGAATTCATTCCTAATTTGTCACCAGAGCTGAAGAAACCACTGTCTGAAGGG'
+                     'CAGCCATCATTGAAGAAAATAATACTTTCCCGCAAAAAGAGAAGTGGACGTCACAGATTT'
+                     'GATCCATTCTGTTGTGAAGTAATTTGTGACGATGGAACTTCAGTTAAATTATGTACATAG')
+
+        ins4_protein = ('MASLFRSYLPAIWLLLSQLLRESLAAELRGCGPRFGKHLLSYCPMPEKTFTTTPGGWLLE'
+                        'SGRPKEMVSTSNNKDGQALGTTSEFIPNLSPELKKPLSEGQPSLKKIILSRKKRSGRHRF'
+                        'DPFCCEVICDDGTSVKLCT')
+
+        self.assertEqual(h.make_pep(ins4_cdna, strand='+', phase=0, terminate=True),
+                         ins4_protein)
+
+        # TODO: translate all phases/strands
+
+        pass

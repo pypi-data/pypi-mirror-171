6eeba193ea27381f30e79377e107971da88c2d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:00 +0000

    Add package functions-by-krystsinali/0.3.0/functions_by_krystsinali-0.3.0.tar.gz

---
+++ packages/functions-by-krystsinali/functions_by_krystsinali-0.3.0.tar.gz/functions_by_krystsinali-0.3.0/README.md
@@ -0,0 +1,14 @@
+# Instructions
+
+#### This is our test project. 
+
+#### Please install this package
+```
+pip install functions-by-krystsinali==0.2.0
+```
+
+
+#### You can also install older packages
+```
+pip install functions-by-krystsinali==VERSION_NUMBER
+```
---
+++ packages/functions-by-krystsinali/functions_by_krystsinali-0.3.0.tar.gz/functions_by_krystsinali-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_krystsinali']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-krystsinali',
+    'version': '0.3.0',
+    'description': 'This is our test project',
+    'long_description': '# Instructions\n\n#### This is our test project. \n\n#### Please install this package\n```\npip install functions-by-krystsinali==0.2.0\n```\n\n\n#### You can also install older packages\n```\npip install functions-by-krystsinali==VERSION_NUMBER\n```',
+    'author': 'Krystsina Liaudanskaya',
+    'author_email': 'krystsinali@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

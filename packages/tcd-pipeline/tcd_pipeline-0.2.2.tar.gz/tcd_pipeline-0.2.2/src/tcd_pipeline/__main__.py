6476c47ae982d283e193d4b80ef529cb446c7e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package tcd-pipeline/0.2.2/tcd_pipeline-0.2.2.tar.gz

---
+++ packages/tcd-pipeline/tcd_pipeline-0.2.2.tar.gz/tcd_pipeline-0.2.2/src/tcd_pipeline/__main__.py
@@ -0,0 +1,18 @@
+"""Main CLI application"""
+from importlib.metadata import version
+
+import click
+
+from tcd_pipeline.commands import generate_workflows
+
+
+@click.group(help="TCD Pipeline tools")
+@click.version_option(version("tcd-pipeline"))
+def cli():
+    """Main cli function"""
+
+
+cli.add_command(generate_workflows.generate_workflows)
+
+if __name__ == '__main__':
+    cli()

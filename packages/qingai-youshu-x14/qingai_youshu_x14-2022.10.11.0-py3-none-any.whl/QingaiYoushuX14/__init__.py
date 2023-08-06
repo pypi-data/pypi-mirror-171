fff3aeafbe123ef8e0b96200ea8f8a26ed3aafcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:28 +0000

    Add package qingai-youshu-x14/2022.10.11.0/qingai_youshu_x14-2022.10.11.0-py3-none-any.whl

---
+++ packages/qingai-youshu-x14/qingai_youshu_x14-2022.10.11.0-py3-none-any.whl/QingaiYoushuX14/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qingai-youshu-x14
+https://github.com/apachecn/qingai-youshu-x14"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

ad8585b5a30558050cfe41fb466e566bfccb94a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:54 +0000

    Add package molano/0.12.0/molano-0.12.0.tar.gz

---
+++ packages/molano/molano-0.12.0.tar.gz/molano-0.12.0/molano/runtime/ioc.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+# 
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+from typing import Any
+
+
+GLOBAL_DEPENDENCIES: list[dict[str, Any]] = [
+    {
+        'kind': 'symbol',
+        'spec': {
+            'name': 'PicqerRepository',
+            'qualname': 'molano.infra.PicqerRepository'
+        }
+    }
+]

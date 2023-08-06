9c7db9959170e70440dd543d1473892aa47a9336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:23 +0000

    Add package ticguide/0.4.1/ticguide-0.4.1.tar.gz

---
+++ packages/ticguide/ticguide-0.4.1.tar.gz/ticguide-0.4.1/ticguide/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['cli', 'pipeline']
+__author__ = 'Ashley <ashleychontos@astro.princeton.edu>'
+
+from .version import __version__
+
+_ROOT = os.path.abspath(os.getcwd())
+PATH = os.path.join(_ROOT,'')

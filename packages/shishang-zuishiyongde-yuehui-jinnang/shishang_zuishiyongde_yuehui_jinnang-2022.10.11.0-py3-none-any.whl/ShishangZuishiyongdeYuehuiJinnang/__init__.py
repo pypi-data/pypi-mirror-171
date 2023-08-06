e0fda9fef40de9185c9e6d42d0ef0629fbc5e262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:13 +0000

    Add package shishang-zuishiyongde-yuehui-jinnang/2022.10.11.0/shishang_zuishiyongde_yuehui_jinnang-2022.10.11.0-py3-none-any.whl

---
+++ packages/shishang-zuishiyongde-yuehui-jinnang/shishang_zuishiyongde_yuehui_jinnang-2022.10.11.0-py3-none-any.whl/ShishangZuishiyongdeYuehuiJinnang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shishang-zuishiyongde-yuehui-jinnang
+https://github.com/apachecn/shishang-zuishiyongde-yuehui-jinnang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

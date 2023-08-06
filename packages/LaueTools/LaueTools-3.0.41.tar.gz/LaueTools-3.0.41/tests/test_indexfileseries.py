23c400d7c190c5612517b39409d97fac046cb905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:21 +0000

    Add package LaueTools/3.0.41/LaueTools-3.0.41.tar.gz

---
+++ packages/LaueTools/LaueTools-3.0.41.tar.gz/LaueTools-3.0.41/tests/test_indexfileseries.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+## as of now only test imports
+## testing of GUI to be done after installation manually with example case
+# import pytest
+# if __name__ == '__main__':     #enclosing required because of multiprocessing
+
+import LaueTools
+print("LaueTools passed import")
+
+
+import pkg_resources

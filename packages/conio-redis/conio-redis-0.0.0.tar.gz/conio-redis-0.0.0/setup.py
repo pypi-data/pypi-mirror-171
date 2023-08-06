b9c9e3731265df7b4e8f0b89eeb8c5f32dfdb8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:24 +0000

    Add package conio-redis/0.0.0/conio-redis-0.0.0.tar.gz

---
+++ packages/conio-redis/conio-redis-0.0.0.tar.gz/conio-redis-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='conio-redis',
+      version='0.0.0',
+      description='Foo',
+      url='https://squadrone@bitbucket.org/bar/foo.git',
+      author='Squadrone',
+      author_email='foo@bar.com',
+      packages=['foo'],
+      zip_safe=False)

f0dbed3f3847812baaf4b367d2f736f53f385382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:56 +0000

    Add package functions-by-nizamimeydan/0.2.0/functions_by_nizamimeydan-0.2.0-py3-none-any.whl

---
+++ packages/functions-by-nizamimeydan/functions_by_nizamimeydan-0.2.0-py3-none-any.whl/functions_by_nizamimeydan/math_functions.py
@@ -0,0 +1,11 @@
+def multiplication(NUMBER1,NUMBER2):
+    return(NUMBER1*NUMBER2)
+
+def division(NUMBER1,NUMBER2):
+    return(NUMBER1/NUMBER2)
+
+def addition(NUMBER1,NUMBER2):
+    return(NUMBER1+NUMBER2)
+
+def substraction(NUMBER1,NUMBER2):
+    return(NUMBER1-NUMBER2)

b1d2774df688f11d8783106db378f611e42d1f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:49 +0000

    Add package giteasy/7.2.1/giteasy-7.2.1.tar.gz

---
+++ packages/giteasy/giteasy-7.2.1.tar.gz/giteasy-7.2.1/tests/t_github.py
@@ -0,0 +1,36 @@
+"""
+
+  """
+
+from pathlib import Path
+
+from src.giteasy.githubb import add_overwrite_a_file_2_repo
+from src.giteasy.githubb import ret_fps_and_pygithub_repo_obj
+from src.giteasy.githubb import ret_pygithub_repo_obj
+
+
+##
+rp = 'https://github.com/imahdimir/td-u-d0-FirmTicker-MonthlySales'
+ob = ret_pygithub_repo_obj(rp)
+fp = Path('/Users/mahdi/Downloads/forest-doc.pdf')
+
+with open(fp , 'rb') as f :
+    content = f.read()
+
+ob.create_file(fp.name , 'test commit' , content)
+
+##
+rp = 'https://github.com/imahdimir/td-u-d0-FirmTicker-MonthlySales'
+fp = Path('/Users/mahdi/Downloads/forest-doc.pdf')
+add_overwrite_a_file_2_repo(fp , rp)
+
+##
+
+rp = 'https://github.com/imahdimir/td-u-d0-FirmTicker-MonthlySales'
+dp = '/Users/mahdi/Dropbox/1-git-dirs/PyCharm/u-d0-FirmTicker-MonthlySales/link-htmls'
+diir = Path(dp)
+sf = 'html'
+fu = ret_fps_and_pygithub_repo_obj
+fu(diir , sf , rp)
+
+##

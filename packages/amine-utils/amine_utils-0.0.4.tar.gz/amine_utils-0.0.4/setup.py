5b95c154899b6a21513f12bbf8ae45854721fb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:43 +0000

    Add package amine-utils/0.0.4/amine_utils-0.0.4.tar.gz

---
+++ packages/amine-utils/amine_utils-0.0.4.tar.gz/amine_utils-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = "This package consists of some utilities I needed but could not find anywhere else."
+
+# Setting up
+setup(
+    name="amine_utils",
+    version=VERSION,
+    author="Amine ElQaraoui",
+    author_email="<aelqaraoui101@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', 'utilities'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

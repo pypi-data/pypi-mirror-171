179306619f80a8662cf24438b950452c4678cbba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:26 +0000

    Add package pymino/0.3.7/pymino-0.3.7-py3-none-any.whl

---
+++ packages/pymino/pymino-0.3.7-py3-none-any.whl/pymino/ext/_global.py
@@ -0,0 +1,8 @@
+from .utilities.generate import *
+
+class Global:
+    def __init__(self, session: Session):
+        """
+        WIP - TO BE FINISHED LATER
+        """
+        self.session = session

375991473125f87ee46033b08a89d8ed05cdfb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:47 +0000

    Add package functions-by-aselya2507/0.2.0/functions_by_aselya2507-0.2.0.tar.gz

---
+++ packages/functions-by-aselya2507/functions_by_aselya2507-0.2.0.tar.gz/functions_by_aselya2507-0.2.0/functions_by_aselya2507/README.md
@@ -0,0 +1,12 @@
+# instructions
+
+#### This is our project.
+
+#### Please install this package
+        
+pip install functions-by-aselya2507
+
+#### You can also install older package
+```
+
+```
---
+++ packages/functions-by-aselya2507/functions_by_aselya2507-0.2.0.tar.gz/functions_by_aselya2507-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_aselya2507']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-aselya2507',
+    'version': '0.2.0',
+    'description': 'this is our test project',
+    'long_description': '',
+    'author': 'Assel Agaidarova',
+    'author_email': 'aselya25789@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

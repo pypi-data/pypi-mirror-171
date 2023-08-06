3221af05b5a5d9bc5c320f1bc5f93ab6e62f3add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:34 +0000

    Add package zhanxing-wanjia-shouce/2022.10.14.0/zhanxing_wanjia_shouce-2022.10.14.0-py3-none-any.whl

---
+++ packages/zhanxing-wanjia-shouce/zhanxing_wanjia_shouce-2022.10.14.0-py3-none-any.whl/ZhanxingWanjiaShouce/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhanxing-wanjia-shouce
+https://github.com/apachecn/zhanxing-wanjia-shouce"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.14.0"

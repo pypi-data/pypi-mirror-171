eabaf1c5bf605b748c61783a8eb6d54a1827e752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:46 +0000

    Add package starlite/1.27.0/starlite-1.27.0-py3-none-any.whl

---
+++ packages/starlite/starlite-1.27.0-py3-none-any.whl/starlite/logging/picologging.py
@@ -0,0 +1,35 @@
+import atexit
+from io import StringIO
+from logging import StreamHandler
+from queue import Queue
+from typing import Any, List, Optional
+
+from starlite.exceptions import MissingDependencyException
+from starlite.logging.utils import resolve_handlers
+
+try:
+    from picologging.handlers import QueueHandler, QueueListener
+except ImportError as e:
+    raise MissingDependencyException("picologging is not installed") from e
+
+
+class QueueListenerHandler(QueueHandler):  # type: ignore[misc]
+    def __init__(self, handlers: Optional[List[Any]] = None) -> None:
+        """Configures queue listener and handler to support non-blocking
+        logging configuration.
+
+        Args:
+            handlers: Optional 'ConvertingList'
+
+        Notes:
+            - Requires `picologging` to be installed.
+        """
+        super().__init__(Queue(-1))
+        if handlers:
+            handlers = resolve_handlers(handlers)
+        else:
+            handlers = [StreamHandler(StringIO())]
+        self.listener = QueueListener(self.queue, *handlers)
+        self.listener.start()
+
+        atexit.register(self.listener.stop)

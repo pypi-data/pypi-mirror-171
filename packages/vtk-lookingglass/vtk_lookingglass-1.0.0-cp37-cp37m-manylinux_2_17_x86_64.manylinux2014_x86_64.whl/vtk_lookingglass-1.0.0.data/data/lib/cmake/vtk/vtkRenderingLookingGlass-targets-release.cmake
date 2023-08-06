74454f571ef3d40616f057486ec9643ac86cd856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:38 +0000

    Add package vtk-lookingglass/1.0.0/vtk_lookingglass-1.0.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vtk-lookingglass/vtk_lookingglass-1.0.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vtk_lookingglass-1.0.0.data/data/lib/cmake/vtk/vtkRenderingLookingGlass-targets-release.cmake
@@ -0,0 +1,20 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "VTK::RenderingLookingGlass" for configuration "Release"
+set_property(TARGET VTK::RenderingLookingGlass APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(VTK::RenderingLookingGlass PROPERTIES
+  IMPORTED_LINK_DEPENDENT_LIBRARIES_RELEASE "VTK::IOImage;VTK::IOMovie;VTK::IOOggTheora"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/vtkmodules/libvtkRenderingLookingGlass.so.0.1.0"
+  IMPORTED_SONAME_RELEASE "libvtkRenderingLookingGlass.so.1"
+  )
+
+list(APPEND _cmake_import_check_targets VTK::RenderingLookingGlass )
+list(APPEND _cmake_import_check_files_for_VTK::RenderingLookingGlass "${_IMPORT_PREFIX}/lib/vtkmodules/libvtkRenderingLookingGlass.so.0.1.0" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)

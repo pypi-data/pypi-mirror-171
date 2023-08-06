06c1df300c13f957621edede602a998b451ff21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:01 +0000

    Add package dbt-dry-run/0.4.1/dbt-dry-run-0.4.1.tar.gz

---
+++ packages/dbt-dry-run/dbt-dry-run-0.4.1.tar.gz/dbt-dry-run-0.4.1/dbt_dry_run/exception.py
@@ -0,0 +1,18 @@
+class NotCompiledException(Exception):
+    pass
+
+
+class UpstreamFailedException(Exception):
+    pass
+
+
+class NodeExecutionException(Exception):
+    pass
+
+
+class SchemaChangeException(Exception):
+    pass
+
+
+class SnapshotConfigException(Exception):
+    pass

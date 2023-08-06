1a7cc7224bf943c20b1fda98cbc94f52509c29f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:25 +0000

    Add package yidong-pingjunxian-mima-vol2/2022.10.15.0/yidong_pingjunxian_mima_vol2-2022.10.15.0-py3-none-any.whl

---
+++ packages/yidong-pingjunxian-mima-vol2/yidong_pingjunxian_mima_vol2-2022.10.15.0-py3-none-any.whl/YidongPingjunxianMimaVol2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""yidong-pingjunxian-mima-vol2
+https://github.com/apachecn/yidong-pingjunxian-mima-vol2"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

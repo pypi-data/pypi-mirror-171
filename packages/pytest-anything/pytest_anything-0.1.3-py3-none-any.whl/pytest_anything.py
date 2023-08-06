407e485f8095a16dbc9a285db37f02dc1e38cf28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:29 +0000

    Add package pytest-anything/0.1.3/pytest_anything-0.1.3-py3-none-any.whl

---
+++ packages/pytest-anything/pytest_anything-0.1.3-py3-none-any.whl/pytest_anything.py
@@ -0,0 +1,44 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def Anything():
+    """`Anything` is always Anything."""
+    return type.__new__(
+        type,
+        "Anything",
+        (object,),
+        {
+            "__repr__": lambda self: self.__class__.__name__,
+            "__eq__": lambda _, __: True,
+        },
+    )()
+
+
+@pytest.fixture(scope="session")
+def Something():
+    SomethingMeta = type.__new__(
+        type,
+        "Something",
+        (type,),
+        {
+            "__repr__": lambda cls: cls.__name__,
+            "__eq__": lambda _, other: other is not None,
+        },
+    )
+
+    class Something(metaclass=SomethingMeta):
+        """`Something` is not None.
+        You can call Something with a test to specialize your assertion.
+        """
+
+        def __init__(self, special):
+            self.special = special
+
+        def __repr__(self):
+            return f"{self.__class__.__name__}({self.special})"
+
+        def __eq__(self, other):
+            return self.special(other)
+
+    return Something

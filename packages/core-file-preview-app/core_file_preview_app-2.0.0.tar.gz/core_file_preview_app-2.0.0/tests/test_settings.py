39aae545af835a943dbba724f1ff1402f7b4cd44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:38 +0000

    Add package core-file-preview-app/2.0.0/core_file_preview_app-2.0.0.tar.gz

---
+++ packages/core-file-preview-app/core_file_preview_app-2.0.0.tar.gz/core_file_preview_app-2.0.0/tests/test_settings.py
@@ -0,0 +1,13 @@
+""" Tests Settings
+"""
+
+SECRET_KEY = "fake-key"
+
+INSTALLED_APPS = [
+    # Django apps
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    # Local app
+    "tests",
+]

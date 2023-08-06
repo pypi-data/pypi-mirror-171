55345e9c40474ff03142e3e400ce62ddac72f491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:05 +0000

    Add package mmselfsup/1.0.0rc2/mmselfsup-1.0.0rc2-py3-none-any.whl

---
+++ packages/mmselfsup/mmselfsup-1.0.0rc2-py3-none-any.whl/mmselfsup/.mim/configs/benchmarks/classification/_base_/datasets/imagenet.py
@@ -0,0 +1,50 @@
+# dataset settings
+dataset_type = 'ImageNet'
+data_root = 'data/imagenet/'
+file_client_args = dict(backend='disk')
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='RandomResizedCrop', scale=224, backend='pillow'),
+    dict(type='RandomFlip', prob=0.5, direction='horizontal'),
+    dict(type='PackClsInputs'),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='ResizeEdge', scale=256, edge='short', backend='pillow'),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='PackClsInputs'),
+]
+
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=4,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='meta/train.txt',
+        data_prefix='train',
+        pipeline=train_pipeline),
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    collate_fn=dict(type='default_collate'),
+    persistent_workers=True,
+    pin_memory=True,
+)
+
+val_dataloader = dict(
+    batch_size=32,
+    num_workers=4,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='meta/val.txt',
+        data_prefix='val',
+        pipeline=test_pipeline),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    persistent_workers=True,
+)
+val_evaluator = dict(type='mmcls.Accuracy', topk=(1, 5))
+
+# If you want standard test, please manually configure the test dataset
+test_dataloader = val_dataloader
+test_evaluator = val_evaluator
---
+++ packages/mmselfsup/mmselfsup-1.0.0rc2-py3-none-any.whl/mmselfsup/models/backbones/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .cae_vit import CAEViT
+from .mae_vit import MAEViT
+from .mocov3_vit import MoCoV3ViT
+from .resnet import ResNet, ResNetSobel, ResNetV1d
+from .resnext import ResNeXt
+from .simmim_swin import SimMIMSwinTransformer
+
+__all__ = [
+    'ResNet', 'ResNetSobel', 'ResNetV1d', 'ResNeXt', 'MAEViT', 'MoCoV3ViT',
+    'SimMIMSwinTransformer', 'CAEViT'
+]

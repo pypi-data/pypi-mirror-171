a6e40b7940ac48a92b09244cb6d489103ca4dcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:14 +0000

    Add package conda-pip-minimal/0.1.3/conda_pip_minimal-0.1.3-py3-none-any.whl

---
+++ packages/conda-pip-minimal/conda_pip_minimal-0.1.3-py3-none-any.whl/conda_pip_minimal/cmd.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+import json
+from more_itertools import collapse
+from typing import List
+import ast
+import trio
+
+from loguru import logger
+
+
+async def run_cmd(args) -> str:
+    proc = await trio.run_process(args, capture_stdout=True, check=True)  # type: ignore
+    return proc.stdout.decode("utf-8").strip()
+
+
+@dataclass
+class Cmd:
+    binary: str
+    args: List[str] = field(default_factory=list)
+
+    async def __call__(self, *args) -> str:
+        logger.debug(f"Starting {self.binary=} {self.args=} {args=}")
+        result = await run_cmd(self.construct_args(args))
+        logger.debug(f"Finished {self.binary=} {self.args=} {args=}")
+        return result
+
+    def construct_args(self, *args) -> List[str]:
+        return list(collapse([self.binary, self.args, args]))
+
+    async def json(self, *args):
+        return json.loads(await self(*args))
+
+    async def literal(self, *args):
+        return ast.literal_eval(await self(*args))

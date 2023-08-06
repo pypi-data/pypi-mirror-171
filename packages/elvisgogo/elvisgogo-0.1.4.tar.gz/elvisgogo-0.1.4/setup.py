4e82f2a168c5e72a7e2d934c1921d6db096ee7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:15 +0000

    Add package elvisgogo/0.1.4/elvisgogo-0.1.4.tar.gz

---
+++ packages/elvisgogo/elvisgogo-0.1.4.tar.gz/elvisgogo-0.1.4/setup.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup,find_packages
+
+setup(
+    name="elvisgogo",
+    version="0.1.4",
+    description="数据挖掘实验4",
+    author="lry",
+    author_email="1224137702@qq.com",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'xlrd',
+        'xlrd3',
+        'matplotlib',
+        'scikit-learn',
+        'LRY',
+        'torch',
+        'torchvision',
+        'tensorflow',
+        'urllib'
+    ]
+)

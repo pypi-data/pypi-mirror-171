0da5a828f639e3df4b5a2d6cc3489ed8e51ef484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:20 +0000

    Add package c4m-pdk-sky130/0.0.3/c4m_pdk_sky130-0.0.3-py3-none-any.whl

---
+++ packages/c4m-pdk-sky130/c4m_pdk_sky130-0.0.3-py3-none-any.whl/c4m/pdk/sky130/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: GPL-2.0-or-later OR AGPL-3.0-or-later OR CERN-OHL-S-2.0+
+from .pdkmaster import *
+from .stdcell import *
+from .io import *
+from .pyspice import *
+from .mem import *
+from .factory import *
+
+__libs__ = [stdcelllib, iolib, macrolib]

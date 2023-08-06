b57f25fce98a120a149076fad1b1b3a0c00cec0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:05 +0000

    Add package omito-config-plugin-env/0.1.5/omito_config_plugin_env-0.1.5-py3-none-any.whl

---
+++ packages/omito-config-plugin-env/omito_config_plugin_env-0.1.5-py3-none-any.whl/omito_config_plugin_env/__init__.py
@@ -0,0 +1,13 @@
+from omito_config_core import BasePlugin
+import os
+
+
+class EnvPlugin(BasePlugin):
+
+  def protocol(self) -> str:
+    return 'env'
+
+  def get_value(self, key: str):
+    if key not in os.environ:
+      raise Exception(f'Environment variable {key} is not defined')
+    return os.environ[key]

81f308b7c9d81ff223cddc1e1e690d1b0341e868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:59 +0000

    Add package nanoprep/0.1.9/nanoprep-0.1.9.tar.gz

---
+++ packages/nanoprep/nanoprep-0.1.9.tar.gz/nanoprep-0.1.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nanoprep", # Replace with your own username
+    version="0.1.9",
+    author="Xiaoping Li",
+    author_email="lixiaopi@vt.edu",
+    description="Provides tools to preprocess nanopore amplicon reads more flexibly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xpli2020/NanoPrep.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/xpli2020/NanoPrep.git/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=["pandas", "biopython"],
+    python_requires=">=3.6",
+)

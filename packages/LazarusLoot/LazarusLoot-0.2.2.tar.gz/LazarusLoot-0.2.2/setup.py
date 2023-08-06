5db1ce8a1fa4f97081a6248fbed6723e47d7c10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:29 +0000

    Add package LazarusLoot/0.2.2/LazarusLoot-0.2.2.tar.gz

---
+++ packages/LazarusLoot/LazarusLoot-0.2.2.tar.gz/LazarusLoot-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='LazarusLoot',
+    version='0.2.2',
+    packages=['src',],
+    py_modules=['lazarus_loot', 'loot_settings_parser'],
+    install_requires=[
+        'Click', 'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'lazarus_loot = src.lazarus_loot:cli',
+        ],
+    },
+    package_data={'lazarus_loot': ['*.csv']},
+)

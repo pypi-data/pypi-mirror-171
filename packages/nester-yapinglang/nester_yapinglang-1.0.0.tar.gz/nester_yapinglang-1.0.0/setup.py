82586818d36ebb2b121717790d5c6b708cfd694d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:45 +0000

    Add package nester-yapinglang/1.0.0/nester_yapinglang-1.0.0.tar.gz

---
+++ packages/nester-yapinglang/nester_yapinglang-1.0.0.tar.gz/nester_yapinglang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='nester_yapinglang',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='JohnDow',
+    author_email='lilyyapinglang1024@outlook.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)

705a71571c9e0d73043e8501e2e112dad66c4d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package makeit/2.10.7/makeit-2.10.7.tar.gz

---
+++ packages/makeit/makeit-2.10.7.tar.gz/makeit-2.10.7/tests/test_self_source.py
@@ -0,0 +1,47 @@
+import dataclasses
+
+from makeit import SourceOfSelf, execute_on_change, DataclassTask
+
+
+num_of_executions = 0
+
+
+@dataclasses.dataclass
+class A(DataclassTask):
+    _: SourceOfSelf = None
+
+    def label(self):
+        return "X"
+
+    def execute(self):
+        global num_of_executions
+
+        num_of_executions += 1
+
+
+@dataclasses.dataclass
+class B(DataclassTask):
+    _: SourceOfSelf = None
+
+    def label(self):
+        return "X"
+
+    def execute(self):
+        global num_of_executions
+
+        num_of_executions += 1
+
+
+def test_self_source(tmp_path):
+    backend = tmp_path / "makeit.json"
+
+    execute_on_change([A()], backend)
+    assert num_of_executions == 1
+
+    execute_on_change([A()], backend)
+    assert num_of_executions == 1
+
+    execute_on_change([B()], backend)
+    assert num_of_executions == 2
+
+    print(backend.read_text())

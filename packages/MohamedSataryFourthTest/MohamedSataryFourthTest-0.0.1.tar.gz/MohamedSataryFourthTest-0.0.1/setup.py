3d77358a965d06d319a15c686d45f0810969be7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:04 +0000

    Add package MohamedSataryFourthTest/0.0.1/MohamedSataryFourthTest-0.0.1.tar.gz

---
+++ packages/MohamedSataryFourthTest/MohamedSataryFourthTest-0.0.1.tar.gz/MohamedSataryFourthTest-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+      name='MohamedSataryFourthTest',
+      version='0.0.1',
+      description='''Testing out uploading a library''',
+      py_modules=['helloWorld', 'bye', 'goodBye'],
+      package_dir={'': 'src'},
+      )

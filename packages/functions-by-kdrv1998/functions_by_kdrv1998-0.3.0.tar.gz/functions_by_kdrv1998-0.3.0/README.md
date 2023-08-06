684c360b632eb11b07012b9ca86f0f148b54488c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:43 +0000

    Add package functions-by-kdrv1998/0.3.0/functions_by_kdrv1998-0.3.0.tar.gz

---
+++ packages/functions-by-kdrv1998/functions_by_kdrv1998-0.3.0.tar.gz/functions_by_kdrv1998-0.3.0/README.md
@@ -0,0 +1,14 @@
+# Instructions
+
+#### This is our test project.
+
+#### Please install this package 
+```
+pip install functions-by-kdrv1998
+```
+
+
+#### You can also install older package 
+```
+pip install functions-by-kdrv1998==VERSION_NUMBER
+```
---
+++ packages/functions-by-kdrv1998/functions_by_kdrv1998-0.3.0.tar.gz/functions_by_kdrv1998-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_kdrv1998']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-kdrv1998',
+    'version': '0.3.0',
+    'description': 'This is our test project',
+    'long_description': '# Instructions\n\n#### This is our test project.\n\n#### Please install this package \n```\npip install functions-by-kdrv1998\n```\n\n\n#### You can also install older package \n```\npip install functions-by-kdrv1998==VERSION_NUMBER\n```',
+    'author': 'Zhaniya Kadyrova',
+    'author_email': 'kdrv.jn@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

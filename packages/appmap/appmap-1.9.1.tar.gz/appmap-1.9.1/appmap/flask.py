c4a04ecd0c1487a6d81100aa012ec902b4fc55cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:20 +0000

    Add package appmap/1.9.1/appmap-1.9.1.tar.gz

---
+++ packages/appmap/appmap-1.9.1.tar.gz/appmap-1.9.1/appmap/flask.py
@@ -0,0 +1,194 @@
+import datetime
+import json
+import os.path
+import time
+from functools import wraps
+
+import flask
+import flask.cli
+import jinja2
+from flask import _app_ctx_stack, request
+from werkzeug.exceptions import BadRequest
+from werkzeug.routing import parse_rule
+
+from appmap._implementation import generation, web_framework
+from appmap._implementation.env import Env
+from appmap._implementation.event import HttpServerRequestEvent, HttpServerResponseEvent
+from appmap._implementation.recorder import Recorder
+from appmap._implementation.recording import Recording
+from appmap._implementation.web_framework import AppmapMiddleware
+from appmap._implementation.web_framework import TemplateHandler as BaseTemplateHandler
+
+from ._implementation.metadata import Metadata
+from ._implementation.utils import patch_class, values_dict
+
+try:
+    # pylint: disable=unused-import
+    from . import sqlalchemy  # noqa: F401
+except ImportError:
+    # not using sqlalchemy
+    pass
+
+
+def request_params(req):
+    """Extract request parameters as a dict.
+
+    Parses query and form data and JSON request body.
+    Multiple parameter values are represented as lists."""
+    params = req.values.copy()
+    params.update(req.view_args or {})
+    try:
+        params.update(req.json or {})
+    except BadRequest:
+        pass  # probably a JSON parse error
+
+    return values_dict(params.lists())
+
+
+class AppmapFlask(AppmapMiddleware):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not Env.current.enabled:
+            return
+
+        if not app:
+            return
+
+        self.recording = Recording()
+
+        self.record_url = "/_appmap/record"
+
+        # print('in init_app')
+        app.add_url_rule(
+            self.record_url,
+            "appmap_record_get",
+            view_func=self.record_get,
+            methods=["GET"],
+        )
+        app.add_url_rule(
+            self.record_url,
+            "appmap_record_post",
+            view_func=self.record_post,
+            methods=["POST"],
+        )
+        app.add_url_rule(
+            self.record_url,
+            "appmap_record_delete",
+            view_func=self.record_delete,
+            methods=["DELETE"],
+        )
+
+        app.before_request(self.before_request)
+        app.after_request(self.after_request)
+
+    def record_get(self):
+        return {"enabled": self.recording.is_running()}
+
+    def record_post(self):
+        if self.recording.is_running():
+            return "Recording is already in progress", 409
+
+        self.recording.start()
+        return "", 200
+
+    def record_delete(self):
+        if not self.recording.is_running():
+            return "No recording is in progress", 404
+
+        self.recording.stop()
+
+        return json.loads(generation.dump(self.recording))
+
+    def before_request(self):
+        if not Env.current.enabled:
+            return
+
+        rec, start, call_event_id = self.before_request_hook(
+            request, request.path, self.record_url, self.recording.is_running()
+        )
+
+    def before_request_main(self, rec, request):
+        Metadata.add_framework("flask", flask.__version__)
+        np = None
+        # See
+        # https://github.com/pallets/werkzeug/blob/2.0.0/src/werkzeug/routing.py#L213
+        # for a description of parse_rule.
+        if request.url_rule:
+            np = "".join(
+                [
+                    f"{{{p}}}" if c else p
+                    for c, _, p in parse_rule(request.url_rule.rule)
+                ]
+            )
+        call_event = HttpServerRequestEvent(
+            request_method=request.method,
+            path_info=request.path,
+            message_parameters=request_params(request),
+            normalized_path_info=np,
+            protocol=request.environ.get("SERVER_PROTOCOL"),
+            headers=request.headers,
+        )
+        rec.add_event(call_event)
+
+        appctx = _app_ctx_stack.top
+        appctx.appmap_request_event = call_event
+        appctx.appmap_request_start = time.monotonic()
+
+        return None, None
+
+    def after_request(self, response):
+        if not Env.current.enabled:
+            return
+
+        return self.after_request_hook(
+            request,
+            request.path,
+            self.record_url,
+            self.recording.is_running(),
+            request.method,
+            request.base_url,
+            response,
+            response.headers,
+            None,
+            None,
+        )
+
+    def after_request_main(self, rec, response, start, call_event_id):
+        appctx = _app_ctx_stack.top
+        parent_id = appctx.appmap_request_event.id
+        duration = time.monotonic() - appctx.appmap_request_start
+
+        return_event = HttpServerResponseEvent(
+            parent_id=parent_id,
+            elapsed=duration,
+            status_code=response.status_code,
+            headers=response.headers,
+        )
+        rec.add_event(return_event)
+
+
+@patch_class(jinja2.Template)
+class TemplateHandler(BaseTemplateHandler):
+    # pylint: disable=missing-class-docstring, too-few-public-methods
+    pass
+
+
+def wrap_cli_fn(fn):
+    @wraps(fn)
+    def install_middleware(*args, **kwargs):
+        app = fn(*args, **kwargs)
+        if app:
+            appmap_flask = AppmapFlask()
+            appmap_flask.init_app(app)
+        return app
+
+    return install_middleware
+
+
+if Env.current.enabled:
+    flask.cli.call_factory = wrap_cli_fn(flask.cli.call_factory)
+    flask.cli.locate_app = wrap_cli_fn(flask.cli.locate_app)
---
+++ packages/appmap/appmap-1.9.1.tar.gz/appmap-1.9.1/appmap/test/test_configuration.py
@@ -0,0 +1,214 @@
+"""Test Configuration"""
+# pylint: disable=missing-function-docstring
+
+import sys
+from distutils.dir_util import copy_tree
+from pathlib import Path
+
+import pytest
+
+import appmap
+import appmap._implementation
+import appmap._implementation.env as impl_env
+from appmap._implementation.configuration import Config, ConfigFilter
+from appmap._implementation.env import Env
+from appmap._implementation.importer import Filterable, NullFilter
+
+
+def test_can_be_enabled():
+    """
+    Test that recording is enabled when APPMAP=true.
+    """
+    Env.current.set("APPMAP", "true")
+
+    assert appmap.enabled()
+
+
+@pytest.mark.appmap_enabled
+def test_can_be_configured():
+    """
+    Test the happy path: APPMAP is true, appmap.yml is found, and the
+    YAML is valid.
+    """
+    assert appmap.enabled()
+
+    c = Config()
+    assert c.file_present
+    assert c.file_valid
+
+
+@pytest.mark.appmap_enabled(config="appmap-broken.yml")
+def test_reports_invalid():
+    """
+    Test that a parse error keeps recording from being enabled, and
+    indicates that the config is not valid.
+    """
+    assert not appmap.enabled()
+    assert not Config().file_valid
+
+
+def test_is_disabled_when_unset():
+    """Test that recording is disabled when APPMAP is unset"""
+    assert Env.current.get("APPMAP", None) is None
+
+    assert not appmap.enabled()
+
+
+def test_is_disabled_when_false():
+    """Test that recording is disabled when APPMAP=false"""
+    Env.current.set("APPMAP", "false")
+    assert not appmap.enabled()
+
+
+@pytest.mark.appmap_enabled(appmap_enabled=None)
+def test_is_disabled_with_valid_config():
+    c = Config()
+    assert c.file_present
+    assert c.file_valid
+
+    assert not appmap.enabled()
+
+
+def test_config_not_found(caplog):
+    appmap._implementation.initialize(
+        env={  # pylint: disable=protected-access
+            "APPMAP": "true",
+            "APPMAP_CONFIG": "notfound.yml",
+        }
+    )
+    assert Config().name is None
+    assert not Config().file_present
+    assert not Config().file_valid
+
+    assert not appmap.enabled()
+    not_found = Path("notfound.yml").resolve()
+    assert not not_found.exists()
+    assert f'"{not_found}" is missing' in caplog.text
+
+
+def test_config_no_message(caplog):
+    """
+    Messages about a missing config should only be logged when
+    recording is enabled
+    """
+
+    assert Config().name is None
+    assert not appmap.enabled()
+    assert caplog.text is ""
+
+
+cf = lambda: ConfigFilter(NullFilter())
+
+
+@pytest.mark.appmap_enabled(config="appmap-class.yml")
+def test_class_included():
+    f = Filterable("package1.package2.Mod1Class", None)
+    assert cf().filter(f) is True
+
+
+@pytest.mark.appmap_enabled(config="appmap-func.yml")
+def test_function_included():
+    f = Filterable("package1.package2.Mod1Class.func", None)
+    assert cf().filter(f) is True
+
+
+@pytest.mark.appmap_enabled(config="appmap-class.yml")
+def test_function_included_by_class():
+    f = Filterable("package1.package2.Mod1Class.func", None)
+    assert cf().filter(f) is True
+
+
+@pytest.mark.appmap_enabled
+class TestConfiguration:
+    def test_package_included(self):
+        f = Filterable("package1.cls", None)
+        assert cf().filter(f) is True
+
+    def test_function_included_by_package(self):
+        f = Filterable("package1.package2.Mod1Class.func", None)
+        assert cf().filter(f) is True
+
+    def test_class_prefix_doesnt_match(self):
+        f = Filterable("package1_prefix.cls", None)
+        assert cf().filter(f) is False
+
+
+class TestDefaultConfig:
+    def check_default_config(self, expected_name):
+        assert appmap.enabled()
+
+        default_config = Config()
+        assert default_config.name == expected_name
+        assert len(default_config.packages) == 2
+        assert sorted(default_config.packages, key=lambda p: p["path"]) == [
+            {"path": "package"},
+            {"path": "test"},
+        ]
+
+    def test_created(self, git, data_dir, monkeypatch):
+        repo_root = git.cwd
+        copy_tree(data_dir / "config", str(repo_root))
+        monkeypatch.chdir(repo_root)
+
+        # pylint: disable=protected-access
+        appmap._implementation.initialize(cwd=repo_root, env={"APPMAP": "true"})
+
+        self.check_default_config(repo_root.name)
+
+    def test_created_outside_repo(self, data_dir, tmpdir, monkeypatch):
+        copy_tree(data_dir / "config", str(tmpdir))
+        monkeypatch.chdir(tmpdir)
+
+        # pylint: disable=protected-access
+        appmap._implementation.initialize(cwd=tmpdir, env={"APPMAP": "true"})
+        self.check_default_config(Path(tmpdir).name)
+
+    def test_skipped_when_overridden(self):
+        appmap._implementation.initialize(
+            env={  # pylint: disable=protected-access
+                "APPMAP": "true",
+                "APPMAP_CONFIG": "/tmp/appmap.yml",
+            }
+        )
+        assert not Config().name
+        assert not appmap.enabled()
+
+    def test_exclusions(self, data_dir, tmpdir, mocker, monkeypatch):
+        copy_tree(data_dir / "config-exclude", str(tmpdir))
+        monkeypatch.chdir(tmpdir)
+        mocker.patch(
+            "appmap._implementation.configuration._get_sys_prefix",
+            return_value=str(tmpdir / "venv"),
+        )
+
+        # pylint: disable=protected-access
+        appmap._implementation.initialize(cwd=tmpdir, env={"APPMAP": "true"})
+        self.check_default_config(Path(tmpdir).name)
+
+    def test_created_if_missing_and_enabled(self, git, data_dir, monkeypatch):
+        repo_root = git.cwd
+        copy_tree(data_dir / "config", str(repo_root))
+        monkeypatch.chdir(repo_root)
+
+        path = Path(repo_root / "appmap.yml")
+        assert not path.is_file()
+
+        # pylint: disable=protected-access
+        appmap._implementation.initialize(cwd=repo_root, env={"APPMAP": "true"})
+
+        c = Config()
+        assert path.is_file()
+
+    def test_not_created_if_missing_and_not_enabled(self, git, data_dir, monkeypatch):
+        repo_root = git.cwd
+        copy_tree(data_dir / "config", str(repo_root))
+        monkeypatch.chdir(repo_root)
+
+        path = Path(repo_root / "appmap.yml")
+        assert not path.is_file()
+
+        # pylint: disable=protected-access
+        appmap._implementation.initialize(cwd=repo_root, env={"APPMAP": "false"})
+
+        c = Config()
+        assert not path.is_file()

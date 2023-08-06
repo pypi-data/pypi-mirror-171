a11a6db00900bb3196bd557472d1971d9eb2df70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:56 +0000

    Add package functions-by-jvefferson/0.2.0/functions_by_jvefferson-0.2.0.tar.gz

---
+++ packages/functions-by-jvefferson/functions_by_jvefferson-0.2.0.tar.gz/functions_by_jvefferson-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_jvefferson']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-jvefferson',
+    'version': '0.2.0',
+    'description': 'This is our test project',
+    'long_description': '# Instructions \n\n### This is our test project.',
+    'author': 'Julia Efferson',
+    'author_email': 'jvefferson@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

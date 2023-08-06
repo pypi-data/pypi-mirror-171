225feb986d8b16c4941a3970e197b2f92cdd7022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:22 +0000

    Add package goutongde-yishu/2022.10.11.0/goutongde-yishu-2022.10.11.0.tar.gz

---
+++ packages/goutongde-yishu/goutongde-yishu-2022.10.11.0.tar.gz/goutongde-yishu-2022.10.11.0/GoutongdeYishu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""goutongde-yishu
+https://github.com/apachecn/goutongde-yishu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

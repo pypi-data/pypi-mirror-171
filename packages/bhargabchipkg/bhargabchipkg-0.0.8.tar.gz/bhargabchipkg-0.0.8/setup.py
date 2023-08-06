c870403fb6208a8996f0e0b01330f7bc3c9a4c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:35 +0000

    Add package bhargabchipkg/0.0.8/bhargabchipkg-0.0.8.tar.gz

---
+++ packages/bhargabchipkg/bhargabchipkg-0.0.8.tar.gz/bhargabchipkg-0.0.8/setup.py
@@ -0,0 +1,51 @@
+from distutils.core import setup
+
+
+long_description1 = """
+                      This package can be used to classify salesmen on its performance of total sale and total unsold with
+                      two different groups.
+
+                      In easy words lets we want to see performance(good and bad) salesman for urban market, rural market 
+                      there we can use this package by passing a dataframe, column name of salesman id, 
+                      column name of saleflag(indicating 0 for unsold and 1 for sold),column name of grouping column (having only two unique values or levels).
+
+                      We are using chi square, a statistical method which says performance quality. 
+                      
+                      You can get the description by reading the readme.md file from my github.
+                      link of github:
+
+                      https://github.com/bhargabganguli/bhargabchipkg.git
+
+                      Here you can find 
+
+                      - how to install
+
+                      - how to create object and use the method
+
+                      - how to call the method and print the output
+                    """
+
+setup(name='bhargabchipkg',
+      packages = ['bhargabchipkg'],
+      version='0.0.8',
+      description='helpful chi-square package for sales data',
+      long_description= long_description1,
+      url='https://github.com/bhargabganguli/bhargabchipkg.git',
+      download_url = 'https://github.com/bhargabganguli/bhargabchipkg/archive/0.0.8.tar.gz', #FILL IN LATER
+      author='Bhargab',
+      author_email='bhargab.ganguli@gmail.com',
+      keywords = ['chi_test',],
+      license='MIT', #YOUR LICENSE HERE!
+
+      install_requires=['pandas','numpy','scipy',],  #YOUR DEPENDENCIES HERE
+  
+
+      classifiers=[
+        'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', # Your License Here  
+        'Programming Language :: Python :: 3',    # List Python versions that you support Here  
+        'Programming Language :: Python :: 3.4',
+        ],
+)

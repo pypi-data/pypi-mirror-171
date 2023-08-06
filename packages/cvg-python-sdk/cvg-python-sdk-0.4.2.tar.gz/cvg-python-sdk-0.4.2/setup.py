2a0aa23fcc36d846e0056e7a8f30b7d51d6856de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:50 +0000

    Add package cvg-python-sdk/0.4.2/cvg-python-sdk-0.4.2.tar.gz

---
+++ packages/cvg-python-sdk/cvg-python-sdk-0.4.2.tar.gz/cvg-python-sdk-0.4.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "cvg-python-sdk"
+VERSION = "0.4.2"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+  "urllib3 >= 1.25.3",
+  "python-dateutil",
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="VIER Cognitive Voice Gateway SDK",
+    author="VIER GmbH",
+    author_email="support@vier.ai",
+    url="https://cognitivevoice.io",
+    keywords=["OpenAPI", "OpenAPI-Generator", "VIER", "VIER Cognitive Voice Gateway SDK"],
+    python_requires=">=3.6",
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    license="MIT",
+)

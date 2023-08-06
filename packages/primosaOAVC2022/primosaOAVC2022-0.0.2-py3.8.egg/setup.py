d98a3fe522f19ffc2b5d990263cdc314668abb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:57 +0000

    Add package primosaOAVC2022/0.0.2/primosaOAVC2022-0.0.2-py3.8.egg

---
+++ packages/primosaOAVC2022/primosaOAVC2022-0.0.2-py3.8.egg/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='primos',
+    version='0.0.1',
+    description='Calculate prime numbers',
+    package_dir={'':'/Users/oscarandresvalladarescardona/Desktop/src'}
+)

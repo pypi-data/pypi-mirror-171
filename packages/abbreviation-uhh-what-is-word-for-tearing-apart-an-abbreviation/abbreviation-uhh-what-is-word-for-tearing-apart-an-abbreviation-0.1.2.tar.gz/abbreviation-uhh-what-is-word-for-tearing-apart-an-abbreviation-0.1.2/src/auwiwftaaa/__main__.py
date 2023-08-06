579deaa8335be7163b7116247486fa9f69a74c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:32 +0000

    Add package abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation/0.1.2/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation-0.1.2.tar.gz

---
+++ packages/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation-0.1.2.tar.gz/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation-0.1.2/src/auwiwftaaa/__main__.py
@@ -0,0 +1,8 @@
+def main():
+    welcome_message = "Welcome!\n"
+    print(welcome_message)
+    name = input("Please enter your name: ")
+    print(f"\n\nHello {name}~\n:D")
+
+if __name__ == "__main__":
+    main()

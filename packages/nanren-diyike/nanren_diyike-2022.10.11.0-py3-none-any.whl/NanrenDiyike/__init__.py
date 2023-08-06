3884d84c0f1718409407dbfba1b6c62caffeb824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:13 +0000

    Add package nanren-diyike/2022.10.11.0/nanren_diyike-2022.10.11.0-py3-none-any.whl

---
+++ packages/nanren-diyike/nanren_diyike-2022.10.11.0-py3-none-any.whl/NanrenDiyike/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nanren-diyike
+https://github.com/apachecn/nanren-diyike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

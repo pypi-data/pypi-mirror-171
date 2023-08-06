cb658e0cf8b527e64f54b87fa1e53d259e4faa65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:45 +0000

    Add package JusteTestPackage/1.0.0/JusteTestPackage-1.0.0.tar.gz

---
+++ packages/JusteTestPackage/JusteTestPackage-1.0.0.tar.gz/JusteTestPackage-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="JusteTestPackage",
+    version="1.0.0",
+    description="A sample package description",
+    author="Mhammed Jeddou",
+    author_email="jeddou.ehtp@gmail.com",
+    packages=["mysci_22"],
+    install_requires=[])

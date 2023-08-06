d3e70dfd4809ba12e3828484c685018668833ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:18 +0000

    Add package abstra-cli/0.4.3/abstra_cli-0.4.3-py3-none-any.whl

---
+++ packages/abstra-cli/abstra_cli-0.4.3-py3-none-any.whl/abstra_cli/apis.py
@@ -0,0 +1,175 @@
+import json
+import requests
+import urllib.request
+import urllib.response
+
+from .utils_config import get_auth_info, get_credentials
+
+
+HACKERFORMS_API_URL = "https://hackerforms-api.abstra.cloud"
+
+
+def hf_api_runner(method, path, data=None):
+    api_token, workspace_id, _ = get_auth_info()
+    response = requests.request(
+        method,
+        f"{HACKERFORMS_API_URL}/workspaces/{workspace_id}/{path}",
+        data=json.dumps(data) if data else None,
+        headers={"content-type": "application/json", "API-Authorization": api_token},
+    )
+    return response.json()
+
+
+def upload_file(filepath, file):
+    response_json = hf_api_runner("POST", "put-url", {"filepath": filepath})
+    req = urllib.request.Request(
+        url=response_json["putURL"], method="PUT", data=file.read()
+    )
+    res = urllib.request.urlopen(req)
+    return res.status < 400
+
+
+def get_file_signed_url(filepath):
+    response_json = hf_api_runner("POST", "get-url", {"filepath": filepath})
+    return response_json.get("getURL")
+
+
+def list_workspace_files():
+    return hf_api_runner("GET", "files")
+
+
+def delete_file(filepath):
+    return hf_api_runner("DELETE", "file", {"filepath": filepath})
+
+
+HACKERFORMS_HASURA_URL = "https://hackerforms-hasura.abstra.cloud/v1/graphql"
+
+
+def hf_hasura_runner(query, variables={}):
+    api_token = get_credentials()
+    response = requests.post(
+        HACKERFORMS_HASURA_URL,
+        data=json.dumps({"query": query, "variables": variables}),
+        headers={"content-type": "application/json", "API-Authorization": api_token},
+    )
+    jsond = response.json()
+    # print(jsond)
+    return jsond["data"]
+
+
+def list_workspace_packages():
+    query = """
+        query GetPackages {
+            packages {
+                name
+                version
+            }
+        }
+    """
+    return hf_hasura_runner(query).get("packages", [])
+
+
+def list_workspace_vars():
+    query = """
+        query GetVars {
+            environment_variables {
+                name
+                value
+            }
+        }
+    """
+    return hf_hasura_runner(query).get("environment_variables", [])
+
+
+def add_workspace_vars(raw_vars):
+    _, workspace_id, _ = get_auth_info()
+    vars = [
+        {"name": v["name"], "value": v["value"], "workspace_id": workspace_id}
+        for v in raw_vars
+    ]
+    query = """
+        mutation InsertVars($vars: [environment_variables_insert_input!]!) {
+            insert_environment_variables(
+                objects: $vars
+                on_conflict: {
+                    constraint: environment_variables_name_workspace_id_key
+                    update_columns: [value, name]
+                }
+            ) {
+                returning {
+                    name
+                    value
+                }
+            }
+        }
+    """
+    return (
+        hf_hasura_runner(query, {"vars": vars})
+        .get("insert_environment_variables", {})
+        .get("returning", [])
+    )
+
+
+def add_workspace_packages(raw_packages):
+    _, workspace_id, _ = get_auth_info()
+    packages = [
+        {"name": p["name"], "version": p["version"], "workspace_id": workspace_id}
+        for p in raw_packages
+    ]
+    query = """
+        mutation InsertPackages($packages: [packages_insert_input!]!) {
+            insert_packages(
+                objects: $packages
+                on_conflict: {
+                    constraint: packages_workspace_id_name_key  
+                    update_columns: [version]
+                }
+            ) {
+                returning {
+                    name
+                    version
+                }
+            }
+        }
+    """
+    return (
+        hf_hasura_runner(query, {"packages": packages})
+        .get("insert_packages", {})
+        .get("returning", [])
+    )
+
+
+def delete_workspace_packages(packages):
+    query = """
+        mutation DeletePackages($packages: [String!]) {
+            delete_packages(where: {name: {_in: $packages}}) {
+                returning {
+                    name
+                    version
+                }
+            }
+        }
+    """
+    return (
+        hf_hasura_runner(query, {"packages": packages})
+        .get("delete_packages", {})
+        .get("returning", [])
+    )
+
+
+def delete_workspace_vars(vars):
+    query = """
+        mutation DeleteVars($vars: [String!]) {
+            delete_environment_variables(where: {name: {_in: $vars}}) {
+                returning {
+                    name
+                    value
+                }
+            }
+        }
+    """
+    return (
+        hf_hasura_runner(query, {"vars": vars})
+        .get("delete_environment_variables", {})
+        .get("returning", [])
+    )
---
+++ packages/abstra-cli/abstra_cli-0.4.3-py3-none-any.whl/abstra_cli/cli.py
@@ -0,0 +1,64 @@
+import fire
+
+from .cli_helpers import read_credentials
+from .utils_config import credentials_check, save_credentials
+from .resources import Files, Vars, Packages
+
+
+def not_implemented(*args, **kwargs):
+    print("Invalid command")
+
+
+class CLI(object):
+    def configure(self, api_token=None):
+        save_credentials(api_token or read_credentials())
+        print("Done!")
+
+    @credentials_check
+    def list(self, resource, *args, **kwargs):
+        list_func = {
+            "vars": Vars.list,
+            "files": Files.list,
+            "packages": Packages.list,
+        }.get(resource, not_implemented)
+
+        list_func(*args, **kwargs)
+
+    @credentials_check
+    def add(self, resource, *args, **kwargs):
+        add_func = {
+            "vars": Vars.add,
+            "files": Files.add,
+            "packages": Packages.add,
+        }.get(resource, not_implemented)
+
+        add_func(*args, **kwargs)
+
+    @credentials_check
+    def remove(self, resource, *args, **kwargs):
+        add_func = {
+            "vars": Vars.remove,
+            "files": Files.remove,
+            "packages": Packages.remove,
+        }.get(resource, not_implemented)
+
+        add_func(*args, **kwargs)
+
+    # Aliases
+    def upload(self, *args, **kwargs):
+        if not len(args):
+            args = ["."]
+        self.add("files", *args, **kwargs)
+
+    def ls(self, *args, **kwargs):
+        self.list("files", *args, **kwargs)
+
+    def rm(self, *args, **kwargs):
+        self.remove("files", *args, **kwargs)
+
+    def install(self, *args, **kwargs):
+        self.add("packages", *args, **kwargs)
+
+
+def main():
+    fire.Fire(CLI)

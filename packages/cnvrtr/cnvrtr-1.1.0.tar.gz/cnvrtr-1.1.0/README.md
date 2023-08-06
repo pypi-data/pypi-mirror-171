637f710af2d542a36ac51887d833199f320e1941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:15 +0000

    Add package cnvrtr/1.1.0/cnvrtr-1.1.0.tar.gz

---
+++ packages/cnvrtr/cnvrtr-1.1.0.tar.gz/cnvrtr-1.1.0/README.md
@@ -0,0 +1,41 @@
+# Description
+
+Different functions to convert between hex, dec, bin, ascii etc.
+
+# Installation
+
+`pip install cnvrtr`
+
+# Usage
+
+
+```python
+
+from cnvrtr.Converter import Converter
+
+cnvrtr = Converter()
+
+leHex = cnvrtr.toLittleEndian("af d0 fe")
+# fed0af
+
+hexToBin = cnvrtr.hexToBin("fed0af")
+# 111111101101000010101111
+
+hexToDec = cnvrtr.hexToDec("fed0af")
+# 16699567
+
+binToDec = cnvrtr.binToDec("11010101")
+# 213
+
+decToAscii = cnvrtr.decToAscii(65)
+# A
+
+hexToAsciiString = cnvrtr.hexToAsciiString("68656c6c6f20776f726c64")
+# hello world
+
+```
+
+
+# License
+
+MIT
---
+++ packages/cnvrtr/cnvrtr-1.1.0.tar.gz/cnvrtr-1.1.0/src/cnvrtr/Converter.py
@@ -0,0 +1,36 @@
+class Converter():
+    def __init__(self) -> None:
+        pass
+
+    def toLittleEndian(self, value):
+        # example for value = "20 21 00"
+        s = value.split(" ")
+        h_ = "".join(s[i] for i in range(len(s)-1, -1, -1))
+        return h_
+
+    def hexToBin(self, hex):
+        return bin(int(hex, base=16)).replace("0b","")
+
+    def hexToDec(self, hex_):
+        return int(hex_, 16)
+
+    def binToDec(self, bin):
+        # example for bin = "100001"
+        return int(bin, 2)
+
+    def decToAscii(self, dec):
+        result = ""
+        if (dec >= 32 and dec <= 126):
+            result = chr(dec) 
+        return result
+
+    def hexToAsciiString(self, hex):
+        result = ""
+        hexValues = self.__splitHex(hex.lower())
+        for v in hexValues:
+            dec = self.hexToDec(v)
+            result += self.decToAscii(dec)
+        return result
+
+    def __splitHex(self, hex):
+        return " ".join(hex[i:i+2] for i in range(0, len(hex), 2)).split(" ")

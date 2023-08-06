f4d07078393f46c922fa1c2f19b868ed608f65a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:46 +0000

    Add package weishenme-youxieren-yongbu-shoushang/2022.10.11.0/weishenme_youxieren_yongbu_shoushang-2022.10.11.0-py3-none-any.whl

---
+++ packages/weishenme-youxieren-yongbu-shoushang/weishenme_youxieren_yongbu_shoushang-2022.10.11.0-py3-none-any.whl/WeishenmeYouxierenYongbuShoushang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weishenme-youxieren-yongbu-shoushang
+https://github.com/apachecn/weishenme-youxieren-yongbu-shoushang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

022037eaba5854982c03db488e98b4343acd683e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:43 +0000

    Add package echopype/0.6.3/echopype-0.6.3.tar.gz

---
+++ packages/echopype/echopype-0.6.3.tar.gz/echopype-0.6.3/echopype/calibrate/calibrate_azfp.py
@@ -0,0 +1,178 @@
+import numpy as np
+
+from ..echodata import EchoData
+from ..utils import uwa
+from .calibrate_base import CAL_PARAMS
+from .calibrate_ek import CalibrateBase
+
+
+class CalibrateAZFP(CalibrateBase):
+    def __init__(self, echodata: EchoData, env_params, cal_params, **kwargs):
+        super().__init__(echodata, env_params)
+
+        # initialize cal params
+        self.cal_params = dict.fromkeys(CAL_PARAMS["AZFP"])
+
+        # load env and cal parameters
+        self.get_env_params()
+        if cal_params is None:
+            cal_params = {}
+        self.get_cal_params(cal_params)
+
+        # self.range_meter computed under self._cal_power()
+        # because the implementation is different for Sv and TS
+
+    def get_cal_params(self, cal_params):
+        """Get cal params using user inputs or values from data file.
+
+        Parameters
+        ----------
+        cal_params : dict
+        """
+
+        # Get params from Beam_group1
+        self.cal_params["equivalent_beam_angle"] = (
+            cal_params["equivalent_beam_angle"]
+            if "equivalent_beam_angle" in cal_params
+            else self.echodata["Sonar/Beam_group1"]["equivalent_beam_angle"]
+        )
+
+        # Get params from the Vendor_specific group
+        for p in ["EL", "DS", "TVR", "VTX", "Sv_offset"]:
+            # substitute if None in user input
+            self.cal_params[p] = (
+                cal_params[p] if p in cal_params else self.echodata["Vendor_specific"][p]
+            )
+
+    def get_env_params(self):
+        """Get env params using user inputs or values from data file.
+
+        Parameters
+        ----------
+        env_params : dict
+        """
+        # Temperature comes from either user input or data file
+        self.env_params["temperature"] = (
+            self.env_params["temperature"]
+            if "temperature" in self.env_params
+            else self.echodata["Environment"]["temperature"]
+        )
+
+        # Salinity and pressure always come from user input
+        if ("salinity" not in self.env_params) or ("pressure" not in self.env_params):
+            raise ReferenceError("Please supply both salinity and pressure in env_params.")
+        else:
+            self.env_params["salinity"] = self.env_params["salinity"]
+            self.env_params["pressure"] = self.env_params["pressure"]
+
+        # Always calculate sound speed and absorption
+        self.env_params["sound_speed"] = uwa.calc_sound_speed(
+            temperature=self.env_params["temperature"],
+            salinity=self.env_params["salinity"],
+            pressure=self.env_params["pressure"],
+            formula_source="AZFP",
+        )
+        self.env_params["sound_absorption"] = uwa.calc_absorption(
+            frequency=self.echodata["Sonar/Beam_group1"]["frequency_nominal"],
+            temperature=self.env_params["temperature"],
+            salinity=self.env_params["salinity"],
+            pressure=self.env_params["pressure"],
+            formula_source="AZFP",
+        )
+
+    def compute_range_meter(self, cal_type):
+        """Calculate range (``echo_range``) in meter using AZFP formula.
+
+        Note the range calculation differs for Sv and TS per AZFP matlab code.
+
+        Parameters
+        ----------
+        cal_type : str
+            'Sv' for calculating volume backscattering strength, or
+            'TS' for calculating target strength
+        """
+        self.range_meter = self.echodata.compute_range(self.env_params, azfp_cal_type=cal_type)
+
+    def _cal_power(self, cal_type, **kwargs):
+        """Calibrate to get volume backscattering strength (Sv) from AZFP power data.
+
+        The calibration formulae used here is based on Appendix G in
+        the GU-100-AZFP-01-R50 Operator's Manual.
+        Note a Sv_offset factor that varies depending on frequency is used
+        in the calibration as documented on p.90.
+        See calc_Sv_offset() in convert/azfp.py
+        """
+        # Compute range in meters
+        self.compute_range_meter(
+            cal_type=cal_type
+        )  # range computation different for Sv and TS per AZFP matlab code
+
+        # Compute derived params
+
+        # Harmonize time coordinate between Beam_groupX data and env_params
+        # Use self.echodata["Sonar/Beam_group1"] because complex sample is always in Beam_group1
+        for p in self.env_params.keys():
+            self.env_params[p] = self.echodata._harmonize_env_param_time(
+                self.env_params[p], ping_time=self.echodata["Sonar/Beam_group1"].ping_time
+            )
+
+        # TODO: take care of dividing by zero encountered in log10
+        spreading_loss = 20 * np.log10(self.range_meter)
+        absorption_loss = 2 * self.env_params["sound_absorption"] * self.range_meter
+        SL = self.cal_params["TVR"] + 20 * np.log10(self.cal_params["VTX"])  # eq.(2)
+
+        # scaling factor (slope) in Fig.G-1, units Volts/dB], see p.84
+        a = self.cal_params["DS"]
+        EL = (
+            self.cal_params["EL"]
+            - 2.5 / a
+            + self.echodata["Sonar/Beam_group1"].backscatter_r / (26214 * a)
+        )  # eq.(5)  # has beam dim due to backscatter_r
+
+        if cal_type == "Sv":
+            # eq.(9)
+            out = (
+                EL
+                - SL
+                + spreading_loss
+                + absorption_loss
+                - 10
+                * np.log10(
+                    0.5
+                    * self.env_params["sound_speed"]
+                    * self.echodata["Sonar/Beam_group1"]["transmit_duration_nominal"]
+                    * self.cal_params["equivalent_beam_angle"]
+                )
+                + self.cal_params["Sv_offset"]
+            )  # see p.90-91 for this correction to Sv
+            out.name = "Sv"
+
+        elif cal_type == "TS":
+            # eq.(10)
+            out = EL - SL + 2 * spreading_loss + absorption_loss
+            out.name = "TS"
+        else:
+            raise ValueError("cal_type not recognized!")
+
+        # Attach calculated range (with units meter) into data set
+        out = out.to_dataset()
+        out = out.merge(self.range_meter)
+
+        # Add frequency_nominal to data set
+        out["frequency_nominal"] = self.echodata["Sonar/Beam_group1"]["frequency_nominal"]
+
+        # Add env and cal parameters
+        out = self._add_params_to_output(out)
+
+        # Order the dimensions
+        out["echo_range"] = out["echo_range"].transpose("channel", "ping_time", "range_sample")
+
+        # Squeeze out the beam dim
+        # doing it here because both out and self.cal_params["equivalent_beam_angle"] has beam dim
+        return out.squeeze("beam", drop=True)
+
+    def compute_Sv(self, **kwargs):
+        return self._cal_power(cal_type="Sv")
+
+    def compute_TS(self, **kwargs):
+        return self._cal_power(cal_type="TS")
---
+++ packages/echopype/echopype-0.6.3.tar.gz/echopype-0.6.3/echopype/preprocess/api.py
@@ -0,0 +1,331 @@
+"""
+Functions for enhancing the spatial and temporal coherence of data.
+"""
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from ..utils.prov import echopype_prov_attrs
+from .noise_est import NoiseEst
+
+
+def _check_range_uniqueness(da):
+    """
+    Check if range (``echo_range``) changes across ping in a given frequency channel.
+    """
+    # squeeze to remove "channel" dim if present
+    # TODO: not sure why not already removed for the AZFP case. Investigate.
+    da = da.squeeze()
+
+    # remove pings with NaN entries if exist
+    # since goal here is to check uniqueness
+    if np.unique(da.isnull(), axis=0).shape[0] != 1:
+        da = da.dropna(dim="ping_time", how="any")
+
+    # remove padded NaN entries if exist for all pings
+    da = da.dropna(dim="range_sample", how="all")
+
+    ping_time_idx = np.argwhere([dim == "ping_time" for dim in da.dims])[0][0]
+    if np.unique(da, axis=ping_time_idx).shape[ping_time_idx] == 1:
+        return xr.DataArray(data=True, coords={"channel": da["channel"].values})
+    else:
+        return xr.DataArray(data=False, coords={"channel": da["channel"].values})
+
+
+def _freq_MVBS(ds, rint, pbin):
+    # squeeze to remove "channel" dim if present
+    # TODO: not sure why not already removed for the AZFP case. Investigate.
+    ds = ds.squeeze()
+
+    # average should be done in linear domain
+    sv = 10 ** (ds["Sv"] / 10)
+
+    # set 1D coordinate using the 1st ping echo_range since identical for all pings
+    # remove padded NaN entries if exist for all pings
+    er = (
+        ds["echo_range"]
+        .dropna(dim="range_sample", how="all")
+        .dropna(dim="ping_time")
+        .isel(ping_time=0)
+    )
+
+    # use first ping er as indexer for sv
+    sv = sv.sel(range_sample=er.range_sample.values)
+    sv.coords["echo_range"] = (["range_sample"], er.values)
+    sv = sv.swap_dims({"range_sample": "echo_range"})
+    sv_groupby_bins = (
+        sv.resample(ping_time=pbin, skipna=True)
+        .mean(skipna=True)
+        .groupby_bins("echo_range", bins=rint, right=False, include_lowest=True)
+        .mean(skipna=True)
+    )
+    sv_groupby_bins.coords["echo_range"] = (["echo_range_bins"], rint[:-1])
+    sv_groupby_bins = sv_groupby_bins.swap_dims({"echo_range_bins": "echo_range"}).drop_vars(
+        "echo_range_bins"
+    )
+    return 10 * np.log10(sv_groupby_bins)
+
+
+def _set_MVBS_attrs(ds):
+    """
+    Attach common attributes.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        dataset containing MVBS
+    """
+    ds["ping_time"].attrs = {
+        "long_name": "Ping time",
+        "standard_name": "time",
+        "axis": "T",
+    }
+
+    ds["Sv"].attrs = {
+        "long_name": "Mean volume backscattering strength (MVBS, mean Sv re 1 m-1)",
+        "units": "dB",
+        "actual_range": [
+            round(float(ds["Sv"].min().values), 2),
+            round(float(ds["Sv"].max().values), 2),
+        ],
+    }
+
+
+def compute_MVBS(ds_Sv, range_meter_bin=20, ping_time_bin="20S"):
+    """
+    Compute Mean Volume Backscattering Strength (MVBS)
+    based on intervals of range (``echo_range``) and ``ping_time`` specified in physical units.
+
+    Output of this function differs from that of ``compute_MVBS_index_binning``, which computes
+    bin-averaged Sv according to intervals of ``echo_range`` and ``ping_time`` specified as
+    index number.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing Sv and ``echo_range`` [m]
+    range_meter_bin : Union[int, float]
+        bin size along ``echo_range`` in meters, default to ``20``
+    ping_time_bin : str
+        bin size along ``ping_time``, default to ``20S``
+
+    Returns
+    -------
+    A dataset containing bin-averaged Sv
+    """
+
+    if not ds_Sv["echo_range"].groupby("channel").apply(_check_range_uniqueness).all():
+        raise ValueError(
+            "echo_range variable changes across pings in at least one of the frequency channels."
+        )
+
+    # Groupby freq in case of different echo_range (from different sampling intervals)
+    range_interval = np.arange(0, ds_Sv["echo_range"].max() + range_meter_bin, range_meter_bin)
+    ds_MVBS = (
+        ds_Sv.groupby("channel")
+        .apply(_freq_MVBS, args=(range_interval, ping_time_bin))
+        .to_dataset()
+    )
+    # Added this check to support the test in test_process.py::test_compute_MVBS
+    if "filenames" in ds_MVBS.variables:
+        ds_MVBS = ds_MVBS.drop_vars("filenames")
+
+    # ping_time_bin parsing and conversions
+    # Need to convert between pd.Timedelta and np.timedelta64 offsets/frequency strings
+    # https://xarray.pydata.org/en/stable/generated/xarray.Dataset.resample.html
+    # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.resample.html
+    # https://pandas.pydata.org/docs/reference/api/pandas.Timedelta.html
+    # https://pandas.pydata.org/docs/reference/api/pandas.Timedelta.resolution_string.html
+    # https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#dateoffset-objects
+    # https://numpy.org/devdocs/reference/arrays.datetime.html#datetime-units
+    timedelta_units = {
+        "d": {"nptd64": "D", "unitstr": "day"},
+        "h": {"nptd64": "h", "unitstr": "hour"},
+        "t": {"nptd64": "m", "unitstr": "minute"},
+        "min": {"nptd64": "m", "unitstr": "minute"},
+        "s": {"nptd64": "s", "unitstr": "second"},
+        "l": {"nptd64": "ms", "unitstr": "millisecond"},
+        "ms": {"nptd64": "ms", "unitstr": "millisecond"},
+        "u": {"nptd64": "us", "unitstr": "microsecond"},
+        "us": {"nptd64": "ms", "unitstr": "millisecond"},
+        "n": {"nptd64": "ns", "unitstr": "nanosecond"},
+        "ns": {"nptd64": "ms", "unitstr": "millisecond"},
+    }
+    ping_time_bin_td = pd.Timedelta(ping_time_bin)
+    # res = resolution (most granular time unit)
+    ping_time_bin_resunit = ping_time_bin_td.resolution_string.lower()
+    ping_time_bin_resvalue = int(
+        ping_time_bin_td / np.timedelta64(1, timedelta_units[ping_time_bin_resunit]["nptd64"])
+    )
+    ping_time_bin_resunit_label = timedelta_units[ping_time_bin_resunit]["unitstr"]
+
+    # Attach attributes
+    _set_MVBS_attrs(ds_MVBS)
+    ds_MVBS["echo_range"].attrs = {"long_name": "Range distance", "units": "m"}
+    ds_MVBS["Sv"] = ds_MVBS["Sv"].assign_attrs(
+        {
+            "cell_methods": (
+                f"ping_time: mean (interval: {ping_time_bin_resvalue} {ping_time_bin_resunit_label} "  # noqa
+                "comment: ping_time is the interval start) "
+                f"echo_range: mean (interval: {range_meter_bin} meter "
+                "comment: echo_range is the interval start)"
+            ),
+            "binning_mode": "physical units",
+            "range_meter_interval": str(range_meter_bin) + "m",
+            "ping_time_interval": ping_time_bin,
+            "actual_range": [
+                round(float(ds_MVBS["Sv"].min().values), 2),
+                round(float(ds_MVBS["Sv"].max().values), 2),
+            ],
+        }
+    )
+
+    prov_dict = echopype_prov_attrs(process_type="processing")
+    prov_dict["processing_function"] = "preprocess.compute_MVBS"
+    ds_MVBS = ds_MVBS.assign_attrs(prov_dict)
+    ds_MVBS["frequency_nominal"] = ds_Sv["frequency_nominal"]  # re-attach frequency_nominal
+
+    return ds_MVBS
+
+
+def compute_MVBS_index_binning(ds_Sv, range_sample_num=100, ping_num=100):
+    """
+    Compute Mean Volume Backscattering Strength (MVBS)
+    based on intervals of ``range_sample`` and ping number (``ping_num``) specified in index number.
+
+    Output of this function differs from that of ``compute_MVBS``, which computes
+    bin-averaged Sv according to intervals of range (``echo_range``) and ``ping_time`` specified
+    in physical units.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    range_sample_num : int
+        number of samples to average along the ``range_sample`` dimension, default to 100
+    ping_num : int
+        number of pings to average, default to 100
+
+    Returns
+    -------
+    A dataset containing bin-averaged Sv
+    """
+    da_sv = 10 ** (ds_Sv["Sv"] / 10)  # average should be done in linear domain
+    da = 10 * np.log10(
+        da_sv.coarsen(ping_time=ping_num, range_sample=range_sample_num, boundary="pad").mean(
+            skipna=True
+        )
+    )
+
+    # Attach attributes and coarsened echo_range
+    da.name = "Sv"
+    ds_MVBS = da.to_dataset()
+    ds_MVBS.coords["range_sample"] = (
+        "range_sample",
+        np.arange(ds_MVBS["range_sample"].size),
+        {"long_name": "Along-range sample number, base 0"},
+    )  # reset range_sample to start from 0
+    ds_MVBS["echo_range"] = (
+        ds_Sv["echo_range"]
+        .coarsen(  # binned echo_range (use first value in each average bin)
+            ping_time=ping_num, range_sample=range_sample_num, boundary="pad"
+        )
+        .min(skipna=True)
+    )
+    _set_MVBS_attrs(ds_MVBS)
+    ds_MVBS["Sv"] = ds_MVBS["Sv"].assign_attrs(
+        {
+            "cell_methods": (
+                f"ping_time: mean (interval: {ping_num} pings "
+                "comment: ping_time is the interval start) "
+                f"range_sample: mean (interval: {range_sample_num} samples along range "
+                "comment: range_sample is the interval start)"
+            ),
+            "comment": "MVBS binned on the basis of range_sample and ping number specified as index numbers",  # noqa
+            "binning_mode": "sample number",
+            "range_sample_interval": f"{range_sample_num} samples along range",
+            "ping_interval": f"{ping_num} pings",
+            "actual_range": [
+                round(float(ds_MVBS["Sv"].min().values), 2),
+                round(float(ds_MVBS["Sv"].max().values), 2),
+            ],
+        }
+    )
+
+    prov_dict = echopype_prov_attrs(process_type="processing")
+    prov_dict["processing_function"] = "preprocess.compute_MVBS_index_binning"
+    ds_MVBS = ds_MVBS.assign_attrs(prov_dict)
+    ds_MVBS["frequency_nominal"] = ds_Sv["frequency_nominal"]  # re-attach frequency_nominal
+
+    return ds_MVBS
+
+
+def estimate_noise(ds_Sv, ping_num, range_sample_num, noise_max=None):
+    """
+    Remove noise by using estimates of background noise
+    from mean calibrated power of a collection of pings.
+
+    See ``remove_noise`` for reference.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    ping_num : int
+        number of pings to obtain noise estimates
+    range_sample_num : int
+        number of samples along the ``range_sample`` dimension to obtain noise estimates
+    noise_max : float
+        the upper limit for background noise expected under the operating conditions
+
+    Returns
+    -------
+    A DataArray containing noise estimated from the input ``ds_Sv``
+    """
+    noise_obj = NoiseEst(ds_Sv=ds_Sv.copy(), ping_num=ping_num, range_sample_num=range_sample_num)
+    noise_obj.estimate_noise(noise_max=noise_max)
+    return noise_obj.Sv_noise
+
+
+def remove_noise(ds_Sv, ping_num, range_sample_num, noise_max=None, SNR_threshold=3):
+    """
+    Remove noise by using estimates of background noise
+    from mean calibrated power of a collection of pings.
+
+    Reference: De Robertis & Higginbottom. 2007.
+    A post-processing technique to estimate the signal-to-noise ratio
+    and remove echosounder background noise.
+    ICES Journal of Marine Sciences 64(6): 1282–1291.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    ping_num : int
+        number of pings to obtain noise estimates
+    range_sample_num : int
+        number of samples along the ``range_sample`` dimension to obtain noise estimates
+    noise_max : float
+        the upper limit for background noise expected under the operating conditions
+    SNR_threshold : float
+        acceptable signal-to-noise ratio, default to 3 dB
+
+    Returns
+    -------
+    The input dataset with additional variables, including
+    the corrected Sv (``Sv_corrected``) and the noise estimates (``Sv_noise``)
+    """
+    noise_obj = NoiseEst(ds_Sv=ds_Sv.copy(), ping_num=ping_num, range_sample_num=range_sample_num)
+    noise_obj.remove_noise(noise_max=noise_max, SNR_threshold=SNR_threshold)
+    ds_Sv = noise_obj.ds_Sv
+
+    prov_dict = echopype_prov_attrs(process_type="processing")
+    prov_dict["processing_function"] = "preprocess.remove_noise"
+    ds_Sv = ds_Sv.assign_attrs(prov_dict)
+
+    return ds_Sv
+
+
+def regrid():
+    return 1

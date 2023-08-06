276b9d42cc3f3c0a1681957ccc11b6b98920f736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:15 +0000

    Add package oarepo-model-builder-multilingual/0.2.3/oarepo_model_builder_multilingual-0.2.3-py3-none-any.whl

---
+++ packages/oarepo-model-builder-multilingual/oarepo_model_builder_multilingual-0.2.3-py3-none-any.whl/oarepo_model_builder_multilingual/model_preprocessors/multilingual.py
@@ -0,0 +1,10 @@
+from oarepo_model_builder.model_preprocessors import ModelPreprocessor
+
+class MultilingualModelPreprocessor(ModelPreprocessor):
+    def transform(self, schema, settings):
+        self.set(settings.python, 'multilingual-dumper-class',
+                 lambda: f'{settings.python.record_records_package}.multilingual_dumper.MultilingualDumper')
+        self.set(settings.python, 'multilingual-schema-class',
+                 lambda: f'{settings.python.record_services_package}.multilingual_schema.MultilingualSchema')
+        self.set(settings.python, 'i18n-schema-class',
+                 lambda: f'{settings.python.record_services_package}.i18nStr_schema.i18nStrSchema')

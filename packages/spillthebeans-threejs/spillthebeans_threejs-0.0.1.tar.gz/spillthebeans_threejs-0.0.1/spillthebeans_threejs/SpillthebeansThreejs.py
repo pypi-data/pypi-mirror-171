05cdebeefaf9fe13e9da5b2bbea29da4f4de8931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:35 +0000

    Add package spillthebeans-threejs/0.0.1/spillthebeans_threejs-0.0.1.tar.gz

---
+++ packages/spillthebeans-threejs/spillthebeans_threejs-0.0.1.tar.gz/spillthebeans_threejs-0.0.1/spillthebeans_threejs/SpillthebeansThreejs.py
@@ -0,0 +1,39 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class SpillthebeansThreejs(Component):
+    """A SpillthebeansThreejs component.
+
+
+Keyword arguments:
+
+- id (string; required):
+    The ID used to identify the container for the IFC viewer
+    component.
+
+- maxAngle (number; required)
+
+- rotation (number; required)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'spillthebeans_threejs'
+    _type = 'SpillthebeansThreejs'
+    @_explicitize_args
+    def __init__(self, id=Component.REQUIRED, rotation=Component.REQUIRED, maxAngle=Component.REQUIRED, axis=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'maxAngle', 'rotation']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'maxAngle', 'rotation']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['id', 'maxAngle', 'rotation']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(SpillthebeansThreejs, self).__init__(**args)

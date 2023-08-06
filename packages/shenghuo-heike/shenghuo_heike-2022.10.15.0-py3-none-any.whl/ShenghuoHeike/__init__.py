b8574f2cfaece26d8f6da1cd5eae0ea7c77f1f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:40 +0000

    Add package shenghuo-heike/2022.10.15.0/shenghuo_heike-2022.10.15.0-py3-none-any.whl

---
+++ packages/shenghuo-heike/shenghuo_heike-2022.10.15.0-py3-none-any.whl/ShenghuoHeike/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shenghuo-heike
+https://github.com/apachecn/shenghuo-heike"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

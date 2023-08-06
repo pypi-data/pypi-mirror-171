22e2b8134d391eecd0cf8644daecff382dc3234e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:57 +0000

    Add package epigenetics-utilities/0.1.5/epigenetics_utilities-0.1.5.tar.gz

---
+++ packages/epigenetics-utilities/epigenetics_utilities-0.1.5.tar.gz/epigenetics_utilities-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+	name='epigenetics_utilities',
+	packages=find_packages(include=['epigenetics_utilities']),
+	version='0.1.5',
+	description='Epigenetics Utilities from the Downing Lab',
+	author='Me',
+	license='MIT',
+	install_requires=[],
+	setup_requires=['pytest-runner'],
+	tests_require=['pytest'],
+	test_suite='tests',
+)

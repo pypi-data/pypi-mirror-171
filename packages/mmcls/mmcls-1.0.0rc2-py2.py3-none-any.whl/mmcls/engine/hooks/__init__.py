f7495fdb707a96bd6391067aa59561291ca4e11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:20 +0000

    Add package mmcls/1.0.0rc2/mmcls-1.0.0rc2-py2.py3-none-any.whl

---
+++ packages/mmcls/mmcls-1.0.0rc2-py2.py3-none-any.whl/mmcls/engine/hooks/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .class_num_check_hook import ClassNumCheckHook
+from .precise_bn_hook import PreciseBNHook
+from .visualization_hook import VisualizationHook
+
+__all__ = [
+    'ClassNumCheckHook',
+    'PreciseBNHook',
+    'VisualizationHook',
+]
---
+++ packages/mmcls/mmcls-1.0.0rc2-py2.py3-none-any.whl/mmcls/utils/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .collect_env import collect_env
+from .setup_env import register_all_modules
+
+__all__ = ['collect_env', 'register_all_modules']

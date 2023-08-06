e6bfb1e0867080174e316834710a05a83022f020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:14 +0000

    Add package conan/2.0.0b4/conan-2.0.0b4.tar.gz

---
+++ packages/conan/conan-2.0.0b4.tar.gz/conan-2.0.0b4/conan/cli/commands/search.py
@@ -0,0 +1,31 @@
+from collections import OrderedDict
+
+from conan.api.conan_api import ConanAPIV2
+from conan.cli.command import conan_command, Extender, COMMAND_GROUPS
+from conan.cli.commands.list import print_list_recipes, default_json_formatter
+from conan.cli.common import get_remote_selection
+
+
+# FIXME: "conan search" == "conan list recipes -r="*" -c" --> implement @conan_alias_command??
+@conan_command(group=COMMAND_GROUPS['consumer'], formatters={"text": print_list_recipes, "json": default_json_formatter})
+def search(conan_api: ConanAPIV2, parser, *args):
+    """
+    Searches for package recipes in a remote or remotes
+    """
+    parser.add_argument("query",
+                        help="Search query to find package recipe reference, e.g., 'boost', 'lib*'")
+    parser.add_argument("-r", "--remote", default="*", action=Extender,
+                        help="Remote names. Accepts wildcards. If not specified it searches "
+                             "in all remotes")
+    args = parser.parse_args(*args)
+
+    remotes = get_remote_selection(conan_api, args.remote)
+
+    results = OrderedDict()
+    for remote in remotes:
+        name = getattr(remote, "name", None)
+        try:
+            results[name] = {"recipes": conan_api.search.recipes(args.query, remote)}
+        except Exception as e:
+            results[name] = {"error": str(e)}
+    return results
---
+++ packages/conan/conan-2.0.0b4.tar.gz/conan-2.0.0b4/conan/tools/microsoft/layout.py
@@ -0,0 +1,34 @@
+import os
+
+from conan.tools.microsoft.msbuild import msbuild_arch
+from conans.errors import ConanException
+
+
+def vs_layout(conanfile):
+    """
+    Initialize a layout for a typical Visual Studio project.
+
+    :param conanfile: ``< ConanFile object >`` The current recipe object. Always use ``self``.
+    """
+    conanfile.folders.test_output = ""
+    if not conanfile.settings.get_safe("build_type"):
+        raise ConanException("The 'vs_layout' requires the 'build_type' setting")
+    if not conanfile.settings.get_safe("arch"):
+        raise ConanException("The 'vs_layout' requires the 'arch' setting")
+
+    if conanfile.settings.arch != "x86":
+        arch = msbuild_arch(conanfile.settings.arch)
+        if not arch:
+            raise ConanException("The 'vs_layout' doesn't "
+                                 "work with the arch '{}'".format(conanfile.settings.arch))
+        bindirs = os.path.join(arch, str(conanfile.settings.build_type))
+    else:
+        bindirs = str(conanfile.settings.build_type)
+
+    subproject = conanfile.folders.subproject
+    conanfile.folders.build = subproject or "."
+    conanfile.folders.generators = os.path.join(subproject, "conan") if subproject else "conan"
+    conanfile.folders.source = subproject or "."
+    conanfile.cpp.build.libdirs = [bindirs]
+    conanfile.cpp.build.bindirs = [bindirs]
+    conanfile.cpp.source.includedirs = ["include"]

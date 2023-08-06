8ac6565ba45ec22d9c835d97d24e777a4951d7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:01 +0000

    Add package poetry-build-testing-12345/0.1.3/poetry_build_testing_12345-0.1.3.tar.gz

---
+++ packages/poetry-build-testing-12345/poetry_build_testing_12345-0.1.3.tar.gz/poetry_build_testing_12345-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_build_testing_12345']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+extras_require = \
+{'boto3': ['boto3>=1.24.89,<2.0.0']}
+
+setup_kwargs = {
+    'name': 'poetry-build-testing-12345',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': '',
+    'author': 'Matthew',
+    'author_email': 'matthew.proxenos@genomicsengland.co.uk',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

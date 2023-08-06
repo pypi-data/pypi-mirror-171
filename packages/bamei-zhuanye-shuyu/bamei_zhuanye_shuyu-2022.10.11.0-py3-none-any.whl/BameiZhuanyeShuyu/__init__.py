450f861a65e8b317af90559b75f9622b67f808db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:51 +0000

    Add package bamei-zhuanye-shuyu/2022.10.11.0/bamei_zhuanye_shuyu-2022.10.11.0-py3-none-any.whl

---
+++ packages/bamei-zhuanye-shuyu/bamei_zhuanye_shuyu-2022.10.11.0-py3-none-any.whl/BameiZhuanyeShuyu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""bamei-zhuanye-shuyu
+https://github.com/apachecn/bamei-zhuanye-shuyu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

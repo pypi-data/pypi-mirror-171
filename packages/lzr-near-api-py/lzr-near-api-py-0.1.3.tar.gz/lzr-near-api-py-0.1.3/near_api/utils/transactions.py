d8b59c6ed37b8386726a612e7286337d5f444e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:17 +0000

    Add package lzr-near-api-py/0.1.3/lzr-near-api-py-0.1.3.tar.gz

---
+++ packages/lzr-near-api-py/lzr-near-api-py-0.1.3.tar.gz/lzr-near-api-py-0.1.3/near_api/utils/transactions.py
@@ -0,0 +1,20 @@
+import time
+import logging
+
+from near_api import transactions
+from near_api.providers import JsonProviderError
+
+
+def tx_retry_with_cool_off(callback, receiver_id: str,
+                           actions: list['transactions.Action'],
+                           delay: int = 0.5, nonce_max_retry: int = 8,
+                           cool_of_factor=1.5):
+    delay_time = delay
+    for i in range(nonce_max_retry):
+        try:
+            result = callback(receiver_id, actions)
+            return result
+        except JsonProviderError as err:
+            time.sleep(delay_time)
+            delay_time *= cool_of_factor
+    return None

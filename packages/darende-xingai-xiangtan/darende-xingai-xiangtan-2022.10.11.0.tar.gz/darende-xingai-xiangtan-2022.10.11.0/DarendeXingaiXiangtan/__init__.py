27e18fe8131b03413405d6a4454f358e98e896e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:58 +0000

    Add package darende-xingai-xiangtan/2022.10.11.0/darende-xingai-xiangtan-2022.10.11.0.tar.gz

---
+++ packages/darende-xingai-xiangtan/darende-xingai-xiangtan-2022.10.11.0.tar.gz/darende-xingai-xiangtan-2022.10.11.0/DarendeXingaiXiangtan/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""darende-xingai-xiangtan
+https://github.com/apachecn/darende-xingai-xiangtan"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

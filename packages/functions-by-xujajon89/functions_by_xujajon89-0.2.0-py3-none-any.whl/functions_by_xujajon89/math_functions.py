e6c0753a75c360ca625082ed0908045a7e7e7aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:59 +0000

    Add package functions-by-xujajon89/0.2.0/functions_by_xujajon89-0.2.0-py3-none-any.whl

---
+++ packages/functions-by-xujajon89/functions_by_xujajon89-0.2.0-py3-none-any.whl/functions_by_xujajon89/math_functions.py
@@ -0,0 +1,12 @@
+def multiplication(NUMBER1, NUMBER2):
+    print(NUMBER1*NUMBER2)
+
+def division(NUMBER1, NUMBER2):
+    print(NUMBER1/NUMBER2)
+   
+   
+def addition(NUMBER1, NUMBER2):
+    print(NUMBER1 + NUMBER2)
+
+def substraction(NUMBER1, NUMBER2):
+    print(NUMBER1- NUMBER2)

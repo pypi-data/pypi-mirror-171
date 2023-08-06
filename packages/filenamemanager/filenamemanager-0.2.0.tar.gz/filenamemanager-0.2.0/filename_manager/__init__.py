ebe544829877b02357625b09045c044d81f7f577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:42 +0000

    Add package filenamemanager/0.2.0/filenamemanager-0.2.0.tar.gz

---
+++ packages/filenamemanager/filenamemanager-0.2.0.tar.gz/filenamemanager-0.2.0/filename_manager/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Filename-Manager."""
+
+__author__ = """Cornelius Emde"""
+__email__ = 'c.emde@me.com'
+__version__ = '0.2.0'
+
+from .filename_manager import FilenameManager
+from .parameter import Parameter

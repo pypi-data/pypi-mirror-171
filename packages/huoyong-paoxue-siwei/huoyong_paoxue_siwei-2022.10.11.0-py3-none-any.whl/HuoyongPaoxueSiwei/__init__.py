9e7e2e6fc8f9f127be40a75cfb7b9d70cb8d6431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:19:39 +0000

    Add package huoyong-paoxue-siwei/2022.10.11.0/huoyong_paoxue_siwei-2022.10.11.0-py3-none-any.whl

---
+++ packages/huoyong-paoxue-siwei/huoyong_paoxue_siwei-2022.10.11.0-py3-none-any.whl/HuoyongPaoxueSiwei/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""huoyong-paoxue-siwei
+https://github.com/apachecn/huoyong-paoxue-siwei"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

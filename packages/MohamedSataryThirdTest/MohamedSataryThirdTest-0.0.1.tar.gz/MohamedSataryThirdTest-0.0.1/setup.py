e5db34c7638ffc5e31e8e6afacf255a83af72967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:01 +0000

    Add package MohamedSataryThirdTest/0.0.1/MohamedSataryThirdTest-0.0.1.tar.gz

---
+++ packages/MohamedSataryThirdTest/MohamedSataryThirdTest-0.0.1.tar.gz/MohamedSataryThirdTest-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+      name='MohamedSataryThirdTest',
+      version='0.0.1',
+      description='''Testing out uploading a library''',
+      py_modules=['helloWorld'],
+      package_dir={'': 'src'},
+      )

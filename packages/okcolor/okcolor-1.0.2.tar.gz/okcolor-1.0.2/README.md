8c86a63c2803a6535804be5793a725b320f8bf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:27 +0000

    Add package okcolor/1.0.2/okcolor-1.0.2.tar.gz

---
+++ packages/okcolor/okcolor-1.0.2.tar.gz/okcolor-1.0.2/README.md
@@ -0,0 +1,23 @@
+# okcolor
+_Generates aesthetically pleasing RGB colors_
+
+# Install
+```bash
+python3 -m pip install okcolor
+```
+
+# Usage
+To generate RGB values:
+```python
+from okcolor import colors
+tuple(colors.visualize(sat=0.5, val=0.95))
+```
+
+To visualize random RGB values:
+```python
+colors.visualize()
+```
+![Screen Shot 2022-10-13 at 11 46 50 PM](https://user-images.githubusercontent.com/24948340/195758007-37126352-1776-44e5-adcf-0fdd512c6243.png)
+
+# License
+Under [MIT License](https://github.com/pncnmnp/okcolor/blob/master/LICENSE)
---
+++ packages/okcolor/okcolor-1.0.2.tar.gz/okcolor-1.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="okcolor",
+    version="1.0.2",
+    author="Parth Parikh",
+    author_email="parthparikh1999p@gmail.com",
+    description="Generates aesthetically pleasing RGB colors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pncnmnp/okcolor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+                'Pillow'
+                 ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)

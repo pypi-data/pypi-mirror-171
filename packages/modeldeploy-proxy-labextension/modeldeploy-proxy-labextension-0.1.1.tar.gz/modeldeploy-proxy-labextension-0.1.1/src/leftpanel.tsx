88d9f8ff8db77d6a16e03203d53dca6f16da165c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:35 +0000

    Add package modeldeploy-proxy-labextension/0.1.1/modeldeploy-proxy-labextension-0.1.1.tar.gz

---
+++ packages/modeldeploy-proxy-labextension/modeldeploy-proxy-labextension-0.1.1.tar.gz/modeldeploy-proxy-labextension-0.1.1/src/leftpanel.tsx
@@ -0,0 +1,54 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin, ILayoutRestorer, ILabShell } from '@jupyterlab/application';
+import { IToolbarWidgetRegistry, ReactWidget } from '@jupyterlab/apputils';
+import { ITranslator } from '@jupyterlab/translation';
+import { TransformerLeftPanel } from './widgets/TransformerLeftPanel';
+import { INotebookTracker } from '@jupyterlab/notebook';
+import * as React from 'react';
+import '../style/index.css';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import { SETTINGS_ID } from './settings';
+
+let transformerSettings: ISettingRegistry.ISettings;
+export default {
+    id: 'modeldeploy-proxy-labextension:leftpanel',
+    requires: [ILabShell, ISettingRegistry, ILayoutRestorer, INotebookTracker, IToolbarWidgetRegistry],
+    optional: [ITranslator],
+    autoStart: true,
+    activate: async (
+        app: JupyterFrontEnd,
+        labShell: ILabShell,
+        settingRegistry: ISettingRegistry,
+        restorer: ILayoutRestorer,
+        tracker: INotebookTracker,
+        toolbarRegistry: IToolbarWidgetRegistry,
+        translator: ITranslator,
+    ) => {
+        Promise.all([settingRegistry.load(SETTINGS_ID)]).then(([settings]) => {
+            transformerSettings = settings;
+        });
+        let widget: ReactWidget;
+        async function loadPanel() {
+            if (! widget.isAttached) {
+                labShell.add(widget, 'left');
+            }
+        }
+
+        app.started.then(() => {
+            widget = ReactWidget.create(
+                <TransformerLeftPanel
+                  tracker={tracker}
+                  transformerSettings={transformerSettings}
+                />,
+            );
+            widget.id = 'modeldeploy-proxy-labextension/transformer-leftpanel-widget';
+            widget.title.iconClass = 'transformer-logo jp-sidebar-tabicon-transformer';
+            widget.title.caption = 'Transformer Panel';
+            widget.node.classList.add('transformer-panel');
+            restorer.add(widget, widget.id);
+        });
+
+        app.restored.then(() => {
+            loadPanel();
+        });
+    },
+} as JupyterFrontEndPlugin<void>;

90c3766a059bc445ea8c90b25a1bf8e201db9d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:57 +0000

    Add package df-qwerty/0.9.0/df_qwerty-0.9.0.tar.gz

---
+++ packages/df-qwerty/df_qwerty-0.9.0.tar.gz/df_qwerty-0.9.0/setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup, find_packages
+import pathlib
+from setuptools.command.install import install
+
+from utils.downgrade_patch import downgrade
+
+
+LOCATION = pathlib.Path(__file__).parent.resolve()
+
+
+class Downgrade(install):
+    def run(self):
+        downgrade(pathlib.Path("."))
+        install.run(self)
+
+
+# Get the long description from the README file
+readme_file = LOCATION / "README.md"
+
+readme_lines = [line.strip() for line in readme_file.open(encoding="utf-8").readlines()]
+description = [line for line in readme_lines if line and not line.startswith("#")][0]
+long_description = "\n".join(readme_lines)
+
+
+def read_requirements():
+    """parses requirements from requirements.txt"""
+    reqs_file = LOCATION / "requirements.txt"
+    reqs = [line.strip() for line in reqs_file.open(encoding="utf8").readlines() if not line.strip().startswith("#")]
+
+    names = []
+    links = []
+    for req in reqs:
+        if "://" in req:
+            links.append(req)
+        else:
+            names.append(req)
+    return {"install_requires": names, "dependency_links": links}
+
+
+setup(
+    name="df_qwerty",
+    version="0.9.0",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepmipt/dialog_flow_engine",
+    author="Denis Kuznetsov",
+    author_email="kuznetsov.den.p@gmail.com",
+    classifiers=[  # Optional
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="chatbots",  # Optional
+    packages=find_packages(where="."),  # Required
+    python_requires=">=3.6, <4",
+    install_requires=["pydantic>=1.8.2"],  # Optional
+    cmdclass={"install": Downgrade},
+)

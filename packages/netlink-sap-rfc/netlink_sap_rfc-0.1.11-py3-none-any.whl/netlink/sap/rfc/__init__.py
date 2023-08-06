10fc0ef84c611e13ad053ad4df50ab98d2238ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:01 +0000

    Add package netlink-sap-rfc/0.1.11/netlink_sap_rfc-0.1.11-py3-none-any.whl

---
+++ packages/netlink-sap-rfc/netlink_sap_rfc-0.1.11-py3-none-any.whl/netlink/sap/rfc/__init__.py
@@ -0,0 +1,4 @@
+from ._connection import sso, login, login_sid, Server, keepass, Connection
+from ._landscape import Landscape
+from .util import dats_to_date, tims_to_time, datstims_to_datetime, bapi_return
+

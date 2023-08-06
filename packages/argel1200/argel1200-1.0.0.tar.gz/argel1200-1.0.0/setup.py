397ab37dfc644a46f1e560e8b927116cfe1c8a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:19 +0000

    Add package argel1200/1.0.0/argel1200-1.0.0.tar.gz

---
+++ packages/argel1200/argel1200-1.0.0.tar.gz/argel1200-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='argel1200',
+    version='1.0.0',
+    packages=['argel1200'],
+    url='https://github.com/argel1200/argel1200-python',
+    license='MIT',
+    author='argel1200',
+    author_email='argel@msn.com',
+    description='My utility functions'
+    )

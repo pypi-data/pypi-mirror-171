249342d763caa626bcce814b746598852943c838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:28 +0000

    Add package functions-by-nomadstalker/0.2.0/functions_by_nomadstalker-0.2.0.tar.gz

---
+++ packages/functions-by-nomadstalker/functions_by_nomadstalker-0.2.0.tar.gz/functions_by_nomadstalker-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_nomadstalker']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-nomadstalker',
+    'version': '0.2.0',
+    'description': 'This is our test project',
+    'long_description': '# Instructions\n\n#### This is our test project.',
+    'author': 'Omurbek Orozaliev',
+    'author_email': 'boexpresskg@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

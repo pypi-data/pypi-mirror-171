70370f47bdd4c826aba8d3d4a6af4f6feeb61256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:31 +0000

    Add package speeddb/1.1.3/speeddb-1.1.3.tar.gz

---
+++ packages/speeddb/speeddb-1.1.3.tar.gz/speeddb-1.1.3/speeddb/__init__.py
@@ -0,0 +1,17 @@
+'''
+# SpeedDB
+## a simple open source multi-model database
+
+* [Documentation](https://speeddb.github.io/)
+* [Repository](https://github.com/SpeedDB/speeddb)
+* [Discord](https://discord.gg/Az8McWNAcg)
+
+###### Made by Nawaf Alqari in 2022 :)
+
+'''
+
+__version__ = "1.1.3"
+__all__ = ["connect", "DocumentDatabase", "KeyValDatabase", "init", "build_db", "destroy_db", "TasksQueue", "Collection"]
+
+from .database import DocumentDatabase, KeyValDatabase, Collection, connect, init, build_db, destroy_db
+from .tasks_queue import TasksQueue

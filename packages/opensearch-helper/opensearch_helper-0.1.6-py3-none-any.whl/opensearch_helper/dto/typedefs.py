3a1df7b18cfcec3add29cbc933625af94c7f58d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:49 +0000

    Add package opensearch-helper/0.1.6/opensearch_helper-0.1.6-py3-none-any.whl

---
+++ packages/opensearch-helper/opensearch_helper-0.1.6-py3-none-any.whl/opensearch_helper/dto/typedefs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+""" Define Strict Types for the Project """
+
+
+from typing import Any
+from typing import List
+from typing import Dict
+from typing import Optional
+from typing import TypedDict
+
+
+class MultiMatchInner2(TypedDict):
+    query: str
+    fields: List[str]
+
+
+class MultiMatchInner1(TypedDict):
+    multi_match: MultiMatchInner2
+
+
+class MultiMatchQuery(TypedDict):
+    size: int
+    query: MultiMatchInner1
+
+
+class OpenSearchResult(TypedDict):
+    response: Dict[str, Any]
+    events: List[Dict[str, Any]]
+
+
+class ScoreResult(TypedDict):
+    score: float
+    type: Optional[str]

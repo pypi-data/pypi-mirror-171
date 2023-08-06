08c218f542c16030d44ff1cbfc4b25017756570b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:15 +0000

    Add package fast-tmp/1.0.8/fast-tmp-1.0.8.tar.gz

---
+++ packages/fast-tmp/fast-tmp-1.0.8.tar.gz/fast-tmp-1.0.8/fast_tmp/amis/crud.py
@@ -0,0 +1,22 @@
+from typing import List, Optional, Union
+
+from fast_tmp.amis.base import BaseAmisModel, _Action
+from fast_tmp.amis.buttons import Operation
+from fast_tmp.amis.enums import TypeEnum
+from fast_tmp.amis.forms import Column
+from fast_tmp.amis.forms.filter import FilterModel
+
+
+class CRUD(BaseAmisModel):
+    type = TypeEnum.crud
+    api: str  # 相对路径
+    # 可以在后面跟上按钮，则默认每一行都有按钮，
+    # 参考：https://baidu.gitee.io/amis/docs/components/dialog?page=1
+    columns: List[Union[Column, _Action, Operation]]
+    affixHeader: bool = False
+    quickSaveItemApi: Optional[str]  # 快速保存
+    syncLocation: Optional[bool]
+    filter: Optional[FilterModel]
+
+    class Config:
+        arbitrary_types_allowed = True

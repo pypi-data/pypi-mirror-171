c7d813157a7437b2b1662594231af13e32554f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:08 +0000

    Add package caosadvancedtools/0.6.0/caosadvancedtools-0.6.0-py3-none-any.whl

---
+++ packages/caosadvancedtools/caosadvancedtools-0.6.0-py3-none-any.whl/caosadvancedtools/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM caosadvancedtools SETUP.PY
+#
+short_version = '0.6.0'
+version = '0.6.0'
+full_version = '0.6.0'
+git_revision = '479e5545a68c1d9434efd5c6a7d69e6ef846f94f'
+release = True
+
+if not release:
+    version = full_version

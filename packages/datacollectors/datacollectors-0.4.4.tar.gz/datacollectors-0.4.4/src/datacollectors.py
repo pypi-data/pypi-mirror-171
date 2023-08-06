28424a4884e43e2ceae247f3593653145791cb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:50 +0000

    Add package datacollectors/0.4.4/datacollectors-0.4.4.tar.gz

---
+++ packages/datacollectors/datacollectors-0.4.4.tar.gz/datacollectors-0.4.4/src/datacollectors.py
@@ -0,0 +1,23 @@
+import datacollector_ECB
+import datacollector_ICE
+import datacollector_Boliga
+import datacollector_Business_insider
+import datacollector_Entsoe
+
+
+class ECB(datacollector_ECB.ECB):
+    pass
+
+class ICE(datacollector_ICE.ICE):
+    pass
+
+class Entsoe(datacollector_Entsoe.Entsoe):
+    pass
+
+class Boliga(datacollector_Boliga.Boliga):
+    pass
+
+class Business_insider(datacollector_Business_insider.Business_insider):
+    pass
+
+

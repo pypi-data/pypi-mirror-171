89a8d45fe6ed00c5f4502dbac67e7493999543d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:53 +0000

    Add package beidaozhuide-mimi/2022.10.11.0/beidaozhuide_mimi-2022.10.11.0-py3-none-any.whl

---
+++ packages/beidaozhuide-mimi/beidaozhuide_mimi-2022.10.11.0-py3-none-any.whl/BeidaozhuideMimi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""beidaozhuide-mimi
+https://github.com/apachecn/beidaozhuide-mimi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

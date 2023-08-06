55d29c1260a7a602a6437c4cdfb487e45b5b0dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:24 +0000

    Add package chanlun-jingjie/2022.10.15.0/chanlun_jingjie-2022.10.15.0-py3-none-any.whl

---
+++ packages/chanlun-jingjie/chanlun_jingjie-2022.10.15.0-py3-none-any.whl/ChanlunJingjie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chanlun-jingjie
+https://github.com/apachecn/chanlun-jingjie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

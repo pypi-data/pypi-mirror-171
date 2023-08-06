a4be3c8e7c567fe68d6186898b7574bb820ecdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:55 +0000

    Add package functions-by-darimco/0.2.0/functions_by_darimco-0.2.0.tar.gz

---
+++ packages/functions-by-darimco/functions_by_darimco-0.2.0.tar.gz/functions_by_darimco-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['functions_by_darimco']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'functions-by-darimco',
+    'version': '0.2.0',
+    'description': 'This is our test project',
+    'long_description': '# Instructions\n\n### This is our test project.',
+    'author': 'Darimco',
+    'author_email': 'dasha.rzhanova789@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)

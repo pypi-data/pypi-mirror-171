2d19713d3dbcf2607e367ae77c874058ee1238a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:37 +0000

    Add package sparkpipelineframework/1.0.29/sparkpipelineframework-1.0.29-py3-none-any.whl

---
+++ packages/sparkpipelineframework/sparkpipelineframework-1.0.29-py3-none-any.whl/spark_pipeline_framework/transformers/framework_fixed_width_loader/v1/framework_fixed_width_loader.py
@@ -0,0 +1,171 @@
+from logging import Logger
+from pathlib import Path
+from typing import Union, List, Optional, Dict, Any, NamedTuple
+
+from pyspark import keyword_only
+from pyspark.ml.param import Param
+from pyspark.sql import DataFrameReader
+from pyspark.sql.dataframe import DataFrame
+from pyspark.sql.functions import col, trim
+from pyspark.sql.types import DataType, Row
+
+from spark_pipeline_framework.logger.yarn_logger import get_logger
+
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+
+from spark_pipeline_framework.transformers.framework_transformer.v1.framework_transformer import (
+    FrameworkTransformer,
+)
+from spark_pipeline_framework.utilities.file_helpers import get_absolute_paths
+
+
+class ColumnSpec(NamedTuple):
+    """
+    The definition of a column for fixed width file formats
+        column_name: the name of the column
+
+        start_pos: the starting position of the data for this column in the file
+
+        length: the length of the data for this column
+
+        data_type: the data type for the data in the column, e.g. StringType(), IntegerType()
+
+    example:
+      ColumnSpec(column_name="id", start_pos=1, length=3, data_type=StringType())
+    """
+
+    column_name: str
+    start_pos: int
+    length: int
+    data_type: DataType
+
+
+class FrameworkFixedWidthLoader(FrameworkTransformer):
+    """
+    Load fixed width files into a dataframe by specifying the path to the file and the schema
+    """
+
+    # noinspection PyUnusedLocal
+    @keyword_only
+    def __init__(
+        self,
+        view: str,
+        filepath: Union[str, List[str], Path],
+        columns: List[ColumnSpec],
+        has_header: bool = True,
+        name: Optional[str] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+    ) -> None:
+        """
+        Initializes the fixed_width_file_loader
+
+        :param view: The name of the view that the resultant DataFrame will be stored in
+        :param filepath: The path to the fixed width file to load
+        :param columns: A list of columns defined using a ColumnSpec that defines the name, start_index, length and DataType for the column
+        :param name: sets the name of the transformer as it will appear in logs
+        :param parameters:
+        :param progress_logger:
+
+        example:
+            FrameworkFixedWidthLoader(
+                view="my_view",
+                filepath=test_file_path,
+
+                columns=[
+                    ColumnSpec(column_name="id", start_pos=1, length=3, data_type=StringType()),
+
+                    ColumnSpec(
+                        column_name="some_date", start_pos=4, length=8, data_type=StringType()
+                    ),
+
+                    ColumnSpec(
+                        column_name="some_string",
+                        start_pos=12,
+                        length=3,
+                        data_type=StringType(),
+                    ),
+
+                    ColumnSpec(
+                        column_name="some_integer",
+                        start_pos=15,
+                        length=4,
+                        data_type=IntegerType(),
+                    ),
+                ],
+            )
+        """
+        super().__init__(
+            name=name, parameters=parameters, progress_logger=progress_logger
+        )
+        self.logger: Logger = get_logger(__name__)
+
+        self.view: Param[str] = Param(self, "view", "")
+        self._setDefault(view=None)
+
+        self.filepath: Param[str] = Param(self, "filepath", "")
+        self._setDefault(filepath=None)
+
+        self.columns: Param[List[ColumnSpec]] = Param(self, "columns", "")
+        self._setDefault(columns=None)
+
+        self.has_header: Param[bool] = Param(self, "has_header", "")
+        self._setDefault(has_header=True)
+
+        if not filepath:
+            raise ValueError("filepath is None or empty")
+
+        self.logger.info(f"Received filepath: {filepath}")
+
+        kwargs = self._input_kwargs
+        self.setParams(**kwargs)
+
+    def _transform(self, df: DataFrame) -> DataFrame:
+        view = self.getView()
+        filepath: Union[str, List[str], Path] = self.getFilepath()
+        columns: List[ColumnSpec] = self.getColumns()
+        progress_logger: Optional[ProgressLogger] = self.getProgressLogger()
+        paths = get_absolute_paths(filepath=filepath)
+        has_header = self.getHasHeader()
+
+        if progress_logger:
+            progress_logger.write_to_log(f"Loading file for view {view}: {paths}")
+            progress_logger.log_param("data_path", ", ".join(paths))
+
+        df_reader: DataFrameReader = df.sql_ctx.read
+        df_text = df_reader.text(paths=paths)
+        if has_header:
+            first: Optional[Row] = df_text.first()
+            if first is not None:
+                header = first[0]
+                df_text = df_text.filter(~col("value").contains(header))
+        df_text = df_text.select(
+            *[
+                trim(col("value").substr(column.start_pos, column.length))
+                .cast(column.data_type)
+                .alias(column.column_name)
+                for column in columns
+            ]
+        )
+        df_text.createOrReplaceTempView(view)
+
+        progress_logger and progress_logger.write_to_log(
+            f"Finished Loading file for View[{view}]: {paths}"
+        )
+        return df
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getView(self) -> str:
+        return self.getOrDefault(self.view)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getFilepath(self) -> Union[str, List[str], Path]:
+        return self.getOrDefault(self.filepath)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getColumns(self) -> List[ColumnSpec]:
+        return self.getOrDefault(self.columns)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getHasHeader(self) -> bool:
+        return self.getOrDefault(self.has_header)
---
+++ packages/sparkpipelineframework/sparkpipelineframework-1.0.29-py3-none-any.whl/spark_pipeline_framework/transformers/framework_json_loader/v1/framework_json_loader.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from typing import Union, List, Dict, Any, Optional
+
+from pyspark import keyword_only
+from pyspark.ml.param import Param
+
+from pyspark.sql.dataframe import DataFrame
+
+from pyspark.sql.types import StructType
+
+from spark_pipeline_framework.progress_logger.progress_logger import ProgressLogger
+
+from spark_pipeline_framework.transformers.framework_local_file_loader.v1.framework_local_file_loader import (
+    FrameworkLocalFileLoader,
+)
+from spark_pipeline_framework.utilities.file_modes import FileReadModes
+
+
+class FrameworkJsonLoader(FrameworkLocalFileLoader):
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    @keyword_only
+    def __init__(
+        self,
+        view: str,
+        filepath: Union[str, List[str], Path],
+        clean_column_names: bool = False,
+        name: Optional[str] = None,
+        progress_logger: Optional[ProgressLogger] = None,
+        parameters: Optional[Dict[str, Any]] = None,
+        limit: int = -1,
+        delimiter: str = ",",
+        has_header: bool = True,
+        infer_schema: bool = False,
+        cache_table: bool = True,
+        schema: Optional[StructType] = None,
+        create_file_path: bool = False,
+        use_schema_from_view: Optional[str] = None,
+        mode: str = FileReadModes.MODE_PERMISSIVE,
+    ) -> None:
+        super().__init__(
+            view=view,
+            filepath=filepath,
+            clean_column_names=clean_column_names,
+            name=name,
+            parameters=parameters,
+            progress_logger=progress_logger,
+            delimiter=delimiter,
+            limit=limit,
+            has_header=has_header,
+            infer_schema=infer_schema,
+            cache_table=cache_table,
+            schema=schema,
+            create_file_path=create_file_path,
+            use_schema_from_view=use_schema_from_view,
+            mode=mode,
+        )
+
+        self.multiLine: Param[bool] = Param(self, "multiLine", "")
+        self._setDefault(multiLine=False)
+        self._set(multiLine=False)
+
+    def preprocess(self, df: DataFrame, absolute_paths: List[str]) -> None:
+        """
+        In pre-processing we try to detect whether the file is a normal json or ndjson
+        :param df: DataFrame
+        :param absolute_paths: list of paths
+        """
+        assert absolute_paths
+        text_df: DataFrame = df.sql_ctx.read.text(absolute_paths)
+        # read the first line of the file
+        first_line: str = text_df.select("value").limit(1).collect()[0][0]
+        if first_line.lstrip().startswith("["):
+            self.setMultiLine(True)
+        else:
+            self.setMultiLine(False)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def setMultiLine(self, value: bool) -> "FrameworkJsonLoader":
+        self._paramMap[self.multiLine] = value
+        return self
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getMultiLine(self) -> bool:
+        return self.getOrDefault(self.multiLine)
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getReaderFormat(self) -> str:
+        return "json"
+
+    # noinspection PyPep8Naming,PyMissingOrEmptyDocstring
+    def getReaderOptions(self) -> Dict[str, Any]:
+        options: Dict[str, Any] = {"multiLine": self.getMultiLine()}
+        return options

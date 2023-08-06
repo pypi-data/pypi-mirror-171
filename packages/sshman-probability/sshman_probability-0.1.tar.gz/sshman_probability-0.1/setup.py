ef2e6438a8252f75b76936cf4ca01cdcf62305e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:39 +0000

    Add package sshman-probability/0.1/sshman_probability-0.1.tar.gz

---
+++ packages/sshman-probability/sshman_probability-0.1.tar.gz/sshman_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sshman_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sshman_probability'],
+      zip_safe=False)

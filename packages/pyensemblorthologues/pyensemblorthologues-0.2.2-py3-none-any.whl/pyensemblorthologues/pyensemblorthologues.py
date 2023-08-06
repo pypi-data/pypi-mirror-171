fc522fd390bb7ae8bc68f287df93ec3ec3d7f476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:20 +0000

    Add package pyensemblorthologues/0.2.2/pyensemblorthologues-0.2.2-py3-none-any.whl

---
+++ packages/pyensemblorthologues/pyensemblorthologues-0.2.2-py3-none-any.whl/pyensemblorthologues/pyensemblorthologues.py
@@ -0,0 +1,12 @@
+from .compara_consumer import ComparaConsumer
+
+"""Main module."""
+
+
+if __name__ == "__main__":
+    # cc = ComparaConsumer()
+    # s = cc.species_sets()
+    # ort = cc.regions()
+    pass
+    # print(ss)
+    # print(ort)

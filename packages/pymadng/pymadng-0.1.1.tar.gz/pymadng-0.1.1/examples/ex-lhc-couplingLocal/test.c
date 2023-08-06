5dcaff4631577ebec06bf69c3ecaef4893757412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:06 +0000

    Add package pymadng/0.1.1/pymadng-0.1.1.tar.gz

---
+++ packages/pymadng/pymadng-0.1.1.tar.gz/pymadng-0.1.1/examples/ex-lhc-couplingLocal/test.c
@@ -0,0 +1,5 @@
+#include <fcntl.h>
+
+int main(){
+    O_NDELAY;
+}
---
+++ packages/pymadng/pymadng-0.1.1.tar.gz/pymadng-0.1.1/src/pymadng/__init__.py
@@ -0,0 +1,13 @@
+from .main import MAD
+
+__title__ = "pymadng"
+__version__ = "0.1.1"
+
+__summary__ = " Python interface to MAD-NG running as subprocess and using Pexpect "
+__uri__ = "https://github.com/MethodicalAcceleratorDesign/MADpy"
+
+__credits__ = """
+Creator: Joshua Gray <joshua.mark.gray@cern.ch>
+"""
+
+__all__ = ["MAD"]

d580b071f3dee2071bc120804354e0aec55e60dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:04 +0000

    Add package Marvel-anup-anm/0.1/Marvel_anup_anm-0.1.tar.gz

---
+++ packages/Marvel-anup-anm/Marvel_anup_anm-0.1.tar.gz/Marvel_anup_anm-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import *
+
+setup(
+    name="Marvel_anup_anm",
+    version=0.1,
+    description=" fetch data from marvel and use filters",
+    author="Anup Kumar Dey",
+    packages=['Marvel_anup_anm'],
+    install_requires=[]
+)

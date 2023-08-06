13da97438a5ed628ebe8d1cfadf36134892e3080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:04 +0000

    Add package abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation/2.2.0/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation-2.2.0.tar.gz

---
+++ packages/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation-2.2.0.tar.gz/abbreviation-uhh-what-is-word-for-tearing-apart-an-abbreviation-2.2.0/src/auwiwftaaa/__init__.py
@@ -0,0 +1,15 @@
+"""
+:copyright: (c) 2022-present SqdNoises
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "auwiwftaaa"
+__full_title__ = "abbreviation uhh what is word for tearing apart an abbreviation"
+__author__ = "SqdNoises"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present SqdNoises"
+__version__ = "2.2.0"
+
+from .start import main as start
+from .start import color
+from . import config

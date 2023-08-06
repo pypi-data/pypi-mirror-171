24d38b213bc0e09e87e72044e7b65027378e599e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:37 +0000

    Add package g4rzk/0.5/g4rzk-0.5.tar.gz

---
+++ packages/g4rzk/g4rzk-0.5.tar.gz/g4rzk-0.5/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup, Extension
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name = "g4rzk",
+  packages = ["g4rzk"],
+  version = "0.5",
+  license ="MIT",
+  description = "Sebuah module packages yang biasa di pakai buat anu ea, ytta",
+  long_description = long_description,
+  long_description_content_type = "text/markdown", 
+  author = "Angga Kurniawan",
+  author_email = "g4rzkurniawan@gmail.com",
+  keywords = ["g4rzk", "garzk", "garz.id", "anggaxd"], 
+  install_requires = [
+          "bs4",
+          "rich", 
+          "inquirer", 
+          "requests", 
+      ],
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.10",
+  ],
+)

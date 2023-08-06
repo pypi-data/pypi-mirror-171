0aae89a5ec8a572fc917bd1a99293c6f416b8c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:43 +0000

    Add package ellipsis/3.0.0/ellipsis-3.0.0-py3-none-any.whl

---
+++ packages/ellipsis/ellipsis-3.0.0-py3-none-any.whl/ellipsis/path/raster/timestamp/order/root.py
@@ -0,0 +1,33 @@
+
+from ellipsis import sanitize
+from ellipsis import apiManager
+
+def get(token):
+    token = sanitize.validString('token', token, True)
+    r = apiManager.get('/path/raster/timestamp/order', None, token)
+    return r
+
+def order(pathId, timestampId, token, extent = None, uploadId = None):
+    
+    token = sanitize.validString('token', token, True)
+    pathId = sanitize.validUuid('pathId', pathId, True)
+    timestampId = sanitize.validUuid('timestampId', timestampId, True)
+    extent = sanitize.validBounds('extent', extent, False)
+    uploadId = sanitize.validUuid('uploadId', uploadId, False)
+
+    body = {'uploadId': uploadId, 'extent':extent}
+    
+    r = apiManager.post('/path/' + pathId + '/raster/timestamp/' + timestampId + '/order', body, token)    
+
+    return r
+
+def download(orderId, filePath, token):
+    
+    token = sanitize.validString('token', token, True)
+    orderId = sanitize.validUuid('orderId', orderId, True)
+    filePath = sanitize.validString('filePath', filePath, True)
+
+    if filePath[len(filePath)-4 : len(filePath) ] != '.tif':
+        raise ValueError('filePath must end with .tif')
+
+    apiManager.download('/path/raster/timestamp/order/' + orderId + '/download', filePath, token)

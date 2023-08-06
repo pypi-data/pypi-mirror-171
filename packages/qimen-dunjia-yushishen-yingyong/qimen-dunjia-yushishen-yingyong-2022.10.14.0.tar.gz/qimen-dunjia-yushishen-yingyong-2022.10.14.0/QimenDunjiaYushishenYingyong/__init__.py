0fb7f7a25427324ba82e53a51d5dfa4d39067f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:52 +0000

    Add package qimen-dunjia-yushishen-yingyong/2022.10.14.0/qimen-dunjia-yushishen-yingyong-2022.10.14.0.tar.gz

---
+++ packages/qimen-dunjia-yushishen-yingyong/qimen-dunjia-yushishen-yingyong-2022.10.14.0.tar.gz/qimen-dunjia-yushishen-yingyong-2022.10.14.0/QimenDunjiaYushishenYingyong/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""qimen-dunjia-yushishen-yingyong
+https://github.com/apachecn/qimen-dunjia-yushishen-yingyong"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.14.0"

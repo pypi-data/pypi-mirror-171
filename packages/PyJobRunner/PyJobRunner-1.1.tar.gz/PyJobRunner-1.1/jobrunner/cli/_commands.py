46f80e9c492fc6bd457c5f460ff97c6e713ad044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:52 +0000

    Add package PyJobRunner/1.1/PyJobRunner-1.1.tar.gz

---
+++ packages/PyJobRunner/PyJobRunner-1.1.tar.gz/PyJobRunner-1.1/jobrunner/cli/_commands.py
@@ -0,0 +1,185 @@
+# Standard libraries
+import os
+import subprocess
+from datetime import date
+
+# Feature libraries
+import click
+
+from . import jobrunner
+from .. import lib
+
+
+@jobrunner.command(name="setup")
+@click.argument("workdir_list", required=True, nargs=-1, type=str)
+def setup(workdir_list):
+    """
+    \b
+    Run setup scripts in a directory
+    \b
+
+    \b
+    Jobfiles in a directory tree provide a list of
+    setup scripts which are composed into a job.setup
+    file and run along the directory tree
+    \b
+
+    \b
+    Bash Variables
+    --------------
+    JobWorkDir - Path to working directory of the job
+    """
+    # Get base directory
+    basedir = os.getcwd()
+
+    # loop over workdir_list
+    for workdir in workdir_list:
+
+        # chdir to working directory
+        print(f"-------------------------------------------------------------")
+        os.chdir(workdir)
+        workdir = os.getcwd()
+        print(f"Working directory: {workdir}")
+
+        # Build main dictionary
+        print(f"Parsing Jobfiles in directory tree")
+        main_dict = lib.ParseJobToml(basedir, workdir)
+
+        # Build setupfile
+        print(f"Creating setup file: job.setup")
+        lib.CreateSetupFile(main_dict)
+
+        # Run setup
+        print(f"Running setup")
+        subprocess.run(f"bash job.setup", shell=True, check=True)
+
+        # Return to base directory
+        os.chdir(basedir)
+
+    print(f"-------------------------------------------------------------")
+
+
+@jobrunner.command(name="submit")
+@click.argument("workdir_list", required=True, nargs=-1, type=str)
+def submit(workdir_list):
+    """
+    \b
+    Submit a job from a directory
+    \b
+
+    \b
+    Jobfiles in a directory tree provide a list of
+    submit scripts which are composed into a job.submit
+    file for linux schedulars
+    \b
+
+    \b
+    Bash Variables
+    --------------
+    JobWorkDir - Path to working directory of the job
+    """
+    # Get base directory
+    basedir = os.getcwd()
+
+    # loop over workdir_list
+    for workdir in workdir_list:
+
+        # chdir to working directory
+        print(f"-------------------------------------------------------------")
+        os.chdir(workdir)
+        workdir = os.getcwd()
+        print(f"Working directory: {workdir}")
+
+        # Build main dictionary
+        print(f"Parsing Jobfiles in directory tree")
+        main_dict = lib.ParseJobToml(basedir, workdir)
+
+        # Build inputfile
+        print(f"Creating input file: job.input")
+        lib.CreateInputFile(main_dict)
+
+        # Build jobfile
+        print(f"Creating submit file: job.submit")
+        lib.CreateSubmitFile(main_dict)
+
+        # Submit job
+        print(f"Submitting job")
+        subprocess.run(
+            f'{main_dict["schedular"]["command"]} job.submit', shell=True, check=True
+        )
+
+        # Return to base directory
+        os.chdir(basedir)
+
+
+@jobrunner.command(name="clean")
+@click.argument("workdir_list", required=True, nargs=-1, type=str)
+def clean(workdir_list):
+    """
+    \b
+    Remove artifacts from a directory
+    \b
+
+    \b
+    This command removes job.input, job.setup, and
+    job.submit files from a working directory
+    \b
+    """
+    # Get base directory
+    basedir = os.getcwd()
+
+    # run cleanup
+    for workdir in workdir_list:
+
+        process = subprocess.run(
+            f'rm -vf {workdir + "/" + "job.input"}',
+            shell=True,
+            check=True,
+        )
+
+        process = subprocess.run(
+            f'rm -vf {workdir + "/" + "job.submit"}',
+            shell=True,
+            check=True,
+        )
+
+        process = subprocess.run(
+            f'rm -vf {workdir + "/" + "job.setup"}',
+            shell=True,
+            check=True,
+        )
+
+
+@jobrunner.command(name="archive")
+@click.option(
+    "--tag", "-t", help="name of the archive", default=str(date.today()), type=str
+)
+@click.argument("workdir_list", required=True, nargs=-1, type=str)
+def archive(tag, workdir_list):
+    """
+    \b
+    Create an archive along a directory tree
+    \b
+    """
+    # Get base directory
+    basedir = os.getcwd()
+
+    # loop over workdir_list
+    for workdir in workdir_list:
+
+        # chdir to working directory
+        print(f"-------------------------------------------------------------")
+        os.chdir(workdir)
+        workdir = os.getcwd()
+        print(f"Working directory: {workdir}")
+
+        # Build main dictionary
+        print(f"Parsing Jobfile configuration")
+        main_dict = lib.ParseJobToml(basedir, workdir)
+
+        # Create archive
+        print(f"Creating archive tag: {tag}")
+        lib.CreateArchive(main_dict, tag)
+
+        # Return to base directory
+        os.chdir(basedir)

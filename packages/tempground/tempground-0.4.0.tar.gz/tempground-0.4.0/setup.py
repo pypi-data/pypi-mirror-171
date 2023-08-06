156ead4ef557b09bdce21352862d1949657ac7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:44 +0000

    Add package tempground/0.4.0/tempground-0.4.0.tar.gz

---
+++ packages/tempground/tempground-0.4.0.tar.gz/tempground-0.4.0/setup.py
@@ -0,0 +1,37 @@
+
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+name = "tempground"
+
+setup(
+    name=name,
+    version="0.4.0",
+
+    author="Artsiom iG",
+    author_email="ortemeo@gmail.com",
+
+    packages=find_packages(include=['tempground', 'tempground.*']),
+    package_data={'tempground': ['py.typed']},
+
+    python_requires='>=3.10',  # 3.10 for `match`
+    install_requires=[],
+
+    long_description=(Path(__file__).parent / 'README.md')
+        .read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
+
+    license="MIT",
+
+    keywords="temo library testing integration".split(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows"
+    ],
+)
+

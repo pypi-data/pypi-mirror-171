af18a9c02a2f9607edf958f7084fe58b3d936fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:51 +0000

    Add package MohamedSatarySixthTest/0.0.1/MohamedSatarySixthTest-0.0.1.tar.gz

---
+++ packages/MohamedSatarySixthTest/MohamedSatarySixthTest-0.0.1.tar.gz/MohamedSatarySixthTest-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name='MohamedSatarySixthTest',
+      version='0.0.1',
+      description='''This library converts Names (or words) from arabic to its 
+      Arabizi form, and vice versa (from Arabizi to arabic)''',
+      py_modules=['helloWorld', 'bye', 'goodBye'],
+      package_dir={'': 'src'},
+      )

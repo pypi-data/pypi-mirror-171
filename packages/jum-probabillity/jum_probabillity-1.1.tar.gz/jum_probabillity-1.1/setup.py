53d668b086256fe6b6ee628833bc922a191e1981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:08 +0000

    Add package jum-probabillity/1.1/jum_probabillity-1.1.tar.gz

---
+++ packages/jum-probabillity/jum_probabillity-1.1.tar.gz/jum_probabillity-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jum_probabillity',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jum_probabillity'],
+      author = 'Jesper Schumacher',
+      author_email = 'mail4jms@gmail.com',
+      zip_safe=False)

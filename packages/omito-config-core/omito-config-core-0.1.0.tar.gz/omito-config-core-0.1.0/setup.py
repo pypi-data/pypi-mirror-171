443862b2b5d37d52b9aab25c141c1f6f3ca07204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:43 +0000

    Add package omito-config-core/0.1.0/omito-config-core-0.1.0.tar.gz

---
+++ packages/omito-config-core/omito-config-core-0.1.0.tar.gz/omito-config-core-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['src']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'deepmerge>=1.0.1,<2.0.0',
+ 'flatten-dict>=0.4.2,<0.5.0',
+ 'python-dotenv>=0.21.0,<0.22.0',
+ 'schematics>=2.1.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'omito-config-core',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Barath Kumar',
+    'author_email': 'barath@mergedup.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

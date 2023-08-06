e6c6c623176c7e8a379669992ccc0aec25b57f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:26 +0000

    Add package gdbd-probablity/1.0/gdbd_probablity-1.0.tar.gz

---
+++ packages/gdbd-probablity/gdbd_probablity-1.0.tar.gz/gdbd_probablity-1.0/gdbd_probablity/__init__.py
@@ -0,0 +1,4 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+from .Generaldistribution  import General
+
---
+++ packages/gdbd-probablity/gdbd_probablity-1.0.tar.gz/gdbd_probablity-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gdbd_probablity',
+      version='1.0',
+      description='Gaussian ,General and binomial distributions',
+      long_description='calculating and visualizing Gaussian, General and Binomial distribution',
+      packages=['gdbd_probablity'],
+      author='Amit Ranjan',
+      author_email='amitranjan1139@gmail.com',
+      zip_safe=False)

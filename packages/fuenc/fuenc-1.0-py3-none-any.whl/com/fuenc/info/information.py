1f3e978ea365d7263b44ca61685e86c9428746a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:48 +0000

    Add package fuenc/1.0/fuenc-1.0-py3-none-any.whl

---
+++ packages/fuenc/fuenc-1.0-py3-none-any.whl/com/fuenc/info/information.py
@@ -0,0 +1,3 @@
+#coding:UTF-8
+def get_information():
+    print('百度网 www.baidu.com')
---
+++ packages/fuenc/fuenc-1.0-py3-none-any.whl/com/fuenc/info/message.py
@@ -0,0 +1,8 @@
+#coding:UTF-8
+_url = 'www.fuenc.com'
+def _hello():
+    print('hello Python!')
+
+def get_info():
+    print('get_info()中主函数__name__:%s'%(__name__))
+    return '测试网 www.test.com'

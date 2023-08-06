c9e449976faf339a0bb154aaf9ba682018972495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:16 +0000

    Add package Record/3.6/Record-3.6.tar.gz

---
+++ packages/Record/Record-3.6.tar.gz/Record-3.6/setup.py
@@ -0,0 +1,63 @@
+##############################################################################
+#
+# Copyright (c) 2010 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '3.6'
+
+setup(
+    name='Record',
+    version=version,
+    url='https://github.com/zopefoundation/Record',
+    license='ZPL 2.1',
+    description="Special Record objects used in Zope.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=open('README.rst').read() + '\n' +
+        open('CHANGES.rst').read(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope :: 4",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    python_requires=', '.join([
+        '>=2.7',
+        '!=3.0.*',
+        '!=3.1.*',
+        '!=3.2.*',
+        '!=3.3.*',
+        '!=3.4.*',
+    ]),
+    install_requires=['ExtensionClass >= 4.1a1'],
+    include_package_data=True,
+    zip_safe=False,
+)

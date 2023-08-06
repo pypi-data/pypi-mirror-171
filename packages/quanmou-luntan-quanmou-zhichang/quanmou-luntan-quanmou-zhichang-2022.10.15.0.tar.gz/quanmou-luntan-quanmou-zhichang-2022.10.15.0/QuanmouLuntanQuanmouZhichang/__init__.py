86721f9d17c95f7d2d528f05128c0cd30233b7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:48 +0000

    Add package quanmou-luntan-quanmou-zhichang/2022.10.15.0/quanmou-luntan-quanmou-zhichang-2022.10.15.0.tar.gz

---
+++ packages/quanmou-luntan-quanmou-zhichang/quanmou-luntan-quanmou-zhichang-2022.10.15.0.tar.gz/quanmou-luntan-quanmou-zhichang-2022.10.15.0/QuanmouLuntanQuanmouZhichang/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""quanmou-luntan-quanmou-zhichang
+https://github.com/apachecn/quanmou-luntan-quanmou-zhichang"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

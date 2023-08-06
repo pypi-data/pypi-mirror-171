bd13765fcf9ec936025062a30c617670c1d32033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:16 +0000

    Add package heiya/1.2.4/heiya-1.2.4-py3-none-any.whl

---
+++ packages/heiya/heiya-1.2.4-py3-none-any.whl/heiya/__init__.py
@@ -0,0 +1,15 @@
+from . import to_hei
+from . import from_hei
+from . import he_img
+from . import tools
+from . import extensions
+from . import meta_transfer
+
+__all__ = [
+    'to_hei',
+    'from_hei',
+    'he_img',
+    'tools',
+    'extensions',
+    'meta_transfer'
+]

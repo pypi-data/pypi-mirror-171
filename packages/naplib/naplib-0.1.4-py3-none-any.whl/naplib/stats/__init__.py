cf77528f557e185e393a9c9031d363a6d92dcbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:43 +0000

    Add package naplib/0.1.4/naplib-0.1.4-py3-none-any.whl

---
+++ packages/naplib/naplib-0.1.4-py3-none-any.whl/naplib/stats/__init__.py
@@ -0,0 +1,7 @@
+from .encoding import dprime
+from .encoding import fratio
+from .mixedeffectsmodel import LinearMixedEffectsModel
+from .pvalues import stars
+from .responsive_elecs import responsive_ttest
+
+__all__  = ['dprime','fratio','LinearMixedEffectsModel','stars','responsive_ttest']

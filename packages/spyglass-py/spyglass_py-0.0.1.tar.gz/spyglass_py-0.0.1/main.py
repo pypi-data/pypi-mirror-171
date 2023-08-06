4af917c6dd1b317f531c97f99285865935ff1e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:40 +0000

    Add package spyglass-py/0.0.1/spyglass_py-0.0.1.tar.gz

---
+++ packages/spyglass-py/spyglass_py-0.0.1.tar.gz/spyglass_py-0.0.1/main.py
@@ -0,0 +1,14 @@
+"""
+MVP Notes
+  User Flow
+    Sign into site, get project ID/secret
+    Install middleware, add config (i.e. project id/secret, service name, environment)
+    Run locally and/or deploy
+    See dashboards and alerts
+  Flask middleware
+    Send metrics for latency, success, errors
+  Command Center
+    Config
+    Dashboards per service
+    Alerts per service
+"""

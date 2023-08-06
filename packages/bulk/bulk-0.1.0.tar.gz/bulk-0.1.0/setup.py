899a1a71d6b6e6c019102867c180ed0c63470bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:25 +0000

    Add package bulk/0.1.0/bulk-0.1.0.tar.gz

---
+++ packages/bulk/bulk-0.1.0.tar.gz/bulk-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bulk",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["typer>=0.4.1", "bokeh>=2.4.3", "pandas>=1.0.0"],
+    extras_require={
+        "dev": ["pytest-playwright==0.3.0"],
+    },
+    entry_points={
+        'console_scripts': [
+            'bulk = bulk.__main__:app',
+        ],
+    },
+)

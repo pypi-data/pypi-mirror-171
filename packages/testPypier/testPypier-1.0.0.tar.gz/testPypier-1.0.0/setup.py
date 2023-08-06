ba80bdc74422d32ce4badf1b6e21c4fd1896370f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:46 +0000

    Add package testPypier/1.0.0/testPypier-1.0.0.tar.gz

---
+++ packages/testPypier/testPypier-1.0.0.tar.gz/testPypier-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+setup(
+    name="testPypier",
+    version="1.0.0",
+    author="jmax",
+    author_email="testPypier@testPypier.com",
+    description='''
+testPypier
+testPypier
+testPypier
+    ''',
+    long_description_content_type="text/markdown",
+    long_description='''
+testPypiertestPypiertestPypiertestPypier
+testPypier
+testPypier
+testPypier
+    ''',
+    packages=find_packages(),
+    keywords=["testPypier","testPypier2",],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    include_package_data=True,
+    
+)
+        

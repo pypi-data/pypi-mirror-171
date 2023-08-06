c9740234b4218238a97a02dbeef4a85d06e5e5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:08 +0000

    Add package oa-missionaries-and-cannibals/0.0.5/oa_missionaries_and_cannibals-0.0.5.tar.gz

---
+++ packages/oa-missionaries-and-cannibals/oa_missionaries_and_cannibals-0.0.5.tar.gz/oa_missionaries_and_cannibals-0.0.5/oa_missionaries_and_cannibals/src/oa_missionaries_and_cannibals.py
@@ -0,0 +1,23 @@
+from fileinput import filename
+import os
+import sys
+
+def filePath(fileName):
+    return os.path.join(sys.path[0],fileName)
+
+def display():
+    print('1. bfs 2. state space search')
+    choice = int(input())
+
+    if choice == 1:
+        with open(filePath('bfs.py')) as f:
+            print("===================BFS====================")
+            for line in f:
+                print(line, end='')
+    elif choice == 2:
+        with open(filePath('state_space_search.py')) as f:
+            print("===================State Space Search====================")
+            for line in f:
+                print(line, end='')
+    else:
+        print("wrong choice")

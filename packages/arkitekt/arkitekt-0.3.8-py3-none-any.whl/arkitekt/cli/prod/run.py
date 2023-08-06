c4528f3555c3dc9a411e3edff652f2fb72ca15bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:21:01 +0000

    Add package arkitekt/0.3.8/arkitekt-0.3.8-py3-none-any.whl

---
+++ packages/arkitekt/arkitekt-0.3.8-py3-none-any.whl/arkitekt/cli/prod/run.py
@@ -0,0 +1,39 @@
+from importlib import import_module
+import logging
+import sys
+import os
+from rich.console import Console
+from arkitekt import Arkitekt
+
+logger = logging.getLogger(__name__)
+
+
+class Run:
+    def __init__(
+        self,
+        path=None,
+        entrypoint="run",
+    ) -> None:
+
+        if path == ".":
+            self.watch_path = os.getcwd()
+            sys.path.insert(0, self.watch_path)
+        else:
+            self.watch_path = os.path.join(os.getcwd(), path)
+
+        self.module_path = f"{path}.{entrypoint}" if path != "." else f"{entrypoint}"
+        self.module = import_module(self.module_path)
+        self.console = Console()
+        os.environ["ARKITEKT_AGENT_DEBUG"] = "False"
+
+    async def run(self):
+
+        app = Arkitekt()
+
+        async with app:
+            await app.rekuest.run()
+
+
+async def import_directory_and_start(path="watch", entrypoint="run"):
+    host = Run(path=path)
+    await host.run()

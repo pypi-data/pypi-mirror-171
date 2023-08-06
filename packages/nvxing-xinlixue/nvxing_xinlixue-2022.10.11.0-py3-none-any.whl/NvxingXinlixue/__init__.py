6d79aa87238b3c66aeaccd9f6902ff7ce9b66c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:58 +0000

    Add package nvxing-xinlixue/2022.10.11.0/nvxing_xinlixue-2022.10.11.0-py3-none-any.whl

---
+++ packages/nvxing-xinlixue/nvxing_xinlixue-2022.10.11.0-py3-none-any.whl/NvxingXinlixue/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""nvxing-xinlixue
+https://github.com/apachecn/nvxing-xinlixue"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

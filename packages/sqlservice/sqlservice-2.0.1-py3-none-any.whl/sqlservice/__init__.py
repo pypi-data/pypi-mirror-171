e5f1ef52611122819b310dd7c74b5092613ffb76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:14 +0000

    Add package sqlservice/2.0.1/sqlservice-2.0.1-py3-none-any.whl

---
+++ packages/sqlservice/sqlservice-2.0.1-py3-none-any.whl/sqlservice/__init__.py
@@ -0,0 +1,10 @@
+"""The sqlservice package."""
+
+__version__ = "2.0.1"
+
+from . import event
+from .async_database import AsyncDatabase
+from .async_session import AsyncSession
+from .database import Database
+from .model import ModelBase, ModelMeta, as_declarative, declarative_base
+from .session import Session

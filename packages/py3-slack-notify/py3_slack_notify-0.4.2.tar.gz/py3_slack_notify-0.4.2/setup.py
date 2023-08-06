981f4297c6f5a8727a872ac7e263ecaa7e0ef85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:27 +0000

    Add package py3-slack-notify/0.4.2/py3_slack_notify-0.4.2.tar.gz

---
+++ packages/py3-slack-notify/py3_slack_notify-0.4.2.tar.gz/py3_slack_notify-0.4.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="py3_slack_notify",
+    packages=find_packages(include=["py_slack_notify"]),
+    version="0.4.2",
+    description="Python package for sending notification to Slack in thread with emoji",
+    author="thaopn",
+    url="https://github.com/ThaoPN/py_slack_notify",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+    test_suite="",
+)

8b9096cd0e63b083f2498a0d18f0c50e95b517be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:23 +0000

    Add package bgutils-hddly/1.0.4/bgutils_hddly-1.0.4-py3-none-any.whl

---
+++ packages/bgutils-hddly/bgutils_hddly-1.0.4-py3-none-any.whl/bgutils/bgutils.py
@@ -0,0 +1,10 @@
+from U0.bgutils.bgutils.ftpUtil import bgftpUtil
+from U0.bgutils.bgutils.mysqlUtil import bgmysqlUtil
+
+#
+# class bgmysq(bgmysqlUtil):
+#     pass
+#
+# class bgftp(bgftpUtil):
+#     pass
+

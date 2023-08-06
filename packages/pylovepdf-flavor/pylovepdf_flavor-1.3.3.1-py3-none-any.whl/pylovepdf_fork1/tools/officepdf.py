9e8998c24f97c1fc828b9da2be12d0c4b56db0e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:25 +0000

    Add package pylovepdf-flavor/1.3.3.1/pylovepdf_flavor-1.3.3.1-py3-none-any.whl

---
+++ packages/pylovepdf-flavor/pylovepdf_flavor-1.3.3.1-py3-none-any.whl/pylovepdf_fork1/tools/officepdf.py
@@ -0,0 +1,9 @@
+from pylovepdf_flavor.task import Task
+
+
+class OfficePdf(Task):
+
+    def __init__(self, public_key, verify_ssl, proxies):
+
+        self.tool = 'officepdf'
+        super(OfficePdf, self).__init__(public_key, True, verify_ssl, proxies)

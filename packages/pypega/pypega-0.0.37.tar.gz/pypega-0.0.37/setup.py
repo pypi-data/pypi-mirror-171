94ac60966e613aed3beec48707b396c5a68062f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:53 +0000

    Add package pypega/0.0.37/pypega-0.0.37.tar.gz

---
+++ packages/pypega/pypega-0.0.37.tar.gz/pypega-0.0.37/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypega']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests-oauthlib>=1.3.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pypega',
+    'version': '0.0.37',
+    'description': 'Python package for orchestrating Pega environments',
+    'long_description': '# pyPega (Python for Pega)\n\nPython package for orchestrating Pega environments - details coming soon.',
+    'author': 'Rob Smart',
+    'author_email': 'rob.smart@pega.com',
+    'maintainer': 'Rob Smart',
+    'maintainer_email': 'rob.smart@pega.com',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+}
+
+
+setup(**setup_kwargs)

6fce169741e7c5ce5c2b03a69ffe31cebb7864fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:38 +0000

    Add package xpsd/0.0.2/xpsd-0.0.2.tar.gz

---
+++ packages/xpsd/xpsd-0.0.2.tar.gz/xpsd-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='xpsd',
+    version='0.0.2',
+    description='',
+    url='https://github.com/jesseneverdie/xpsd.git',
+    author='Jesse Kim',
+    author_email='jesse@nextwith.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False
+)

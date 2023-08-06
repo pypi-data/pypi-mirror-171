a97ccdb0867bdd82b00ed85d1c01d7939fa1404f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:04 +0000

    Add package MohamedSataryNinthTest/0.0.1/MohamedSataryNinthTest-0.0.1.tar.gz

---
+++ packages/MohamedSataryNinthTest/MohamedSataryNinthTest-0.0.1.tar.gz/MohamedSataryNinthTest-0.0.1/MohamedSataryNinthTest/anothaone/goodBye.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 12:43:39 2022
+
+@author: mohamed.satary
+"""
+
+def say_bye():
+    print('goodBye!')
---
+++ packages/MohamedSataryNinthTest/MohamedSataryNinthTest-0.0.1.tar.gz/MohamedSataryNinthTest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='MohamedSataryNinthTest',
+      version='0.0.1',
+      description='''This library converts Names (or words) from arabic to its 
+      Arabizi form, and vice versa (from Arabizi to arabic)''',
+      #py_modules=['helloWorld', 'bye', 'goodBye'],
+      #package_dir={'': 'src'},
+      packages=find_packages()
+      )

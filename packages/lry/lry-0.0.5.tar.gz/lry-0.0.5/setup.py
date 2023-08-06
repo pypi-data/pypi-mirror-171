5a8ace631b64ab166479e6e1084584bf7ac70543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:36 +0000

    Add package lry/0.0.5/lry-0.0.5.tar.gz

---
+++ packages/lry/lry-0.0.5.tar.gz/lry-0.0.5/setup.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name="lry",
+    version="0.0.5",
+    description="数据挖掘实验4",
+    author="lry",
+    author_email="1224137702@qq.com",
+    packages=["LRY"],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'xlrd',
+        'xlrd3',
+        'matplotlib',
+        'scikit-learn'
+    ]
+)

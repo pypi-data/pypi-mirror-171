0d497b5560f4825d1cb928e2b3904570f03b975c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:37 +0000

    Add package vinor/0.1.0a1/vinor-0.1.0a1.tar.gz

---
+++ packages/vinor/vinor-0.1.0a1.tar.gz/vinor-0.1.0a1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vinor']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'vinor',
+    'version': '0.1.0a1',
+    'description': '',
+    'long_description': '# Vinor\n\nFull-stack Web Framework',
+    'author': 'VinorTeam',
+    'author_email': 'opensource@vinor.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)

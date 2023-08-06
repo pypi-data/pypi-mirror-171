70a328c6d350af09b4611d94e98a31fb2f56eb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:01 +0000

    Add package async-get-vedio/3.0/async_get_vedio-3.0.tar.gz

---
+++ packages/async-get-vedio/async_get_vedio-3.0.tar.gz/async_get_vedio-3.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name = 'async_get_vedio',
+    version = '3.0',
+    author = 'WUT_ljs',
+    author_email = '3480339804@qq.com',
+    url = 'https://github.com/wutljs/async_get_vedio',
+    description = 'Get vedio',
+    long_description = '相比2.1能更好的实现预期功能,简化用户调用函数的流程'
+                       '具体文档请进入:https://github.com/wutljs/async_get_vedio 查看.',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)

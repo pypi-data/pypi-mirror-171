a15d1376199d4030148cae92544351060ebc588f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:45 +0000

    Add package cmsworkflow/0.0.13/cmsworkflow-0.0.13.tar.gz

---
+++ packages/cmsworkflow/cmsworkflow-0.0.13.tar.gz/cmsworkflow-0.0.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cmsworkflow',
+      author="Hasan Ozturk, Hamed Bakhshiansohi, Luca Lavezzo",
+      version='0.0.13',
+      description="A high level package which provides tools to get useful information about CMS workflows",
+      url="https://github.com/pypa/sampleproject",
+      python_requires=">=3.6",
+      packages=['workflow'],
+
+)

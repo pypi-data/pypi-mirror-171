b2ae8ff2bb405c45a57981ec54aa5bfa757194af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:50 +0000

    Add package shuiku-oushen-wenji-vol10/2022.10.15.0/shuiku_oushen_wenji_vol10-2022.10.15.0-py3-none-any.whl

---
+++ packages/shuiku-oushen-wenji-vol10/shuiku_oushen_wenji_vol10-2022.10.15.0-py3-none-any.whl/ShuikuOushenWenjiVol10/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""shuiku-oushen-wenji-vol10
+https://github.com/apachecn/shuiku-oushen-wenji-vol10"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

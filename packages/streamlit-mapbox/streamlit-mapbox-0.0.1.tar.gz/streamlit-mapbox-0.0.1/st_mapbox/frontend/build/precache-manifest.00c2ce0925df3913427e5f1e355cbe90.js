7351e387b1ba59c185a927be37e478630dd46f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:12 +0000

    Add package streamlit-mapbox/0.0.1/streamlit-mapbox-0.0.1.tar.gz

---
+++ packages/streamlit-mapbox/streamlit-mapbox-0.0.1.tar.gz/streamlit-mapbox-0.0.1/st_mapbox/frontend/build/precache-manifest.00c2ce0925df3913427e5f1e355cbe90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335cde8d1856ce4c2f0a6d01ef635547",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7819e284fd6e48e20fd5",
+    "url": "./static/js/2.f1188555.chunk.js"
+  },
+  {
+    "revision": "657f6026a285ef7363ed9fb08d9071bd",
+    "url": "./static/js/2.f1188555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c56f513b166e1c389c",
+    "url": "./static/js/main.af9bd733.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);

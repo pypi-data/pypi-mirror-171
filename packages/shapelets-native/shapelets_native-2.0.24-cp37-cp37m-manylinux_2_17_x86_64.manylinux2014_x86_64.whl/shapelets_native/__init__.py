b52c95f5d9e4b3ad6ae65eb2bee42bd466e28c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:37 +0000

    Add package shapelets-native/2.0.24/shapelets_native-2.0.24-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/shapelets-native/shapelets_native-2.0.24-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/shapelets_native/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022 Shapelets.io
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from __future__ import annotations
+
+from ._shapelets_extension import *

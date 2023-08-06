8b4fc6a0bf2e7c80b55cda35c51badf7d6784109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:22 +0000

    Add package splinepy/0.0.3/splinepy-0.0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/splinepy/splinepy-0.0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/splinepy/__init__.py
@@ -0,0 +1,42 @@
+from splinepy import _splinepy as _s
+from splinepy._version import __version__
+from splinepy.bezier import Bezier
+from splinepy.rational_bezier import RationalBezier
+from splinepy.bspline import BSpline
+from splinepy.nurbs import NURBS
+from splinepy._spline import Spline
+from splinepy.load import (load_splines,
+                              load_solution)
+
+# Alias for "Legacy" classes
+BSplineCurve2D = _s.BSpline1P2D
+BSplineCurve3D = _s.BSpline1P3D
+BSplineSurface2D = _s.BSpline2P2D
+BSplineSurface3D = _s.BSpline2P3D
+BSplineSolid = _s.BSpline3P3D
+
+NurbsCurve2D = _s.NURBS1P2D
+NurbsCurve3D = _s.NURBS1P3D
+NurbsSurface2D = _s.NURBS2P2D
+NurbsSurface3D = _s.NURBS2P3D
+NurbsSolid = _s.NURBS3P3D
+
+__all__ = [
+    "Spline",
+    "Bezier",
+    "RationalBezier",
+    "BSpline",
+    "NURBS",
+    "BSplineCurve2D",
+    "BSplineCurve3D",
+    "BSplineSurface2D",
+    "BSplineSurface3D",
+    "BSplineSolid",
+    "NurbsCurve2D",
+    "NurbsCurve3D",
+    "NurbsSurface2D",
+    "NurbsSurface3D",
+    "NurbsSolid",
+    "load_splines",
+    "load_solution",
+]
---
+++ packages/splinepy/splinepy-0.0.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/splinepy/nurbs.py
@@ -0,0 +1,59 @@
+from splinepy._splinepy import *
+from splinepy._spline import Spline
+from splinepy.rational_bezier import RationalBezier
+
+
+class NURBS(Spline):
+
+    def __init__(self,
+                 degrees=None,
+                 knot_vectors=None,
+                 control_points=None,
+                 weights=None,
+                 ):
+        """
+        NURBS.
+
+        Parameters
+        -----------
+        degrees: (para_dim,) list-like
+        knot_vectors: (para_dim, n) list
+        control_points: (m, dim) list-like
+        weights: (m,) list-like
+
+        Returns
+        --------
+        None
+        """
+        super().__init__(
+            degrees=degrees,
+            knot_vectors=knot_vectors,
+            control_points=control_points,
+            weights=weights,
+        )
+
+    def extract_bezier_patches(self):
+        """
+        Extract all knot spans as Bezier patches to perform further operations
+        such as compositions and multiplications
+
+        Parameters
+        ----------
+        None
+
+        Returns 
+        -------
+        extracted Beziers : list
+        """
+        # Extract bezier patches and create PyRationalBezier objects
+        list_of_c_object_beziers = self._c_spline.extract_bezier_patches()
+
+        # Transform into Rational Bezier Splinepy objects
+        extracted_bezier_patches = []
+        for c_object_spline in list_of_c_object_beziers:
+            i_rational_bezier = RationalBezier()
+            i_rational_bezier._c_spline = c_object_spline
+            i_rational_bezier._update_p()
+            extracted_bezier_patches.append(i_rational_bezier)
+
+        return extracted_bezier_patches

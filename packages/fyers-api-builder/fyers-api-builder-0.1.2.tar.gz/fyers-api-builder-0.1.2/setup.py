b3b75dbad794357ea5eb5689bc7053c66585a9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:51 +0000

    Add package fyers-api-builder/0.1.2/fyers-api-builder-0.1.2.tar.gz

---
+++ packages/fyers-api-builder/fyers-api-builder-0.1.2.tar.gz/fyers-api-builder-0.1.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+  include_package_data = True,
+  name = "fyers-api-builder",
+  version = "0.1.2",
+  description = "Fyers Api Builder",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/krunaldodiya/fyers-api-builder",
+  author = "Krunal Dodiya",
+  author_email = "kunal.dodiya1@gmail.com",
+  packages = setuptools.find_packages(),
+  install_requires = [
+    "requests",
+    "fyers-apiv2"
+  ],
+  classifiers =[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent"
+  ]
+)

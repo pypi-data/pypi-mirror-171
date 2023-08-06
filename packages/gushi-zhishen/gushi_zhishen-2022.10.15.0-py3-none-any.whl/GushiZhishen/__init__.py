4d130fcf9f9eb30b08556b4e8c8e88bff00c4f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:09 +0000

    Add package gushi-zhishen/2022.10.15.0/gushi_zhishen-2022.10.15.0-py3-none-any.whl

---
+++ packages/gushi-zhishen/gushi_zhishen-2022.10.15.0-py3-none-any.whl/GushiZhishen/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""gushi-zhishen
+https://github.com/apachecn/gushi-zhishen"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

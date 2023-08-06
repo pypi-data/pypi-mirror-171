ac3eb80d96d1d5132c10a49562e5f144f416769d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:35 +0000

    Add package chanzhongshuochan-jishu-lilun-tujie/2022.10.15.0/chanzhongshuochan-jishu-lilun-tujie-2022.10.15.0.tar.gz

---
+++ packages/chanzhongshuochan-jishu-lilun-tujie/chanzhongshuochan-jishu-lilun-tujie-2022.10.15.0.tar.gz/chanzhongshuochan-jishu-lilun-tujie-2022.10.15.0/ChanzhongshuochanJishuLilunTujie/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""chanzhongshuochan-jishu-lilun-tujie
+https://github.com/apachecn/chanzhongshuochan-jishu-lilun-tujie"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"
---
+++ packages/chanzhongshuochan-jishu-lilun-tujie/chanzhongshuochan-jishu-lilun-tujie-2022.10.15.0.tar.gz/chanzhongshuochan-jishu-lilun-tujie-2022.10.15.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ChanzhongshuochanJishuLilunTujie
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ChanzhongshuochanJishuLilunTujie'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="chanzhongshuochan-jishu-lilun-tujie",
+    version=ChanzhongshuochanJishuLilunTujie.__version__,
+    url="https://github.com/apachecn/chanzhongshuochan-jishu-lilun-tujie",
+    author=ChanzhongshuochanJishuLilunTujie.__author__,
+    author_email=ChanzhongshuochanJishuLilunTujie.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="缠中说禅技术理论图解",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "chanzhongshuochan-jishu-lilun-tujie=ChanzhongshuochanJishuLilunTujie.__main__:main",
+            "ChanzhongshuochanJishuLilunTujie=ChanzhongshuochanJishuLilunTujie.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

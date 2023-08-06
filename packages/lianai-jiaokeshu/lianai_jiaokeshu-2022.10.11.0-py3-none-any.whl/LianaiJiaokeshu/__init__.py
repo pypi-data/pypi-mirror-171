d58638cdd9a1563d085dfaed5e0215ce516a53c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:12 +0000

    Add package lianai-jiaokeshu/2022.10.11.0/lianai_jiaokeshu-2022.10.11.0-py3-none-any.whl

---
+++ packages/lianai-jiaokeshu/lianai_jiaokeshu-2022.10.11.0-py3-none-any.whl/LianaiJiaokeshu/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""lianai-jiaokeshu
+https://github.com/apachecn/lianai-jiaokeshu"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

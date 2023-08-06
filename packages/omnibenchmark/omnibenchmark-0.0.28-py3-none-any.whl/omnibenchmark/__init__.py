27eb6453b7344623cdce7ec0b86d84fe17af4663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:04 +0000

    Add package omnibenchmark/0.0.28/omnibenchmark-0.0.28-py3-none-any.whl

---
+++ packages/omnibenchmark/omnibenchmark-0.0.28-py3-none-any.whl/omnibenchmark/__init__.py
@@ -0,0 +1,10 @@
+"""Omnibenchmark"""
+
+# Version of the omnibenchmark package
+
+__version__ = "0.0.28"
+
+
+from omnibenchmark import core, management, renku_commands, utils
+
+__all__ = ("core", "management", "renku_commands", "utils")

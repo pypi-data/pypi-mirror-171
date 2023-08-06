3c6afe053394062ce454488d6978628fe74ee7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:49 +0000

    Add package home-assistant-bluetooth/1.6.0/home_assistant_bluetooth-1.6.0.tar.gz

---
+++ packages/home-assistant-bluetooth/home_assistant_bluetooth-1.6.0.tar.gz/home_assistant_bluetooth-1.6.0/src/home_assistant_bluetooth/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.6.0"
+
+from .models import SOURCE_LOCAL, BaseServiceInfo, BluetoothServiceInfo
+
+__all__ = ["BaseServiceInfo", "BluetoothServiceInfo", "SOURCE_LOCAL"]
---
+++ packages/home-assistant-bluetooth/home_assistant_bluetooth-1.6.0.tar.gz/home_assistant_bluetooth-1.6.0/src/home_assistant_bluetooth/models.py
@@ -0,0 +1,72 @@
+"""The bluetooth integration service info."""
+from __future__ import annotations
+
+import dataclasses
+from functools import cached_property
+from typing import TYPE_CHECKING, Final, TypeVar
+
+SOURCE_LOCAL: Final = "local"
+
+if TYPE_CHECKING:
+    from bleak.backends.device import BLEDevice
+    from bleak.backends.scanner import AdvertisementData
+
+    _BluetoothServiceInfoSelfT = TypeVar(
+        "_BluetoothServiceInfoSelfT", bound="BluetoothServiceInfo"
+    )
+
+
+@dataclasses.dataclass
+class BaseServiceInfo:
+    """Base class for discovery ServiceInfo."""
+
+
+@dataclasses.dataclass
+class BluetoothServiceInfo(BaseServiceInfo):
+    """Prepared info from bluetooth entries."""
+
+    name: str
+    address: str
+    rssi: int
+    manufacturer_data: dict[int, bytes]
+    service_data: dict[str, bytes]
+    service_uuids: list[str]
+    source: str
+
+    @classmethod
+    def from_advertisement(
+        cls: type[_BluetoothServiceInfoSelfT],
+        device: BLEDevice,
+        advertisement_data: AdvertisementData,
+        source: str,
+    ) -> _BluetoothServiceInfoSelfT:
+        """Create a BluetoothServiceInfo from an advertisement."""
+        return cls(
+            name=advertisement_data.local_name or device.name or device.address,
+            address=device.address,
+            rssi=advertisement_data.rssi,
+            manufacturer_data=advertisement_data.manufacturer_data,
+            service_data=advertisement_data.service_data,
+            service_uuids=advertisement_data.service_uuids,
+            source=source,
+        )
+
+    @cached_property
+    def manufacturer(self) -> str | None:
+        """Convert manufacturer data to a string."""
+        from bleak.backends._manufacturers import (  # pylint: disable=import-outside-toplevel
+            MANUFACTURERS,
+        )
+
+        for manufacturer in self.manufacturer_data:
+            if manufacturer in MANUFACTURERS:
+                name: str = MANUFACTURERS[manufacturer]
+                return name
+        return None
+
+    @cached_property
+    def manufacturer_id(self) -> int | None:
+        """Get the first manufacturer id."""
+        for manufacturer in self.manufacturer_data:
+            return manufacturer
+        return None

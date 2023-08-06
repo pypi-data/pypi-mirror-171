67fa198059b570ad0c09214d7be516a544a48e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:14 +0000

    Add package pyird/0.2.0/pyird-0.2.0-py3.8.egg

---
+++ packages/pyird/pyird-0.2.0-py3.8.egg/pyird/spec/wavcal_.py
@@ -0,0 +1,20 @@
+from pyird.io.read_linelist import read_linelist
+#from astropy.io import fits
+import numpy as np
+#from scipy.signal import medfilt
+#from scipy.optimize import leastsq
+#import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    """test
+
+    Args:
+       x: test
+    
+    Returns:
+       tmp
+    
+    """
+    return x

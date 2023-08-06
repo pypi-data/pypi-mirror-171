942406a63b16107772eab35ada013b97296ebef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:56 +0000

    Add package flake8-cached/0.2.0/flake8_cached-0.2.0-py3-none-any.whl

---
+++ packages/flake8-cached/flake8_cached-0.2.0-py3-none-any.whl/flake8_cached/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Package entry point."""
+
+
+import sys
+
+from flake8_cached.cli import main
+
+
+if __name__ == "__main__":  # pragma: no cover
+    sys.exit(main())
---
+++ packages/flake8-cached/flake8_cached-0.2.0-py3-none-any.whl/flake8_cached/cli.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+import flake8.checker as f8checker
+
+from flake8_cached.cacher import Cacher
+
+
+CACHE_PATH = ".cache/flake8"
+
+
+class FileCheckerCached(f8checker.FileChecker):
+    def __init__(self, *, filename, plugins, options):
+        super().__init__(filename=filename, plugins=plugins, options=options)
+        self.cacher = Cacher(filename, CACHE_PATH)
+
+    def run_checks(self):
+        """Cache wrapper of super.run_checks."""
+        # handle both cases where cache is enabled/disabled or invalid
+        saved = self.cacher.get()
+        if saved is not None:
+            self.filename, self.results, self.statistics = saved
+        else:
+            result = super().run_checks()
+            self.cacher.save(result)
+        return self.filename, self.results, self.statistics
+
+
+if not os.path.exists(CACHE_PATH):
+    os.makedirs(CACHE_PATH)
+
+from flake8 import checker
+
+
+# patch the class
+checker.FileChecker = FileCheckerCached
+
+
+def main():
+    from flake8.main import cli
+
+    return cli.main()
+
+
+if __name__ == "__main__":
+    sys.exit(main())

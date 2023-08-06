44b5b9027d9215da7f3532ada1fac1f056dec93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:36 +0000

    Add package lyscripts/0.5.10/lyscripts-0.5.10-py3-none-any.whl

---
+++ packages/lyscripts/lyscripts-0.5.10-py3-none-any.whl/lyscripts/plot/__init__.py
@@ -0,0 +1,28 @@
+"""
+Provide various plotting utilities for displaying results of e.g. the inference
+or prediction process.
+"""
+import argparse
+from pathlib import Path
+
+from ..helpers import clean_docstring
+from . import __doc__, corner, histograms, thermo_int
+
+
+def _add_parser(
+    subparsers: argparse._SubParsersAction,
+    help_formatter,
+):
+    """
+    Add an `ArgumentParser` to the subparsers action and then add more subparsers.
+    """
+    parser = subparsers.add_parser(
+        Path(__file__).parent.name,
+        description=clean_docstring(__doc__),
+        help=clean_docstring(__doc__),
+        formatter_class=help_formatter,
+    )
+    subparsers = parser.add_subparsers()
+    corner._add_parser(subparsers, help_formatter=parser.formatter_class)
+    histograms._add_parser(subparsers, help_formatter=parser.formatter_class)
+    thermo_int._add_parser(subparsers, help_formatter=parser.formatter_class)

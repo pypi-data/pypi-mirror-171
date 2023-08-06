d3caca85dad6386a709b2b8b17fc5bfb3520ad61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:23 +0000

    Add package zhiliao-mima/2022.10.14.0/zhiliao_mima-2022.10.14.0-py3-none-any.whl

---
+++ packages/zhiliao-mima/zhiliao_mima-2022.10.14.0-py3-none-any.whl/ZhiliaoMima/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""zhiliao-mima
+https://github.com/apachecn/zhiliao-mima"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.14.0"

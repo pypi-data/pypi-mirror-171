411a4cb780e94aa011de6af7d0e479ced539cea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package anai-opensource/0.1.7/anai_opensource-0.1.7-py3-none-any.whl

---
+++ packages/anai-opensource/anai_opensource-0.1.7-py3-none-any.whl/anai/__init__.py
@@ -0,0 +1,337 @@
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+
+import inspect
+import os
+import shutil
+import warnings
+from distributed import Client, LocalCluster
+try:
+    clust = LocalCluster(name='ANAI-Cluster', n_workers=2, threads_per_worker=2, processes=True,
+                        host='0.0.0.0', protocol='tcp://', scheduler_port=0, dashboard_address=0)
+    client = Client(clust)
+except Exception as e:
+    pass
+import modin.pandas as pd
+from colorama import Fore
+from optuna.samplers._tpe.sampler import TPESampler
+
+from anai import *
+from anai.supervised import Classification, Regression
+from anai.utils.connectors import load_data_from_config
+from anai.utils.connectors.data_handler import __df_loader_single, df_loader
+
+
+
+if os.path.exists(os.getcwd() + "/dask-worker-space"):
+    shutil.rmtree(os.getcwd() + "/dask-worker-space")
+
+
+warnings.filterwarnings("ignore")
+
+
+def run(
+    df=None,
+    target: str = None,
+    filepath: str = None,
+    df_kwargs: dict = {},
+    config: bool = False,
+    except_columns: list = [],
+    predictor: list = [],
+    params: dict = {},
+    tune: bool = False,
+    test_size: float = 0.2,
+    cv_folds: int = 10,
+    random_state: int = 42,
+    pca_kernel: str = "linear",
+    n_components_lda: int = 1,
+    lda: bool = False,
+    pca: bool = False,
+    n_components_pca: int = 2,
+    smote: bool = "False",
+    k_neighbors: int = 1,
+    verbose: bool = False,
+    exclude_models: list = [],
+    path: str = None,
+    optuna_sampler=TPESampler(multivariate=True),
+    optuna_direction: str = "maximize",
+    optuna_n_trials: int = 100,
+    metric: str = None,
+    suppress_task_detection=False,
+    task=None,
+    ensemble: bool =True,
+    mode: str = "None",
+):
+    """Initializes ANAI run.
+
+    Arguments:
+            df : Pandas DataFrame
+                DataFrame to be used for modelling.
+            target : str
+                Target Column Name 
+            filepath : str
+                Filepath of the dataframe to be loaded.
+            df_kwargs : dict
+                Keyword arguments for the dataframe loading function. Only used if filepath is not None.
+            except_columns : list, optional
+                List of Columns to be excluded from the dataset
+            predictor : list
+                        Predicting models to be used
+            params : dict
+                        dictionary containing parameters for model.
+                        Not available when using predictor = all or multiple predictors.
+            test_size: float or int, default=.2
+                        If float, should be between 0.0 and 1.0 and represent
+                        the proportion of the dataset to include in
+                        the test split.
+                        If int, represents the absolute number of test samples.
+            cv_folds : int
+                    No. of cross validation folds. Default = 10
+            pca : bool
+                if True will apply PCA on Train and Validation set. Default = False
+            lda : str
+                if True will apply LDA on Train and Validation set. Default = False
+            pca_kernel : str
+                    Kernel to be use in PCA. Default = 'linear'
+            n_components_lda : int
+                    No. of components for LDA. Default = 1
+            n_components_pca : int
+                    No. of components for PCA. Default = 2
+            smote : Bool,
+                Whether to apply SMOTE. Default = True
+            k_neighbors : int
+                No. of neighbors for SMOTE. Default = 1
+            verbose : boolean
+                Verbosity of models. Default = False
+            exclude_models : list
+                List of models to be excluded when using predictor = 'all' . Default = []
+            path : list
+                List containing path to saved model and scaler. Default = None
+                Example: [model.pkl, scaler.pkl]
+            random_state : int
+                Random random_state for reproducibility. Default = 42
+            tune : boolean
+                    when True Applies Optuna to find best parameters for model
+                    Default is False
+            optuna_sampler : Function
+                Sampler to be used in optuna. Default = TPESampler()
+            optuna_direction : str
+                Direction of optimization. Default = 'maximize'
+                Available Directions:
+                    maximize : Maximize
+                    minimize : Minimize
+            optuna_n_trials : int
+                No. of trials for optuna. Default = 100
+            metric : str,
+                metric to be used for model evaluation. Default = 'r2' for regressor and 'accuracy' for classifier
+            suppress_task_detection: Bool 
+                Whether to suppress automatic task detection. Default = False
+            task : str
+                Task to be used for model evaluation. Default = None
+                Only applicable when suppress_task_detection = True
+                Available Tasks:
+                    classification : Classification
+                    regression : Regression
+            ensemble : bool
+                Whether to use ensemble. Default = True
+            mode : str
+                Model selection mode. Default = 'None'
+        Returns:
+
+            ai : regression or classification object
+                Returns a regression or classification object
+
+        Example:
+            import anai
+            
+            ai = anai.run(
+                        filepath='examples/Folds5x2_pp.xlsx', 
+                        df_kwargs={'sheet_name': 'Sheet1'},
+                        target='PE',
+                        predictor=['lin'],
+            )
+
+    """
+    
+    
+    
+    try:
+        if not suppress_task_detection:
+            print(
+                Fore.YELLOW
+                + "ANAITaskWarning: Task is getting detected automatically. To suppress this behaviour, set suppress_task_detection=True and specify task with task=<regression or classification> argument",
+                Fore.RESET,
+            )
+        else:
+            if task is None:
+                raise ValueError(
+                    "task argument is required when suppress_task_detection=True"
+                )
+        if config:
+            if os.path.exists(os.getcwd() + "/anai_config.yaml"):
+                df, target = load_data_from_config(
+                    os.getcwd() + "/anai_config.yaml", suppress=True
+                )
+            else:
+                raise FileNotFoundError("ANAI Config File Not Found")
+        if df is None:
+            if filepath is not None:
+                df = df_loader(filepath, suppress=True, **df_kwargs)
+            else:
+                raise ValueError("Please provide a dataframe or a filepath")
+        if __task(df, target) and not suppress_task_detection or task == "regression":
+            print(Fore.BLUE + "Task: Regression", Fore.RESET)
+            if len(predictor) == 0 and mode is None:
+                predictor = ["lin"]
+            if mode == 'auto':
+                predictor = ['all']
+                ensemble = True
+            if metric is None:
+                metric = "r2"
+            regressor = Regression(
+                df=df,
+                target=target,
+                filepath=filepath,
+                df_kwargs=df_kwargs,
+                config=config,
+                except_columns=except_columns,
+                predictor=predictor,
+                params=params,
+                tune=tune,
+                test_size=test_size,
+                cv_folds=cv_folds,
+                random_state=random_state,
+                pca_kernel=pca_kernel,
+                n_components_lda=n_components_lda,
+                lda=lda,
+                pca=pca,
+                n_components_pca=n_components_pca,
+                smote=smote,
+                k_neighbors=k_neighbors,
+                verbose=verbose,
+                exclude_models=exclude_models,
+                path=path,
+                optuna_sampler=optuna_sampler,
+                optuna_direction=optuna_direction,
+                optuna_n_trials=optuna_n_trials,
+                metric=metric,
+                ensemble=ensemble,
+            )
+            regressor.fit()
+            return regressor
+        elif (
+            not __task(df, target)
+            and not suppress_task_detection
+            or task == "classification"
+        ):
+            print(Fore.BLUE + "Task: Classification", Fore.RESET)
+            if len(predictor) == 0 and mode is None:
+                predictor = ["lr"]
+            if mode == 'auto':
+                predictor = ['all']
+                ensemble = True
+            if metric is None:
+                metric = "accuracy"
+            classifier = Classification(
+                df=df,
+                target=target,
+                filepath=filepath,
+                df_kwargs=df_kwargs,
+                config=config,
+                except_columns=except_columns,
+                predictor=predictor,
+                params=params,
+                tune=tune,
+                test_size=test_size,
+                cv_folds=cv_folds,
+                random_state=random_state,
+                pca_kernel=pca_kernel,
+                n_components_lda=n_components_lda,
+                lda=lda,
+                pca=pca,
+                n_components_pca=n_components_pca,
+                smote=smote,
+                k_neighbors=k_neighbors,
+                verbose=verbose,
+                ensemble=ensemble,
+                exclude_models=exclude_models,
+            )
+            classifier.fit()
+            return classifier
+    except KeyboardInterrupt:
+        if os.path.exists(os.getcwd() + "/dask-worker-space"):
+            shutil.rmtree(os.getcwd() + "/dask-worker-space")
+    except InterruptedError:
+        if os.path.exists(os.getcwd() + "/dask-worker-space"):
+            shutil.rmtree(os.getcwd() + "/dask-worker-space")
+
+
+def __get_args(df, target):
+    if target not in df.columns:
+        raise ValueError(f"target {target} not in df columns")
+    target = df[target]
+    if target.nunique() > len(df) / 10:
+        arg = zip(
+            inspect.getfullargspec(Regression).args,
+            inspect.getfullargspec(Regression).defaults,
+        )
+        print("regression")
+    else:
+        arg = zip(
+            inspect.getfullargspec(Classification).args,
+            inspect.getfullargspec(Classification).defaults,
+        )
+        print("classification")
+    args = dict(arg)
+    args.pop("self")
+    return args
+
+
+def __task(df, target):
+    if target not in df.columns:
+        raise ValueError(f"target {target} not in df columns")
+    target = df[target]
+    if target.nunique() > len(df) / 10:
+        return True
+    else:
+        return False
+
+
+def load(df_filepath, legacy =False, **df_kwargs):
+    """Loads a dataframe from a filepath.
+
+    Args:
+        df_filepath (str): Filepath of the dataframe to be loaded.
+        legacy (bool, optional): If True, loads the dataframe using pandas.read_csv. 
+            If False, loads the dataframe using modin.pandas.read_csv. 
+            Defaults to False.
+        df_kwargs (dict): Keyword arguments to be passed to df_loader function.
+
+    Returns:
+        pd.DataFrame : Loaded dataframe.
+    """
+    
+    
+    suppress = False
+    if type(df_filepath) is str:
+        df = __df_loader_single(df_filepath, suppress=False, legacy = legacy, **df_kwargs)
+    elif type(df_filepath) is list:
+        print(Fore.YELLOW + "Loading Data [*]\n")
+        df = pd.concat(
+            [
+                __df_loader_single(
+                    df_filepath[i], suppress=True, legacy=legacy, **df_kwargs)
+                for i in range(len(df_filepath))
+            ]
+        )
+        if df is not None:
+            print(
+                Fore.GREEN + "Data Loaded Successfully [", "\u2713", "]\n"
+            ) if not suppress else None
+
+        else:
+            print(
+                Fore.RED + "Data Loading Failed [", "\u2717", "]\n"
+            ) if not suppress else None
+    return df
---
+++ packages/anai-opensource/anai_opensource-0.1.7-py3-none-any.whl/anai/utils/ensembler.py
@@ -0,0 +1,173 @@
+import numpy as np
+import pandas as pd
+
+from mlxtend.classifier import (EnsembleVoteClassifier, StackingClassifier,
+                                StackingCVClassifier)
+from mlxtend.regressor import StackingCVRegressor, StackingRegressor
+from sklearn.ensemble import VotingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.linear_model import (BayesianRidge, ElasticNet, LinearRegression,
+                                  LogisticRegression, SGDClassifier,
+                                  SGDRegressor)
+from sklearn.metrics import (accuracy_score, mean_absolute_error,
+                             mean_squared_error, r2_score)
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+
+class Ensembler:
+    def __init__(self, task: str, n_estimators: int = 3, n_clusters: int = 3, estimators: list = None, verbose: bool = False, random_state: int = 42, pre_fitted: bool = False):
+        self.n_estimators = n_estimators
+        self.n_clusters = n_clusters
+        self.estimators = estimators
+        self.verbose = verbose
+        self.X_train = None
+        self.y_train = None
+        self.X_val = None
+        self.y_val = None
+        self.task = task
+        self.accuracy = None
+        self.mae = None
+        self.rmse = None
+        self.kfold_accuracy = None
+        self.pre_fitted = pre_fitted
+        self.random_state = random_state
+        self.est = None
+        self.__ensembling_models_reg = {
+            'Stacking Ensembler': self._stacking_ensembler,
+            'Stacking Ensembler with CV': self._stacking_ensembler_cv,
+            'Max Voting Ensembler': self._max_voting
+        }
+        self.__ensembling_models_clf = {
+            #
+            'Stacking Ensembler': self._stacking_ensembler,
+            'Stacking Ensembler with CV': self._stacking_ensembler_cv,
+            'Max Voting Ensembler': self._max_voting
+        }
+        self.result = {}
+        self.mode = None
+
+    def ensemble(self, X_train: np.array, y_train: np.array, X_val: np.array, y_val: np.array, cv_folds: int = 2, estimators: list = [], est: list = None):
+
+        # self.__ensembling_models[self.task]()
+        # self.models = models
+        cnt = 0
+        self.X_train = X_train
+        self.y_train = y_train
+        self.cv_folds = cv_folds
+        self.X_val = X_val
+        self.y_val = y_val
+        self.result = pd.DataFrame(index=None)
+        mae = []
+        rmse = []
+        cv_acc = []
+        acc = []
+        m_list = []
+        self.result_anom = {}
+        self.est = est
+        if self.task == 'regression':
+            self.models_reg = [('lin', LinearRegression()), ('sgd', SGDRegressor(
+            )), ('krr', KernelRidge()), ('elas', ElasticNet()), ('brr', BayesianRidge())]
+            self.__ensembling_models = self.__ensembling_models_reg
+            self.models = self.models_reg if len(
+                estimators) == 0 else estimators
+        elif self.task == 'classification':
+            self.models_clf = [('lr', LogisticRegression()), ('sgd', SGDClassifier(
+            )), ('knn', KNeighborsClassifier()), ('svc', SVC())]
+            self.__ensembling_models = self.__ensembling_models_clf
+            self.models = self.models_clf if len(
+                estimators) == 0 else estimators
+        elif self.task == 'anomaly':
+            self.__ensembling_models = self.__ensembling_models_anom
+            self.models = self.models_clf
+        self.result['Name'] = self.__ensembling_models.keys()
+        for i, model_name in enumerate(self.__ensembling_models):
+            self.model = self.__ensembling_models[model_name]()
+            self.evaluator(self.model, model_name)
+            acc.append(float('{:.2f}'.format(self.accuracy*100)))
+            mae.append(self.mae)
+            rmse.append(self.rmse)
+            cv_acc.append(float('{:.2f}'.format(
+                self.kfold_accuracy.mean()*100)))
+            m_list.append(self.model)
+            # self.result[model_name] = [self.accuracy, self.m_absolute_error, self.rm_squared_error, self.model]
+        if self.task == 'regression':
+            self.result["R^2 Score"] = acc
+            self.result["Mean Absolute Error"] = mae
+            self.result["Root Mean Squared Error"] = rmse
+            self.result["Cross Validated Accuracy"] = cv_acc
+            self.result["Model"] = m_list
+        elif self.task == 'classification':
+            self.result["Accuracy"] = acc
+            self.result["Cross Validated Accuracy"] = cv_acc
+            self.result["Model"] = m_list
+        elif self.task == 'anomaly':
+            # self.result_anom[model_name] = [self.accuracy, self.m_absolute_error, self.rm_squared_error, self.model]
+            self.result_anom = []
+        return self.result if self.task != 'anomaly' else self.result_anom
+
+    def _stacking_ensembler(self):
+        if self.task == 'regression':
+            stacking_model = StackingRegressor(
+                regressors=self.models,
+                meta_regressor=LinearRegression(),
+                verbose=self.verbose
+            )
+        elif self.task == 'classification':
+            stacking_model = StackingClassifier(
+                classifiers=self.models,
+                meta_classifier=LogisticRegression(),
+                verbose=self.verbose,
+
+            )
+        stacking_model.fit(self.X_train, self.y_train)
+        return stacking_model
+
+    def _stacking_ensembler_cv(self):
+        if self.task == 'regression':
+            stacking_model = StackingCVRegressor(
+                regressors=self.models,
+                meta_regressor=LinearRegression(),
+                verbose=self.verbose,
+                cv=self.cv_folds,
+                random_state=self.random_state,
+                refit=False,
+            )
+        elif self.task == 'classification':
+            stacking_model = StackingCVClassifier(
+                classifiers=self.models,
+                meta_classifier=LogisticRegression(),
+                verbose=self.verbose,
+                random_state=self.random_state,
+                n_jobs=-1,
+            )
+        stacking_model.fit(self.X_train, self.y_train)
+        return stacking_model
+
+    def _max_voting(self):
+        if self.task == 'regression':
+            ensembler = VotingRegressor(
+                estimators=self.est, n_jobs=-1, verbose=self.verbose)
+            ensembler.fit(self.X_train, self.y_train)
+        elif self.task == 'classification':
+            ensembler = EnsembleVoteClassifier(
+                clfs=self.models, verbose=self.verbose)
+            ensembler.fit(self.X_train, self.y_train)
+        return ensembler
+
+    def evaluator(self, model, model_name):
+        self.y_pred = model.predict(self.X_val)
+        if self.task == 'regression':
+            self.kfold_accuracy = cross_val_score(
+                model, self.X_train, self.y_train, cv=self.cv_folds, scoring='r2')
+            self.accuracy = r2_score(self.y_val, self.y_pred)
+            self.mae = mean_absolute_error(
+                self.y_val, self.y_pred)
+            self.rmse = mean_squared_error(
+                self.y_val, self.y_pred, squared=False
+            )
+        elif self.task == 'classification':
+            self.kfold_accuracy = cross_val_score(
+                model, self.X_train, self.y_train, cv=self.cv_folds, scoring='accuracy')
+            self.accuracy = accuracy_score(self.y_val, self.y_pred)

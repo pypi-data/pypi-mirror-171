7d9519749c702597732af0b0f0c3f49065bf11cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:04 +0000

    Add package nester-yapinglang/1.1.0/nester_yapinglang-1.1.0.tar.gz

---
+++ packages/nester-yapinglang/nester_yapinglang-1.1.0.tar.gz/nester_yapinglang-1.1.0/nester.py
@@ -0,0 +1,18 @@
+
+'''This is nester.py module, and it provides one function called print_lol(), what this function does
+is to print lists that may or may not include nested lists'''
+
+def print_lol(the_list, the_level):
+    '''This function takes a positional argument called "the_list", which is any Python list (of, possibly, nested lists). Each data item in the provided list is (recursively) printed to the screen on its own line.'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, the_level + 1)
+        else:
+            for tab_stop in range(the_level):
+                print("\t", end='')
+            print(each_item)
+
+
+
+#cast = ['Parlin','Cleese',['Idle','Jones'],'Gilliam','Chapman']
+#print_lol(cast, 2)

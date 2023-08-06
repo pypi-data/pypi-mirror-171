47bb746100d0315528b500d7ddf79a574d4f19ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:34 +0000

    Add package dataclass-wizard/0.22.2/dataclass-wizard-0.22.2.tar.gz

---
+++ packages/dataclass-wizard/dataclass-wizard-0.22.2.tar.gz/dataclass-wizard-0.22.2/tests/conftest.py
@@ -0,0 +1,50 @@
+__all__ = [
+    'does_not_raise',
+    'data_file_path',
+    'PY36',
+    'PY39_OR_ABOVE',
+    'PY310_OR_ABOVE',
+    # For compatibility with Python 3.6 and 3.7
+    'Literal',
+    'TypedDict',
+    'Annotated',
+    'Deque'
+]
+
+import sys
+from pathlib import Path
+
+
+# Directory for test files
+TEST_DATA_DIR = Path(__file__).resolve().parent / 'testdata'
+
+# Check if we are running Python 3.6
+PY36 = sys.version_info[:2] == (3, 6)
+
+# Check if we are running Python 3.9+
+PY39_OR_ABOVE = sys.version_info[:2] >= (3, 9)
+
+# Check if we are running Python 3.10+
+PY310_OR_ABOVE = sys.version_info[:2] >= (3, 10)
+
+# Ref: https://docs.pytest.org/en/6.2.x/example/parametrize.html#parametrizing-conditional-raising
+if sys.version_info[:2] >= (3, 7):
+    from contextlib import nullcontext as does_not_raise
+else:
+    from contextlib import ExitStack as does_not_raise
+
+try:
+    from typing import Literal
+    from typing import TypedDict
+    from typing import Annotated
+    from typing import Deque
+except ImportError:
+    from typing_extensions import Literal
+    from typing_extensions import TypedDict
+    from typing_extensions import Annotated
+    from typing_extensions import Deque
+
+
+def data_file_path(name: str) -> str:
+    """Returns the full path to a test file."""
+    return str((TEST_DATA_DIR / name).absolute())

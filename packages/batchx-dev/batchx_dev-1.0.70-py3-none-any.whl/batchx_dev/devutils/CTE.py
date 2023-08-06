6fe8de36a03c59a0448993f6ebdd938da80a3ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:24 +0000

    Add package batchx-dev/1.0.70/batchx_dev-1.0.70-py3-none-any.whl

---
+++ packages/batchx-dev/batchx_dev-1.0.70-py3-none-any.whl/batchx_dev/devutils/CTE.py
@@ -0,0 +1,9 @@
+# useful fs constants
+MF_FP = "/batchx/manifest/manifest.json"
+INPUT_FP = "/batchx/input/input.json"
+OUTPUT_FP = "/batchx/output/output.json"
+DAG_FP = "/batchx/output/dag.svg"
+
+# runtime constants
+DEFAULT_MEMORY_MB = 4096
+DEFAULT_VCPUS = 1

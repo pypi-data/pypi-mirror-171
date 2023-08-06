fea8c45c43a4138a56831af2d05af2187cbc8b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:57 +0000

    Add package damuffin/0.0.1/damuffin-0.0.1.tar.gz

---
+++ packages/damuffin/damuffin-0.0.1.tar.gz/damuffin-0.0.1/damufffin/__init__.py
@@ -0,0 +1,1 @@
+from .damuffin import status
---
+++ packages/damuffin/damuffin-0.0.1.tar.gz/damuffin-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="damuffin",
+    version="0.0.1",
+    url="https://github.com/DaMuffinDev/damuffin.py",
+    description="A simple python module for the DaMuffinApi",
+    author="DaMuffinDev",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords="damuffin",
+    packages=find_packages(),
+    install_requires=[]
+)

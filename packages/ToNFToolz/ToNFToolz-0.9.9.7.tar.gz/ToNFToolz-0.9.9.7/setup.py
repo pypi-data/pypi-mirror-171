817ba4ad044f34fdc198746485c6c31f86171df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:22 +0000

    Add package ToNFToolz/0.9.9.7/ToNFToolz-0.9.9.7.tar.gz

---
+++ packages/ToNFToolz/ToNFToolz-0.9.9.7.tar.gz/ToNFToolz-0.9.9.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+requirements = ["tonsdk>=1.0.6", "ton>=0.24", "aiohttp>=3.8.1", "setuptools>=65.3.0"]
+
+setup(
+    name='ToNFToolz',
+    version='0.9.9.7',
+    packages=['ToNFToolz'],
+    url='',
+    license='MIT',
+    author='yungwine',
+    author_email='cyrbatoff@gmail.com',
+    description='Explore NFT Items in TON Blockchain',
+    install_requires=requirements,
+)

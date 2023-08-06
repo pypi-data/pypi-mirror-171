9257273abf8574f8e1ef1c9c27351cb63fef1241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:42 +0000

    Add package DefPy/1.1/DefPy-1.1.tar.gz

---
+++ packages/DefPy/DefPy-1.1.tar.gz/DefPy-1.1/DefPy/DefPy.py
@@ -0,0 +1,20 @@
+import os 
+from time import *
+def chisl():
+    a = input("Введите число: ")
+    b = a+'Øèðîê-' + a
+    print("Вы написали: " + str(b))
+def notepad():
+    os.system('C:/Windows/notepad.exe')  
+def gyl():
+    print("1000-7...") 
+    sleep(1.3)
+    gylenish = 1000
+    while gylenish > -1:  
+        gylenish=gylenish-7
+        print(gylenish)
+        if gylenish == -1:
+            print("Я ГУЛЬ!!!") 
+def deb1s():
+    print("Денис вообше какашечка >:(")                   
+ 
---
+++ packages/DefPy/DefPy-1.1.tar.gz/DefPy-1.1/DefPy/__init__.py
@@ -0,0 +1,4 @@
+from DefPy import chisl
+from DefPy import notepad
+from DefPy import gyl
+from DefPy import deb1s 

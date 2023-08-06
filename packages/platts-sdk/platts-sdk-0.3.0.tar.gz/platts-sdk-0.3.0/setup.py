8b5175242f9fbe7b4915f493fc7d91d2f92b0f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:27 +0000

    Add package platts-sdk/0.3.0/platts-sdk-0.3.0.tar.gz

---
+++ packages/platts-sdk/platts-sdk-0.3.0.tar.gz/platts-sdk-0.3.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['platts_sdk']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black>=22.10.0,<23.0.0',
+ 'cachetools>=5.2.0,<6.0.0',
+ 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'platts-sdk',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

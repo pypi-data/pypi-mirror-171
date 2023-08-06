ce055763f7fc99742bae3b05c8b67d5278042b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:45 +0000

    Add package primosaOAVC2/0.0.0/primosaOAVC2-0.0.0.tar.gz

---
+++ packages/primosaOAVC2/primosaOAVC2-0.0.0.tar.gz/primosaOAVC2-0.0.0/primosaOAVC2.py
@@ -0,0 +1,16 @@
+import rpy2.robjects as ro
+from rpy2.robjects import r
+
+def primosOAVC2(n):
+  r.assign("n", n)
+  r('''
+  if (n >= 1) {
+  x = seq(1, n)
+  for (i in seq(2, n)) {
+  if (any(x == i)) {
+  print(i)
+  x = c(x[(x %% i) != 0], i)
+  }
+  }
+  }
+  ''')
---
+++ packages/primosaOAVC2/primosaOAVC2-0.0.0.tar.gz/primosaOAVC2-0.0.0/setup.py
@@ -0,0 +1,18 @@
+import email
+from setuptools import setup
+
+setup(
+    name='primosa02',
+    version='0.0.1',
+    description='Calculate prime numbers',
+)
+
+setup(classifiers=["Programming Language :: Python :: 3 :: Only",
+"License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+"Operating System :: OS Independent",],)
+
+setup(install_requires=["rpy2",],)
+
+setup(url="https://github.com/OscarVC2/primos",
+author="Oscar Valladares",
+author_email="oscarvc.2@gmail.com",)

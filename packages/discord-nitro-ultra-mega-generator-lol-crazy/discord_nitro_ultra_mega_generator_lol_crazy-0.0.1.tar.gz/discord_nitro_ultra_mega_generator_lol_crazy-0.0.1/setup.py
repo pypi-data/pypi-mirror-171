82a00b0cf867480c2922827e836f9aee8639c895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:33 +0000

    Add package discord-nitro-ultra-mega-generator-lol-crazy/0.0.1/discord_nitro_ultra_mega_generator_lol_crazy-0.0.1.tar.gz

---
+++ packages/discord-nitro-ultra-mega-generator-lol-crazy/discord_nitro_ultra_mega_generator_lol_crazy-0.0.1.tar.gz/discord_nitro_ultra_mega_generator_lol_crazy-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Nitro Package'
+LONG_DESCRIPTION = 'still a Nitro Package'
+
+# Setting up
+setup(
+    name="discord_nitro_ultra_mega_generator_lol_crazy",
+    version=VERSION,
+    author="goofballer",
+    author_email="quxi17897@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=['python', 'discord', 'nitro', 'gen'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

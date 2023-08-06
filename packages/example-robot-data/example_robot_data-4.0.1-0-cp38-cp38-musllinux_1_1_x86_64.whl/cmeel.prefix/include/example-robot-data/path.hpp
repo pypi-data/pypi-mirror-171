0475a5a12040bfdbf347ae12cc1cb7f3b2040f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:23 +0000

    Add package example-robot-data/4.0.1/example_robot_data-4.0.1-0-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/example-robot-data/example_robot_data-4.0.1-0-cp38-cp38-musllinux_1_1_x86_64.whl/cmeel.prefix/include/example-robot-data/path.hpp
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_ROBOT_DATA_PATH_HPP
+#define EXAMPLE_ROBOT_DATA_PATH_HPP
+
+#define EXAMPLE_ROBOT_DATA_MODEL_DIR "/tmp/cmeel-_q026s6f/pfx/cmeel.prefix/share/example-robot-data/robots"
+
+#endif

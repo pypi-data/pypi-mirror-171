8f5bdc447b5105d8bb84d9810018eed69177d7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:50 +0000

    Add package quanmou-luntan-quanmou-zhuanji/2022.10.15.0/quanmou-luntan-quanmou-zhuanji-2022.10.15.0.tar.gz

---
+++ packages/quanmou-luntan-quanmou-zhuanji/quanmou-luntan-quanmou-zhuanji-2022.10.15.0.tar.gz/quanmou-luntan-quanmou-zhuanji-2022.10.15.0/README.md
@@ -0,0 +1,27 @@
+# 权谋论坛：权谋转机
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/quanmou-luntan-quanmou-zhuanji
+docker run -tid -p <port>:80 apachecn0/quanmou-luntan-quanmou-zhuanji
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install quanmou-luntan-quanmou-zhuanji
+quanmou-luntan-quanmou-zhuanji <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g quanmou-luntan-quanmou-zhuanji
+quanmou-luntan-quanmou-zhuanji <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/quanmou-luntan-quanmou-zhuanji/quanmou-luntan-quanmou-zhuanji-2022.10.15.0.tar.gz/quanmou-luntan-quanmou-zhuanji-2022.10.15.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import QuanmouLuntanQuanmouZhuanji
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('QuanmouLuntanQuanmouZhuanji'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="quanmou-luntan-quanmou-zhuanji",
+    version=QuanmouLuntanQuanmouZhuanji.__version__,
+    url="https://github.com/apachecn/quanmou-luntan-quanmou-zhuanji",
+    author=QuanmouLuntanQuanmouZhuanji.__author__,
+    author_email=QuanmouLuntanQuanmouZhuanji.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="权谋论坛：权谋转机",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "quanmou-luntan-quanmou-zhuanji=QuanmouLuntanQuanmouZhuanji.__main__:main",
+            "QuanmouLuntanQuanmouZhuanji=QuanmouLuntanQuanmouZhuanji.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

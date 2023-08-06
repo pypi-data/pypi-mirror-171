c1404247f7c64ec2701c98c161b7b383eda53ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package rdhyee_test/0.1.3/rdhyee_test-0.1.3-py2.py3-none-any.whl

---
+++ packages/rdhyee_test/rdhyee_test-0.1.3-py2.py3-none-any.whl/rdhyee_test/__init__.py
@@ -0,0 +1,10 @@
+"""An amazing sample package for rdhyee to test stuff"""
+
+__version__ = "0.1.3"
+
+
+def hello(msg="world"):
+    """
+    print hello world by default
+    """
+    print(f"hello {msg}")

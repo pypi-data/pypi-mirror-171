4c5464d6916dee32e4b6acdcc512f4c233cc96f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package TestMath-rjkhan/0.0.6/TestMath_rjkhan-0.0.6.tar.gz

---
+++ packages/TestMath-rjkhan/TestMath_rjkhan-0.0.6.tar.gz/TestMath_rjkhan-0.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Arithmatic Operations'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+from Cython.Compiler import Options
+Options.docstrings = True
+PACKAGES = find_packages()
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="TestMath_rjkhan",
+    version=VERSION,
+    author="Rabnawaz",
+    author_email="rabnawazjansher8@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)

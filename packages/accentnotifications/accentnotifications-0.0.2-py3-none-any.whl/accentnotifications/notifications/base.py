ec4bbb1fed4305d2db9f7b6c68efefef1f543b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:19 +0000

    Add package accentnotifications/0.0.2/accentnotifications-0.0.2-py3-none-any.whl

---
+++ packages/accentnotifications/accentnotifications-0.0.2-py3-none-any.whl/accentnotifications/notifications/base.py
@@ -0,0 +1,42 @@
+from typing import Type
+
+from pydantic import BaseModel, BaseSettings
+
+
+class BaseResponse(BaseModel):
+    success: bool
+
+
+class BaseNotification(BaseSettings):
+    response: BaseResponse = None
+
+    @property
+    def backend(self) -> Type["BaseBackend"]:
+        raise NotImplementedError()
+
+
+class BaseBackend:
+    options: BaseNotification
+
+    def __init__(self, options: BaseNotification) -> None:
+        self.options = options
+
+    async def __aenter__(self):
+        try:
+            await self.open()
+        except Exception:
+            await self.close()
+            raise
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.close()
+
+    async def open(self):
+        pass
+
+    async def close(self):
+        pass
+
+    async def send(self) -> bool:
+        raise NotImplementedError()

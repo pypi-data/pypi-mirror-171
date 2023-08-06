df2808f41e274bb3551ec09b1f2d87ec1c019330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:52 +0000

    Add package sams-dsnd-probability/1.2/sams_dsnd_probability-1.2.tar.gz

---
+++ packages/sams-dsnd-probability/sams_dsnd_probability-1.2.tar.gz/sams_dsnd_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sams_dsnd_probability',
+      version=1.2,
+      description='Gaussian and Binomial distribution',
+      packages=['sams_dsnd_probability'],
+      authour='Samuel Ajayi',
+      author_email='ajayisamuelofficial@gmail.com',
+      zip_safe=False)

577a4048623df1b40b659250367bf1174dfdcc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:34 +0000

    Add package multiplier/0.1.1/multiplier-0.1.1.tar.gz

---
+++ packages/multiplier/multiplier-0.1.1.tar.gz/multiplier-0.1.1/multiplier/multipy_two_numbers.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+
+class Multiplication:
+    def __init__(self, num1: float, num2: float) -> None:
+        self.num1 = num1
+        self.num2 = num2
+    def get_product(self) -> float:
+        """
+        Multiply two numbers .
+        
+        :param num1: first number to multipy .
+        :type num1: float
+        
+        :param num2: second number to multipy.
+        :type num2: float
+        """
+    
+        return self.num1 * self.num2

9b21433ce04dffb52612207839d74c8361562d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:18 +0000

    Add package cornflakes/1.5.5/cornflakes-1.5.5-cp38-cp38-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-1.5.5-cp38-cp38-manylinux_2_35_x86_64.whl/cornflakes/common/click/_rich_command.py
@@ -0,0 +1,45 @@
+import sys
+from typing import Any
+
+import click
+
+from cornflakes.common.click._rich_config import Config as RichConfig
+from cornflakes.common.click._rich_config import default_config
+
+from ._rich_click import rich_abort_error, rich_format_error, rich_format_help
+
+
+class RichCommand(click.Command):
+    """Richly formatted click Command.
+
+    Inherits click.Command and overrides help and error methods
+    to print richly formatted output.
+    """
+
+    standalone_mode = False
+
+    def __init__(self, config: RichConfig = None, *args, **kwargs):
+        """Init function of RichGroup with extra config argument."""
+        self.config = config or default_config
+        super().__init__(*args, **kwargs)
+
+    def main(self, *args, standalone_mode: bool = True, **kwargs) -> Any:
+        """Main function of RichGroup."""
+        try:
+            rv = super().main(*args, standalone_mode=False, **kwargs)  # type: ignore
+            if not standalone_mode:
+                return rv
+        except click.ClickException as e:
+            if not standalone_mode:
+                raise
+            rich_format_error(e, config=self.config)
+            sys.exit(e.exit_code)
+        except click.exceptions.Abort:
+            if not standalone_mode:
+                raise
+            rich_abort_error(config=self.config)
+            sys.exit(1)
+
+    def format_help(self, ctx: click.Context, formatter: click.HelpFormatter) -> None:
+        """Format function of RichGroup."""
+        rich_format_help(self, ctx, formatter, config=self.config)

c375aa88a521f34a3d74d5d90973ad1fda9213a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:52 +0000

    Add package habu-snowflake-cli/3.5.0/habu_snowflake_cli-3.5.0-py3-none-any.whl

---
+++ packages/habu-snowflake-cli/habu_snowflake_cli-3.5.0-py3-none-any.whl/redbeard.py
@@ -0,0 +1,19 @@
+import sys
+from dotenv import load_dotenv
+
+sys.path.append(".")
+sys.path.append("..")
+
+import click
+
+from redbeard_cli.commands.init import cli as init_cli
+
+
+@click.group()
+def cli():
+    pass
+
+
+load_dotenv(dotenv_path="./.env", override=True)
+cli.add_command(init_cli.init)
+cli.add_command(init_cli.version)

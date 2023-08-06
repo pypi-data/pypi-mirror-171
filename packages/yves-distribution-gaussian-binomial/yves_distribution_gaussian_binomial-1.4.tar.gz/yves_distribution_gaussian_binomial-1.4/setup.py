5c0745c784f87345ec69a9a8ae6cfa3cc44168ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:26 +0000

    Add package yves-distribution-gaussian-binomial/1.4/yves_distribution_gaussian_binomial-1.4.tar.gz

---
+++ packages/yves-distribution-gaussian-binomial/yves_distribution_gaussian_binomial-1.4.tar.gz/yves_distribution_gaussian_binomial-1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yves_distribution_gaussian_binomial',
+      version='1.4',
+      description='Gaussian and Binomial distributions',
+      packages=['yves_distribution_gaussian_binomial'],
+      author = 'YVES PANFULE GOUE',
+      author_email = 'goueyvespanfule@gmail.com',
+      zip_safe=False)

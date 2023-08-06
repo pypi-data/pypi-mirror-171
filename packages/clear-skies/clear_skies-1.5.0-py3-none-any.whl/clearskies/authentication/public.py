fb7e338c9265e67575d47292356b65c983843325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:20:01 +0000

    Add package clear-skies/1.5.0/clear_skies-1.5.0-py3-none-any.whl

---
+++ packages/clear-skies/clear_skies-1.5.0-py3-none-any.whl/clearskies/authentication/public.py
@@ -0,0 +1,25 @@
+class Public:
+    is_public = True
+    can_authorize = False
+    has_dynamic_credentials = False
+
+    def headers(self, retry_auth=False):
+        return {}
+
+    def configure(self):
+        pass
+
+    def authenticate(self, input_output):
+        return True
+
+    def authorize(self, authorization):
+        raise ValueError("Public endpoints do not support authorization")
+
+    def documentation_request_parameters(self):
+        return []
+
+    def documentation_security_scheme(self):
+        return {}
+
+    def documentation_security_scheme_name(self):
+        return ''

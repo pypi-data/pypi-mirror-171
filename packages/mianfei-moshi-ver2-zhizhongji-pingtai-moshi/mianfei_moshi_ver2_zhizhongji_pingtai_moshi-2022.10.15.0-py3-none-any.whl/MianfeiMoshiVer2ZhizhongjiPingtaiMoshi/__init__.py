5c893be68f956e44692ebde2674c0cdeae240254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:42 +0000

    Add package mianfei-moshi-ver2-zhizhongji-pingtai-moshi/2022.10.15.0/mianfei_moshi_ver2_zhizhongji_pingtai_moshi-2022.10.15.0-py3-none-any.whl

---
+++ packages/mianfei-moshi-ver2-zhizhongji-pingtai-moshi/mianfei_moshi_ver2_zhizhongji_pingtai_moshi-2022.10.15.0-py3-none-any.whl/MianfeiMoshiVer2ZhizhongjiPingtaiMoshi/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mianfei-moshi-ver2-zhizhongji-pingtai-moshi
+https://github.com/apachecn/mianfei-moshi-ver2-zhizhongji-pingtai-moshi"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.15.0"

9e04614727b458d12dd1b4c787a591c8283d48ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:14 +0000

    Add package mofa-shijie-zhilv/2022.10.14.0/mofa_shijie_zhilv-2022.10.14.0-py3-none-any.whl

---
+++ packages/mofa-shijie-zhilv/mofa_shijie_zhilv-2022.10.14.0-py3-none-any.whl/MofaShijieZhilv/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""mofa-shijie-zhilv
+https://github.com/apachecn/mofa-shijie-zhilv"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.14.0"

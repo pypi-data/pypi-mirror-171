0df590d2e0761b35011d72bd3b91750bbb3a5cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:18 +0000

    Add package gtmarket/0.1.7/gtmarket-0.1.7.tar.gz

---
+++ packages/gtmarket/gtmarket-0.1.7.tar.gz/gtmarket-0.1.7/gtmarket/api.py
@@ -0,0 +1,43 @@
+class Importable:
+    """
+    This is a descriptor that knows how to load modules
+    """
+    def __init__(self, module_path, artifact_name, instantiate=False):
+        self.instantiate = instantiate
+        self._module_path = module_path
+        self._artifact_name = artifact_name
+
+    def __get__(self, instance, owner):
+        # Import the module
+        import importlib
+        module = importlib.import_module(self._module_path)
+
+        # Get the item (class/function/object) from the module
+        thing = getattr(module, self._artifact_name)
+
+        # If an object was requested, instantiate it
+        if self.instantiate:
+            thing = thing()
+        return thing
+
+    def __set__(self, instance, value):
+        raise NotImplementedError('You cannot set this attribute')
+
+
+class API:
+    """
+    Every object in the api is lazy loaded through a descriptor.  The actual
+    attributes returned are classes/functions unless the instantiate flag is set.
+
+    The lazy loading ensures that you only run imports if they are needed.
+    """
+    OrderProducts = Importable('gtmarket.core', 'OrderProducts')
+    OppLoader = Importable('gtmarket.core', 'OppLoader')
+    AccountLoader = Importable('gtmarket.core', 'AccountLoader')
+    OppHistoryLoader = Importable('gtmarket.core', 'OppHistoryLoader')
+    PipeStats = Importable('gtmarket.core', 'PipeStats')
+    ModelParams = Importable('gtmarket.predictor', 'ModelParams')
+    ModelParamsHist = Importable('gtmarket.predictor', 'ModelParamsHist')
+    SDRTeam = Importable('gtmarket.predictor', 'SDRTeam')
+    Deals = Importable('gtmarket.predictor', 'Deals')
+    GTMTranslator = Importable('gtmarket.predictor', 'GTMTranslator')

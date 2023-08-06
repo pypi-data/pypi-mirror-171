197e70ca0d45867bee52c7cf51903128e6170e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:13 +0000

    Add package starfish-library/1.0.0/starfish_library-1.0.0.tar.gz

---
+++ packages/starfish-library/starfish_library-1.0.0.tar.gz/starfish_library-1.0.0/MyLibrary/common.py
@@ -0,0 +1,33 @@
+import os
+
+
+def test1():
+    import requests
+    print('hogehoge')
+
+
+def test2():
+    print('hogefuga')
+
+
+# Twitterで利用
+# v1.1 に対応
+def generate_twitter_api(CK, CKS, AT, ATS):
+    import tweepy
+    auth = tweepy.OAuthHandler(CK, CKS)
+    auth.set_access_token(AT, ATS)
+
+    return tweepy.API(auth, wait_on_rate_limit=True)
+
+
+# GCPで利用
+# 認証キーの取得に利用
+# リソースIDのコピーから取得可能
+def gcp_secret_manager():
+    if os.name == 'nt':
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'E:/WorkSpace/Codes/service_account.json'
+    elif os.name == 'posix':
+        file_path = '/secret/gcp_service_account'
+        with open(file_path, 'r', encoding='utf-8') as f:
+            secret = f.read()
+            os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = secret
---
+++ packages/starfish-library/starfish_library-1.0.0.tar.gz/starfish_library-1.0.0/README.md
@@ -0,0 +1,5 @@
+参考サイト
+https://zenn.dev/sikkim/articles/490f4043230b5a
+
+ライブラリの反映手順
+setup.py のバージョンを更新

155ea7f14fbee2a23c795d9401c3a70777aa4e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:19 +0000

    Add package drgn/0.0.21/drgn-0.0.21.tar.gz

---
+++ packages/drgn/drgn-0.0.21.tar.gz/drgn-0.0.21/tests/linux_kernel/helpers/test_slab.py
@@ -0,0 +1,167 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from collections import defaultdict
+from pathlib import Path
+
+from drgn import NULL
+from drgn.helpers.linux.mm import pfn_to_virt
+from drgn.helpers.linux.slab import (
+    find_containing_slab_cache,
+    find_slab_cache,
+    for_each_slab_cache,
+    slab_cache_for_each_allocated_object,
+    slab_cache_is_merged,
+)
+from tests.linux_kernel import (
+    LinuxKernelTestCase,
+    skip_unless_have_full_mm_support,
+    skip_unless_have_test_kmod,
+)
+
+
+def get_proc_slabinfo_names():
+    with open("/proc/slabinfo", "rb") as f:
+        # Skip the version and header.
+        f.readline()
+        f.readline()
+        return [line.split()[0] for line in f]
+
+
+def fallback_slab_cache_names(prog):
+    # SLOB does not provide /proc/slabinfo. It is also disabled for SLUB if
+    # CONFIG_SLUB_DEBUG=n. Before Linux kernel commit 5b36577109be ("mm:
+    # slabinfo: remove CONFIG_SLABINFO") (in v4.15), it could also be disabled
+    # for SLAB. So, pick a few slab caches which we know exist to test against.
+    # In case they were merged into other caches, get their names from the
+    # structs rather than just returning the names.
+    return {
+        prog["dentry_cache"].name.string_(),
+        prog["mm_cachep"].name.string_(),
+        prog["uid_cachep"].name.string_(),
+    }
+
+
+class TestSlab(LinuxKernelTestCase):
+    def _slab_cache_aliases(self):
+        slab_path = Path("/sys/kernel/slab")
+        if not slab_path.exists():
+            self.skipTest(f"{slab_path} does not exist")
+        aliases = defaultdict(list)
+        for child in slab_path.iterdir():
+            if not child.name.startswith(":"):
+                aliases[child.stat().st_ino].append(child.name)
+        return aliases
+
+    def test_slab_cache_is_merged_false(self):
+        for aliases in self._slab_cache_aliases().values():
+            if len(aliases) == 1:
+                break
+        else:
+            self.skipTest("no unmerged slab caches")
+        self.assertFalse(slab_cache_is_merged(find_slab_cache(self.prog, aliases[0])))
+
+    def test_slab_cache_is_merged_true(self):
+        for aliases in self._slab_cache_aliases().values():
+            if len(aliases) > 1:
+                break
+        else:
+            self.skipTest("no merged slab caches")
+        for alias in aliases:
+            slab_cache = find_slab_cache(self.prog, alias)
+            if slab_cache is not None:
+                break
+        else:
+            self.fail("couldn't find slab cache")
+        self.assertTrue(slab_cache_is_merged(slab_cache))
+
+    def test_for_each_slab_cache(self):
+        try:
+            slab_cache_names = get_proc_slabinfo_names()
+        except FileNotFoundError:
+            # The found names should be a superset of the fallback names.
+            self.assertGreaterEqual(
+                {s.name.string_() for s in for_each_slab_cache(self.prog)},
+                fallback_slab_cache_names(self.prog),
+            )
+        else:
+            self.assertCountEqual(
+                [s.name.string_() for s in for_each_slab_cache(self.prog)],
+                slab_cache_names,
+            )
+
+    def test_find_slab_cache(self):
+        try:
+            slab_cache_names = get_proc_slabinfo_names()
+        except FileNotFoundError:
+            slab_cache_names = fallback_slab_cache_names(self.prog)
+        for name in slab_cache_names:
+            slab = find_slab_cache(self.prog, name)
+            self.assertEqual(name, slab.name.string_())
+
+    @skip_unless_have_full_mm_support
+    @skip_unless_have_test_kmod
+    def test_slab_cache_for_each_allocated_object(self):
+        for size in ("small", "big"):
+            with self.subTest(size=size):
+                cache = self.prog[f"drgn_test_{size}_kmem_cache"]
+                objects = self.prog[f"drgn_test_{size}_slab_objects"]
+                if self.prog["drgn_test_slob"]:
+                    self.assertRaisesRegex(
+                        ValueError,
+                        "SLOB is not supported",
+                        next,
+                        slab_cache_for_each_allocated_object(
+                            cache, f"struct drgn_test_{size}_slab_object"
+                        ),
+                    )
+                else:
+                    self.assertEqual(
+                        sorted(
+                            slab_cache_for_each_allocated_object(
+                                cache, f"struct drgn_test_{size}_slab_object"
+                            ),
+                            key=lambda obj: obj.value.value_(),
+                        ),
+                        list(objects),
+                    )
+
+    @skip_unless_have_full_mm_support
+    @skip_unless_have_test_kmod
+    def test_find_containing_slab_cache(self):
+        for size in ("small", "big"):
+            with self.subTest(size=size):
+                cache = self.prog[f"drgn_test_{size}_kmem_cache"]
+                if self.prog["drgn_test_slob"]:
+                    cache = NULL(self.prog, "struct kmem_cache *")
+                objects = self.prog[f"drgn_test_{size}_slab_objects"]
+                for obj in objects:
+                    self.assertEqual(
+                        find_containing_slab_cache(self.prog, obj.value_()), cache
+                    )
+                    self.assertEqual(find_containing_slab_cache(obj), cache)
+
+    @skip_unless_have_full_mm_support
+    @skip_unless_have_test_kmod
+    def test_find_containing_slab_cache_invalid(self):
+        start_addr = pfn_to_virt(self.prog["min_low_pfn"])
+        end_addr = pfn_to_virt(self.prog["max_pfn"]) + self.prog["PAGE_SIZE"]
+
+        self.assertEqual(
+            find_containing_slab_cache(self.prog, start_addr - 1),
+            NULL(self.prog, "struct kmem_cache *"),
+        )
+        self.assertEqual(
+            find_containing_slab_cache(self.prog, end_addr),
+            NULL(self.prog, "struct kmem_cache *"),
+        )
+
+        self.assertEqual(
+            find_containing_slab_cache(self.prog, self.prog.symbol("jiffies").address),
+            NULL(self.prog, "struct kmem_cache *"),
+        )
+
+        self.assertEqual(
+            find_containing_slab_cache(self.prog, self.prog["drgn_test_va"]),
+            NULL(self.prog, "struct kmem_cache *"),
+        )
---
+++ packages/drgn/drgn-0.0.21.tar.gz/drgn-0.0.21/tests/linux_kernel/test_stack_trace.py
@@ -0,0 +1,96 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+import unittest
+
+from drgn import Object, Program, cast
+from drgn.helpers.linux.pid import find_task
+from tests import assertReprPrettyEqualsStr
+from tests.linux_kernel import LinuxKernelTestCase, fork_and_sigwait, setenv
+from util import NORMALIZED_MACHINE_NAME
+
+
+class TestStackTrace(LinuxKernelTestCase):
+    def _assert_trace_in_sigwait(self, trace):
+        for frame in trace:
+            if frame.name and "sigtimedwait" in frame.name:
+                return
+        self.fail(f"sigwait frame not found in {str(trace)!r}")
+
+    def test_by_task_struct(self):
+        with fork_and_sigwait() as pid:
+            self._assert_trace_in_sigwait(
+                self.prog.stack_trace(find_task(self.prog, pid))
+            )
+
+    def _test_by_pid(self, orc):
+        old_orc = int(os.environ.get("DRGN_PREFER_ORC_UNWINDER", "0")) != 0
+        with setenv("DRGN_PREFER_ORC_UNWINDER", "1" if orc else "0"):
+            if orc == old_orc:
+                prog = self.prog
+            else:
+                prog = Program()
+                prog.set_kernel()
+                self._load_debug_info(prog)
+            with fork_and_sigwait() as pid:
+                self._assert_trace_in_sigwait(prog.stack_trace(pid))
+
+    def test_by_pid_dwarf(self):
+        self._test_by_pid(False)
+
+    @unittest.skipUnless(
+        NORMALIZED_MACHINE_NAME == "x86_64",
+        f"{NORMALIZED_MACHINE_NAME} does not use ORC",
+    )
+    def test_by_pid_orc(self):
+        self._test_by_pid(True)
+
+    def test_local_variable(self):
+        with fork_and_sigwait() as pid:
+            for frame in self.prog.stack_trace(pid):
+                if frame.name in ("context_switch", "__schedule"):
+                    try:
+                        prev = frame["prev"]
+                    except KeyError:
+                        continue
+                    if not prev.absent_:
+                        self.assertEqual(prev.pid, pid)
+                        break
+            else:
+                self.skipTest("prev not found in context_switch or __schedule")
+
+    def test_pt_regs(self):
+        # This won't unwind anything useful, but at least make sure it accepts
+        # a struct pt_regs.
+        self.prog.stack_trace(Object(self.prog, "struct pt_regs", value={}))
+
+        # Likewise, this is nonsense, but we should also accept a struct
+        # pt_regs *.
+        task = find_task(self.prog, os.getpid())
+        self.prog.stack_trace(cast("struct pt_regs *", task.stack))
+
+    def test_registers(self):
+        # Smoke test that we get at least one register and that
+        # StackFrame.registers() agrees with StackFrame.register().
+        with fork_and_sigwait() as pid:
+            trace = self.prog.stack_trace(pid)
+            have_registers = False
+            for frame in trace:
+                for name, value in frame.registers().items():
+                    self.assertEqual(frame.register(name), value)
+                    have_registers = True
+            self.assertTrue(have_registers)
+
+    def test_prog(self):
+        self.assertEqual(
+            self.prog.stack_trace(Object(self.prog, "struct pt_regs", value={})).prog,
+            self.prog,
+        )
+
+    def test_stack__repr_pretty_(self):
+        with fork_and_sigwait() as pid:
+            trace = self.prog.stack_trace(pid)
+            assertReprPrettyEqualsStr(trace)
+            for frame in trace:
+                assertReprPrettyEqualsStr(frame)

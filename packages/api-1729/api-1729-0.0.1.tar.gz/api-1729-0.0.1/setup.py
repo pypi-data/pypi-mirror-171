382bd3e5c7fa6530277cc0aa4da62d3471117e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:26 +0000

    Add package api-1729/0.0.1/api-1729-0.0.1.tar.gz

---
+++ packages/api-1729/api-1729-0.0.1.tar.gz/api-1729-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='api-1729',
+    version='0.0.1',
+    author = 'Akhil',
+    author_email='akhilchinnu499@gmail.com',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
+
+
+
+
+

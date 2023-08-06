04f203467efa2561b816bea6a2741db2e1eaff47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:54 +0000

    Add package tictactoe-gym/1.0.1/tictactoe_gym-1.0.1.tar.gz

---
+++ packages/tictactoe-gym/tictactoe_gym-1.0.1.tar.gz/tictactoe_gym-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+      name='tictactoe_gym',
+      version='1.0.1',
+      description='An OpenAI gym environment for playing Tic Tac Toe',
+      url='https://github.com/alfiebeard/tictactoe-gym',
+      author='Alfie Beard',
+      license='MIT License',
+      install_requires=['gym']
+)

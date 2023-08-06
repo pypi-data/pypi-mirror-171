47af0adf4c327aef4dc1adb7f3b58a01c7ebf68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:56 +0000

    Add package Tomation/0.1.0/Tomation-0.1.0.tar.gz

---
+++ packages/Tomation/Tomation-0.1.0.tar.gz/Tomation-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Tomation',
+    version='0.1.0',
+    author='Eekk2k2',
+    author_email='eekk2k2@gmail.com',
+    description='Python library for automation of different tasks.',
+    packages=find_packages(),
+    license='MIT',
+)

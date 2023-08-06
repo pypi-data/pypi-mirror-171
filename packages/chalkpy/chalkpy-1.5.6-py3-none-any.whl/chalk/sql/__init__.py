98fa2d0743de658cf784d9e62431e6d0f46b0206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:57 +0000

    Add package chalkpy/1.5.6/chalkpy-1.5.6-py3-none-any.whl

---
+++ packages/chalkpy/chalkpy-1.5.6-py3-none-any.whl/chalk/sql/__init__.py
@@ -0,0 +1,8 @@
+from chalk.sql.integrations import BigQuerySource, MySQLSource, RedshiftSource, SnowflakeSource
+
+__all__ = [
+    "BigQuerySource",
+    "MySQLSource",
+    "RedshiftSource",
+    "SnowflakeSource",
+]

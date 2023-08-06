2f6235dcb20013b6e7fef4ab60f70b360de41bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:08 +0000

    Add package urbanity/0.3.2/urbanity-0.3.2.tar.gz

---
+++ packages/urbanity/urbanity-0.3.2.tar.gz/urbanity-0.3.2/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['urbanity']
+
+package_data = \
+{'': ['*'], 'urbanity': ['map_data/*', 'svi_data/*']}
+
+install_requires = \
+['Shapely==1.8.4',
+ 'charset-normalizer==2.1.1',
+ 'geopandas==0.11.1',
+ 'ipyleaflet==0.17.1',
+ 'networkx==2.8.6',
+ 'numpy==1.23.3',
+ 'pyrobuf==0.9.3',
+ 'pyrosm==0.6.1',
+ 'rasterio==1.3.2',
+ 'rasterstats==0.17.0',
+ 'urllib3>=1.26.11,<2.0.0',
+ 'vt2geojson>=0.2.1,<0.3.0']
+
+setup_kwargs = {
+    'name': 'urbanity',
+    'version': '0.3.2',
+    'description': '',
+    'long_description': None,
+    'author': 'winstonyym',
+    'author_email': 'winstonyym@u.nus.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)

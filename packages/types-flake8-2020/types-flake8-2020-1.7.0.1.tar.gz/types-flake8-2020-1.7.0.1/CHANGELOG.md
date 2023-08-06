b4e7486260dd574af396a6375c8a4b7214f7b86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:17 +0000

    Add package types-flake8-2020/1.7.0.1/types-flake8-2020-1.7.0.1.tar.gz

---
+++ packages/types-flake8-2020/types-flake8-2020-1.7.0.1.tar.gz/types-flake8-2020-1.7.0.1/CHANGELOG.md
@@ -0,0 +1,22 @@
+## 1.7.0.1 (2022-10-15)
+
+Mark `flake8-2020` as complete (#8902)
+
+## 1.7.0 (2022-08-05)
+
+[stubsabot] Bump flake8-2020 to 1.7.* (#8492)
+
+Co-authored-by: stubsabot <>
+
+## 1.6.3 (2022-04-16)
+
+Third-party stubs: import from `collections.abc` where possible (#7637)
+
+## 1.6.2 (2022-01-08)
+
+Use lowercase `type` everywhere (#6853)
+
+## 1.6.0 (2021-12-10)
+
+Add stubs for flake8-2020 (#6555)
+
---
+++ packages/types-flake8-2020/types-flake8-2020-1.7.0.1.tar.gz/types-flake8-2020-1.7.0.1/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-flake8-2020"
+description = "Typing stubs for flake8-2020"
+long_description = '''
+## Typing stubs for flake8-2020
+
+This is a PEP 561 type stub package for the `flake8-2020` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `flake8-2020`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/flake8-2020. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `ed4bc2b2e6db471f7dfa3116c39ff0876b2de981`.
+'''.lstrip()
+
+setup(name=name,
+      version="1.7.0.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/flake8-2020.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['flake8_2020-stubs'],
+      package_data={'flake8_2020-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)

1daf551bc3e35cfc555ce0958b64c44cbf7ae6a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:59 +0000

    Add package mock-event-generator/1.3.4/mock_event_generator-1.3.4.tar.gz

---
+++ packages/mock-event-generator/mock_event_generator-1.3.4.tar.gz/mock_event_generator-1.3.4/build-local.sh
@@ -0,0 +1,10 @@
+set -ex
+
+IMAGE=emfollow/mock-event-generator:local
+LIGO_CERTIFICATE_PATH=/tmp/x509up_u$(id -u)
+
+DOCKER_BUILDKIT=1 docker build \
+    --pull \
+    --secret id=x509,src=${LIGO_CERTIFICATE_PATH} \
+    --progress=plain \
+    -t ${IMAGE} .

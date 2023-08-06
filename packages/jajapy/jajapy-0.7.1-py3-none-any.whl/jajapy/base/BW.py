4f52a6f141d2691cb6d3892caa2eb701bb45447d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:51 +0000

    Add package jajapy/0.7.1/jajapy-0.7.1-py3-none-any.whl

---
+++ packages/jajapy/jajapy-0.7.1-py3-none-any.whl/jajapy/base/BW.py
@@ -0,0 +1,214 @@
+from sys import platform
+from multiprocessing import cpu_count, Pool
+from numpy import array, dot, append, zeros, ones, float64
+from datetime import datetime
+from .Set import Set
+
+NB_PROCESS = cpu_count()-1
+
+class BW:
+	"""
+	Abstract class for general Baum-Welch algorithm.
+	"""
+	def __init__(self):
+		pass
+
+	def h_tau(self,s1: int,s2: int,obs: str) -> float:
+		"""
+		Probability of moving from ``s1`` to ``s2`` generating ``obs`` under
+		the current hypothesis.
+
+		Parameters
+		----------
+		s1 : int
+			source state ID.
+		s2 : int
+			destination state ID.
+		obs : str
+			observation.
+
+		Returns
+		-------
+		float:
+			probability of moving from ``s1`` to ``s2`` generating ``obs``.
+		"""
+		return self.h.tau(s1,s2,obs)
+
+	def computeAlphas(self, sequence: list, dtype=False) -> array:
+		"""
+		Compute the alpha values for ``sequence`` under the current BW
+		hypothesis.
+
+		Parameters
+		----------
+		sequence : list of str
+			sequence of observations.
+		dtype : numpy.scalar
+			If it set, the output will be a numpy array of this type,
+			otherwise it is a numpy array of float64.
+
+		Returns
+		-------
+		2-D narray
+			array containing the alpha values.
+		"""
+		len_seq = len(sequence)
+		init_arr = self.h.initial_state
+		zero_arr = zeros(shape=(len_seq*self.nb_states,))
+		alpha_matrix = append(init_arr,zero_arr).reshape(len_seq+1,self.nb_states)
+		if dtype != False:
+			alpha_matrix = alpha_matrix.astype(dtype)
+		else:
+			dtype = float64
+		for k in range(len_seq):
+			for s in range(self.nb_states):
+				p = array([self.h_tau(ss,s,sequence[k]) for ss in range(self.nb_states)],dtype=dtype)
+				alpha_matrix[k+1,s] = dot(alpha_matrix[k],p)
+		return alpha_matrix.T
+
+	def computeBetas(self, sequence: list, dtype=False) -> array:
+		"""
+		Compute the beta values for ``sequence`` under the current BW
+		hypothesis.
+
+		Parameters
+		----------
+		sequence : list of str
+			sequence of observations.
+		dtype : numpy.scalar
+			If it set, the output will be a numpy array of this type,
+			otherwise it is a numpy array of float64.
+
+		Returns
+		-------
+		2-D narray
+			array containing the beta values.
+		"""
+		len_seq = len(sequence)
+		init_arr = ones(self.nb_states)
+		zero_arr = zeros(shape=(len_seq*self.nb_states,))
+		beta_matrix = append(zero_arr,init_arr).reshape(len_seq+1,self.nb_states)
+		if dtype != False:
+			beta_matrix = beta_matrix.astype(dtype)
+		else:
+			dtype = float64
+		for k in range(len(sequence)-1,-1,-1):
+			for s in range(self.nb_states):
+				p = array([self.h_tau(s,ss,sequence[k]) for ss in range(self.nb_states)],dtype=dtype)
+				beta_matrix[k,s] = dot(beta_matrix[k+1],p)
+		return beta_matrix.T
+
+	def _processWork(self,sequence,times):
+		#overrided
+		pass
+
+	def _generateHhat(self):
+		#overrided
+		pass
+
+	def _runProcesses(self,training_set):
+		if platform != "win32" and platform != "darwin":
+			p = Pool(processes = NB_PROCESS)
+			tasks = []
+			for seq,times in zip(training_set.sequences,training_set.times):
+				tasks.append(p.apply_async(self._processWork, [seq, times,]))
+			return [res.get() for res in tasks if res.get() != False]
+		else:
+			return [self._processWork(seq, times) for seq,times in zip(training_set.sequences,training_set.times)]
+	
+	def _endPrint(self,it,rt):
+		print()
+		print("---------------------------------------------")
+		print("Learning finished")
+		print("Iterations:\t  ",it)
+		print("Running time:\t  ",rt)
+		print("---------------------------------------------")
+		print()
+
+	def fit(self,training_set: Set,initial_model,output_file: str,
+			epsilon: float, max_it: int, pp: str, verbose: bool,
+			return_data: bool, stormpy_output: bool = False):
+		"""
+		Fits the model according to ``traces``.
+
+		Parameters
+		----------
+		traces : Set
+			The training set.
+		initial_model : Model
+			The first hypothesis.
+		output_file : str
+			If set path file of the output model. Otherwise the output model
+			will not be saved into a text file.
+		epsilon : float
+			The learning process stops when the difference between the
+			loglikelihood of the training set under the two last hypothesis is
+			lower than ``epsilon``. The lower this value the better the output,
+			but the longer the running time.
+		max_it: int
+			Maximal number of iterations. The algorithm will stop after `max_it`
+			iterations.
+			Default is infinity.
+		pp : str
+			Will be printed at each iteration.
+		verbose: bool
+			Print or not a small recap at the end of the learning.
+		return_data: bool
+			If set to True, a dictionary containing following values will be
+			returned alongside the hypothesis once the learning is done.
+			'learning_rounds', 'learning_time', 'training_set_loglikelihood'.
+		stormpy_output: bool
+			If set to True the output model will be a Stormpy sparse model.
+			Doesn't work for GOHMM and MGOHMM.
+
+		Returns
+		-------
+		Model
+			fitted model.
+		"""
+
+		if stormpy_output:
+			try:
+				#import stormpy as st
+				from ..with_stormpy import modeltoStorm
+			except ModuleNotFoundError:
+				print("WARNING: stormpy not found. The output model will not be a stormpy sparse model")
+				stormpy_output = False
+		
+		
+
+		start_time = datetime.now()
+		self.h = initial_model
+		self.nb_states = self.h.nb_states
+
+		counter = 0
+		prevloglikelihood = 10
+		nb_traces = sum(training_set.times)
+		while counter < max_it:
+			print(pp, datetime.now(),counter, prevloglikelihood/nb_traces,end='\r')
+			temp = self._runProcesses(training_set)
+			self.hhat, currentloglikelihood = self._generateHhat(temp)
+			counter += 1
+			self.h = self.hhat
+			if abs(prevloglikelihood - currentloglikelihood) < epsilon:
+				break
+			else:
+				prevloglikelihood = currentloglikelihood
+		
+		running_time = datetime.now()-start_time
+		running_time = running_time.seconds+running_time.microseconds*10**-6
+
+		if output_file:
+			self.h.save(output_file)
+		
+		if verbose:
+			self._endPrint(counter,running_time)
+
+		if stormpy_output:
+			self.h = modeltoStorm(self.h)
+
+		if return_data:
+			info = {"learning_rounds":counter,"learning_time":running_time,"training_set_loglikelihood":currentloglikelihood}
+			return self.h, info
+			
+		return self.h
---
+++ packages/jajapy/jajapy-0.7.1-py3-none-any.whl/jajapy/with_stormpy/model_converter.py
@@ -0,0 +1,158 @@
+import stormpy as st
+from numpy import zeros, where, full
+from ..mc import MC
+from ..mdp import MDP
+from ..ctmc import CTMC
+
+def modeltoStorm(h):
+	"""
+	Returns a trace-equivalent model that can be use by Storm.
+	The input model can be a jajapy.HMM, a jajapy.MC, a jajapy.CTMC or
+	a jajapy.MDP.
+	The output object will be a stormpy.SparseCtmc, stormpy.SparseDtmc,
+	or a stormpy.SparseMdp depending on the input.
+
+	Parameters
+	----------
+	h : jajapy.HMM, jajapy.MC, jajapy.CTMC or jajapy.MDP
+		The model to convert.
+
+	Returns
+	-------
+	stormpy.SparseCtmc, stormpy.SparseDtmc or stormpy.SparseMdp
+		A trace-equivalent model.
+	"""
+	state_index, c =  _buildStateIndex(h)
+	transition_matrix, exit_rates = _buildTransitionMatrix(h,state_index,c)
+	state_labeling = _buildStateLabeling(h,state_index,c)
+	if type(h) == MDP:
+		model = _buildMDP(h,c,transition_matrix,state_labeling)
+	elif type(h) == CTMC:
+		model = _buildCTMC(transition_matrix,state_labeling,exit_rates)
+	else:
+		model = _buildDTMC(transition_matrix,state_labeling)
+	return model
+
+def _buildMDP(h,c,transition_matrix,state_labeling):
+	nb_actions = len(h.getActions())
+	choice_labeling = st.storage.ChoiceLabeling(c*nb_actions)
+	for a in h.getActions():
+		choice_labeling.add_label(a)
+	for ia,a in enumerate(h.getActions()):
+		choice_labeling.add_label_to_choice(a,ia)
+
+	reward_models = {}
+	action_reward = [-1.0 for _ in range(len(transition_matrix))]
+	reward_models["nb_executed_actions"] = st.SparseRewardModel(optional_state_action_reward_vector = action_reward)
+	components = st.SparseModelComponents(transition_matrix=transition_matrix,
+										  state_labeling=state_labeling,
+										  reward_models=reward_models)
+	components.choice_labeling = choice_labeling
+	mdp = st.storage.SparseMdp(components)
+	return mdp
+
+def _buildCTMC(transition_matrix,state_labeling,exit_rates):
+	components = st.SparseModelComponents(transition_matrix=transition_matrix,
+										  state_labeling=state_labeling,
+										  rate_transitions=True)
+	components.exit_rates = exit_rates
+	ctmc = st.storage.SparseCtmc(components)
+	return ctmc
+
+def _buildDTMC(transition_matrix, state_labeling):
+	components = st.SparseModelComponents(transition_matrix=transition_matrix,
+										  state_labeling=state_labeling)
+	mc = st.storage.SparseDtmc(components)
+	return mc
+
+def _buildStateLabeling(h,state_index,c):
+	state_labeling = st.storage.StateLabeling(c)
+	for o in h.getAlphabet():
+		state_labeling.add_label(o)
+	state_labeling.add_label('init')
+	states, observations = where(state_index > -1)
+	for s,o in zip(states,observations):
+		if o == len(h.getAlphabet()):
+			state_labeling.add_label_to_state('init',state_index[s][o])
+		else:
+			state_labeling.add_label_to_state(h.getAlphabet()[o],state_index[s][o])
+	return state_labeling
+
+def _buildTransitionMatrix(h,state_index,c):
+	if type(h) == MDP:
+		nb_actions = len(h.getActions())
+		transition_matrix = zeros((c*nb_actions,c))
+		for s in range(h.nb_states):
+			for a in h.getActions(s):
+				p = zeros(c)
+				ia = h.actions.index(a)
+				states, observations = where(h.matrix[s][ia] > 0.0)
+				for s2, o in zip(states,observations):
+					p[state_index[s2][o]] = h.tau(s,a,s2,h.getAlphabet()[o])
+				for x in [i for i in state_index[s] if i > -1]:
+					transition_matrix[x*nb_actions+ia] = p
+		return st.build_sparse_matrix(transition_matrix,[nb_actions*i for i in range(c)]), None
+		
+	elif type(h) == CTMC:
+		transition_matrix = zeros((c,c))
+		exit_rates = zeros(c)
+		for s in range(h.nb_states):
+			p = zeros(c)
+			states, observations = where(h.matrix[s] > 0.0)
+			for s2, o in zip(states,observations):
+				p[state_index[s2][o]] = h.tau(s,s2,h.getAlphabet()[o])
+			for x in [i for i in state_index[s] if i > -1]:
+				transition_matrix[x] = p
+				exit_rates[x] = h.e(s)
+		return st.build_sparse_matrix(transition_matrix), exit_rates
+	else:
+		transition_matrix = zeros((c,c))
+		for s in range(h.nb_states):
+			p = zeros(c)
+			if type(h) == MC:
+				states, observations = where(h.matrix[s] > 0.0)
+				for s2, o in zip(states,observations):
+					p[state_index[s2][o]] = h.tau(s,s2,h.getAlphabet()[o])
+			else:
+				states = where(h.matrix[s] > 0.0)[0]
+				observations = where(h.output[s] > 0.0)[0]
+				for s2 in states:
+					for o in observations:
+						p[state_index[s2][o]] = h.tau(s,s2,h.getAlphabet()[o])
+			for x in [i for i in state_index[s] if i > -1]:
+				transition_matrix[x] = p
+		return st.build_sparse_matrix(transition_matrix), None
+
+
+def _buildStateIndex(h):
+	if type(h) == MDP:
+		state_index = full((h.nb_states, len(h.getAlphabet())+1),-1,dtype=int)
+		c = 0
+
+		for s in where(h.initial_state > 0.0)[0]:
+			state_index[s][len(h.getAlphabet())] = c
+			c += 1
+		for s in range(h.nb_states):
+			_, states, observations = where(h.matrix[s] > 0.0)
+			for s2, o in zip(states,observations):
+				if state_index[s2][o] == -1:
+					state_index[s2][o] = c
+					c += 1
+	else:
+		state_index = full((h.nb_states, len(h.getAlphabet())+1),-1,dtype=int)
+		c = 0
+		for s in where(h.initial_state > 0.0)[0]:
+			state_index[s][len(h.getAlphabet())] = c
+			c += 1
+		for s in range(h.nb_states):
+			if type(h) == MC or type(h) == CTMC:
+				states, observations = where(h.matrix[s] > 0.0)
+			else:
+				states = where(h.matrix[s] > 0.0)[0]
+				observations = where(h.output[s] > 0.0)[0]
+			for s2 in states:
+				for o in observations:
+					if state_index[s2][o] == -1:
+						state_index[s2][o] = c
+						c += 1
+	return state_index, c

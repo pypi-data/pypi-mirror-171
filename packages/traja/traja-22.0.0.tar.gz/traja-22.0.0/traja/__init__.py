b5b9e74bad9d1ce3a9e778e96cb25a197a6f94d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:46 +0000

    Add package traja/22.0.0/traja-22.0.0.tar.gz

---
+++ packages/traja/traja-22.0.0.tar.gz/traja-22.0.0/traja/__init__.py
@@ -0,0 +1,18 @@
+import logging
+
+from traja import dataset
+from traja import models
+from .accessor import TrajaAccessor
+from .frame import TrajaDataFrame, TrajaCollection
+from .parsers import read_file, from_df
+from .plotting import *
+from .trajectory import *
+
+__author__ = "justinshenk"
+__version__ = "22.0.0"
+
+logging.basicConfig(level=logging.INFO)
+
+
+def set_traja_axes(axes: list):
+    TrajaAccessor._set_axes(axes)

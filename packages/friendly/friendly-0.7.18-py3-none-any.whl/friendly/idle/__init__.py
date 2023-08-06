3867580fdccc82923e1570e7b8335a1c0c0130a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:10 +0000

    Add package friendly/0.7.18/friendly-0.7.18-py3-none-any.whl

---
+++ packages/friendly/friendly-0.7.18-py3-none-any.whl/friendly/idle/__init__.py
@@ -0,0 +1,20 @@
+import friendly
+from .main import *  # noqa
+from .main import install
+from friendly import settings
+from friendly_traceback.console_helpers import friendly_tb
+from friendly_traceback import config
+
+settings.ENVIRONMENT = "IDLE"
+
+__all__ = list(helpers)  # noqa
+__all__.append("run")  # noqa
+__all__.append("start_console")  # noqa
+__all__.append("Friendly")  # noqa
+__all__.remove("disable")
+__all__.remove("enable")
+install()
+
+if config.did_exception_occur_before():
+    friendly.print_repl_header()
+    friendly_tb()  # noqa

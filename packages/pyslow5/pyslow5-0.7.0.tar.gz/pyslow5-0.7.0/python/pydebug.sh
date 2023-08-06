fdeaccccc07aabdb563f53c7c2ffa022afdab600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:49 +0000

    Add package pyslow5/0.7.0/pyslow5-0.7.0.tar.gz

---
+++ packages/pyslow5/pyslow5-0.7.0.tar.gz/pyslow5-0.7.0/python/pydebug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+make clean
+rm -rf *.so python/pyslow5.cpp python/pyslow5.c build/lib.* build/temp.*
+GCC_ASAN_PRELOAD=$(gcc -print-file-name=libasan.so)
+CFLAGS="-fsanitize=address -fno-omit-frame-pointer" python3 setup.py build
+cp build/lib.*/*.so  ./
+echo $GCC_ASAN_PRELOAD
+LD_PRELOAD=$GCC_ASAN_PRELOAD  python3 < python/example.py
+# LD_PRELOAD=$GCC_ASAN_PRELOAD  python3 -m unittest -v python/test.py

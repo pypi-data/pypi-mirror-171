6892d9c60da07ba3a588f2e19f4c1ffc0a84fd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:39 +0000

    Add package prq/0.0.13/prq-0.0.13.tar.gz

---
+++ packages/prq/prq-0.0.13.tar.gz/prq-0.0.13/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages  # type: ignore
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    packages=find_packages(),
+    author_email="gavrilovstepan78@gmail.com",
+    name="prq",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="0.0.13",
+    py_modules=["prq"],
+    entry_points={
+        "console_scripts": [
+            "prq=prq:worker",
+        ]
+    },
+    keywords=["python", "queue", "redis", "parallel", "workers", "tasks", "jobs"],
+    install_requires=["rq >= 0.4.6", "gevent >= 1.0"],
+)

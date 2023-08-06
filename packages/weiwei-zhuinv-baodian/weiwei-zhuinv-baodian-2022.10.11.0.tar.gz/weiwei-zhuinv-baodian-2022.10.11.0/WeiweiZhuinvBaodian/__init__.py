3c870f33410a32ccbd17348e6a986fd37f676602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:49 +0000

    Add package weiwei-zhuinv-baodian/2022.10.11.0/weiwei-zhuinv-baodian-2022.10.11.0.tar.gz

---
+++ packages/weiwei-zhuinv-baodian/weiwei-zhuinv-baodian-2022.10.11.0.tar.gz/weiwei-zhuinv-baodian-2022.10.11.0/WeiweiZhuinvBaodian/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""weiwei-zhuinv-baodian
+https://github.com/apachecn/weiwei-zhuinv-baodian"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2022.10.11.0"

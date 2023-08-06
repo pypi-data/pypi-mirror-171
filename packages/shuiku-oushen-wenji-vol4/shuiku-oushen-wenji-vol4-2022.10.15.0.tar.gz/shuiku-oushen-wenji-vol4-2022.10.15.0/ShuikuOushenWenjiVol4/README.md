e636099b73878e2ca8b8d7a3bf95c05d63a04b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:29 +0000

    Add package shuiku-oushen-wenji-vol4/2022.10.15.0/shuiku-oushen-wenji-vol4-2022.10.15.0.tar.gz

---
+++ packages/shuiku-oushen-wenji-vol4/shuiku-oushen-wenji-vol4-2022.10.15.0.tar.gz/shuiku-oushen-wenji-vol4-2022.10.15.0/ShuikuOushenWenjiVol4/README.md
@@ -0,0 +1,27 @@
+# 水库欧神文集_4_杂文一
+
+## 下载
+
+### Docker
+
+```
+docker pull apachecn0/shuiku-oushen-wenji-vol4
+docker run -tid -p <port>:80 apachecn0/shuiku-oushen-wenji-vol4
+# 访问 http://localhost:{port} 查看文档
+```
+
+### PYPI
+
+```
+pip install shuiku-oushen-wenji-vol4
+shuiku-oushen-wenji-vol4 <port>
+# 访问 http://localhost:{port} 查看文档
+```
+
+### NPM
+
+```
+npm install -g shuiku-oushen-wenji-vol4
+shuiku-oushen-wenji-vol4 <port>
+# 访问 http://localhost:{port} 查看文档
+```
---
+++ packages/shuiku-oushen-wenji-vol4/shuiku-oushen-wenji-vol4-2022.10.15.0.tar.gz/shuiku-oushen-wenji-vol4-2022.10.15.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import ShuikuOushenWenjiVol4
+import os
+from os import path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+for subdir, _, _ in os.walk('ShuikuOushenWenjiVol4'):
+    fname = path.join(subdir, '__init__.py')
+    open(fname, 'a').close()
+    
+setuptools.setup(
+    name="shuiku-oushen-wenji-vol4",
+    version=ShuikuOushenWenjiVol4.__version__,
+    url="https://github.com/apachecn/shuiku-oushen-wenji-vol4",
+    author=ShuikuOushenWenjiVol4.__author__,
+    author_email=ShuikuOushenWenjiVol4.__email__,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: Other/Proprietary License",
+        "Natural Language :: Chinese (Simplified)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Markup :: Markdown",
+        "Topic :: Text Processing :: Markup :: HTML",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Documentation",
+    ],
+    description="水库欧神文集_4_杂文一",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[],
+    install_requires=[],
+    python_requires=">=3.6",
+    entry_points={
+        'console_scripts': [
+            "shuiku-oushen-wenji-vol4=ShuikuOushenWenjiVol4.__main__:main",
+            "ShuikuOushenWenjiVol4=ShuikuOushenWenjiVol4.__main__:main",
+        ],
+    },
+    packages=setuptools.find_packages(),
+    package_data={'': ['*']},
+)

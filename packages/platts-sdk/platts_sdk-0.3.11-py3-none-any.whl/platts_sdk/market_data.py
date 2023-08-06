9938813811bbb661eed9fbf1da40431d9c41526a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:11 +0000

    Add package platts-sdk/0.3.11/platts_sdk-0.3.11-py3-none-any.whl

---
+++ packages/platts-sdk/platts_sdk-0.3.11-py3-none-any.whl/platts_sdk/market_data.py
@@ -0,0 +1,43 @@
+from typing import Any, List, Union
+import typing
+import pandas as pd
+
+from platts_sdk.tok import TokenClient
+
+
+class MarketData:
+    def __init__(self, token_client: TokenClient, as_df=True):
+        self.token_client = token_client
+        self.as_df = as_df
+        return
+
+    def get_current_assessments_by_mdc(self, mdc: str) -> Union[pd.DataFrame, Any]:
+        # get the current assessments for all symbols in a MDC (market data category)
+        params = {"filter": f'mdc: "{mdc}"', "pagesize": 10000}
+
+        data = self.token_client.get(
+            path="/market-data/v3/value/current/mdc",
+            params=params,
+        )
+        if self.as_df:
+            return pd.json_normalize(
+                data["results"], record_path=["data"], meta="symbol"
+            )
+
+        return data
+
+    def get_current_assessments(self, symbols: List[str]) -> Union[pd.DataFrame, Any]:
+        # get the current assessments for a list of symbols
+        symbols = ['"' + x + '"' for x in symbols]
+        params = {"filter": f"symbol in ({','.join(symbols)})"}
+
+        data = self.token_client.get(
+            "/market-data/v3/value/current/symbol",
+            params=params,
+        )
+        if self.as_df:
+            return pd.json_normalize(
+                data["results"], record_path=["data"], meta="symbol"
+            )
+
+        return data
---
+++ packages/platts-sdk/platts_sdk-0.3.11-py3-none-any.whl/platts_sdk/tok.py
@@ -0,0 +1,50 @@
+from functools import lru_cache
+from typing import Dict
+import requests
+import logging
+
+
+class TokenClient:
+    __token_endpoint = "https://api.platts.com/auth/api"
+    __endpoint = "https://api.platts.com"
+
+    def __init__(self, username: str, password: str, apikey: str):
+        self.un = username
+        self.pw = password
+        self.apikey = apikey
+        return
+
+    def get(self, path: str, params: Dict[str, str]):
+        headers = {
+            "appkey": self.apikey,
+            "Authorization": f"Bearer {self.token}",
+            "User-Agent": "platts-py-sdk",
+        }
+
+        r = requests.get(f"{self.__endpoint}{path}", headers=headers, params=params)
+        r.raise_for_status()
+        try:
+            return r.json()
+        except Exception as err:
+            if r.status_code >= 500:
+                logging.error(err)
+            else:
+                logging.error(r.status_code, r.json())
+            raise
+
+    @property
+    @lru_cache()
+    def token(self):
+        body = {"username": self.un, "password": self.pw}
+        headers = {"appkey": self.apikey}
+
+        r = requests.post(self.__token_endpoint, data=body, headers=headers)
+        r.raise_for_status()
+        try:
+            return r.json()["access_token"]
+        except Exception as err:
+            if r.status_code >= 500:
+                logging.error(f"[{r.status_code}] - {err}")
+            else:
+                logging.error(f"[{r.status_code}] -  {r.json()}")
+            raise

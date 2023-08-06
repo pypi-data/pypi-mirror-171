811688604f37c6c8f4f68df27f6f8e77c6b9af81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:39 +0000

    Add package SigProfilerExtractor/1.1.14/SigProfilerExtractor-1.1.14-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.14-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.14'
+version = '1.1.14'
+Update = '1. Resolve input handling issues with bedpe and seg files'
+    
+    

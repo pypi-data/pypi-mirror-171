df060ae0332cda8290edc0e04634daca755d02f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:39 +0000

    Add package Dimv/0.0.1/Dimv-0.0.1-py3-none-any.whl

---
+++ packages/Dimv/Dimv-0.0.1-py3-none-any.whl/dimv/__init__.py
@@ -0,0 +1,6 @@
+from .Dimv import create_service
+from .Dimv import find_file
+from .Dimv import find_files
+from .Dimv import download
+from .Dimv import download_file
+from .Dimv import download_files

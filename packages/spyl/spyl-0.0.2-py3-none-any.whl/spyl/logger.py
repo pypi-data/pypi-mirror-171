c636bf11b02d3e1bba7311c62267cd92aadc5db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:48 +0000

    Add package spyl/0.0.2/spyl-0.0.2-py3-none-any.whl

---
+++ packages/spyl/spyl-0.0.2-py3-none-any.whl/spyl/logger.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from colorama import Fore
+
+
+class LogLevel:
+    def __init__(self, name, color: str = Fore.RESET, isFatal: bool = False):
+        self.name = name
+        self.color = color
+        self.isFatal = isFatal
+
+    def log(self, message):
+        logger = Logger()
+        logger.log(message, self)
+        if self.isFatal and Logger.quitWhenLogFatal:
+            quit()
+
+
+class Logger:
+    logLevel = True
+    colorLevelText = True
+    quitWhenLogFatal = True
+    colorText = True
+    warnLevel = LogLevel("WARN", Fore.YELLOW)
+    infoLevel = LogLevel("INFO", Fore.RESET)
+    debugLevel = LogLevel("DEBUG", Fore.WHITE)
+    errorLevel = LogLevel("ERROR", Fore.LIGHTRED_EX)
+    fatalLevel = LogLevel("FATAL", Fore.RED)
+
+    def log(self, message: str, level: LogLevel = debugLevel, end: str = "\n"):
+        current_time = datetime.now()
+        if Logger.colorText:
+            if Logger.logLevel:
+                print(
+                    f"[{current_time.strftime('%X')}] {level.color if Logger.colorLevelText else Fore.RESET}[{level.name}]",
+                    level.color + str(message), end=end + Fore.RESET)
+
+            else:
+                print(f"[{current_time.strftime('%X')}]", level.color + str(message), end=end + Fore.RESET)
+        else:
+            if Logger.logLevel:
+                print(f"[{current_time.strftime('%X')}] [{level.name}]",
+                      str(message), end=end)
+
+            else:
+                print(f"[{current_time.strftime('%X')}]", str(message), end=end)
+
+    def log_warning(self, message: str):
+        self.log(message, Logger.warnLevel)
+
+    def log_info(self, message: str):
+        self.log(message, Logger.infoLevel)
+
+    def log_debug(self, message: str):
+        self.log(message, Logger.debugLevel)
+
+    def log_error(self, message: str):
+        self.log(message, Logger.errorLevel)
+
+    def log_fatal(self, message: str, exception=""):
+        self.log(message, Logger.fatalLevel)
+        if Logger.quitWhenLogFatal:
+            quit(exception)

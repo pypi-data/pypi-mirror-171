953651ea85280b2554ff17eabea0cfe444099600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:15 +0000

    Add package master-dac/2022.1.4/master_dac-2022.1.4.tar.gz

---
+++ packages/master-dac/master_dac-2022.1.4.tar.gz/master_dac-2022.1.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import sys
+import os
+
+def is_venv():
+    return (
+        hasattr(sys, 'real_prefix') or
+        (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix) or
+        (os.environ.get("CONDA_PREFIX", None) is not None)
+    )
+
+def is_colab():
+    try:
+        return get_ipython().__class__.__module__ == "google.colab._shell"
+    except:
+        return False
+
+##  assert is_colab() or is_venv(), "L'installation doit se faire dans un environnement virtuel: python -m venv ...REPERTOIRE..."
+
+# Let's go
+setup()

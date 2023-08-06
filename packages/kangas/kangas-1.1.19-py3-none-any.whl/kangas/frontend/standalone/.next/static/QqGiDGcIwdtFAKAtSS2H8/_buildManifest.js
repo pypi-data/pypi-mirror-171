3c84b03f1617d359b03cdbb49731f1342b32112d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:13 +0000

    Add package kangas/1.1.19/kangas-1.1.19-py3-none-any.whl

---
+++ packages/kangas/kangas-1.1.19-py3-none-any.whl/kangas/frontend/standalone/.next/static/QqGiDGcIwdtFAKAtSS2H8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/771-770a75dcaa34a2a2.js","static/chunks/pages/index-9447b2faf7f4e4cf.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
